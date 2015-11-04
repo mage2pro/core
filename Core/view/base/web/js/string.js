c9ac4671a77e4f10459c00668447313b1bb5edb8
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-11-04 03:14:04 +0300

    no message

--- Core/view/base/web/js/string.js
+++ Core/view/base/web/js/string.js
@@ -1,11 +1,24 @@
 define([], function() {return {
 	/**
+	 * 2015-11-04
+	 * Вставляет подстроку newChunk в заданное место position строки string.
+	 * http://stackoverflow.com/a/4314050
+	 * http://stackoverflow.com/a/4314044
+	 * @param {String} string
+	 * @param {Number} position
+	 * @param {String} newChunk
+	 * @returns {String}
+	 */
+	splice: function(string, position, newChunk) {
+		return string.slice(0, position) + newChunk + string.slice(position);
+	}
+	/**
 	 * 2015-11-02
 	 * http://stackoverflow.com/a/7616484
 	 * @param {String} string
 	 * @returns {Number}
 	 */
-	hash: function(string) {
+	,hash: function(string) {
 		/** @type {Number} */
 		var result = 0;
 		if (string.length) {
