cf0632d66009d444f16dd46e29de44728fcf44ec
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-11-28 19:34:03 +0300

    no message

--- Framework/View/adminhtml/web/formElement/googleFont/main.js
+++ Framework/View/adminhtml/web/formElement/googleFont/main.js
@@ -50,14 +50,77 @@ define(['jquery', 'Df_Core/Select2', 'domReady!'], function($) {return (
 			),
 			/**
 			 * 2015-11-28
+			 * http://stackoverflow.com/a/19701390
+			 * @name Select2Item
+			 * @property {?Select2Item[]} children
+			 * https://github.com/select2/select2/blob/4.0.1/dist/js/select2.full.js#L4734-L4750
+			 * @property {Boolean} disabled
+			 * @property {HTMLOptionElement} element
+			 * @property {String} id		Например: "ABeeZee:regular"
+			 * @property {Boolean} selected
+			 * @property {String} text	Например: "regular"
+			 */
+			/**
+			 * 2015-11-28
+			 * https://select2.github.io/announcements-4.0.html#new-matcher
+			 *
+			 * 1) https://github.com/select2/select2/blob/4.0.1/dist/js/select2.full.js#L3272-L3276
+					SelectAdapter.prototype.matches = function (params, data) {
+						var matcher = this.options.get('matcher');
+						return matcher(params, data);
+					};
+			 * 2) https://github.com/select2/select2/blob/4.0.1/dist/js/select2.full.js#L4728-L4771
+			 *
+			 * @param {Object} params
+			 * @param {?String} params.term
+			 * https://github.com/select2/select2/blob/4.0.1/dist/js/select2.full.js#L4762
+			 * @param {Select2Item} item
+			 * @param {?Select2Item} parent [optional]
+			 * @returns {?Select2Item}
+			 * https://github.com/select2/select2/blob/4.0.1/dist/js/select2.full.js#L4770
+			 */
+			matcher: function matcher(params, item, parent) {
+				//return $.fn.select2.defaults.defaults.matcher(params, item);
+				// Always return the object if there is nothing to compare
+				/** @type {?Select2Item} */
+				var result;
+				if ('' === $.trim(params.term)) {
+					result = item;
+				}
+				else if (!item.children || !item.children.length) {
+					var dfContains = function(haystack, needle) {
+						return -1 < haystack.toUpperCase().indexOf(needle.toUpperCase());
+					};
+					result =
+						dfContains(item.text, params.term)
+						|| (parent && dfContains(parent.text, params.term))
+						? item
+						: null
+					;
+				}
+				// Do a recursive check for options with children
+				else {
+					// Clone the data object if there are children
+					// This is required as we modify the object to remove any non-matches
+					var match = $.extend(true, {}, item);
+					// Check each child of the option
+					for (var c = item.children.length - 1; 0 <= c; c--) {
+						var child = item.children[c];
+						var matches = matcher(params, child, item);
+						// If there wasn't a match, remove the object in the array
+						if (!matches) {
+							match.children.splice(c, 1);
+						}
+					}
+					result = match.children.length ? match : matcher(params, match, parent);
+				}
+				return result;
+			},
+			/**
+			 * 2015-11-28
 			 * http://stackoverflow.com/a/33971933
-			 * @param {Object} item
-			 * @param {Boolean} item.disabled
-			 * @param {HTMLOptionElement} item.element
-			 * @param {String} item.id		Например: "ABeeZee:regular"
-			 * @param {Boolean} item.selected
-			 * @param {String} item.text	Например: "regular"
-			 * @returns {*}
+			 * @param {Select2Item} item
+			 * @returns {String}
 			 */
 			templateSelection: function(item) {
 				/** @type {jQuery} HTMLOptionElement */
@@ -66,6 +129,5 @@ define(['jquery', 'Df_Core/Select2', 'domReady!'], function($) {return (
 				return $optGroup.attr('label') + ' (' + item.text + ')';
 			}
 		});});
-		//debugger;
 	}
 );});
