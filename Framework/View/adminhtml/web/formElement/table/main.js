396c286ad754c4bb1ca821cc988371733bee011e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-12-16 10:46:17 +0300

    no message

--- Framework/View/adminhtml/web/formElement/table/main.js
+++ Framework/View/adminhtml/web/formElement/table/main.js
@@ -7,10 +7,14 @@ define(['jquery', 'Df_Core/Handsontable', 'domReady!'], function($) {return (
 	 */
 	function(config) {
 		var $element = $(document.getElementById(config.id));
-		var $container = $('<div id="test"/>');
+		var $container = $('<div class="df-table"/>');
 		$element.after($container);
 		var $table = new Handsontable($container.get(0), {
-			data:
+			cell: [
+				//{row: 1, col: 0, className: 'htCenter htMiddle'}
+				//,{row: 1, col: 1, className: 'htCenter htMiddle'}
+			]
+			,data:
 				/** @returns {Array} */
 				function() {
 					/** @type {String} */
@@ -30,25 +34,61 @@ define(['jquery', 'Df_Core/Handsontable', 'domReady!'], function($) {return (
 			// http://docs.handsontable.com/0.20.2/Options.html#colHeaders
 			,colHeaders: config.columns
 			// 2015-12-16
+			// Позволяет переупорядочивать строки таблицы мышкой.
 			// http://docs.handsontable.com/0.20.2/demo-moving-rows-and-columns.html
 			// http://docs.handsontable.com/0.20.2/Options.html#manualRowMove
-			//
-			// Похоже, что manualRowMove не обновляет источник данных:
-			// http://stackoverflow.com/a/27625311/
 			,manualRowMove: true
+			// 2015-12-16
+			// Похоже, мы нуждаемся в этой опции,
+			// чтобы при нажатии кнопки «Delete» мы могли использовать $table.getSelected()
+			// http://stackoverflow.com/a/17389359
+			,outsideClickDeselects: false
 			// 2015-12-16
 			// Вынуждены включать, чтобы работало manualRowMove
 			// http://docs.handsontable.com/0.20.2/demo-moving-rows-and-columns.html
 			,rowHeaders: true
+			// 2015-12-16
+			// Растягивает таблицу по ширине родительского контейнера.
+			// Значение «all» означает, что все колонки растягиваются равномерно.
+			// http://docs.handsontable.com/0.20.2/Options.html#stretchH
+			// http://code.dmitry-fedyuk.com/discourse/df-table/blob/330c130a98a4e4bc26ef855ffcda401726ba1b33/assets/javascripts/models/editor.js.es6#L41
+			,stretchH: 'all'
 		});
-		/** @type {jQuery} HTMLFormElement */
-		var $form = $element.closest('form');
-		/**
-		 * 2015-12-16
-		 * По аналогии с http://code.dmitry-fedyuk.com/m2e/markdown/blob/d030a44bfe75765d54d68acf106e2fbb9bd66b4c/view/adminhtml/web/main.js#L364
-		 */
-		$form.bind('beforeSubmit', function() {
-			$element.val(JSON.stringify($table.getData()));
-		});
+		(function() {
+			/** @type {jQuery} HTMLFormElement */
+			var $form = $element.closest('form');
+			/**
+			 * 2015-12-16
+			 * По аналогии с http://code.dmitry-fedyuk.com/m2e/markdown/blob/d030a44bfe75765d54d68acf106e2fbb9bd66b4c/view/adminhtml/web/main.js#L364
+			 */
+			$form.bind('beforeSubmit', function() {
+				$element.val(JSON.stringify($table.getData()));
+			});
+		})();
+		(function() {
+			var $toolbar = $('<div/>').addClass('toolbar');
+			$container.after($toolbar);
+			var button = function(title, onClick) {
+				var $result = $('<button>');
+				$result.append($('<span/>').append(title));
+				$result.click(function(event) {
+					event.preventDefault();
+					onClick();
+				});
+				$toolbar.append($result);
+				return $result;
+			};
+			button('Add', function() {
+				// http://stackoverflow.com/a/29427470
+				// Если порядковый номер добавляемой строки вторым параметром не указан,
+				// то строка добавляется внизу таблицы.
+				// https://github.com/handsontable/handsontable/issues/290#issuecomment-14597650
+				$table.alter('insert_row');
+			});
+			button('Delete', function() {
+				// http://stackoverflow.com/a/17389359
+				$table.alter('remove_row', $table.getSelected()[0]);
+			});
+		})();
 	}
 );});
