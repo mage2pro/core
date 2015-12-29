814bdb18364d1146ec4edd6684bd89bada1f6488
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-12-30 01:59:15 +0300

    no message

--- Framework/view/adminhtml/web/formElement/array/main.js
+++ Framework/view/adminhtml/web/formElement/array/main.js
@@ -7,6 +7,8 @@ define(['jquery', 'domReady!'], function($) {return (
 	function(config) {
 		/** @type {jQuery} HTMLFieldSetElement */
 		var $element = $(document.getElementById(config.id));
+		/** @type {jQuery} HTMLFieldSetElement */
+		var $template = $element.children('.df-name-template');
 		(function() {
 			var $toolbar = $('<div/>').addClass('toolbar');
 			$element.after($toolbar);
@@ -22,21 +24,59 @@ define(['jquery', 'domReady!'], function($) {return (
 			};
 			button('Add', function() {
 				/** @type {jQuery} HTMLFieldSetElement */
-				var $template = $element.children('.df-name-template');
 				var $item = $template.clone(true).removeClass('df-hidden');
 				/**
 				 * 2015-12-30
-				 * Нумерация строк начинается с нуля:
-				 *
+				 * Нумерация элементов начинается с нуля:
+				 * http://code.dmitry-fedyuk.com/m2/all/blob/3d99a5fa88177f8aca5d69f6f9b4ca4865735b9f/Framework/Data/Form/Element/ArrayT.php#L22
+				 * При этом один из элементов является шаблоном (невидима),
+				 * поэтому порядковый номер нового элемента
+				 * равен количеству уже имеющихся элементов минус один
+				 * или же (что то же самое) количеству братьев элемента-шаблона.
 				 * @type {Number}
 				 */
-				var ordering = $element.children('.df-field').length;
+				var ordering = $template.siblings('.df-field').length;
 				$('link', $item).remove();
-				$('*', $item).add($item);
+				/**
+				 * 2015-12-30
+				 * «As of jQuery 1.6, the .attr() method returns undefined
+				 * for attributes that have not been set.»
+				 * http://api.jquery.com/attr/#entry-longdesc
+				 * @param {jQuery} $element HTMLElement
+				 * @param {String} attrName
+				 * @param {String} from
+				 * @param {String} to
+				 */
+				var replaceAttr = function($element, attrName, from, to) {
+					/** @type {?String} */
+					var value = $element.attr(attrName);
+					if ('undefined' !== typeof value) {
+						$element.attr(attrName, value.replace(from, to));
+					}
+				};
+				$('*', $item).add($item).each(function() {
+					var $this = $(this);
+					replaceAttr($this, 'class', 'df-name-template', 'df-name-' + ordering);
+					$.each(['id', 'data-ui-id', 'name'], function() {
+						replaceAttr($this, this, '[template]', '[' + ordering + ']');
+					});
+				});
 				$element.append($item);
-				debugger;
 			});
 			button('Delete', function() {});
 		})();
+		/**
+		 * 2015-12-30
+		 * По аналогии с http://code.dmitry-fedyuk.com/m2e/markdown/blob/d030a44bfe75765d54d68acf106e2fbb9bd66b4c/view/adminhtml/web/main.js#L364
+		 *
+		 * По-правильному надо обрабатывать не beforeSubmit, а submit,
+		 * потому что при beforeSubmit мы ещё не знаем, будет ли форма отправлена на сервер:
+		 * вдруг валидаторы запретят отправку на сервер.
+		 * Поэтому перед удалением шаблона нам важно быть уверенным,
+		 * что форма действительно отправляется на сервер.
+		 * Однако система почему-то игнорирует удаление полей формы на submit,
+		 * поэтому прицепился пока на beforeSubmit.
+		 */
+		$element.closest('form').bind('beforeSubmit', function() {$template.remove();});
 	}
 );});
