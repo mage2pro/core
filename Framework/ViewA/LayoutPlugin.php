9fc94965d20811279686009ae9063e8f18dc4767
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-01-01 01:16:09 +0300

    no message

--- Framework/ViewA/LayoutPlugin.php
+++ Framework/ViewA/LayoutPlugin.php
@@ -1,9 +1,20 @@
 <?php
 namespace Df\Framework\ViewA;
-use Magento\Framework\View\Element\UiComponentInterface;
 use Magento\Framework\View\Layout;
-use Magento\Ui\Component\AbstractComponent;
-class LayoutPlugin {
+use Magento\Ui\Component\Wrapper\UiComponent;
+/**
+ * 2016-01-01
+ * Наследуемся от класса @uses \Magento\Ui\Component\Wrapper\UiComponent,
+ * чтобы получить доступ к его свойству @uses \Magento\Ui\Component\Wrapper\UiComponent::$component
+ * https://github.com/magento/magento2/blob/2.0.0/app/code/Magento/Ui/Component/Wrapper/UiComponent.php#L25
+ */
+class LayoutPlugin extends UiComponent {
+	/**
+	 * 2016-01-01
+	 * Потрясающая техника, которую я изобрёл только что.
+	 */
+	public function __construct() {}
+
 	/**
 	 * 2015-09-19
 	 * Цель метода — получение информации о формируемом в данный момент компоненте
@@ -15,11 +26,11 @@ class LayoutPlugin {
 	 * @return string
 	 */
 	public function aroundRenderNonCachedElement(Layout $subject, \Closure $proceed, $name) {
-		/** @var \Magento\Ui\Component\Wrapper\UiComponent|null $wrapper */
+		/** @var UiComponent|null $wrapper */
 		$wrapper = $subject->isUiComponent($name) ? $subject->getUiComponent($name) : null;
 		/** @var string $result */
 		if ($wrapper) {
-			df_state()->componentSet($wrapper[\Df\Framework\ViewA\Element\BlockFactoryPlugin::COMPONENT]);
+			df_state()->componentSet($wrapper->component);
 		}
 		try {
 			$result = $proceed($name);
