3d2f643ad8cb7d9f330408709727ab95fd440efa
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-04-05 09:25:55 +0300

    no message

--- Core/view/base/requirejs-config.js
+++ Core/view/base/requirejs-config.js
@@ -4,6 +4,7 @@ var config = {
 		,'Df_Core/ColorPicker':  'Df_Core/lib/ColorPicker/main'
 		,'Df_Core/Handsontable':  'Df_Core/lib/Handsontable/main'
 		,'Df_Core/HighlightJs':  'Df_Core/lib/HighlightJs/main'
+		,'Df_Core/Mask':  'Df_Core/lib/Mask/main'
 		,'Df_Core/Select2':  'Df_Core/lib/Select2/main'
 	}
 };
