21247d6f42abcf897be471de187add27af7b739a
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-04-05 11:03:27 +0300

    no message

--- Core/view/base/requirejs-config.js
+++ Core/view/base/requirejs-config.js
@@ -1,10 +1,11 @@
 var config = {
 	paths: {
-		'df':  'Df_Core/js/all'
-		,'Df_Core/ColorPicker':  'Df_Core/lib/ColorPicker/main'
-		,'Df_Core/Handsontable':  'Df_Core/lib/Handsontable/main'
-		,'Df_Core/HighlightJs':  'Df_Core/lib/HighlightJs/main'
-		,'Df_Core/Mask':  'Df_Core/lib/Mask/main'
-		,'Df_Core/Select2':  'Df_Core/lib/Select2/main'
+		'df': 'Df_Core/js/all'
+		,'Df_Core/ColorPicker': 'Df_Core/lib/ColorPicker/main'
+		,'Df_Core/Handsontable': 'Df_Core/lib/Handsontable/main'
+		,'Df_Core/HighlightJs': 'Df_Core/lib/HighlightJs/main'
+		,'Df_Core/Mask': 'Df_Core/lib/Mask/main'
+		,'Df_Core/Select2': 'Df_Core/lib/Select2/main'
+		,'Df_Core/Telephone': 'Df_Core/lib/Telephone/js/main'
 	}
 };
