c470e14226361b13d54967c7276623f447ebafee
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-04-09 14:53:45 +0300

    no message

--- Facebook/lib/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Df\Facebook\Settings as S;
-/**
- * 2016-01-09
- * @return string
- */
-function df_facebook_init() {
-	static $r; return $r || !S::s()->appId() ? '' : $r = df_block_r(null, [], 'Df_Facebook::init');
-}
