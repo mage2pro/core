dd0de981e41253f0b02095e43b5996002ed62531
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-04-21 01:25:07 +0300

    no message

--- Core/view/base/web/js/all.js
+++ Core/view/base/web/js/all.js
@@ -4,6 +4,18 @@ define([
 ], function(array, string) {
 	return {
 		array: array
+		/**
+		 * 2016-04-20
+		 * @param value
+		 * @returns {Boolean}
+		 */
+		,defined: function(value) {return 'undefined' !== typeof value;}
 		, string: string
+		/**
+		 * 2016-04-20
+		 * @param value
+		 * @returns {Boolean}
+		 */
+		,undefined: function(value) {return !this.defined(value);}
 	};
 });
