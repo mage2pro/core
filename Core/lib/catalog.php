e7e14bb905e038b358ed07fdac278eceb96fc57b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-04-23 01:54:12 +0300

    no message

--- Core/lib/catalog.php
+++ Core/lib/catalog.php
@@ -1,4 +1,5 @@
 <?php
+use Magento\Catalog\Helper\Image as ImageHelper;
 use Magento\Catalog\Model\Locator\LocatorInterface;
 use Magento\Catalog\Model\Product;
 /**
@@ -7,6 +8,24 @@ use Magento\Catalog\Model\Product;
  * @return LocatorInterface|\Magento\Catalog\Model\Locator\RegistryLocator
  */
 function df_catalog_locator() {return df_o(LocatorInterface::class);}
+/**
+ * 2016-04-23
+ * @return ImageHelper
+ */
+function df_catalog_image_h() {return df_o(ImageHelper::class);}
+/**
+ * 2016-04-23
+ * How is @uses \Magento\Catalog\Helper\Image::getUrl() implemented and used?
+ * https://mage2.pro/t/1316
+ * How to get the base image URL form a product programmatically?
+ * https://mage2.pro/t/1313
+ * @param Product $product
+ * @param string $type [optional]
+ * @return string
+ */
+function df_product_image_url(Product $product, $type = 'product_page_image_large') {
+	return df_catalog_image_h()->init($product, $type)->getUrl();
+}
 /**
  * 2015-11-14
  * @param Product $product
