e09fc9d4b431c13f501bfd0f88e8528b386c1fa9
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-02 21:53:17 +0300

    no message

--- Customer/lib/address.php
+++ Customer/lib/address.php
@@ -3,17 +3,23 @@ use Magento\Customer\Model\Address as CA;
 use Magento\Customer\Model\Address\AbstractAddress as AA;
 use Magento\Customer\Model\AddressRegistry;
 use Magento\Customer\Model\Customer;
+use Magento\Sales\Model\Order;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Model\Quote\Address as QA;
+use Magento\Sales\Model\Order\Address as OA;
 use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Model\Store;
 /**
  * 2016-04-04
- * @param AA|CA|QA $a
- * @return Customer|Quote|null
+ * @param AA|CA|QA|OA $a
+ * @return Customer|Quote|Order|null
  */
-function df_address_owner(AA $a) {
-	return $a instanceof CA ? $a->getCustomer() : ($a instanceof QA ? $a->getQuote() : null);
+function df_address_owner($a) {
+	return $a instanceof CA ? $a->getCustomer() : (
+		$a instanceof QA ? $a->getQuote() : (
+			$a instanceof OA ? $a->getOrder() : null
+		)
+	);
 }
 
 /**
@@ -24,10 +30,10 @@ function df_address_registry() {return df_o(AddressRegistry::class);}
 
 /**
  * 2016-04-04
- * @param AA|CA|QA $a
+ * @param AA|CA|QA|OA $a
  * @return StoreInterface|Store
  */
-function df_address_store(AA $a) {
+function df_address_store($a) {
 	/** @var Customer|Quote|null $owner */
 	$owner = df_address_owner($a);
 	return $owner ? df_store($owner->getStore()) : null;
