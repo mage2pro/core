2bcb1cee006d2bc3d4d8d52e9bbf46a8bec897af
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-22 02:43:42 +0300

    no message

--- Core/lib/sales.php
+++ Core/lib/sales.php
@@ -16,6 +16,24 @@ use Magento\SalesSequence\Model\Meta as _Meta;
 use Magento\Store\Api\Data\StoreInterface;
 
 /**
+ * 2016-05-21
+ * How to get an order backend URL programmatically? https://mage2.pro/t/1639
+ * 2016-05-22
+ * Даже если включена опция «Add Secret Key to URLs», адреса без ключей всё равно работают.
+ * https://mage2.pro/tags/backend-url-secret-key
+ * How to skip adding the secret key to a backend URL using the «_nosecret» parameter?
+ * https://mage2.pro/t/1644
+ * @param int $id
+ * @return string
+ */
+function df_credit_memo_backend_url($id) {
+	df_assert($id);
+	return df_url_backend('sales/order_creditmemo/view', [
+		'creditmemo_id' => $id, '_nosecret' => true
+	]);
+}
+
+/**
  * 2016-03-27
  * @param OrderInterface|Order $order
  * @param int $transactionId
