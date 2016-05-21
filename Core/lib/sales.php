ce85c107aa10aa801aa56291fe6551f0acc9138b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-22 02:33:12 +0300

    no message

--- Core/lib/sales.php
+++ Core/lib/sales.php
@@ -165,10 +165,18 @@ function df_order_shipping_title(Order $order) {
 /**
  * 2016-05-21
  * How to get an order backend URL programmatically? https://mage2.pro/t/1639
+ * 2016-05-22
+ * Даже если включена опция «Add Secret Key to URLs», адреса без ключей всё равно работают.
+ * https://mage2.pro/tags/backend-url-secret-key
+ * How to skip adding the secret key to a backend URL using the «_nosecret» parameter?
+ * https://mage2.pro/t/1644
  * @param int $id
  * @return string
  */
-function df_order_backend_url($id) {return df_url_backend('sales/order/view', ['order_id' => $id]);}
+function df_order_backend_url($id) {
+	df_assert($id);
+	return df_url_backend('sales/order/view', ['order_id' => $id, '_nosecret' => true]);
+}
 
 /**
  * 2016-01-11
