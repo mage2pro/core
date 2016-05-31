2cb05984632f56ba487eaa2f4f079f8167e4c45d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-31 12:01:39 +0300

    1.3.6

--- Framework/Data/Form/Element/Webhook.php
+++ Framework/Data/Form/Element/Webhook.php
@@ -11,13 +11,16 @@ class Webhook extends Element {
 	 */
 	public function getElementHtml() {
 		return df_tag('div', 'df-webhook',
-			!$this->enabled()
+			$this->thirdPartyLocalhost()
 			? 'The notifications are not available,'
 			. ' because the store is running on <b>localhost</b>.'
 			: (
 				!$this->requireHttps() || df_uri_check_https($this->url())
 				? $this->url()
-				: ''
+				: 'Looks like your <a href="https://mage2.pro/t/topic/1723" target="_blank">'
+				 . '«<b>General</b>» → «<b>Web</b>» → «<b>Base URLs (Secure)</b> '
+				 . ' → «<b>Secure Base URL</b>»</a>'
+				 . ' option is misconfigured (does not start with «https»).'
 			)
 		);
 	}
@@ -32,18 +35,7 @@ class Webhook extends Element {
 			$html .= '<p class="note"><span>' . $element->getComment() . '</span></p>';
 		}
 	 */
-	public function getComment() {return !$this->enabled() ? null : parent::getComment();}
-
-	/**
-	 * 2016-05-30
-	 * @return bool
-	 */
-	private function enabled() {
-		if (!isset($this->{__METHOD__})) {
-			$this->{__METHOD__} = !df_is_localhost() || df_is_it_my_local_pc();
-		}
-		return $this->{__METHOD__};
-	}
+	public function getComment() {return $this->thirdPartyLocalhost() ? null : parent::getComment();}
 
 	/**
 	 * 2016-05-30
@@ -51,7 +43,9 @@ class Webhook extends Element {
 	 */
 	private function requireHttps() {
 		if (!isset($this->{__METHOD__})) {
-			$this->{__METHOD__} = df_fe_fc_b($this, 'dfWebhook_requireHTTPS');
+			$this->{__METHOD__} =
+				df_fe_fc_b($this, 'dfWebhook_requireHTTPS') && !$this->thirdPartyLocalhost()
+			;
 		}
 		return $this->{__METHOD__};
 	}
@@ -70,21 +64,35 @@ class Webhook extends Element {
 
 	/**
 	 * 2016-05-30
+	 * @return bool
+	 */
+	private function thirdPartyLocalhost() {
+		if (!isset($this->{__METHOD__})) {
+			$this->{__METHOD__} = df_is_localhost() && !df_is_it_my_local_pc();
+		}
+		return $this->{__METHOD__};
+	}
+
+	/**
+	 * 2016-05-30
+	 * 2016-05-31
+	 * https://mage2.pro/tags/secure-url
+	 * @see \Magento\Framework\Url::getBaseUrl()
+	 * https://github.com/magento/magento2/blob/a5fa3af3/lib/internal/Magento/Framework/Url.php#L437-L439
+		if (isset($params['_secure'])) {
+			$this->getRouteParamsResolver()->setSecure($params['_secure']);
+		}
 	 * @return string
 	 */
 	private function url() {
 		if (!isset($this->{__METHOD__})) {
-			/** @var string $result */
-			$result = df_url_frontend($this->routePath(), ['_secure' => $this->requireHttps()]);
-			if (df_is_it_my_local_pc()) {
-				$result = str_replace(
-					'http://localhost.com:900/store/'
-					, 'https://mage2.pro/sandbox/'
-					, $result
-				);
-			}
-			df_result_string_not_empty($result);
-			$this->{__METHOD__} = $result;
+			$this->{__METHOD__} =
+				df_is_it_my_local_pc()
+				? "https://mage2.pro/sandbox/{$this->routePath()}/"
+				: df_url_frontend($this->routePath(), [
+					'_secure' => $this->requireHttps() ? true : null
+				])
+			;
 		}
 		return $this->{__METHOD__};
 	}
