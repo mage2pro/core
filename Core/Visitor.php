0af901a026401419553d875fabcd181b08c0dd4b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-31 22:17:11 +0300

    1.3.8

--- Core/Visitor.php
+++ Core/Visitor.php
@@ -3,7 +3,7 @@ namespace Df\Core;
 class Visitor extends O {
 	/**
 	 * На английском языке. Например: «Moscow».
-	 * @return string
+	 * @return string|null
 	 */
 	public function city() {return $this->r('city');}
 
@@ -13,42 +13,42 @@ class Visitor extends O {
 	 */
 	public function countryName() {return $this->r('country_name');}
 
-	/** @return string */
+	/** @return string|null */
 	public function iso2() {return $this->r('country_code');}
 
 	/**
 	 * Например: «55.752».
-	 * @return string
+	 * @return string|null
 	 */
 	public function latitude() {return $this->r('latitude');}
 
 	/**
 	 * Например: «37.616».
-	 * @return string
+	 * @return string|null
 	 */
 	public function longitude() {return $this->r('longitude');}
 
 	/**
 	 * Например: «101194».
-	 * @return string
+	 * @return string|null
 	 */
 	public function postCode() {return $this->r('zip_code');}
 
 	/**
 	 * Например: «MOW».
-	 * @return string
+	 * @return string|null
 	 */
 	public function regionCode() {return $this->r('region_code');}
 
 	/**
 	 * На английском языке. Например: «Moscow».
-	 * @return string
+	 * @return string|null
 	 */
 	public function regionName() {return $this->r('region_name');}
 
 	/**
 	 * Например: «Europe/Moscow».
-	 * @return string
+	 * @return string|null
 	 */
 	public function timeZone() {return $this->r('time_zone');}
 
@@ -58,12 +58,27 @@ class Visitor extends O {
 	 */
 	private function r($key) {return dfa($this->responseA(), $key);}
 
-	/** @return array(string => mixed) */
+	/**
+	 * 2016-05-31
+	 * Сегодня заметил, что при запросе из PHP freegeoip.net перестал возвращать мне значение,
+	 * а при запросе из браузера — по прежнему возвращает.
+	 * Видимо, freegeoip.net забанил мой User-Agent?
+	 * В любом случае, нельзя полагаться, что freegeoip.net вернёт непустой ответ.
+	 *
+	 * Стандартное время ожидание ответа сервера задаётся опцией default_socket_timeout:
+	 * http://php.net/manual/en/filesystem.configuration.php#ini.default-socket-timeout
+	 * Её значение по-умолчанию равно 60 секундам.
+	 * Конечно, при оформлении заказа негоже заставлять покупателя ждать 60 секунд
+	 * только ради узнавания его страны вызовом @see df_visitor(),
+	 * поэтому уменьшил время ожидания ответа до 5 секунд.
+	 *
+	 * @return array(string => mixed)
+	 */
 	private function responseA() {
 		if (!isset($this->{__METHOD__})) {
-			$this->{__METHOD__} = df_json_decode(file_get_contents(
-				'https://freegeoip.net/json/' . $this[self::$P__IP]
-			));
+			$this->{__METHOD__} = df_http_json(
+				'https://freegeoip.net/json/' . $this[self::$P__IP], [], [], 5
+			);
 		}
 		return $this->{__METHOD__};
 	}
--- Core/lib/http.php
+++ Core/lib/http.php
@@ -16,15 +16,36 @@
  *
  * @param $urlBase
  * @param array(string => string) $params [optional]
+ *
+ * 2016-05-31
+ * Стандартное время ожидание ответа сервера задаётся опцией default_socket_timeout:
+ * http://php.net/manual/en/filesystem.configuration.php#ini.default-socket-timeout
+ * Её значение по-умолчанию равно 60 секундам.
+ * Конечно, при оформлении заказа негоже заставлять покупателя ждать 60 секунд
+ * только ради узнавания его страны вызовом @see df_visitor()
+ * Поэтому добавил возможность задавать нестандартное время ожидания ответа сервера:
+ * http://stackoverflow.com/a/10236480
+ * https://amitabhkant.com/2011/08/21/using-timeouts-with-file_get_contents-in-php/
+ *
+ * @param int|null $timeout [optional]
+ *
+ *
  * @return string|bool
  * The function returns the read data or FALSE on failure.
  * http://php.net/manual/function.file-get-contents.php
  */
-function df_http_get($urlBase, array $params = []) {
+function df_http_get($urlBase, array $params = [], $timeout = null) {
 	/** @var string $url */
 	$url = !$params ? $urlBase : $urlBase . '?' . http_build_query($params);
-	return file_get_contents($url, null, stream_context_create([
-		'http' => ['ignore_errors' => true]
+	/**
+	 * 2016-05-31
+	 * @uses file_get_contents() может возбудить Warning:
+	 * «failed to open stream: A connection attempt failed
+	 * because the connected party did not properly respond after a period of time,
+	 * or established connection failed because connected host has failed to respond.»
+	 */
+	return @file_get_contents($url, null, stream_context_create([
+		'http' => df_clean(['ignore_errors' => true, 'timeout' => $timeout])
 	]));
 }
 
@@ -33,13 +54,14 @@ function df_http_get($urlBase, array $params = []) {
  * @param $urlBase
  * @param array(string => string) $params [optional]
  * @param mixed $default [optional]
+ * @param int|null $timeout [optional]
  * @return array|mixed
  */
-function df_http_json($urlBase, array $params = [], $default = null) {
+function df_http_json($urlBase, array $params = [], $default = null, $timeout = null) {
 	/** @var array|mixed $result */
 	$result = $default;
 	/** @var string|bool $json */
-	$json = df_http_get($urlBase, $params);
+	$json = df_http_get($urlBase, $params, $timeout);
 	if (false !== $json) {
 		/** @var bool|array|null $decoded */
 		$decoded = df_json_decode($json);
