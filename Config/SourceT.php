efeeb6c33a1378192567c10f674c43f5feb22c84
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-08-07 19:36:46 +0300

    1.5.25

--- Config/SourceT.php
+++ Config/SourceT.php
@@ -2,6 +2,20 @@
 namespace Df\Config;
 abstract class SourceT extends Source {
 	/**
+	 * 2016-08-07
+	 * @used-by \Dfe\AllPay\Settings::methodsLabels()
+	 * @param string[]|null $keys [optional]
+	 * @return string[]
+	 */
+	public function labels($keys = null) {
+		/** @var array(string => string) $labels */
+		$labels = $this->map();
+		return array_values(df_translate_a(
+			is_null($keys) ? $labels : dfa_select_ordered($labels, $keys)
+		));
+	}
+
+	/**
 	 * 2015-11-27
 	 * @override
 	 * @see \Df\Config\Source::toOptionArray()
