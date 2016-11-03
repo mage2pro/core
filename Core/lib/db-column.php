e888c7c61d0427b17541a8077bb89c1725558c85
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-04 02:37:49 +0300

    df_db_column_exists

--- Core/lib/db-column.php
+++ Core/lib/db-column.php
@@ -9,28 +9,28 @@
 function df_db_column_add($table, $name, $definition = 'varchar(255) default null') {
 	$table = df_table($table);
 	df_conn()->query("alter table {$table} add column `{$name}` {$definition};");
-	df_conn()->resetDdlCache();
+	df_conn()->resetDdlCache($table);
 }
 
 /**
  * 2016-11-01
  * http://stackoverflow.com/a/7264865
+ *
+ * 2016-11-04
+ * Раньше (пока не знал о методе ядра) реализация была такой:
+	$table = df_table($table);
+	$query = df_db_quote_into("SHOW COLUMNS FROM `{$table}` LIKE ?", $column);
+	return !!df_conn()->query($query)->fetchColumn();
+ *
  * @param string $table
  * @param string $column
  * @return bool
  */
-function df_db_column_exists($table, $column) {
-	$table = df_table($table);
-	/** @var string $query */
-	$query = df_db_quote_into("SHOW COLUMNS FROM `{$table}` LIKE ?", $column);
-	/**
-	 * 2016-11-01
-	 * @uses Zend_Db_Statement_Pdo::fetchColumn() в данном случае возвращает $column,
-	 * если колонка в таблицен присутствует, и false, если отсутствует.
-	 * http://stackoverflow.com/a/11305431
-	 */
-	return !!df_conn()->query($query)->fetchColumn();
-}
+function df_db_column_exists($table, $column) {return
+	// 2016-11-04
+	// df_table нужно вызывать обязательно!
+	df_conn()->tableColumnExists(df_table($table), $column)
+;}
 
 /**
  * 2016-11-04
@@ -41,5 +41,5 @@ function df_db_column_exists($table, $column) {
  */
 function df_db_column_rename($table, $from, $to) {
 	$table = df_table($table);
-	df_conn()->resetDdlCache();
+	df_conn()->resetDdlCache($table);
 }
