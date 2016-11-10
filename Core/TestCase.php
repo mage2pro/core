ca771df848b721cba5a34a8f9d6883840dbe4db1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-10 06:35:46 +0300

    1.8.18: https://mage2.pro/t/2260

--- Core/TestCase.php
+++ Core/TestCase.php
@@ -3,7 +3,20 @@
 namespace Df\Core;
 use Magento\Framework\App\Bootstrap;
 use Magento\Framework\App\Http;
-abstract class TestCase extends \PHPUnit\Framework\TestCase {
+/**
+ * 2016-11-10
+ * Вынуждены использовать PHPUnit 4, потому что иначе будет сбой компиляции:
+ * https://mage2.pro/t/2260
+ */
+abstract class TestCase extends \PHPUnit_Framework_TestCase {
+    /**
+     * 2016-11-10
+	 * Метод @see \PHPUnit\Framework\TestCase::expectException() появился только в PHPUnit 5.2.0,
+	 * поэтому эмулируем его.
+	 * @param string $exception
+     */
+    public function expectException($exception) {$this->setExpectedException($exception);}
+
 	/**
 	 * 2016-11-03
 	 * @override
