f19ef52e3fb2e1431dfcb97db769079f0ac09c66
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-13 11:27:02 +0300

    1.8.26

---
+++ Payment/Source/ACR.php
@@ -0,0 +1,19 @@
+<?php
+namespace Df\Payment\Source;
+use Magento\Payment\Model\Method\AbstractMethod as M;
+class ACR extends \Df\Config\SourceT {
+	/**
+	 * 2016-03-07
+	 * @override
+	 * @see \Df\Config\Source::map()
+	 * @used-by \Df\Config\Source::toOptionArray()
+	 * @return array(string => string)
+	 */
+	protected function map() {return [
+		M::ACTION_AUTHORIZE => 'Authorize'
+		,M::ACTION_AUTHORIZE_CAPTURE => 'Capture'
+		,self::REVIEW => 'Review'
+	];}
+
+	const REVIEW = 'review';
+}
