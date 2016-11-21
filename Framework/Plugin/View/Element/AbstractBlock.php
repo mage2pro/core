25bded2351ca667eaed2e65a00069ce861b67ae1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-21 11:01:07 +0300

    1.9.13

---
+++ Framework/Plugin/View/Element/AbstractBlock.php
@@ -0,0 +1,22 @@
+<?php
+// 2016-11-21
+namespace Df\Framework\Plugin\View\Element;
+use Magento\Framework\View\Element\AbstractBlock as Sb;
+class AbstractBlock {
+	/**
+	 * 2016-11-21
+	 * Цель плагина — устранение дефекта метода
+	 * @see \Magento\Framework\View\Element\AbstractBlock::extractModuleName(),
+	 * который работает некорректно (возвращает пустую строку),
+	 * если класс модуля не имеет префикса «Block»:
+	 * https://github.com/magento/magento2/blob/2.1.2/lib/internal/Magento/Framework/View/Element/AbstractBlock.php#L846-L860
+	 * @see \Magento\Framework\View\Element\AbstractBlock::getModuleName()
+	 *
+	 * @param Sb $sb
+	 * @param string $r
+	 * @return string
+	 */
+	public function afterGetModuleName(Sb $sb, $r) {return
+		$r ?: $sb['module_name'] = df_module_name($sb)
+	;}
+}
