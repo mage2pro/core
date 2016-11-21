b707735aaf778a5d77331b8e6d091d0875465186
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-21 06:43:49 +0300

    no message

--- Core/lib/test.php
+++ Core/lib/test.php
@@ -14,4 +14,24 @@ function df_test_file($moduleName, $localPath = '') {return
 	df_cc_path(df_module_dir($moduleName), 'T/data', $localPath)
 ;}
 
+/**
+ * 2016-11-21
+ * @param string|object $moduleName
+ * @param string $localPath [optional]
+ * @return string
+ */
+function df_test_file_l($moduleName, $localPath = '') {return
+	file_get_contents(df_test_file($moduleName, $localPath))
+;}
+
+/**
+ * 2016-11-21
+ * @param string|object $moduleName
+ * @param string $localPath [optional]
+ * @return array(string => string|array)
+ */
+function df_test_file_lj($moduleName, $localPath = '') {return
+	df_json_decode(df_test_file_l($moduleName, $localPath))
+;}
+
 
