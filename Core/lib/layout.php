8d54cf99e1e3289b02551c80753aec5d70ac0e01
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-22 13:11:19 +0300

    1.9.17

--- Core/lib/layout.php
+++ Core/lib/layout.php
@@ -9,19 +9,27 @@ use Magento\Framework\View\Element\Template;
  * Добавил возможность передачи в качестве первого параметра @see O
  * причём как в виде объекта, так и строки-класса.
  *
- * Такая возможность позволяет нам эффективно рендерить шаблоны
- * без иерархии своих классов-блоков.
+ * Такая возможность позволяет нам эффективно рендерить шаблоны без иерархии своих классов-блоков.
  * В Российской сборке для Magento 1.x
  * нам приходилось дублировать один и тот же код в классе базовой модели (аналоге класса O),
  * и в 2-х базовых классах блоков (абстрактном и блоке с шаблоном), т.е. в 3 местах.
  * Теперь же нам этого делать не нужно.
  *
+ * @used-by df_phtml()
  * @param string|O|null $type
  * @param string|array(string => mixed) $data [optional]
  * @param string|null $template [optional]
+ *
+ * 2016-11-22
+ * @param array $vars [optional]
+ * Параметры $vars будут доступны в шаблоне в качестве переменных:
+ * @see \Magento\Framework\View\TemplateEngine\Php::render()
+		extract($dictionary, EXTR_SKIP);
+ * https://github.com/magento/magento2/blob/2.1.2/lib/internal/Magento/Framework/View/TemplateEngine/Php.php#L58
+ *
  * @return AbstractBlock|BlockInterface|Template
  */
-function df_block($type, $data = [], $template = null) {
+function df_block($type, $data = [], $template = null, array $vars = []) {
 	/**
 	 * 2015-12-14
 	 * $type может быть как объектом, так и строкой:
@@ -44,11 +52,24 @@ function df_block($type, $data = [], $template = null) {
 		$data = [];
 	}
 	/** @var AbstractBlock|BlockInterface|Template $result */
-	$result = df_layout()->createBlock($type, dfa($data, 'name'), $data);
+	/**
+	 * 2016-11-22
+	 * В отличие от Magento 1.x, в Magento 2 нам нужен синтаксис ['data' => $data]:
+	 * @see \Magento\Framework\View\Layout\Generator\Block::createBlock():
+	 * $block->addData(isset($arguments['data']) ? $arguments['data'] : []);
+	 * https://github.com/magento/magento2/blob/2.1.2/lib/internal/Magento/Framework/View/Layout/Generator/Block.php#L240
+	 * В Magento 1.x было не так:
+	 * https://github.com/OpenMage/magento-mirror/blob/1.9.3.1/app/code/core/Mage/Core/Model/Layout.php#L482-L491
+	 */
+	$result = df_layout()->createBlock($type, dfa($data, 'name'), ['data' => $data]);
+	// 2016-11-22
+	$result->assign($vars);
 	if ($template && $result instanceof Template) {
 		$result->setTemplate(df_append($template, '.phtml'));
 	}
 	if ($context) {
+		// 2016-11-22
+		// «Sets the object that should represent $block in template.»
 		$result->setTemplateContext($context);
 	}
 	return $result;
@@ -74,8 +95,13 @@ function df_layout() {return df_o(\Magento\Framework\View\LayoutInterface::class
  * 2016-11-22
  * @param string|object $module
  * @param string $template [optional]
+ * @param array $vars [optional]
+ * Параметры $vars будут доступны в шаблоне в качестве переменных:
+ * @see \Magento\Framework\View\TemplateEngine\Php::render()
+		extract($dictionary, EXTR_SKIP);
+ * https://github.com/magento/magento2/blob/2.1.2/lib/internal/Magento/Framework/View/TemplateEngine/Php.php#L58
  * @return string
  */
-function df_phtml($module, $template) {return
-	df_block(null, [], df_module_name($module) . '::' . $template)->toHtml()
+function df_phtml($module, $template, array $vars = []) {return
+	df_block(null, [], df_module_name($module) . '::' . $template, $vars)->toHtml()
 ;}
