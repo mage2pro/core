ffc729d92eb6365aa939f16f8a281482d3ab6b12
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-23 14:14:04 +0300

    no message

---
+++ README.md
@@ -0,0 +1,8 @@
+## Installation
+You need not install this package directly: it is installed automatically with the Mage2.PRO [Magento 2 extensions](https://mage2.pro/c/extensions).
+
+## Support
+
+- [Ask a question](https://mage2.pro/c/meta/ask)
+- [Report an issue](https://github.com/mage2pro/core/issues)
+
--- registration.php
+++ registration.php
@@ -1,27 +1,22 @@
 <?php
-use Df\Qa\Message\Failure\Error;
 use Magento\Framework\Component\ComponentRegistrar as R;
 // https://github.com/magento-russia/3/blob/2016-11-22/app/code/local/Df/Core/Boot.php?ts=4#L155-L188
 ini_set('session.gc_probability', 1);
 ini_set('session.gc_divisor', 100);
 /** @var string $base */
-$base = dirname(__FILE__) . '/';
-/** @var string[] $modules */
-$modules = array_diff(scandir($base), ['..', '.', '.git', 'composer.json', 'etc', 'registration.php']);
-foreach (array_merge(['Core'], array_diff($modules, ['Core'])) as $m) {
+foreach (array_merge(['Core'], array_diff(scandir($base = dirname(__FILE__) . '/'), ['Core'])) as $m) {
+	// 2016-11-23
+	// It gets rid of the ['..', '.', '.git', 'etc', composer.json', 'README.md', 'registration.php']
 	/** @var string $baseM */
-	$baseM = $base . $m;
-	R::register(R::MODULE, "Df_{$m}", $baseM);
-	/** @var string $libDir */
-	$libDir = "{$baseM}/lib";
-	if (is_dir($libDir)) {
-		// 2015-02-06
-		// array_slice отсекает «.» и «..».
-		// http://php.net/manual/function.scandir.php#107215
-		foreach (array_slice(scandir($libDir), 2) as $c) {
-			/** @var string $c */
-			require_once $libDir . '/' . $c;
+	if (ctype_upper($m[0]) && is_dir($baseM = $base . $m)) {
+		R::register(R::MODULE, "Df_{$m}", $baseM);
+		/** @var string $libDir */
+		if (is_dir($libDir = "{$baseM}/lib")) {
+			// 2015-02-06
+			// array_slice removes «.» and «..».
+			// http://php.net/manual/function.scandir.php#107215
+			foreach (array_slice(scandir($libDir), 2) as $c) {require_once "{$libDir}/{$c}";}
 		}
 	}
 }
-register_shutdown_function(function() {Error::check();});
+register_shutdown_function(function() {\Df\Qa\Message\Failure\Error::check();});
