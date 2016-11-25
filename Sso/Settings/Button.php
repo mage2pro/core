da81e53eb67cbe699cf974d44dffe3685a4e0efc
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-25 15:58:20 +0300

    no message

--- Sso/Settings/Button.php
+++ Sso/Settings/Button.php
@@ -1,7 +1,12 @@
 <?php
 // 2016-11-24
 namespace Df\Sso\Settings;
-final class Button extends \Df\Config\Settings\Configurable {
+/**
+ * 2016-11-25
+ * Класс нельзя объявлять финальным, потому что от него наследуется
+ * @see \Dfe\FacebookLogin\Settings\Button
+ */
+class Button extends \Df\Config\Settings\Configurable {
 	/**
 	 * 2016-11-24
 	 * The button's label.
