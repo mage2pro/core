494bdbf9359dc4b30b9b538cc07d85b6e7baa4e9
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-12-03 20:47:15 +0300

    no message

--- Customer/Plugin/Block/Form/Register.php
+++ Customer/Plugin/Block/Form/Register.php
@@ -12,18 +12,21 @@ class Register {
 	 * @param O $o
 	 * @return O
 	 */
-	public function afterGetFormData(Sb $sb, O $o) {return
-		!df_my_local() || $o->getData() ? $o : $o->setData([
+	public function afterGetFormData(Sb $sb, O $o) {return $o->setData($o->getData()
+		+ df_customer_session()->getDfSsoRegistrationData()
+		+ (!df_my_local() ? [] : [
 			'dob' => '1982-07-08'
 			,'email' => 'test-customer@mage2.pro'
 			,'firstname' => 'Test'
 			,'lastname' => 'Customer'
-			// 2016-12-03
-			// К сожалению, установить пароль здесь нет возможности.
-			// Если в дальшейшем такая возможносность появится,
-			// то надо не забыть, что минимальная длина — 8 символов.
-			//,'password' => '11111111'
-			//,'password_confirmation' => '11111111'
+			/**
+			 * 2016-12-03
+			 * Установить пароль здесь нет возможности,
+			 * потому что https://github.com/magento/magento2/blob/2.1.2/app/code/Magento/Customer/view/frontend/templates/form/register.phtml#L142-L148
+			 * не устанавливает значение атрибута «value» для <input type="password">.
+			 * Поэтому пароль мы устанавливаем посредством JavaScript:
+			 * https://github.com/mage2pro/core/blob/7e7db69/Sso/view/frontend/web/reg-completion.js#L7
+			 */
 		])
-	;}
+	);}
 }
