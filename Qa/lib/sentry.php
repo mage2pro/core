3b12c84629ac8a3314b3b8316cc5da3af8416d06
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-12-22 17:55:11 +0300

    1.10.13

--- Qa/lib/sentry.php
+++ Qa/lib/sentry.php
@@ -9,9 +9,15 @@ use Magento\Framework\DataObject;
  */
 function df_sentry($v, array $context = []) {
 	if (true || !df_my_local()) {
-		$context = df_extend(['extra' => [
-			'Magento version' => df_magento_version()
-		]], $context);
+		$context = df_extend([
+			// 2016-22-22
+			// https://docs.sentry.io/clients/php/usage/#optional-attributes
+			'extra' => []
+			,'tags' => [
+				'PHP' => phpversion()
+				,'Magento' => df_magento_version()
+			]
+		], $context);
 		if ($v instanceof E) {
 			// 2016-12-22
 			// https://docs.sentry.io/clients/php/usage/#reporting-exceptions
@@ -30,6 +36,18 @@ function df_sentry($v, array $context = []) {
  * 2016-12-22
  * @return \Raven_Client
  */
-function df_sentry_m() {return dfcf(function() {return new \Raven_Client(
-	'https://0574710717d5422abd1c5609012698cd:32ddadc0944c4c1692adbe812776035f@sentry.io/124181'
-);});}
+function df_sentry_m() {return dfcf(function() {
+	/** @var \Raven_Client $result */
+	$result = new \Raven_Client(
+		'https://0574710717d5422abd1c5609012698cd:32ddadc0944c4c1692adbe812776035f@sentry.io/124181'
+	);
+	/**
+	 * 2016-12-22
+	 * «The root path to your application code.»
+	 * https://docs.sentry.io/clients/php/config/#available-settings
+	 * У Airbrake для Ruby есть аналогичный параметр — «root_directory»:
+	 * https://github.com/airbrake/airbrake-ruby/blob/v1.6.0/README.md#root_directory
+	 */
+	$result->setAppPath(BP);
+	return $result;
+});}
