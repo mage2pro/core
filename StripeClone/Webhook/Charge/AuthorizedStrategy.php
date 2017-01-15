1aa2eeacf5f706a74735711ef8e245f7854c7bbf
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-15 06:38:41 +0300

    no message

---
+++ StripeClone/Webhook/Charge/AuthorizedStrategy.php
@@ -0,0 +1,13 @@
+<?php
+// 2017-01-15
+namespace Df\StripeClone\Webhook\Charge;
+class AuthorizedStrategy extends \Df\StripeClone\WebhookStrategy {
+	/**
+	 * 2017-01-15
+	 * @override
+	 * @see \Df\StripeClone\WebhookStrategy::handle()
+	 * @used-by \Df\StripeClone\Webhook::_handle()
+	 * @return void
+	 */
+	final public function handle() {}
+}
