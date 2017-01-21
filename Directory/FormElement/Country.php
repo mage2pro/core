09bf9d9ad3a7b8f668cc71e6d8d946b483c045ea
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-21 14:36:13 +0300

    no message

--- Directory/FormElement/Country.php
+++ Directory/FormElement/Country.php
@@ -5,6 +5,24 @@ class Country extends Dropdown {
 	/**
 	 * 2017-01-21
 	 * @override
+	 * @see \Df\Framework\Form\Element\Select::getValue()
+	 * @used-by \Df\Framework\Form\Element\Select2::setRenderer()
+	 * @return string|null
+	 */
+	public function getValue() {
+		/**
+		 * 2017-01-21
+		 * @todo По хорошему, здесь надо учитывать область действия настроек.
+		 * Мы же пока таким кодом запрашиваем глобальное значение.
+		 */
+		/** @var string $global */
+		$global = df_store_country()->getIso2Code();
+		return parent::getValue() ?: (in_array($global, $this->dfValues()) ? $global : null);
+	}
+	
+	/**
+	 * 2017-01-21
+	 * @override
 	 * @see \Df\Framework\Form\Element\Select2::getValues()
 	 * @used-by \Df\Framework\Form\Element\Select2::setRenderer()
 	 * @return array(array(string => string))
