9472cb27c7e5d19138d7231fdf631d045eb8bcca
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-24 12:51:08 +0300

    no message

--- Payment/Settings/BankCard.php
+++ Payment/Settings/BankCard.php
@@ -5,7 +5,8 @@ class BankCard extends \Df\Payment\Settings {
 	 * 2016-11-10
 	 * «Prefill the Payment Form with Test Data?» 
 	 * @used-by \Df\Payment\ConfigProvider\BankCard::config()
+	 * @see \Dfe\CheckoutCom\Settings::prefill()
 	 * @return string|false|array(string => string)
 	 */
-	public function prefill() {return $this->bv();}	
+	public function prefill() {return $this->bv();}
 }
--- StripeClone/Settings.php
+++ StripeClone/Settings.php
@@ -3,11 +3,10 @@ namespace Df\StripeClone;
 class Settings extends \Df\Payment\Settings\BankCard {
 	/**
 	 * 2016-11-12
+	 * @see \Dfe\Square\Settings::publicKey()
 	 * @return string
 	 */
-	public function publicKey() {return
-		$this->testable('publishableKey', null, function() {return $this->testable('publicKey');})
-	;}
+	public function publicKey() {return $this->testable(
+		'publishableKey', null, function() {return $this->testable('publicKey');}
+	);}
 }
-
-
