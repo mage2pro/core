6f0b3dc269cad4816dee2f5fd60113ed659437ff
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-07 07:45:32 +0300

    no message

--- Payment/lib/method.php
+++ Payment/lib/method.php
@@ -1,12 +1,12 @@
 <?php
-use Df\Payment\Method;
-use Magento\Payment\Model\MethodInterface as IMethod;
+use Df\Payment\Method as M;
+use Magento\Payment\Model\MethodInterface as IM;
 use Magento\Sales\Model\Order\Payment\Transaction as T;
 /**
  * 2016-08-20
  * @see df_trans_by_payment()
  * @param T $t
- * @return IMethod|Method;
+ * @return IM|M;
  */
 function dfp_method_by_trans(T $t) {return dfp_by_trans($t)->getMethodInstance();}
 
@@ -42,10 +42,10 @@ function dfp_method_code_short($c) {return df_trim_text_left(dfp_method_code($c)
  * 2016-08-19
  * @see df_trans_is_my()
  * @used-by dfp_is_my()
- * @param IMethod $method
+ * @param IM $method
  * @return bool
  */
-function dfp_method_is_my(IMethod $method) {return $method instanceof Method;}
+function dfp_method_is_my(IM $method) {return $method instanceof M;}
 
 /**
  * 2016-12-22
