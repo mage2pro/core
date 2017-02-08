27f8abaf26fa0ce00509d1edbadadb5f1b47c042
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-09 00:28:23 +0300

    no message

--- Quote/lib/main.php
+++ Quote/lib/main.php
@@ -1,21 +1,22 @@
 <?php
 use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Quote\Api\CartRepositoryInterface as IQuoteRepository;
-use Magento\Quote\Api\Data\CartInterface as IQuote;
-use Magento\Quote\Model\Quote;
-use Magento\Quote\Model\QuoteRepository;
+use Magento\Quote\Api\CartRepositoryInterface as IQR;
+use Magento\Quote\Api\Data\CartInterface as IQ;
+use Magento\Quote\Model\Quote as Q;
+use Magento\Quote\Model\QuoteRepository as QR;
 /**
  * 2016-07-18
  * @used-by \Df\Payment\ConfigProvider::config()
+ * @used-by \Df\Payment\Method::getInfoInstance()
  * @param int|null $id [optional]
- * @return IQuote|Quote
+ * @return IQ|Q
  * @throws NoSuchEntityException
  */
 function df_quote($id = null) {return $id ? df_quote_r()->get($id) : df_checkout_session()->getQuote();}
 
 /**
  * 2016-07-18
- * @return IQuoteRepository|QuoteRepository
+ * @return IQR|QR
  */
-function df_quote_r() {return df_o(IQuoteRepository::class);}
+function df_quote_r() {return df_o(IQR::class);}
 
