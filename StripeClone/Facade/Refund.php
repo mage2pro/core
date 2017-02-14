b3ac879f914092a7862401e608436435d160de68
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-14 11:21:07 +0300

    no message

--- StripeClone/Facade/Refund.php
+++ StripeClone/Facade/Refund.php
@@ -15,6 +15,8 @@ abstract class Refund extends \Df\StripeClone\Facade {
 	 * Мы записываем его в БД и затем при обработке оповещений от платёжной системы
 	 * смотрим, не было ли это оповещение инициировано нашей же операцией,
 	 * и если было, то не обрабатываем его повторно.
+	 * 2017-02-14
+	 * Этот же идентификатор должен возвращать @see \Df\StripeClone\Webhook\IRefund::eTransId()
 	 * @used-by \Df\StripeClone\Method::_refund()
 	 * @see \Dfe\Omise\Facade\Charge::transId()
 	 * @see \Dfe\Paymill\Facade\Charge::transId()
