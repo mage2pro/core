d5981888f5db82139b22560747d180357f9980c6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-14 11:13:02 +0300

    no message

--- StripeClone/Webhook/IRefund.php
+++ StripeClone/Webhook/IRefund.php
@@ -3,6 +3,7 @@ namespace Df\StripeClone\Webhook;
 /**
  * 2017-01-17
  * @see \Dfe\Omise\Webhook\Refund\Create
+ * @see \Dfe\Paymill\Webhook\Refund\Succeeded
  * @see \Dfe\Stripe\Webhook\Charge\Refunded
  */
 interface IRefund {
@@ -11,6 +12,7 @@ interface IRefund {
 	 * В валюте заказа (платежа), в формате платёжной системы (копейках).
 	 * @used-by \Df\StripeClone\WebhookStrategy\Charge\Refunded::handle()
 	 * @see \Dfe\Omise\Webhook\Refund\Create::amount()
+	 * @see \Dfe\Paymill\Webhook\Refund\Succeeded::amount()
 	 * @see \Dfe\Stripe\Webhook\Charge\Refunded::amount()
 	 * @return int
 	 */
@@ -18,9 +20,15 @@ interface IRefund {
 
 	/**
 	 * 2017-01-19
-	 * Метод должен вернуть идентификатор операции (не платежа!) в платёжной системе
+	 * Метод должен вернуть идентификатор операции (не платежа!) в платёжной системе.
+	 * 2017-02-14
+	 * Он нужен нам для избежания обработки оповещений о возвратах, инициированных нами же
+	 * из административной части Magento: @see \Df\StripeClone\Method::_refund()
+	 * Это должен быть тот же самый идентификатор,
+	 * который возвращает @see \Df\StripeClone\Facade\Refund::transId()
 	 * @used-by \Df\StripeClone\WebhookStrategy\Charge\Refunded::handle()
 	 * @see \Dfe\Omise\Webhook\Refund\Create::eTransId()
+	 * @see \Dfe\Paymill\Webhook\Refund\Succeeded::eTransId()
 	 * @see \Dfe\Stripe\Webhook\Charge\Refunded::eTransId()
 	 * @return string
 	 */
