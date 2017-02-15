1dd15d0658aeed7768ea955f98ae9ffb171ce88f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-15 12:56:35 +0300

    no message

--- StripeClone/Settings.php
+++ StripeClone/Settings.php
@@ -1,6 +1,17 @@
 <?php
 namespace Df\StripeClone;
 use Df\Core\Exception as DFE;
+/**
+ * 2017-02-15
+ * @see \Dfe\CheckoutCom\Settings
+ * @see \Dfe\Iyzico\Settings
+ * @see \Dfe\Omise\Settings
+ * @see \Dfe\Paymill\Settings
+ * @see \Dfe\Spryng\Settings
+ * @see \Dfe\Square\Settings
+ * @see \Dfe\Stripe\Settings
+ * @see \Dfe\TwoCheckout\Settings
+ */
 class Settings extends \Df\Payment\Settings\BankCard {
 	/**
 	 * 2017-02-08
