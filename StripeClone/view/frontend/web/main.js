2e26cc4c40024fa05b43f22af10924acc926dc05
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-16 14:14:14 +0300

    no message

--- StripeClone/view/frontend/web/main.js
+++ StripeClone/view/frontend/web/main.js
@@ -8,6 +8,13 @@ define([
 	* @returns {String}
  	*/
 	publicKey: function() {return this.config('publicKey');},
+																   
+    /**
+	 * 2017-02-16
+	 * @abstract
+	 * @used-by placeOrder()
+	 */
+	createToken: function() {},
 	
 	/**
 	 * 2017-02-16
@@ -19,63 +26,12 @@ define([
 	*/
 	placeOrder: function(_this) {
 		if (this.validate()) {
-			if (!this.isNewCardChosen()) {
-				this.token = this.currentCard();
-				this.placeOrderInternal();
+			if (this.isNewCardChosen()) {
+				this.createToken();
 			}
 			else {
-				this.createToken();
-				// 2017-02-06
-				// https://developers.paymill.com/guides/reference/transactions#direct-tokenization
-				paymill.createToken(
-					{
-						// 2017-02-06
-						// https://blog.paymill.com/en/clarification-on-amount-int/#content-wrapper
-						// 2017-02-07
-						// Whether all the Paymill-supported currencies are 2-decimal
-						// or some currencies are zero-decimal?
-						// https://mage2.pro/t/2675
-						amount_int: this.amountF()
-						,cardholder: this.cardholder()
-						,currency: this.paymentCurrency().code
-						,cvc: this.creditCardVerificationNumber()
-						,exp_month: this.creditCardExpMonth()
-						,exp_year: this.creditCardExpYear()
-						,number: this.creditCardNumber()
-					},
-					/**
-					 * 2017-02-06
-					 * @param {?Object} error
-					 * При успешном получении токена error равно null.
-					 * @param {Object} response
-					 * Пример response при успешном получении токена:
-					 * {
-							bin: "401288"
-							,binCountry: "DE"
-							,brand: "VISA"
-							,ip: "80.147.111.188"
-							,ipCountry: ""
-							,last4Digits: "1881"
-							,token: "tok_48b61d2a802477e42dde2ad9874e"
-					 * }
-					 * 2017-02-08
-					 * [Paymill] The «paymill.createToken» call for the «5105105105105100» card
-					 * with the «07/2021» expiration data should fail in the test mode
-					 * according to the documentation, but it is not:
-					 * https://mage2.pro/t/2677
-					 */
-					function(error, response) {
-						if (!error) {
-							_this.token = response.token;
-							_this.placeOrderInternal();
-						}
-						else {
-							// 2017-02-08
-							// https://developers.paymill.com/guides/reference/bridge#2-creating-a-token
-							_this.showErrorMessage(error.message);
-						}
-					}
-				);
+				this.token = this.currentCard();
+				this.placeOrderInternal();
 			}
 		}
 	},	
