f134a29bc3ad3520139b972f118a4880c9d85ff4
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-18 08:14:59 +0300

    no message

--- Core/lib/http.php
+++ Core/lib/http.php
@@ -165,6 +165,8 @@ function df_request_o() {return df_o(RequestInterface::class);}
 
 /**
  * 2016-12-25
+ * 2017-02-18
+ * Модуль Checkout.com раньше использовал dfa($_SERVER, 'HTTP_USER_AGENT')
  * @return string|false
  */
 function df_request_ua() {return df_request_header('user-agent');}
