4939078d7dcb61922f0e5baa176fdcdcd9e78e26
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-03 17:12:35 +0300

    no message

--- Payment/view/frontend/web/mixin.js
+++ Payment/view/frontend/web/mixin.js
@@ -160,6 +160,8 @@ return {
 	/**
 	 * 2016-08-16
 	 * @used-by dfFormCssClassesS()
+	 * @see Df_GingerPaymentsBase/main::dfFormCssClasses()
+	 * https://github.com/mage2pro/ginger-payments-base/blob/0.1.0/view/frontend/web/main.js?ts=4#L47
 	 * @see Df_Payment/card::dfFormCssClasses()
 	 * https://github.com/mage2pro/core/blob/2.0.25/Payment/view/frontend/web/card.js?ts=4#L76
 	 * @see Dfe_AllPay/main::dfFormCssClasses()
