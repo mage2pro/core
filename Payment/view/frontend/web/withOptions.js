bf8a3f5717d2291bd495624e586ab68185db5e64
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-04 09:24:04 +0300

    no message

--- Payment/view/frontend/web/withOptions.js
+++ Payment/view/frontend/web/withOptions.js
@@ -38,6 +38,10 @@ define([
 	},
 	/**
 	 * 2016-08-15
+	 * @final
+	 * @used-by optionsA()
+	 * @used-by Dfe_AllPay/main::oneOffOptions()
+	 * https://github.com/mage2pro/allpay/blob/1.1.40/view/frontend/web/main.js?ts=4#L103
 	 * @returns {Object}
 	 */
 	options: function() {return this.config('options');},
