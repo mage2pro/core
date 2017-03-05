4acc53664803cf8daba345a3d9fb982bc39bd2f6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-06 01:06:09 +0300

    no message

--- PaypalClone/Charge.php
+++ PaypalClone/Charge.php
@@ -1,14 +1,13 @@
 <?php
 namespace Df\PaypalClone;
 use Df\PaypalClone\Charge\IP;
-use Df\PaypalClone\Charge\IRequestIdKey;
 use Magento\Sales\Model\Order\Payment as OP;
 /**
  * 2016-08-27
  * @see \Dfe\AllPay\Charge
  * @see \Dfe\SecurePay\Charge
  */
-abstract class Charge extends \Df\Payment\Charge implements IP, IRequestIdKey {
+abstract class Charge extends \Df\Payment\Charge implements IP {
 	/**
 	 * 2016-08-27
 	 * @used-by \Df\PaypalClone\Charge::p()
@@ -17,6 +16,15 @@ abstract class Charge extends \Df\Payment\Charge implements IP, IRequestIdKey {
 	 * @return array(string => mixed)
 	 */
 	abstract protected function params();
+
+	/**
+	 * 2016-08-29
+	 * @used-by p()
+	 * @see \Dfe\AllPay\Charge::requestIdKey()
+	 * @see \Dfe\SecurePay\Charge::requestIdKey()
+	 * @return string
+	 */
+	abstract protected function requestIdKey();
 
 	/**
 	 * 2016-08-27
@@ -51,18 +59,6 @@ abstract class Charge extends \Df\Payment\Charge implements IP, IRequestIdKey {
 		/** @var self $i */
 		$i = df_create(df_con($method, 'Charge'), [self::$P__METHOD => $method]);
 		/**
-		 * 2016-08-29
-		 * Метод @uses \Df\PaypalClone\Charge\IRequestIdKey::requestIdKey() статичен,
-		 * но мы вызываем его нестатично (чтобы он был вызван для нужного класса, а не для базового),
-		 * и это успешно работает безо всяких предупреждений интерпретатора:
-		 * https://3v4l.org/N2VD2
-		 * http://stackoverflow.com/a/32746909
-		 * http://stackoverflow.com/a/15756165
-		 * Некоторые утверждают, что якобы на старых версиях PHP
-		 * это может выдавать предупреждение уровня E_STRICT:
-		 * http://stackoverflow.com/a/12874405
-		 * Однако это неправда, я проверил: https://3v4l.org/1JY8i
-		 *
 		 * 2017-01-05
 		 * @uses requestId()
 		 * @uses \Dfe\AllPay\Charge::requestId()
