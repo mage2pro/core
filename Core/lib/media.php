56b403b5095d248a6afbe85395d5fda25bb8ad18
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-15 00:20:35 +0300

    no message

--- Core/lib/media.php
+++ Core/lib/media.php
@@ -1,16 +1,16 @@
 <?php
-use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\Filesystem\DirectoryList as DL;
 use Magento\Framework\Filesystem\Directory\Read as DirectoryRead;
 use Magento\Framework\Filesystem\Directory\ReadInterface as DirectoryReadInterface;
 use Magento\Framework\Filesystem\Directory\Write as DirectoryWrite;
 use Magento\Framework\Filesystem\Directory\WriteInterface as DirectoryWriteInterface;
-use Magento\Framework\UrlInterface;
+use Magento\Framework\UrlInterface as U;
 /**
  * 2015-11-30
  * @param string $path [optional]
  * @return string
  */
-function df_media_path_absolute($path = '') {return df_path_absolute(DirectoryList::MEDIA, $path);}
+function df_media_path_absolute($path = '') {return df_path_absolute(DL::MEDIA, $path);}
 
 /**
  * 2015-12-01
@@ -18,9 +18,9 @@ function df_media_path_absolute($path = '') {return df_path_absolute(DirectoryLi
  * @param string $path [optional]
  * @return string
  */
-function df_media_url($path = '') {
-	return df_store()->getBaseUrl(UrlInterface::URL_TYPE_MEDIA) . df_media_path_relative($path);
-}
+function df_media_url($path = '') {return 
+	df_store()->getBaseUrl(U::URL_TYPE_MEDIA) . df_media_path_relative($path)
+;}
 
 /**
  * 2015-11-30
@@ -28,7 +28,7 @@ function df_media_url($path = '') {
  * @param string $path
  * @return string
  */
-function df_media_path_relative($path) {return df_path_relative($path, DirectoryList::MEDIA);}
+function df_media_path_relative($path) {return df_path_relative($path, DL::MEDIA);}
 
 /**
  * 2015-12-08
@@ -36,32 +36,31 @@ function df_media_path_relative($path) {return df_path_relative($path, Directory
  * @return string
  */
 function df_media_read($mediaPath) {
-	return df_file_read(DirectoryList::MEDIA, df_media_path_relative($mediaPath));
+	return df_file_read(DL::MEDIA, df_media_path_relative($mediaPath));
 }
 
 /**
  * 2015-11-30
  * @return DirectoryRead|DirectoryReadInterface
  */
-function df_media_reader() {return df_fs_r(DirectoryList::MEDIA);}
+function df_media_reader() {return df_fs_r(DL::MEDIA);}
 
 /**
  * 2015-11-29
  * @return DirectoryWrite|DirectoryWriteInterface
  */
-function df_media_writer() {return df_fs_w(DirectoryList::MEDIA);}
+function df_media_writer() {return df_fs_w(DL::MEDIA);}
 
 /**
  * 2015-11-30
  * Иерархия папок создаётся автоматически:
  * @see \Magento\Framework\Filesystem\Directory\Write::openFile()
  * https://github.com/magento/magento2/blob/2.0.0/lib/internal/Magento/Framework/Filesystem/Directory/Write.php#L247
- * @param string $mediaPath
- * @param string $contents
+ * @used-by \Df\GoogleFont\Font\Variant::ttfPath()
+ * @used-by \Df\GoogleFont\Fonts\Png::create()
+ * @used-by \Df\GoogleFont\Font\Sprite::draw()
+ * @param string $path
+ * @param string $data
  * @return void
  */
-function df_media_write($mediaPath, $contents) {
-	df_file_write(DirectoryList::MEDIA, df_media_path_relative($mediaPath), $contents);
-}
-
-
+function df_media_write($path, $data) {df_file_write(DL::MEDIA, df_media_path_relative($path), $data);}
