6b2ee13815c7cb0ce5453e131959d23b5fb82e15
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-20 09:31:49 +0300

    no message

--- StripeClone/W/Handler.php
+++ StripeClone/W/Handler.php
@@ -3,7 +3,9 @@ namespace Df\StripeClone\W;
 /**
  * 2016-12-26
  * @see \Dfe\Iyzico\W\Handler
- * @see \Dfe\Omise\W\Handler
+ * @see \Dfe\Omise\W\Handler\Charge\Capture
+ * @see \Dfe\Omise\W\Handler\Charge\Complete
+ * @see \Dfe\Omise\W\Handler\Refund\Create
  * @see \Dfe\Paymill\W\Handler\Refund\Succeeded
  * @see \Dfe\Paymill\W\Handler\Transaction\Succeeded
  * @see \Dfe\Stripe\W\Handler\Charge\Captured
