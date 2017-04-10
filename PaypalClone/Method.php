675eeca89c56947f5cedb4b4c3ed7f6a415d5aae
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-10 05:34:00 +0300

    no message

--- PaypalClone/Method.php
+++ PaypalClone/Method.php
@@ -3,6 +3,7 @@ namespace Df\PaypalClone;
 /**
  * 2017-01-22
  * @see \Dfe\AllPay\Method
+ * @see \Dfe\IPay88\Method
  * @see \Dfe\SecurePay\Method
  */
 abstract class Method extends \Df\Payment\Method {}
