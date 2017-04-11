df91b174f16a61c1acb87063a35248478bf23615
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-12 02:15:53 +0300

    no message

--- Framework/Form/Element/Webhook.php
+++ Framework/Form/Element/Webhook.php
@@ -1,7 +1,16 @@
 <?php
 namespace Df\Framework\Form\Element;
-// 2016-05-30
-/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+/**
+ * 2016-05-30
+ * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+ * @used-by https://github.com/mage2pro/2checkout/blob/1.4.5/etc/adminhtml/system.xml#L318
+ * @used-by https://github.com/mage2pro/checkout.com/blob/1.3.11/etc/adminhtml/system.xml#L165
+ * @used-by https://github.com/mage2pro/omise/blob/1.8.8/etc/adminhtml/system.xml#L205
+ * @used-by https://github.com/mage2pro/paymill/blob/1.4.6/etc/adminhtml/system.xml#L190
+ * @used-by https://github.com/mage2pro/robokassa/blob/0.0.2/etc/adminhtml/system.xml#L163
+ * @used-by https://github.com/mage2pro/square/blob/1.1.7/etc/adminhtml/system.xml#L189
+ * @used-by https://github.com/mage2pro/stripe/blob/1.9.11/etc/adminhtml/system.xml#L154
+ */
 class Webhook extends Url {
 	/**
 	 * 2016-05-31
