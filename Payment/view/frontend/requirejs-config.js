919726e48652a1c5c5272e760aa40b45a0298a47
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-28 00:22:17 +0300

    no message

---
+++ Payment/view/frontend/requirejs-config.js
@@ -0,0 +1,6 @@
+// 2017-04-28
+// «Replace a default JS component»:
+// http://devdocs.magento.com/guides/v2.0/javascript-dev-guide/javascript/custom_js.html#js_replace
+var config = {paths: {
+	'Magento_Payment/js/model/credit-card-validation/credit-card-number-validator/credit-card-type': 'Df_Payment/bank-card-types'
+}};
