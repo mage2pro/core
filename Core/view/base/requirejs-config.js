0c5874c506c5091c3b2e346b05886c96e4343ebc
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-28 00:02:28 +0300

    no message

--- Core/view/base/requirejs-config.js
+++ Core/view/base/requirejs-config.js
@@ -1,14 +1,12 @@
-var config = {
-	paths: {
-		'df': 'Df_Core/my/all'
-		,'df-lodash': 'https://cdn.jsdelivr.net/lodash/4.14.1/lodash'
-		,'df-lodash-local': 'Df_Core/thirdParty/lodash'
-		,'df-uniform': 'Df_Core/my/uniform'
-		,'Df_Core/ColorPicker': 'Df_Core/thirdParty/ColorPicker/main'
-		,'Df_Core/Handsontable': 'Df_Core/thirdParty/Handsontable/main'
-		,'Df_Core/HighlightJs': 'Df_Core/thirdParty/HighlightJs/main'
-		,'Df_Core/Mask': 'Df_Core/thirdParty/Mask/main'
-		,'Df_Core/Select2': 'Df_Core/thirdParty/Select2/main'
-		,'Df_Core/Telephone': 'Df_Core/thirdParty/Telephone/js/main'
-	}
-};
+var config = {paths: {
+	'df': 'Df_Core/my/all'
+	,'df-lodash': 'https://cdn.jsdelivr.net/lodash/4.14.1/lodash'
+	,'df-lodash-local': 'Df_Core/thirdParty/lodash'
+	,'df-uniform': 'Df_Core/my/uniform'
+	,'Df_Core/ColorPicker': 'Df_Core/thirdParty/ColorPicker/main'
+	,'Df_Core/Handsontable': 'Df_Core/thirdParty/Handsontable/main'
+	,'Df_Core/HighlightJs': 'Df_Core/thirdParty/HighlightJs/main'
+	,'Df_Core/Mask': 'Df_Core/thirdParty/Mask/main'
+	,'Df_Core/Select2': 'Df_Core/thirdParty/Select2/main'
+	,'Df_Core/Telephone': 'Df_Core/thirdParty/Telephone/js/main'
+}};
