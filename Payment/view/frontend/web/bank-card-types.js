8408e51e419d96255d957ac770bc55e1e298a9e4
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-28 11:04:41 +0300

    2.5.31

--- Payment/view/frontend/web/bank-card-types.js
+++ Payment/view/frontend/web/bank-card-types.js
@@ -8,8 +8,8 @@ var types = [
 		gaps: [4, 8, 12],
 		lengths: [16],
 		code: {name: 'CVV', size: 3}
-	},
-	{
+	}
+	,{
 		title: 'MasterCard',
 		type: 'MC',
 		pattern: /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/,
@@ -17,8 +17,8 @@ var types = [
 		lengths: [16],
 		code: {
 			name: 'CVC', size: 3}
-	},
-	{
+	}
+	,{
 		title: 'American Express',
 		type: 'AE',
 		pattern: /^3([47]\d*)?$/,
@@ -26,48 +26,48 @@ var types = [
 		gaps: [4, 10],
 		lengths: [15],
 		code: {name: 'CID', size: 4}
-	},
-	{
+	}
+	,{
 		title: 'Diners',
 		type: 'DN',
 		pattern: /^(3(0[0-5]|095|6|[8-9]))\d*$/,
 		gaps: [4, 10],
 		lengths: [14, 16, 17, 18, 19],
 		code: {name: 'CVV', size: 3}
-	},
-	{
+	}
+	,{
 		title: 'Discover',
 		type: 'DI',
 		pattern: /^(6011(0|[2-4]|74|7[7-9]|8[6-9]|9)|6(4[4-9]|5))\d*$/,
 		gaps: [4, 8, 12],
 		lengths: [16, 17, 18, 19],
 		code: {name: 'CID', size: 3}
-	},
-	{
+	}
+	,{
 		title: 'JCB',
 		type: 'JCB',
 		pattern: /^35(2[8-9]|[3-8])\d*$/,
 		gaps: [4, 8, 12],
 		lengths: [16, 17, 18, 19],
 		code: {name: 'CVV', size: 3}
-	},
-	{
+	}
+	,{
 		title: 'UnionPay',
 		type: 'UN',
 		pattern: /^(622(1(2[6-9]|[3-9])|[3-8]|9([[0-1]|2[0-5]))|62[4-6]|628([2-8]))\d*?$/,
 		gaps: [4, 8, 12],
 		lengths: [16, 17, 18, 19],
 		code: {name: 'CVN', size: 3}
-	},
-	{
+	}
+	,{
 		title: 'Maestro International',
 		type: 'MI',
 		pattern: /^(5(0|[6-9])|63|67(?!59|6770|6774))\d*$/,
 		gaps: [4, 8, 12],
 		lengths: [12, 13, 14, 15, 16, 17, 18, 19],
 		code: {name: 'CVC', size: 3}
-	},
-	{
+	}
+	,{
 		title: 'Maestro Domestic',
 		type: 'MD',
 		pattern: /^6759(?!24|38|40|6[3-9]|70|76)|676770|676774\d*$/,
@@ -77,6 +77,7 @@ var types = [
 	}
 	,{
 		code: {name: 'CVC', size: 3}
+		,exact: true
 		,gaps: [4, 8, 12]
 		,lengths: [16, 19]
 		// 2017-04-28 «How to validate a Hipercard number?» https://mage2.pro/t/3865
@@ -93,6 +94,7 @@ var types = [
 	}
 	,{
 		code: {name: 'CVC', size: 3}
+		,exact: true
 		,gaps: [4, 8, 12]
 		,lengths: [16]
 		// 2017-04-28
@@ -116,10 +118,17 @@ var types = [
 return {
 	/**
 	 * 2017-04-28
-	 * @param {*} n
+	 * @used-by Magento_Payment/js/model/credit-card-validation/credit-card-number-validator
+	 * 		potentialTypes = creditCardTypes.getCardTypes(value);
+	 * https://github.com/magento/magento2/blob/2.1.6/app/code/Magento/Payment/view/base/web/js/model/credit-card-validation/credit-card-number-validator.js#L48
+	 * @param {?String} n
 	 * @return {Object[]}
 	 */
-	getCardTypes: function(n) {return _.isUndefined(n) || null === n ? [] : df.a.clone(
-		'' === n ? types : _.filter(types, function(v) {return v.pattern.test(n);})
-	);}
+	getCardTypes: function(n) {
+		/** @type {Object[]} */ var result;
+		/** @type {?Object} */ var exact;
+		return (exact = _.find(result = _.isUndefined(n) || null === n ? [] : df.a.clone(
+			'' === n ? types : _.filter(types, function(v) {return v.pattern.test(n);})
+		), function(v) {return v.exact;})) ? [exact] : result
+	;}
 };});
