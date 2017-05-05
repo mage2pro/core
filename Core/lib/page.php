1483d7271103f20ab5e9de5645e464e0d3b50cc7
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-05-05 03:42:18 +0300

    2.6.2

--- Core/lib/page.php
+++ Core/lib/page.php
@@ -1,4 +1,7 @@
 <?php
+use Magento\Framework\View\Page\Config;
+use Magento\Framework\View\Result\Page as ResultPage;
+use Magento\Framework\View\Result\PageFactory;
 /**
  * 2015-10-05
  * @param string $name
@@ -6,12 +9,29 @@
  */
 function df_metadata($name, $value) {
 	if (!is_null($value) && '' !== $value) {
-		df_page()->setMetadata($name, $value);
+		df_page_config()->setMetadata($name, $value);
 	}
 }
 
 /**
  * 2015-10-05
- * @return \Magento\Framework\View\Page\Config
+ * @return Config
  */
-function df_page() {return df_o(\Magento\Framework\View\Page\Config::class);}
+function df_page_config() {return df_o(Config::class);}
+
+/**
+ * 2017-05-05
+ * @used-by \Dfe\Cms\Controller\Index\Index::execute()
+ * @param string|null $handle [optional]
+ * @return ResultPage
+ */
+function df_page_result($handle = null) {
+	/** @var PageFactory $f */
+	$f = df_o(PageFactory::class);
+	/** @var ResultPage $result */
+	$result = $f->create();
+	if ($handle) {
+		$result->addHandle($handle);
+	}
+	return $result;
+}
