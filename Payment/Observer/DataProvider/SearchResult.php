57d9e85e01d3c725b463b67446e2685e9f40dc69
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-05-09 19:49:23 +0300

    2.6.14

--- Payment/Observer/DataProvider/SearchResult.php
+++ Payment/Observer/DataProvider/SearchResult.php
@@ -1,6 +1,7 @@
 <?php
 namespace Df\Payment\Observer\DataProvider;
 use Df\Framework\Plugin\View\Element\UiComponent\DataProvider\DataProvider as Plugin;
+use Df\Payment\Method as M;
 use Magento\Framework\Api\Search\SearchResult as ApiSearchResult;
 use Magento\Framework\Api\Search\SearchResultInterface as ISearchResult;
 use Magento\Framework\Event\Observer as Ob;
@@ -8,6 +9,7 @@ use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider as Provider;
 use Magento\Framework\View\Element\UiComponent\DataProvider\Document;
 use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult as UiSearchResult;
+use Magento\Payment\Model\Method\Substitution;
 use Magento\Sales\Model\Order as O;
 use Magento\Sales\Model\ResourceModel\Order\Creditmemo\Grid\Collection as CreditmemoGC;
 use Magento\Sales\Model\ResourceModel\Order\Grid\Collection as OrderGC;
@@ -52,15 +54,46 @@ final class SearchResult implements ObserverInterface {
 			df_map(function(Document $item) use($cacheKey, $prop) {
 				/** @var string|null $methodCode */
 				if (($methodCode = $item[$prop]) && df_starts_with($methodCode, 'dfe_')) {
+					/**
+					 * 2017-05-09
+					 * Как оказалось, наличие приставки «dfe_» ешё не гарантирует,
+					 * что @uses dfpm() (вызываемый ниже опосредованно через dfpm_title())
+					 * действительно вернёт наследника @see \Df\Payment\Method
+					 * Возможна ситуация, когда админтратор установил какой-либо из моих патёжных модулей,
+					 * сделал платёж посредством него, а затем либо удалил модуль,
+					 * либо из-за каких-то некорректных действий модуль стал отключенным,
+					 * и вот тогда M2 вернёт не наследника @see \Df\Payment\Method,
+					 * а экземпляр класса @see \Magento\Payment\Model\Method\Substitution:
+					 * «How is @see \Magento\Payment\Model\Method\Substitution implemented and used?»
+					 * https://mage2.pro/t/3924
+					 * 1) @see \Magento\Sales\Model\Order\Payment\Info::getMethodInstance()
+					 *	try {
+					 *		$instance = $this->paymentData->getMethodInstance($this->getMethod());
+					 *	} catch (\UnexpectedValueException $e) {
+					 *		$instance = $this->paymentData->getMethodInstance(Substitution::CODE);
+					 *	}
+					 * https://github.com/magento/magento2/blob/2.1.6/app/code/Magento/Sales/Model/Order/Payment/Info.php#L113-L117
+					 * 2) @see \Magento\Payment\Model\Info::getMethodInstance()
+					 *	try {
+					 *		$instance = $this->_paymentData->getMethodInstance($this->getMethod());
+					 *	} catch (\UnexpectedValueException $e) {
+					 *		$instance = $this->_paymentData->getMethodInstance(Method\Substitution::CODE);
+					 *	}
+					 * https://github.com/magento/magento2/blob/2.1.6/app/code/Magento/Payment/Model/Info.php#L106-L110
+					 */
 					/** @var int $id */
 					$id = $item['entity_id'];
 					// 2016-07-29
 					// Эта операция очень ресурсоёмка:
 					// для каждой строки таблицы заказов она делает кучу запросов к базе данных.
 					// Поэтому кэшируем результаты в постоянном кэше.
-					$item[$prop] = df_cache_get_simple([$cacheKey, $id], function() use ($id) {return
-						/** @var O $o */df_cc_br(dfpm_title($o = df_order($id)), dfp_choice($o)->title())
-					;});
+					$item[$prop] = df_cache_get_simple([$cacheKey, $id], function() use ($id) {
+						/** @var O $o */
+						$o = df_order($id);
+						/** @var M|Substitution $m */
+						$m = dfpm($o);
+						return !dfp_my($m) ? $m->getTitle() : df_cc_br(dfpm_title($m), dfp_choice($o)->title());
+					});
 				}
 			}, $result);
 		}
