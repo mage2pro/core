f998af79c7d648770a8618449e760b68bd1bb7cb
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-05-10 05:26:41 +0300

    2.6.16

--- Core/lib/credentials.php
+++ Core/lib/credentials.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * 2017-05-10
- * @used-by df_github_key()
+ * 2017-05-10 $_SERVER['HOME'] is defined only in the CLI mode.
+ * @used-by df_github_token()
  * @param string|null $k [optional]
  * @return string|null|array(string => mixed)
  */
 function df_credentials($k = null) {return dfak(dfcf(function() {return df_json_decode(file_get_contents(
-	$_SERVER['HOME'] . '/.credentials/credentials.json'
+	(df_my_local() ? 'C:/tools/shell/home' : '/var/www') . '/.credentials/credentials.json'
 ));}), $k);}
--- Core/lib/github.php
+++ Core/lib/github.php
@@ -1,6 +1,36 @@
 <?php
+use Zend_Http_Client as C;
 /**
  * 2017-05-10
  * @return string
  */
-function df_github_key() {return df_credentials('github');}
+function df_github_token() {return df_credentials('github');}
+
+/**
+ * 2017-05-10
+ * https://developer.github.com/v3/repos/releases/#get-the-latest-release
+ * @used-by df_github_request()
+ * @param string $repo
+ * @return string
+ */
+function df_github_repo_version($repo) {return df_github_request("repos/$repo/releases/latest", 'tag_name');}
+
+/**
+ * 2017-05-10
+ * https://developer.github.com/v3/repos/releases/#get-the-latest-release
+ * @param string $path
+ * @param string|null $k [optional]
+ * @param array(string => mixed) $params [optional]
+ * @return string
+ */
+function df_github_request($path, $k = null, $params = []) {
+	/** @var C $c */
+	$c = (new C)
+		->setHeaders('content-type', 'application/json')
+		->setConfig(['timeout' => 120])
+		->setUri("https://api.github.com/$path")
+		->setParameterGet(['access_token' => df_github_token()] + $params)
+		->setMethod(C::GET)
+	;
+	return dfak(df_json_decode($c->request()->getBody()), $k);
+}
