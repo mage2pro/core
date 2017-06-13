2a4dbc2f5929695c28e672a80ed17c955b04ff9c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-13 09:05:31 +0300

    2.7.9

--- StripeClone/view/frontend/web/main.js
+++ StripeClone/view/frontend/web/main.js
@@ -9,7 +9,7 @@ define(['Df_Payment/card'], function(parent) {'use strict'; return parent.extend
 	 * 2017-02-16
 	 * @abstract
 	 * @used-by placeOrder()
-	 * @param {Object|Number} status
+	 * @param {*} status
 	 * @returns {Boolean}
 	 */
 	tokenCheckStatus: function(status) {return null;},
@@ -17,11 +17,11 @@ define(['Df_Payment/card'], function(parent) {'use strict'; return parent.extend
 	 * 2017-02-16
 	 * @abstract
 	 * @used-by placeOrder()
+	 * https://github.com/mage2pro/core/blob/2.7.8/StripeClone/view/frontend/web/main.js?ts=4#L73
 	 * @param {Object} params
 	 * @param {Function} callback
-	 * @returns {Function}
 	 */
-	tokenCreate: function(params, callback) {return null;},
+	tokenCreate: function(params, callback) {},
     /**
 	 * 2017-02-16
 	 * @abstract
