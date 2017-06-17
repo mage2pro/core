a90a1ce97b43dcbcf1a8e9c38ff28318f93db13f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-17 17:16:17 +0300

    no message

---
+++ Translation/T/Main.php
@@ -0,0 +1,12 @@
+<?php
+// 2017-06-17
+namespace Df\Translation\T;
+class Main extends \Df\Core\TestCase {
+	/** 2017-06-17 */
+	function t00() {}
+
+	/** 2017-06-17 @test */
+	function t01() {echo df_json_encode_pretty([
+		__('Dashboard')->__toString()
+	]);}
+}
