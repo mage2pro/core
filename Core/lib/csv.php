8147619286be2a5fe3cc70d08c0210249a7bd703
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-23 20:06:19 +0300

    2.7.21

--- Core/lib/csv.php
+++ Core/lib/csv.php
@@ -12,40 +12,6 @@ use Magento\Framework\File\Csv;
 function df_csv(...$args) {return implode(',', df_args($args));}
 
 /**
- * 2017-06-23
- * It makes the single-word dictionary values quoted:
- * https://stackoverflow.com/questions/2489553
- * Usually it should be called after:
- * @see \Magento\Framework\File\Csv::saveData()
- * @see \Magento\Framework\Filesystem\Driver\File::filePutCsv()
- * @used-by \Dfr\Core\Console\Update::execute()
- * @used-by \Dfr\Core\Console\Quote::execute()
- * @param string $f
- */
-function df_csv_force_quotes($f) {file_put_contents($f, df_cc_n(df_clean(array_map(function($l) {
-	/** @var string $c *//** @var string $d */
-	$c = ','; $d = '"';
-	$l = df_trim($l);
-	// 2017-06-23
-	// A workaround for the entries like «When using Store Code in URLs»
-	// in https://raw.githubusercontent.com/magento/magento2/2.1.7/app/code/Magento/Backend/i18n/en_US.csv
-	if ($l === $d || $l === "$d$c$d") {
-		$l = null;
-	}
-	if ($l) {
-		if (!df_starts_with($l, $d)) {
-			$p = mb_strpos($l, $c);
-			$l = $d . mb_substr($l, 0, $p) . $d . mb_substr($l, $p);
-		}
-		if (!df_ends_with($l, $d)) {
-			$p = mb_strrpos($l, $c);
-			$l = mb_substr($l, 0, $p + 1) . $d . mb_substr($l, $p + 1) . $d;
-		}
-	}
-	return $l;
-}, df_explode_n(file_get_contents($f))))));}
-
-/**
  * 2017-06-21
  * @used-by df_intl_dic_read()
  * @return Csv
--- Intl/lib/main.php
+++ Intl/lib/main.php
@@ -25,3 +25,30 @@ function df_intl_dic_read($m, $locale = null, $folder = null) {
 	$p = df_intl_dic_path($m, $locale, $folder);
 	return df_try(function() use($p) {return df_csv_o()->getDataPairs($p);}, [])
 ;}
+
+/**
+ * 2017-06-23
+ * It works similar to:
+ * @see \Magento\Framework\File\Csv::saveData()
+ * @see \Magento\Framework\Filesystem\Driver\File::filePutCsv()
+ * But it also makes the single-word dictionary entries quoted:
+ * https://stackoverflow.com/questions/2489553
+ * https://gist.github.com/anonymous/223ea7353626bc6a6a9e#file-csvenclosed-php
+ * @param string|object $m
+ * @param array(string => string) $data
+ * @param string|null $folder [optional]
+ * @param string|null $locale [optional]
+ */
+function df_intl_dic_write($m, array $data, $locale = null, $folder = null) {
+	/** @var string $path */
+	$path = df_intl_dic_path($m, $locale, $folder);
+	/** @var resource $h */
+	$h = fopen($path, 'w');
+	df_map_k(function($k, $v) use($h) {/** @var string $k *//** @var string $v */
+		fputcsv($h, array_map(function($s) {$d = '"'; return
+			$d . str_replace($d, "$d$d", $s) . $d
+		;}, [$k, $v]), ',', chr(0));
+	}, $data);
+	fclose($h);
+	file_put_contents($path, str_replace(chr(0), '', file_get_contents($path)));
+;}
