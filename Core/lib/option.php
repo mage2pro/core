0ed7638bf785d0c018db02a596e56a89c4d7541d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-25 14:54:52 +0300

    2.7.22

--- Core/lib/option.php
+++ Core/lib/option.php
@@ -1,38 +1,33 @@
 <?php
+use Magento\Config\Model\Config\Source\Yesno as YN;
+use Magento\Framework\Phrase;
 /**
  * 2015-12-28
  * Преобразует при необходимости простой одномерный массив в список опций.
- * @param string[] $values
+ * @param string[] $a
  * @return array(array(string => string|int))
  */
-function df_a_to_options(array $values) {
-	/** @var mixed $first */
-	$first = df_first($values);
-	return is_null($first) || isset($first['value'])
-		? $values
-		: df_map_to_options(dfa_combine_self($values))
-	;
-}
+function df_a_to_options(array $a) {return is_null($f = df_first($a)) || isset($f['value']) ? $a :
+	df_map_to_options(dfa_combine_self($a))
+;}
 
 /**
  * 2015-02-11
  * Превращает массив вида array('value' => 'label')
  * в массив вида array(array('value' => '', 'label' => ''))
  * Обратная операция: @see df_options_to_map()
- * @param array(string|int => string) $map
+ * @param array(string|int => string) $m
  * @return array(array(string => string|int))
  */
-function df_map_to_options(array $map) {return array_map('df_option', array_keys($map), $map);}
+function df_map_to_options(array $m) {return array_map('df_option', array_keys($m), $m);}
 
 /**
  * 2015-11-13
  * Делает то же, что и @see df_map_to_options(), но дополнительно локализует значения label'.
- * @param array(string|int => string) $map
+ * @param array(string|int => string) $m
  * @return array(array(string => string|int))
  */
-function df_map_to_options_t(array $map) {return
-	array_map('df_option', array_keys($map), df_translate_a($map))
-;}
+function df_map_to_options_t(array $m) {return array_map('df_option', array_keys($m), df_translate_a($m));}
 
 /**
  * 2015-02-11
@@ -45,26 +40,37 @@ function df_map_to_options_t(array $map) {return
 function df_map_to_options_reverse(array $map) {return array_map('df_option', $map, array_keys($map));}
 
 /**
- * @param string|int $value
- * @param string $label
+ * @param string|int $v
+ * @param string $l
  * @return array(string => string|int)
  */
-function df_option($value, $label) {return ['label' => $label, 'value' => $value];}
+function df_option($v, $l) {return ['label' => $l, 'value' => $v];}
 
 /**
- * @param array(string => string) $option
- * @param string|null|callable $default [optional]
+ * @param array(string => string) $o
+ * @param string|null|callable $d [optional]
  * @return string|null
  */
-function df_option_v(array $option, $default = null) {return dfa($option, 'value', $default);}
+function df_option_v(array $o, $d = null) {return dfa($o, 'value', $d);}
 
 /**
  * Превращает массив вида array(array('value' => '', 'label' => ''))
  * в массив вида array('value').
- * @param array(string => string) $option
+ * @param array(string => string) $oo
  * @return string|null
  */
-function df_option_values(array $options) {return array_column($options, 'value');}
+function df_option_values(array $oo) {return array_column($oo, 'value');}
+
+/**
+ * 2017-06-25
+ * It translates the options labels.
+ * @used-by \Dfr\Email\Plugin\Model\ResourceModel\Template\Collection::afterToOptionArray()
+ * @param array(array(string => string)) $oo
+ * @return array(array(string => string|Phrase))
+ */
+function df_options_t(array $oo) {return array_map(function($o) {return
+	['label' => __($o['label'])] +  $o
+;}, $oo);}
 
 /**
  * Превращает массив вида array(array('value' => '', 'label' => ''))
@@ -75,16 +81,5 @@ function df_option_values(array $options) {return array_column($options, 'value'
  */
 function df_options_to_map(array $options) {return array_column($options, 'label', 'value');}
 
-/**
- * 2015-11-17
- * @return array(array(string => string|int))
- */
-function df_yes_no() {
-	/** @var \Magento\Config\Model\Config\Source\Yesno $o */
-	$o = df_o(\Magento\Config\Model\Config\Source\Yesno::class);
-	return $o->toOptionArray();
-}
-
-
-
-
+/** 2015-11-17 @return array(array(string => string|int)) */
+function df_yes_no() {/** @var YN $o */$o = df_o(YN::class); return $o->toOptionArray();}
