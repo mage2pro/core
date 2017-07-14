cd4576e088fceefd5af7ed9495cd297b3210fee1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-14 11:35:57 +0300

    no message

--- Payment/lib/settings.php
+++ Payment/lib/settings.php
@@ -22,6 +22,7 @@ use Magento\Sales\Model\Order\Payment\Transaction as T;
  * @used-by \Dfe\CheckoutCom\Handler\CustomerReturn::p()
  * @used-by \Dfe\Klarna\Api\Checkout::html()
  * @used-by \Dfe\Klarna\Observer\ShortcutButtonsContainer::execute()
+ * @used-by \Dfe\Moip\T\Data::ga()
  * @used-by \Dfe\Omise\T\TestCase
  * @used-by \Dfe\TwoCheckout\Handler::p()
  * @param M|II|OP|QP|O|Q|T|object|string|null $m
