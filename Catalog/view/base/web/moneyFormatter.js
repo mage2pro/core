d56431fb6a4ad2499d1d14857585179a5d9c07b1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-15 15:04:58 +0300

    no message

--- Catalog/view/base/web/moneyFormatter.js
+++ Catalog/view/base/web/moneyFormatter.js
@@ -12,7 +12,21 @@
  */
 define([
 	'df', 'df-lodash', 'Magento_Catalog/js/price-utils', 'Magento_Checkout/js/model/quote'
-], function (df, _, priceUtils, q) {'use strict'; return(
+], function (df, _, priceUtils, q) {'use strict';
+	/**
+	 * 2017-07-15
+	 * «We're avoiding the usage of toFixed(),
+	 * and using round instead with the e representation to address numbers like 1.005 = 1.01.
+	 * Using toFixed to only provide trailig zeroes in case we have a whole number.»
+	 * https://github.com/magento/magento2/blob/2.2.0-RC1.4/app/code/Magento/Catalog/view/base/web/js/price-utils.js#L62-L63
+	 * @param {Number} v
+	 * @param {Number} prec
+	 * @returns {Number}
+	 */
+	function roundWithPrec(v, prec) {//noinspection JSCheckFunctionSignatures
+		return Number(Math.round(Math.abs(+v || 0) + 'e+' + prec) + ('e-' + prec));
+	}
+return(
 	/**
 	 * 2017-07-15
 	 * @param {Number} a
@@ -31,39 +45,29 @@ define([
 			decimalSymbol: ',', groupLength: 3, groupSymbol: ','
 			,integerRequired: 1, pattern: '%s', requiredPrecision: 2
 		}, q.getPriceFormat(), df.arg(f, {}));
-		/** @type {Number} */ var precision = f.requiredPrecision;
-		/**
-		 * 2017-07-15
-		 * «We're avoiding the usage of to fixed,
-		 * and using round instead with the e representation to address numbers like 1.005 = 1.01.
-		 * Using ToFixed to only provide trailig zeroes in case we have a whole number.»
-		 * https://github.com/magento/magento2/blob/2.2.0-RC1.4/app/code/Magento/Catalog/view/base/web/js/price-utils.js#L62-L63
- 		 * @param {Number} v
-		 * @returns {Number}
-		 */
-		var fixed = function(v) {//noinspection JSCheckFunctionSignatures
-			return Number(Math.round(Math.abs(+v || 0) + 'e+' + precision) + ('e-' + precision));
-		};
-		a = fixed(a);
+		/** @type {Number} */ var prec = f.requiredPrecision;
+		a = roundWithPrec(a, prec);
 		/** @type {String} */ var as = df.int(a) + '';
 		/** @type {Number} */ var pad = as.length >= f.integerRequired ? 0 : f.integerRequired - as.length;
 		as = priceUtils.strPad('0', pad) + as;
-		/** @type {Number} */ var j = as.length <= f.groupLength ? 0 : as.length % f.groupLength;
-		/** @type {RegExp} */ var re = new RegExp('(\\d{' + f.groupLength + '})(?=\\d)', 'g');
-		// replace(/-/, 0) is only for fixing Safari bug which appears
-		// when Math.abs(0).toFixed() executed on '0' number.
-		// Result is '0.-0' :(
+		/** @type {Number} */var firstGroupLength = as.length <= f.groupLength ? 0 : as.length % f.groupLength;
 		// noinspection JSCheckFunctionSignatures
-		/** @type {Number} */
-		/** @type {String} */
-		var r =
-			(j ? as.substr(0, j) + f.groupSymbol : '')
-			+ as.substr(j).replace(re, '$1' + f.groupSymbol)
-			+ (precision ? f.decimalSymbol + fixed(a - as).toFixed(2).replace(/-/, 0).slice(2) : '')
-		;
 		return _.trim((-1 < f.pattern.indexOf('{sign}') ? f.pattern : '{sign}' + f.pattern)
 			.replace('{sign}', false === showSign ? '' : (a < 0 ? '-' : (true === showSign ? '+' : '')))
-			.replace('%s', r)
+			.replace('%s',
+				(firstGroupLength ? as.substr(0, firstGroupLength) + f.groupSymbol : '')
+				+ as.substr(firstGroupLength).replace(
+					new RegExp('(\\d{' + f.groupLength + '})(?=\\d)', 'g'), '$1' + f.groupSymbol
+				)
+				+ (!prec ? '' :
+					// 2017-07-15
+					// «replace(/-/, 0) is only for fixing Safari bug
+					// which appears when Math.abs(0).toFixed() executed on '0' number.
+					// Result is '0.-0' :(»
+					// https://github.com/magento/magento2/blob/2.2.0-RC1.4/app/code/Magento/Catalog/view/base/web/js/price-utils.js#L75-L77
+					f.decimalSymbol + roundWithPrec(a - as, prec).toFixed(2).replace(/-/, 0).slice(2)
+				)
+			)
 		);
 	}
 );});
