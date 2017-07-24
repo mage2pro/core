688fdf1960b1ee3c5c754a7a884b27c78f8811ee
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-24 12:06:25 +0300

    no message

--- Payment/view/frontend/web/loader.js
+++ Payment/view/frontend/web/loader.js
@@ -2,21 +2,18 @@
 define([
 	'df', 'Magento_Checkout/js/model/payment/renderer-list', 'uiComponent'
 ], function(df, rendererList, Component) {'use strict'; return (
-	/**
-	 * @param {String} name
-	 * @param {?String} code
-	 * @returns {Component}
-	 */
-	function(name, code) {
-		/**
-		 * 2016-08-24
-		 * 'Dfe_AllPay' => 'dfe_all_pay'
-		 * @type {String}
-		 */
-		code = code || df.s.lcFirst(df.s.explodeModuleCamel(name)).join('_');
-		if (window.checkoutConfig.payment[code]) {
-			rendererList.push({type: code, component: name + '/main'});
-		}
-		return Component.extend({});
+/**
+ * 2016-08-24
+ * @param {String} name
+ * @param {String=} code
+ * @returns {Component}
+ */
+function(name, code) {
+	// 2016-08-24 'Dfe_AllPay' => 'dfe_all_pay'
+	/** @type {String} */ code = code || df.s.lcFirst(df.s.explodeModuleCamel(name)).join('_');
+	if (window.checkoutConfig.payment[code]) {
+		rendererList.push({type: code, component: name + '/main'});
 	}
+	return Component.extend({});
+}
 );});
