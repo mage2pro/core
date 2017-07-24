b7a2dfc709ef953c3b6aa4d94c7b57e95123310f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-24 15:24:00 +0300

    no message

--- Payment/view/frontend/web/loader.js
+++ Payment/view/frontend/web/loader.js
@@ -12,6 +12,9 @@ function(name, code) {
 	// 2016-08-24 'Dfe_AllPay' => 'dfe_all_pay'
 	/** @type {String} */ code = code || df.s.lcFirst(df.s.explodeModuleCamel(name)).join('_');
 	if (window.checkoutConfig.payment[code]) {
+		// 2017-07-24
+		// `rendererList` is an «observable array»: http://knockoutjs.com/documentation/observableArrays.html
+		// https://github.com/magento/magento2/blob/2.2.0-RC1.5/app/code/Magento/Checkout/view/frontend/web/js/model/payment/renderer-list.js#L11
 		rendererList.push({type: code, component: name + '/main'});
 	}
 	return Component.extend({});
