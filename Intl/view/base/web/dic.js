0e6474ced528cefbf61e4c12f0ee43cfca2c5a7c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-26 18:15:12 +0300

    2.12.19

--- Intl/view/base/web/dic.js
+++ Intl/view/base/web/dic.js
@@ -6,7 +6,7 @@ define([], function() {return {
 	 * @param {String} key
 	 * @returns {String=}
 	 */
-	get: function(key) {return this._dic[key];}
+	get: function(key) {return !this._dic ? null : this._dic[key];}
 	/**
 	 * 2017-06-14
 	 * @used-by Df_Intl/main
