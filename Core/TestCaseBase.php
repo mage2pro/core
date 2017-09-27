9860c64f3c436c7bf07fbdc0222e24e985277f9f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-27 19:03:32 +0300

    2.12.22: Core/TestCaseBase.php: «Each class must be in a file by itself»: https://github.com/mage2pro/core/issues/29

--- Core/TestCaseBase.php
+++ Core/TestCaseBase.php
@@ -7,6 +7,7 @@ namespace Df\Core;
  * `«Fatal error: Class \'PHPUnit\Framework\TestCase\' not found in /app/vendor/mage2pro/core/Core/TestCase.php on line 6» on compilation`: https://mage2.pro/t/2260
  * `PHPUnit has been upgraded from 4.1.0 to 6.2.x in Magento 2.2.0-RC1.8`: https://mage2.pro/t/4275
  */
+// @codingStandardsIgnoreFile
 if (@class_exists('\PHPUnit\Framework\TestCase')) {
 	/** @noinspection PhpMultipleClassesDeclarationsInOneFile */
 	abstract class TestCaseBase extends \PHPUnit\Framework\TestCase {}
