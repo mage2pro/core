40fd4d81486a912bbe623384700f4d13dd8eac26
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-12-13 09:34:10 +0300

    3.4.13

--- Directory/lib/directory.php
+++ Directory/lib/directory.php
@@ -1,8 +1,17 @@
 <?php
-use Magento\Directory\Helper\Data as Helper;
+use Magento\Directory\Helper\Data as H;
 /**
- * 2016-04-05
- * @return Helper
+ * 2016-04-05                               
+ * @used-by df_is_postcode_required()
+ * @used-by \Dfe\Customer\Plugin\Customer\Model\ResourceModel\AddressRepository::_validate()
+ * @used-by vendor/mage2pro/customer/view/frontend/templates/address/edit.phtml:151
+ * @return H
  */
-function df_directory_h() {return df_o(Helper::class);}
+function df_directory() {return df_o(H::class);}
+
+/**
+ * @param string $iso2
+ * @return bool
+ */
+function df_is_postcode_required($iso2) {return !df_directory()->isZipCodeOptional($iso2);}
 
