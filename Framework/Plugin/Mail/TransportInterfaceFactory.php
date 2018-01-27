d315f66d7c94351238d64be0abfc6b7cf8781927
Dmitry Fedyuk <dfediuk@gmail.com>
2018-01-28 01:44:33 +0600

    https://github.com/mage2pro/core/issues/68

---
+++ Framework/Plugin/Mail/TransportInterfaceFactory.php
@@ -0,0 +1,29 @@
+<?php
+namespace Df\Framework\Plugin\Mail;
+use Magento\Framework\Mail\TransportInterfaceFactory as Sb;
+// 2018-01-28
+final class TransportInterfaceFactory {
+	/**
+	 * 2018-01-28
+	 * The purpose of this plugin is to provide an ability to my Dfe_Mailgun and Dfe_SMTP modules
+	 * to use an alternative mail transport instead of \Zend\Mail\Transport\Sendmail
+	 * @see \Magento\Email\Model\Transport::__construct():
+	 * 		$this->zendTransport = new Sendmail($parameters);
+	 * https://github.com/magento/magento2/blob/1a81e05b/app/code/Magento/Email/Model/Transport.php#L73
+	 * https://github.com/mage2pro/mailgun
+	 * https://github.com/mage2pro/smtp
+	 * @see \Magento\Framework\Mail\TransportInterfaceFactory::create():
+	 *		public function create(array $data = []) {
+	 *			return $this->_objectManager->create($this->_instanceName, $data);
+	 *		}
+	 * https://github.com/magento/magento2/blob/2.2.2/lib/internal/Magento/Framework/Mail/TransportInterfaceFactory.php#L42-L51
+	 * https://github.com/magento/magento2/blob/2.0.0/lib/internal/Magento/Framework/Mail/TransportInterfaceFactory.php#L42-L51
+	 * @param Sb $sb
+	 * @param \Closure $f
+	 * @param array $data [optional]
+	 * @return string
+	 */
+	function aroundCreate(Sb $sb, \Closure $f, array $data = []) {
+		return $f($data);
+	}
+}
