6b8154185b371187f1131912cdb314c7ed6fdade
Dmitry Fedyuk <dfediuk@gmail.com>
2018-04-19 19:47:43 +0600

    no message

--- Config/ArrayItem.php
+++ Config/ArrayItem.php
@@ -4,11 +4,15 @@ namespace Df\Config;
  * 2017-01-24
  * @see \Dfe\AllPay\InstallmentSales\Plan\Entity
  * @see \Dfe\CurrencyFormat\O
+ * @see \Doormall\Shipping\Partner\Entity
  */
 abstract class ArrayItem extends O {
 	/**
 	 * 2015-12-31
 	 * @used-by \Df\Config\A::get()
+	 * @see \Dfe\AllPay\InstallmentSales\Plan\Entity::id()
+	 * @see \Dfe\CurrencyFormat\O::id()
+	 * @see \Doormall\Shipping\Partner\Entity::id()
 	 * https://github.com/mage2pro/core/blob/dcc75ea95/Config/A.php?ts=4#L26
 	 * 2017-01-24
 	 * Решил не использовать @see \Df\Core\O::getId(),
@@ -21,6 +25,8 @@ abstract class ArrayItem extends O {
 	 * 2016-08-07
 	 * @used-by \Df\Config\Backend\ArrayT::processI()
 	 * @see \Dfe\AllPay\InstallmentSales\Plan\Entity::sortWeight()
+	 * @see \Dfe\CurrencyFormat\O::sortWeight()
+	 * @see \Doormall\Shipping\Partner\Entity::sortWeight()
 	 * @return int
 	 */
 	function sortWeight() {return 0;}
