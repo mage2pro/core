371800bcf7a9d4145d10510fdc45a314effe3333
Dmitry Fedyuk <dfediuk@gmail.com>
2018-04-22 13:22:06 +0600

    no message

--- Payment/view/frontend/web/createMessagesComponent.js
+++ Payment/view/frontend/web/createMessagesComponent.js
@@ -1,29 +1,14 @@
-// 2016-07-18
-define(['uiLayout'], function(layout) {
-	/**
-	 * 2016-07-16
-	 * Перекрываем ради установки своего шаблона,
-	 * который поддерживает теги HTML в диагностических сообщениях.
-	 * @override
-	 * @see https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Checkout/view/frontend/web/js/view/payment/default.js#L105-L125
-	 * @returns {Component} Chainable
-	 */
-	return function() {
-		layout([{
-			parent: this.name,
-			name: this.name + '.messages',
-			displayArea: 'messages',
-			component: 'Magento_Ui/js/view/messages',
-			config: {
-				messageContainer: this.messageContainer
-				/**
-				 * 2016-07-16
-				 * Добавление этого параметра — это единственное,
-				 * что мы поменяли в родительском методе.
-				 */
-				,template: 'Df_Checkout/messages'
-			}
-		}]);
-		return this;
-	};
-});
+// 2016-07-16
+// Our custom module allows us to use HTML tags in diagnostic messages.
+// @see https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Checkout/view/frontend/web/js/view/payment/default.js#L105-L125
+define(['uiLayout'], function(l) {return function() {l([{
+	component: 'Magento_Ui/js/view/messages',
+	config: {
+		messageContainer: this.messageContainer
+		// 2016-07-16 It is the single line we have changed in the parent module.
+		,template: 'Df_Checkout/messages'
+	},
+	displayArea: 'messages',
+	name: this.name + '.messages',
+	parent: this.name
+}]);return this;};});
