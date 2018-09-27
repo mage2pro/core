1227435d1eb56b3629918012b1efb10c9c7a28a0
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-27 17:26:05 +0600

    3.9.10: https://github.com/mage2pro/markdown/issues/3

--- Catalog/lib/catalog.php
+++ Catalog/lib/catalog.php
@@ -1,9 +1,21 @@
 <?php
 use Magento\Catalog\Model\Locator\LocatorInterface;
+use Magento\Catalog\Model\Product as P;
 
 /**
  * 2016-02-25
  * https://github.com/magento/magento2/blob/e0ed4bad/app/code/Magento/Catalog/etc/adminhtml/di.xml#L10-L10
+ * @used-by df_product_current()
  * @return LocatorInterface|\Magento\Catalog\Model\Locator\RegistryLocator
  */
 function df_catalog_locator() {return df_o(LocatorInterface::class);}
+
+/**
+ * 2018-09-27
+ * @param \Closure|bool|mixed $onError
+ * @return P|null
+ * @throws \Exception
+ */
+function df_product_current($onError = null) {return df_try(function() {return
+	df_catalog_locator()->getProduct()
+;}, $onError);}
