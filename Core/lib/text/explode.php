136857150ff93029835336d0b1b269825eb97cb7
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-27 23:49:00 +0600

    no message

--- Core/lib/text/explode.php
+++ Core/lib/text/explode.php
@@ -71,6 +71,11 @@ function df_explode_multiple(array $delimiters, $s) {
 
 /**
  * 2018-04-24 I have added @uses trim() today.
+ * @used-by df_parse_colon()
+ * @used-by df_tab_multiline()
+ * @used-by df_zf_http_last_req()
+ * @used-by \Df\Core\Helper\Text::parseTextarea()
+ * @used-by \Df\Core\Text\Regex::getSubjectSplitted()
  * @used-by \Dfe\AllPay\Charge::descriptionOnKiosk()
  * @used-by \Dfe\Moip\P\Charge::pInstructionLines()
  * @used-by \Dfe\TBCBank\W\Reader::reqFilter()
