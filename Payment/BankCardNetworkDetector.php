07680874f9e719896568ef5c0c05597d098082e6
Dmitry Fedyuk <dfediuk@gmail.com>
2018-12-18 07:57:44 +0600

    3.12.1

---
+++ Payment/BankCardNetworkDetector.php
@@ -0,0 +1,69 @@
+<?php
+namespace Df\Payment;
+// 2018-12-18 https://github.com/mcred/detect-credit-card-type/blob/09064531/src/Detector.php
+final class BankCardNetworkDetector {
+	/**
+	 * 2018-12-18
+	 * @param string $n
+	 * @return string|null
+	 */
+	static function p($n) {
+		$r = null;  /** @var string|null $r */
+		$types = [
+			self::AE => function($n) {return !!preg_match("/^3$|^3[47][0-9]{0,13}$/i", $n);}
+			,self::DN => function($n) {return !!preg_match("/^3(?:0[0-5]|[68][0-9])[0-9]{4,}$/i", $n);}
+			,self::DS => function($n) {return !!preg_match(
+				"/^6$|^6[05]$|^601[1]?$|^65[0-9][0-9]?$|^6(?:011|5[0-9]{2})[0-9]{0,12}$/i", $n
+			);}
+			,self::JC => function($n) {return !!preg_match("/^(?:2131|1800|35[0-9]{3})[0-9]{3,}$/i", $n);}
+			,self::MC => function($n) {return !!preg_match(
+				"/^5[1-5][0-9]{5,}|222[1-9][0-9]{3,}|22[3-9][0-9]{4,}|2[3-6][0-9]{5,}|27[01][0-9]{4,}|2720[0-9]{3,}$/i", $n
+			);}
+			,self::VI => function($n) {return !!preg_match("/^4[0-9]{0,15}$/i", $n);}
+		];
+		foreach ($types as $t => $f) { /** @var string $t */
+			if ($f($n)) {
+				$r = $t;
+				break;
+			}
+		}
+		return $r;
+	}
+
+	/**
+	 * 2018-12-18
+	 * @used-by p()
+	 * @used-by \Dfe\Vantiv\T\CaseT\Charge::type()
+	 */
+	const AE = 'amex';
+	/**
+	 * 2018-12-18
+	 * @used-by p()
+	 * @used-by \Dfe\Vantiv\T\CaseT\Charge::type()
+	 */
+	const DN = 'diners';
+	/**
+	 * 2018-12-18
+	 * @used-by p()
+	 * @used-by \Dfe\Vantiv\T\CaseT\Charge::type()
+	 */
+	const DS = 'discover';
+	/**
+	 * 2018-12-18
+	 * @used-by p()
+	 * @used-by \Dfe\Vantiv\T\CaseT\Charge::type()
+	 */
+	const JC = 'jcb';
+	/**
+	 * 2018-12-18
+	 * @used-by p()
+	 * @used-by \Dfe\Vantiv\T\CaseT\Charge::type()
+	 */
+	const MC = 'mastercard';
+	/**
+	 * 2018-12-18
+	 * @used-by p()
+	 * @used-by \Dfe\Vantiv\T\CaseT\Charge::type()
+	 */
+	const VI = 'visa';
+}
