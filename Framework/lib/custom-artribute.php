e2adee20fef49aefc9904397d8f897d23ecc2575
Dmitry Fedyuk <dfediuk@gmail.com>
2019-03-06 03:44:35 +0630

    4.2.0

---
+++ Framework/lib/custom-artribute.php
@@ -0,0 +1,14 @@
+<?php
+use Magento\Framework\Api\AbstractExtensibleObject as O;
+use Magento\Framework\Api\AttributeInterface as IA;
+use Magento\Framework\Api\AttributeValue as A;
+/**
+ * 2019-03-06
+ * @param O $o
+ * @param string $k
+ * @return mixed|null
+ */
+function df_cav(O $o, $k) {
+	$a = $o->getCustomAttribute($k); /** @var IA|A $a */
+	return $a ? $a->getValue() : null;
+}
