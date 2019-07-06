fc01554e886eb02bb389054afc1e34f1b973fd48
Dmitry Fedyuk <dfediuk@gmail.com>
2019-07-06 14:41:02 +0600

    no message

--- Payment/Block/Info.php
+++ Payment/Block/Info.php
@@ -460,6 +460,7 @@ abstract class Info extends _P {
 	 * @see \Df\GingerPaymentsBase\Block\Info::prepareUnconfirmed()
 	 * @see \Dfe\AllPay\Block\Info\BankCard::prepareUnconfirmed()
 	 * @see \Dfe\AllPay\Block\Info\Offline::prepareUnconfirmed()
+	 * @used-by \Dfe\Dragonpay\Block\Info::prepareUnconfirmed()
 	 */
 	protected function prepareUnconfirmed() {$this->si('State', __('Review'));}
 
