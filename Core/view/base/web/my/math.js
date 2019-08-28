e5e3f8cdc8f76926327af6a187bde4a896d80bf2
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-28 18:51:03 +0600

    no message

--- Core/view/base/web/my/math.js
+++ Core/view/base/web/my/math.js
@@ -2,6 +2,7 @@
 define([], function() {return {
 	/**
 	 * 2019-08-28
+	 * @used-by https://github.com/inkifi/map/blob/0.0.8/view/frontend/web/js/create.js#L50
 	 * @param {Number} v
 	 * @param {Number} min
 	 * @param {Number} max
@@ -10,6 +11,7 @@ define([], function() {return {
 	minmax: function(v, min, max) {return Math.min(max, Math.max(min, v));},
 	/**
 	 * 2019-08-28 https://stackoverflow.com/a/12830454
+	 * @used-by https://github.com/inkifi/map/blob/0.0.8/view/frontend/web/js/create.js#L50
 	 * @param {Number} v
 	 * @param {Number} precision
 	 * @returns {String}
