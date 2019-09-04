d6a4897bb334932da139d95794ff5b2115d74251
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-04 14:03:23 +0600

    5.1.2

--- Eav/lib/attribute-set.php
+++ Eav/lib/attribute-set.php
@@ -6,13 +6,21 @@ use Magento\Eav\Model\AttributeSetRepository as ASR;
 use Magento\Eav\Model\Entity\Attribute\Set as _AS;
 /**
  * 2019-02-27
- * @used-by \Inkifi\Mediaclip\H\AvailableForDownload\Pureprint::pOI()
+ * @used-by df_att_set_name()
  * @param IAS|_AS|P|int $a
  * @return IAS|_AS
  */
 function df_att_set($a) {return $a instanceof IAS ? $a : df_att_set_r()->get(
 	$a instanceof P ? $a->getAttributeSetId() : $a
 );}
+
+/**
+ * 2019-09-04
+ * @used-by \Inkifi\Mediaclip\H\AvailableForDownload\Pureprint::pOI()
+ * @param IAS|_AS|P|int $a
+ * @return string
+ */
+function df_att_set_name($a) {return df_att_set($a)->getAttributeSetName();}
 
 /**
  * 2019-02-27
