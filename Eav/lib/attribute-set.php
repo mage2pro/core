d733b591312da8a784539ca8148ee219172dd77a
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-04 14:01:31 +0600

    no message

--- Eav/lib/attribute-set.php
+++ Eav/lib/attribute-set.php
@@ -10,13 +10,13 @@ use Magento\Eav\Model\Entity\Attribute\Set as _AS;
  * @param IAS|_AS|P|int $a
  * @return IAS|_AS
  */
-function df_attribute_set($a) {return $a instanceof IAS ? $a : df_att_set_r()->get(
+function df_att_set($a) {return $a instanceof IAS ? $a : df_att_set_r()->get(
 	$a instanceof P ? $a->getAttributeSetId() : $a
 );}
 
 /**
  * 2019-02-27
- * @used-by df_attribute_set()
+ * @used-by df_att_set()
  * @return IASR|ASR
  */
 function df_att_set_r() {return df_o(IASR::class);}
