de51e16623e884aea166192b5cd18077317cedf2
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-07 11:34:50 +0600

    no message

--- Core/lib/array/select.php
+++ Core/lib/array/select.php
@@ -8,9 +8,7 @@
  * @param string[] $keys
  * @return array(string => string)
  */
-function dfa_select($source, array $keys)  {return
-	array_intersect_key(df_ita($source), array_fill_keys($keys, null))
-;}
+function dfa_select($source, array $keys)  {return array_intersect_key(df_ita($source), array_fill_keys($keys, null));}
 
 /**
  * 2015-02-08
