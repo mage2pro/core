6ba312cfee898bc23d50a33d49fbfda489b97c9f
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-08 16:12:55 +0600

    no message

--- Core/lib/lang/main.php
+++ Core/lib/lang/main.php
@@ -124,6 +124,7 @@ function df_nop($v) {return $v;}
  * @used-by \Inkifi\Pwinty\API\Entity\Image::url()
  * @used-by \Inkifi\Pwinty\API\Entity\Order::magentoOrder()
  * @used-by \Wolf\Filter\Customer::garage()
+ * @used-by \Wolf\Filter\Customer::hash()
  * @param object|null|\ArrayAccess $o
  * @param mixed|null $v
  * @param string|mixed|null $d [optional]
