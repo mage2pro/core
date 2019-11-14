28ec338805312db048a6108f0604ea0860251c40
Dmitry Fedyuk <dfediuk@gmail.com>
2019-11-14 19:47:36 +0600

    no message

--- Core/lib/js.php
+++ Core/lib/js.php
@@ -2,19 +2,14 @@
 /**
  * 2015-02-17
  * Экранирует строку для вставки её в код на JavaScript.
- * @uses json_encode() рекомендуют
- * как самый правильный способ вставки строки из PHP в JavaScript:
+ * @uses json_encode() рекомендуют как самый правильный способ вставки строки из PHP в JavaScript:
  * http://stackoverflow.com/a/169035
  * Заменяем символ одинарной кавычки его кодом Unicode,
- * чтобы результат метода можно было вставлять внутрь обрамленной одиночными кавычками строки,
- * например:
- *	var $name = '<?= df_ejs($name); ?>';
- * @used-by df_admin_button_location()
+ * чтобы результат метода можно было вставлять внутрь обрамленной одиночными кавычками строки, например:
+ *		var name = <?= df_ejs($name); ?>;
+ * 2019-11-14
+ * We do not need quotes around a `df_ejs` call: `var name = '<?= df_ejs(name); ?>';` => `var name = '...';`
  * @used-by df_js_data()
- * @used-by Df_Admin_Config_DynamicTable_Column::renderTemplate()
- * @used-by app/design/adminhtml/rm/default/template/df/admin/column/select.phtml
- * @used-by app/design/adminhtml/rm/default/template/df/admin/field/button.phtml
- * @used-by app/design/frontend/rm/default/template/df/checkout/onepage/shipping_method/available/js.phtml
  * @param string $text
  * @return string
  */
