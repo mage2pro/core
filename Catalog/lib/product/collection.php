065975d236a3f05378c8ae0daa4b55158d2f868b
Dmitry Fedyuk <dfediuk@gmail.com>
2019-11-18 13:27:50 +0600

    no message

--- Catalog/lib/product/collection.php
+++ Catalog/lib/product/collection.php
@@ -1,7 +1,8 @@
 <?php
 use Magento\Catalog\Model\ResourceModel\Product\Collection as C;
 /**
- * 2019-09-18
+ * 2019-09-18 
+ * @used-by \Justuno\M2\Controller\Response\Catalog::execute()
  * @return C
  */
 function df_product_c() {return df_new_om(C::class);}
