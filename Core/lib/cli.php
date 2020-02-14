8779417923a0fccb41cc83e663c9f7fcf80b56a3
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-15 00:14:03 +0600

    no message

--- Core/lib/cli.php
+++ Core/lib/cli.php
@@ -1,15 +1,13 @@
 <?php
 /**
- * 2017-03-15
- * Нулевой параметр argv — это имя текущего скрипта.
+ * 2017-03-15 Нулевой параметр argv — это имя текущего скрипта.
  * @used-by df_sentry()
  * @return string[]
  */
 function df_cli_argv() {return array_slice(dfa($_SERVER, 'argv', []), 1);}
 
 /**
- * 2016-12-23
- * http://stackoverflow.com/a/7771601
+ * 2016-12-23 http://stackoverflow.com/a/7771601
  * @see \Magento\Framework\Shell::execute()
  * @return string
  */
