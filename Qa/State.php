f7ba64966e7a2f085e425b313e473e752cceb8ef
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-20 07:26:09 +0600

    no message

--- Qa/State.php
+++ Qa/State.php
@@ -50,26 +50,8 @@ class State extends \Df\Core\O {
 	});}
 
 	/**
-	 * @used-by \Df\Qa\Method::raiseErrorParam()
-	 * @param int $ordering  		zero-based
-	 * @return RP
-	 */
-	function methodParameter($ordering) {return dfc($this, function($ordering) {/** @var RP $r */
-		df_param_integer($ordering, 0);
-		df_assert($this->method()); // Метод должен существовать
-		if ($ordering >= count($this->method()->getParameters())) { // Параметр должен существовать
-			df_error(
-				"Программист ошибочно пытается получить значение параметра с индексом {$ordering}"
-				. " метода «{$this->methodName()}», хотя этот метод принимает всего %d параметров."
-				, count($this->method()->getParameters())
-			);
-		}
-		df_assert_lt(count($this->method()->getParameters()), $ordering);
-		df_assert(($r = dfa($this->method()->getParameters(), $ordering)) instanceof RP);
-		return $r;
-	}, [$ordering]);}
-
-	/**
+	 * @used-by functionA()
+	 * @used-by methodParameter()
 	 * @used-by \Df\Qa\Method::raiseErrorParam()
 	 * @return RM|null
 	 */
@@ -78,14 +60,35 @@ class State extends \Df\Core\O {
 	;});}
 
 	/**
-	 * 2015-04-03
-	 * Для простых функций (не методов) вернёт название функции.
+	 * 2015-04-03 Для простых функций (не методов) вернёт название функции.
+	 * @used-by __toString()
+	 * @used-by methodParameter()
 	 * @used-by \Df\Qa\Method::raiseErrorParam()
 	 * @used-by \Df\Qa\Method::raiseErrorResult()
 	 * @used-by \Df\Qa\Method::raiseErrorVariable()
 	 * @return string
 	 */
 	function methodName() {return df_cc_method($this->className(), $this->functionName());}
+
+	/**
+	 * @used-by \Df\Qa\Method::raiseErrorParam()
+	 * @param int $ordering  		zero-based
+	 * @return RP
+	 */
+	function methodParameter($ordering) {return dfc($this, function($ordering) {/** @var RP $r */
+		df_param_integer($ordering, 0);
+		df_assert($m = $this->method()); /** @var RM|null $m */
+		if ($ordering >= count($m->getParameters())) { // Параметр должен существовать
+			df_error(
+				"Программист ошибочно пытается получить значение параметра с индексом {$ordering}"
+				. " метода «{$this->methodName()}», хотя этот метод принимает всего %d параметров."
+				, count($m->getParameters())
+			);
+		}
+		df_assert_lt(count($m->getParameters()), $ordering);
+		df_assert(($r = dfa($m->getParameters(), $ordering)) instanceof RP);
+		return $r;
+	}, [$ordering]);}
 
 	/**
 	 * @used-by method()
@@ -143,10 +146,22 @@ class State extends \Df\Core\O {
 
 	/**
 	 * 2016-07-31 Без проверки на closure будет сбой: «Function Df\Config\{closure}() does not exist».
+	 * 2020-02-20
+	 * $f could be `include`, `include_once`, `require`, ``require_once``:
+	 * https://www.php.net/manual/function.include.php
+	 * https://www.php.net/manual/function.include-once.php
+	 * https://www.php.net/manual/function.require.php
+	 * https://www.php.net/manual/function.require-once.php
+	 * https://www.php.net/manual/function.debug-backtrace.php#111255
+	 * They are not functions and will lead to a @see \ReflectionException:
+	 * «Function include() does not exist»: https://github.com/tradefurniturecompany/site/issues/60
+	 * https://www.php.net/manual/reflectionfunction.construct.php
+	 * https://www.php.net/manual/class.reflectionexception.php
+	 * @used-by context()
 	 * @return RFA|RF|RM|null
 	 */
 	private function functionA() {return dfc($this, function() {return $this->method() ?: (
-		($f = $this->functionName()) && !$this->isClosure() ? new RF($f) : null
+		(!($f = $this->functionName())) || $this->isClosure() ? null : df_try(function() use($f) {return new RF($f);}, null)
 	);});}
 	
 	/**
