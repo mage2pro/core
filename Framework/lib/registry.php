400f2335c43523415b0833bc66b95e1b7f81cfb3
Dmitry Fedyuk <dfediuk@gmail.com>
2020-03-01 12:10:56 +0600

    6.3.8: https://github.com/tradefurniturecompany/site/issues/130

--- Framework/lib/registry.php
+++ Framework/lib/registry.php
@@ -3,6 +3,7 @@ use Magento\Framework\Registry as R;
 
 /**
  * 2015-10-31
+ * @used-by \MageKey\AdcPopup\Observer\QuoteAddAfter::execute()		tradefurniturecompany.co.uk
  * @used-by \Wolf\Filter\Observer\ControllerActionPredispatch::execute()
  * @param string $k
  * @param mixed $v
@@ -13,8 +14,9 @@ function df_register($k, $v) {df_registry_o()->register($k, $v);}
  * 2015-10-31
  * @used-by df_product_current()
  * @used-by \Dfe\Logo\Frontend::_toHtml()
+ * @used-by \MageKey\AdcPopup\Observer\QuoteAddAfter::execute()		tradefurniturecompany.co.uk
  * @used-by \SayItWithAGift\Options\Frontend::_toHtml()
- * @param string $key
+ * @param string $k
  * @return mixed|null
  */
 function df_registry($k) {return df_registry_o()->registry($k);}
@@ -23,6 +25,14 @@ function df_registry($k) {return df_registry_o()->registry($k);}
  * 2015-11-02 https://mage2.pro/t/95
  * @used-by df_register()
  * @used-by df_registry()
+ * @used-by df_unregister()
  * @return R
  */
 function df_registry_o() {return df_o(R::class);}
+
+/**
+ * 2020-03-01
+ * @used-by \MageKey\AdcPopup\Observer\QuoteAddAfter::execute()		tradefurniturecompany.co.uk
+ * @param string $k
+ */
+function df_unregister($k) {return df_registry_o()->unregister($k);}
