8e766359743eb4b911d4095d0977719616865c4c
Dmitry Fedyuk <dfediuk@gmail.com>
2020-04-30 10:24:04 +0600

    6.4.7: https://github.com/mage2pro/core/issues/98

---
+++ Ui/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+// 2020-04-30
+var config = {config: {mixins: {
+	// 2020-04-30
+	// "Prevent error messages from being hidden in 5 seconds from the frontend checkout screen":
+	// https://github.com/mage2pro/core/issues/98
+	'Magento_UI/js/view/messages': {'Df_Ui/view/messages': true}
+}}};
---
+++ Ui/view/frontend/web/view/messages.js
@@ -0,0 +1,4 @@
+// 2020-04-30
+// "Prevent error messages from being hidden in 5 seconds from the frontend checkout screen":
+// https://github.com/mage2pro/core/issues/98
+define([], function() {'use strict'; return function(sb) {$.extend(sb, {onHiddenChange: function() {}});return sb;};});
