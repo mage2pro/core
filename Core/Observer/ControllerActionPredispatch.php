4b443352722c91abb3324f800fac61928fd8e245
Dmitry Fedyuk <dfediuk@gmail.com>
2020-05-27 07:10:13 +0600

    no message

--- Core/Observer/ControllerActionPredispatch.php
+++ Core/Observer/ControllerActionPredispatch.php
@@ -8,9 +8,9 @@ final class ControllerActionPredispatch implements ObserverInterface {
 	 * @see ObserverInterface::execute()
 	 * @used-by \Magento\Framework\Event\Invoker\InvokerDefault::_callObserverMethod()
 	 * @see \Magento\Framework\App\Action\Action::dispatch()
-	 * https://github.com/magento/magento2/blob/dd47569249206b217e0a9f9a9371e73fd7622724/lib/internal/Magento/Framework/App/Action/Action.php#L91-L92
-	 *	$eventParameters = ['controller_action' => $this, 'request' => $request];
-	 *	$this->_eventManager->dispatch('controller_action_predispatch', $eventParameters)
+	 * 		$eventParameters = ['controller_action' => $this, 'request' => $request];
+	 *		$this->_eventManager->dispatch('controller_action_predispatch', $eventParameters);
+	 * https://github.com/magento/magento2/blob/2.3.5-p1/lib/internal/Magento/Framework/App/Action/Action.php#L96-L102
 	 * @param O $o
 	 */
 	function execute(O $o) {df_state()->controllerSet($o['controller_action']);}
