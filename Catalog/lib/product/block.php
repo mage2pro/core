0fbcfed902bf42ad61a470cbf39852ddf61caf48
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-06 13:20:45 +0600

    6.5.9: https://github.com/blushme/checkout/issues/6

---
+++ Catalog/lib/product/block.php
@@ -0,0 +1,8 @@
+<?php
+use Magento\Catalog\Block\Product\AbstractProduct as B;
+/**
+ * 2020-06-06
+ * @used-by vendor/blushme/checkout/view/frontend/templates/extra/item.phtml:38 (blushme.se)
+ * @return B
+ */
+function df_product_b() {return df_o(B::class);}
