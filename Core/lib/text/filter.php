5ae3668f8b3accbe4cdf21be4a40d42b1c50d1e9
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-20 05:24:10 +0600

    no message

--- Core/lib/text/filter.php
+++ Core/lib/text/filter.php
@@ -33,11 +33,11 @@ function df_is_guid($s) {return 36 === strlen($s) && preg_match(
 );}
 
 /**
- * @param string $text
- * @return string
  * http://darklaunch.com/2009/05/06/php-normalize-newlines-line-endings-crlf-cr-lf-unix-windows-mac
+ * @param string $s
+ * @return string
  */
-function df_normalize($text) {return strtr($text, ["\r\n" => "\n", "\r" => "\n"]);}
+function df_normalize($s) {return strtr($s, ["\r\n" => "\n", "\r" => "\n"]);}
 
 /**
  * 2017-04-22 «+79.6-2» => «7962»
