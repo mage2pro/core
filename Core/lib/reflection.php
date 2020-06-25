05378cacc7d7c7d222566f269f7ade9629d8a0f7
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-26 01:54:20 +0600

    no message

--- Core/lib/reflection.php
+++ Core/lib/reflection.php
@@ -489,10 +489,6 @@ function df_cts_lc_camel($c, $del) {return implode($del, df_explode_class_lc_cam
 /**
  * @param string|object $c
  * @return string[]
- * 2016-10-20
- * Нельзя делать параметр $c опциональным, потому что иначе получим сбой:
- * «get_class() called without object from outside a class»
- * https://3v4l.org/k6Hd5
  */
 function df_explode_class($c) {return df_explode_multiple(['\\', '_'], df_cts($c));}
 
