d44a08cc43e1eb92c0b3f56f03fbbf5a8299c22e
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-26 04:07:46 +0600

    no message

--- Core/lib/test.php
+++ Core/lib/test.php
@@ -2,9 +2,9 @@
 /**
  * 2016-11-17
  * $m could be:
- * 1) A module name: «A_B»
- * 2) A class name: «A\B\C».
- * 3) An object: it comes down to the case 2 via @see get_class()
+ * 1) a module name: «A_B»
+ * 2) a class name: «A\B\C».
+ * 3) an object: it comes down to the case 2 via @see get_class()
  * 4) `null`: it comes down to the case 1 with the «Df_Core» module name.
  * @param string|object|null $m
  * @param string $localPath [optional]
