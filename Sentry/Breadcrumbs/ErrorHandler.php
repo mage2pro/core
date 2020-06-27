e794aa7a4270aa4b1d74f08e02e4cf284b784faa
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-27 06:58:46 +0600

    no message

--- Sentry/Breadcrumbs/ErrorHandler.php
+++ Sentry/Breadcrumbs/ErrorHandler.php
@@ -2,22 +2,22 @@
 namespace Df\Sentry\Breadcrumbs;
 class ErrorHandler
 {
-    private $existingHandler;
+	private $existingHandler;
 
-    /**
-     * @var \Df\Sentry\Client the client object that sends the message to the server
-     */
-    protected $ravenClient;
+	/**
+	 * @var \Df\Sentry\Client the client object that sends the message to the server
+	 */
+	protected $ravenClient;
 
-    /**
-     * @param \Df\Sentry\Client $ravenClient
-     * @param int          $level       The minimum logging level at which this handler will be triggered
-     * @param Boolean      $bubble      Whether the messages that are handled can bubble up the stack or not
-     */
-    function __construct(\Df\Sentry\Client $ravenClient)
-    {
-        $this->ravenClient = $ravenClient;
-    }
+	/**
+	 * @param \Df\Sentry\Client $ravenClient
+	 * @param int          $level       The minimum logging level at which this handler will be triggered
+	 * @param Boolean      $bubble      Whether the messages that are handled can bubble up the stack or not
+	 */
+	function __construct(\Df\Sentry\Client $ravenClient)
+	{
+		$this->ravenClient = $ravenClient;
+	}
 
 	/**
 	 * 2017-07-10
@@ -28,13 +28,13 @@ class ErrorHandler
 	 * @param array $context
 	 * @return bool|mixed
 	 */
-    final function handleError($code, $m, $file = '', $line = 0, $context=[]) {
-    	// 2017-07-10
-    	// «Magento 2.1 php7.1 will not be supported due to mcrypt deprecation»
+	final function handleError($code, $m, $file = '', $line = 0, $context=[]) {
+		// 2017-07-10
+		// «Magento 2.1 php7.1 will not be supported due to mcrypt deprecation»
 		// https://github.com/magento/magento2/issues/5880
 		// [PHP 7.1] How to fix the «Function mcrypt_module_open() is deprecated» bug?
 		// https://mage2.pro/t/2392
-    	if (E_DEPRECATED !== $code || !df_contains($m, 'mcrypt') && !df_contains($m, 'mdecrypt')) {
+		if (E_DEPRECATED !== $code || !df_contains($m, 'mcrypt') && !df_contains($m, 'mdecrypt')) {
 			$this->ravenClient->breadcrumbs->record([
 				'category' => 'error_reporting',
 				'message' => $m,
@@ -45,11 +45,11 @@ class ErrorHandler
 		return !$this->existingHandler ? false : call_user_func(
 			$this->existingHandler, $code, $m, $file, $line, $context
 		);
-    }
+	}
 
-    function install()
-    {
-        $this->existingHandler = set_error_handler(array($this, 'handleError'), E_ALL);
-        return $this;
-    }
+	function install()
+	{
+		$this->existingHandler = set_error_handler(array($this, 'handleError'), E_ALL);
+		return $this;
+	}
 }
