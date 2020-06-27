a12f1d594434042a9a422a296e4ae4b9b798b26e
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-28 05:49:18 +0600

    no message

--- Sentry/Serializer.php
+++ Sentry/Serializer.php
@@ -4,14 +4,7 @@ namespace Df\Sentry;
  * 2020-06-28
  * @see \Df\Sentry\ReprSerializer
  */
-class Serializer
-{
-
-	/*
-	 * Suggested detect order for western countries
-	 */
-	const WESTERN_MB_DETECT_ORDER = 'UTF-8, ASCII, ISO-8859-1, ISO-8859-2, ISO-8859-3, ISO-8859-4, ISO-8859-5, ISO-8859-6, ISO-8859-7, ISO-8859-8, ISO-8859-9, ISO-8859-10, ISO-8859-13, ISO-8859-14, ISO-8859-15, ISO-8859-16, Windows-1251, Windows-1252, Windows-1254';
-	
+class Serializer {
 	/**
 	 * 2020-06-28
 	 * @used-by \Df\Sentry\Stacktrace::get_stack_info()
