e8cdb2f6d286f78adef08dbcf9c2df5a6dbbd8b8
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-28 05:21:44 +0600

    no message

--- Sentry/Compat.php
+++ Sentry/Compat.php
@@ -7,42 +7,4 @@ final class Compat {
 		}
 		return php_uname('n');
 	}
-	
-	static function hash_hmac($algo, $data, $key, $raw_output=false)
-	{
-		if (function_exists('hash_hmac')) {
-			return hash_hmac($algo, $data, $key, $raw_output);
-		}
-
-		return self::_hash_hmac($algo, $data, $key, $raw_output);
-	}
-
-	/**
-	 * Implementation from 'KC Cloyd'.
-	 * See http://nl2.php.net/manual/en/function.hash-hmac.php
-	 */
-	static function _hash_hmac($algo, $data, $key, $raw_output=false)
-	{
-		$algo = strtolower($algo);
-		$pack = 'H'.strlen($algo('test'));
-		$size = 64;
-		$opad = str_repeat(chr(0x5C), $size);
-		$ipad = str_repeat(chr(0x36), $size);
-
-		if (strlen($key) > $size) {
-			$key = str_pad(pack($pack, $algo($key)), $size, chr(0x00));
-		} else {
-			$key = str_pad($key, $size, chr(0x00));
-		}
-
-		$keyLastPos = strlen($key) - 1;
-		for ($i = 0; $i < $keyLastPos; $i++) {
-			$opad[$i] = $opad[$i] ^ $key[$i];
-			$ipad[$i] = $ipad[$i] ^ $key[$i];
-		}
-
-		$output = $algo($opad.pack($pack, $algo($ipad.$data)));
-
-		return ($raw_output) ? pack($pack, $output) : $output;
-	}
 }
