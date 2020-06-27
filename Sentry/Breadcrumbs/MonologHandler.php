dbb0f1ac36ddeda46d4ad5de8d345e464d4c2d7a
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-28 05:02:48 +0600

    no message

--- Sentry/Breadcrumbs/MonologHandler.php
+++ Sentry/Breadcrumbs/MonologHandler.php
@@ -4,33 +4,33 @@ use Monolog\Logger;
 use Monolog\Handler\AbstractProcessingHandler;
 class MonologHandler extends AbstractProcessingHandler
 {
-    /**
-     * Translates Monolog log levels to Raven log levels.
-     */
-    private $logLevels = array(
-        Logger::DEBUG     => \Df\Sentry\Client::DEBUG,
-        Logger::INFO      => \Df\Sentry\Client::INFO,
-        Logger::NOTICE    => \Df\Sentry\Client::INFO,
-        Logger::WARNING   => \Df\Sentry\Client::WARNING,
-        Logger::ERROR     => \Df\Sentry\Client::ERROR,
-        Logger::CRITICAL  => \Df\Sentry\Client::FATAL,
-        Logger::ALERT     => \Df\Sentry\Client::FATAL,
-        Logger::EMERGENCY => \Df\Sentry\Client::FATAL,
-    );
+	/**
+	 * Translates Monolog log levels to Raven log levels.
+	 */
+	private $logLevels = array(
+		Logger::DEBUG     => \Df\Sentry\Client::DEBUG,
+		Logger::INFO      => \Df\Sentry\Client::INFO,
+		Logger::NOTICE    => \Df\Sentry\Client::INFO,
+		Logger::WARNING   => \Df\Sentry\Client::WARNING,
+		Logger::ERROR     => \Df\Sentry\Client::ERROR,
+		Logger::CRITICAL  => \Df\Sentry\Client::FATAL,
+		Logger::ALERT     => \Df\Sentry\Client::FATAL,
+		Logger::EMERGENCY => \Df\Sentry\Client::FATAL,
+	);
 
-    private $excMatch = '/^exception \'([^\']+)\' with message \'(.+)\' in .+$/s';
+	private $excMatch = '/^exception \'([^\']+)\' with message \'(.+)\' in .+$/s';
 
-    /**
-     * @var \Df\Sentry\Client the client object that sends the message to the server
-     */
-    protected $ravenClient;
+	/**
+	 * @var \Df\Sentry\Client the client object that sends the message to the server
+	 */
+	protected $ravenClient;
 
-    /**
+	/**
 	 * 2017-08-09
 	 * @override
 	 * @see \Monolog\Handler\AbstractHandler::__construct()
-     * @param \Df\Sentry\Client $ravenClient
-     * @param int $level [optional] The minimum logging level at which this handler will be triggered
+	 * @param \Df\Sentry\Client $ravenClient
+	 * @param int $level [optional] The minimum logging level at which this handler will be triggered
 	 * 2017-08-09
 	 * Unfortunately, we are unable to specify the type of the $bubble argument
 	 * because of the Magento 2 compilation bug:
@@ -43,65 +43,65 @@ class MonologHandler extends AbstractProcessingHandler
 	 * specifies the $bubble argument type as `Boolean`:
 	 * https://github.com/Seldaek/monolog/blob/1.23.0/src/Monolog/Handler/AbstractHandler.php#L36
 	 * Actually, there is no @see \Monolog\Handler\Boolean class
-     * @param $bubble [optional]
+	 * @param $bubble [optional]
 	 * Whether the messages that are handled can bubble up the stack or not
-     */
-    function __construct(\Df\Sentry\Client $ravenClient, $level = Logger::DEBUG, $bubble = true) {
-        parent::__construct($level, boolval($bubble));
-        $this->ravenClient = $ravenClient;
-    }
+	 */
+	function __construct(\Df\Sentry\Client $ravenClient, $level = Logger::DEBUG, $bubble = true) {
+		parent::__construct($level, boolval($bubble));
+		$this->ravenClient = $ravenClient;
+	}
 
-    protected function parseException($message)
-    {
-        if (!preg_match($this->excMatch, $message, $matches)) {
-            return;
-        }
+	protected function parseException($message)
+	{
+		if (!preg_match($this->excMatch, $message, $matches)) {
+			return;
+		}
 
-        return array($matches[1], $matches[2]);
-    }
+		return array($matches[1], $matches[2]);
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function write(array $record)
-    {
-        // sentry uses the 'nobreadcrumb' attribute to skip reporting
-        if (!empty($record['context']['nobreadcrumb'])) {
-            return;
-        }
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function write(array $record)
+	{
+		// sentry uses the 'nobreadcrumb' attribute to skip reporting
+		if (!empty($record['context']['nobreadcrumb'])) {
+			return;
+		}
 
-        if (isset($record['context']['exception']) && $record['context']['exception'] instanceof \Exception) {
-            $exc = $record['context']['exception'];
-            $crumb = array(
-                'type' => 'error',
-                'level' => $this->logLevels[$record['level']],
-                'category' => $record['channel'],
-                'data' => array(
-                    'type' => get_class($exc),
-                    'value' => $exc->getMessage(),
-                ),
-            );
-        } else {
-            // TODO(dcramer): parse exceptions out of messages and format as above
-            if ($error = $this->parseException($record['message'])) {
-                $crumb = array(
-                    'type' => 'error',
-                    'level' => $this->logLevels[$record['level']],
-                    'category' => $record['channel'],
-                    'data' => array(
-                        'type' => $error[0],
-                        'value' => $error[1],
-                    ),
-                );
-            } else {
-                $crumb = array(
-                    'level' => $this->logLevels[$record['level']],
-                    'category' => $record['channel'],
-                    'message' => $record['message'],
-                );
-            }
-        }
+		if (isset($record['context']['exception']) && $record['context']['exception'] instanceof \Exception) {
+			$exc = $record['context']['exception'];
+			$crumb = array(
+				'type' => 'error',
+				'level' => $this->logLevels[$record['level']],
+				'category' => $record['channel'],
+				'data' => array(
+					'type' => get_class($exc),
+					'value' => $exc->getMessage(),
+				),
+			);
+		} else {
+			// TODO(dcramer): parse exceptions out of messages and format as above
+			if ($error = $this->parseException($record['message'])) {
+				$crumb = array(
+					'type' => 'error',
+					'level' => $this->logLevels[$record['level']],
+					'category' => $record['channel'],
+					'data' => array(
+						'type' => $error[0],
+						'value' => $error[1],
+					),
+				);
+			} else {
+				$crumb = array(
+					'level' => $this->logLevels[$record['level']],
+					'category' => $record['channel'],
+					'message' => $record['message'],
+				);
+			}
+		}
 
-        $this->ravenClient->breadcrumbs->record($crumb);
-    }
+		$this->ravenClient->breadcrumbs->record($crumb);
+	}
 }
