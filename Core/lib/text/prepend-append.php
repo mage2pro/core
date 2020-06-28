6357b4227a93630a8d8fc89569c02a821d13354c
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-28 11:44:47 +0600

    no message

--- Core/lib/text/prepend-append.php
+++ Core/lib/text/prepend-append.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * 2016-03-08 It adds the $tail suffix to the $s string if the suffix is absent in $s.
+ * @used-by df_block()
+ * @used-by df_cc_path_t()
  * @param string $s
  * @param string $tail
  * @return string
