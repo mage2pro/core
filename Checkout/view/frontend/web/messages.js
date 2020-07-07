8acb6b9f0d96a0a117bdadc9e32792f3f861eb6a
Dmitry Fedyuk <dfediuk@gmail.com>
2020-07-07 14:34:09 +0600

    no message

--- Checkout/view/frontend/web/messages.js
+++ Checkout/view/frontend/web/messages.js
@@ -1,22 +1,18 @@
 // 2016-07-14
-define([
-	'jquery', 'Magento_Ui/js/model/messageList'
-], function($, ml) {return (
+define(['jquery', 'Magento_Ui/js/model/messageList'], function($, ml) {return (
 	/**
-	 * @param {Object} config
-	 * @param {Object[]} config.messages
+	 * @param {Object} cfg
+	 * @param {Object[]} cfg.messages
 	 * @returns void
 	 */
-	function(config) {
-		$.each(config.messages, function() {
-			/**
-			 * @type {Object} this
-			 * @type {String} this.text
-			 * @type {Boolean} this.success
-			 */
-			/** @type {Function} fn */
-			var fn = this.success ? ml.addSuccessMessage : ml.addErrorMessage;
-			fn.call(ml, {'message': this.text});
-		})
-	});
+	function(cfg) {$.each(cfg.messages, function() {
+		/**
+		 * @type {Object} this
+		 * @type {String} this.text
+		 * @type {Boolean} this.success
+		 */
+		/** @type {Function} fn */
+		var fn = this.success ? ml.addSuccessMessage : ml.addErrorMessage;
+		fn.call(ml, {'message': this.text});
+	});});
 });
