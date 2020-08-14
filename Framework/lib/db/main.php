1eab65a1b1c8dd30b2890498eafdc9a8dc34ca8f
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-14 15:47:26 +0600

    no message

--- Framework/lib/db/main.php
+++ Framework/lib/db/main.php
@@ -66,9 +66,8 @@ function df_db_name() {
 }
 
 /**
- * 2016-12-23
+ * 2016-12-23 http://stackoverflow.com/a/10414925
  * @used-by df_sentry_m()
- * http://stackoverflow.com/a/10414925
  * @see \Magento\Backup\Model\ResourceModel\Helper::getHeader()
  * https://github.com/magento/magento2/blob/2.1.3/app/code/Magento/Backup/Model/ResourceModel/Helper.php#L178
  * @return string
