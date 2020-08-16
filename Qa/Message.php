8df00318561e0904a400dd6152d90d341a3ccf3e
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-17 00:35:25 +0600

    no message

--- Qa/Message.php
+++ Qa/Message.php
@@ -3,14 +3,12 @@ namespace Df\Qa;
 /**
  * 2020-08-15 "Get rid of the `Df\Core\OLegacy` inheritance for `Df\Qa\Message`" https://github.com/mage2pro/core/issues/109
  * @see \Df\Qa\Message\Failure
- * @see \Df\Qa\Message\Notification
  */
 abstract class Message extends \Df\Core\O {
 	/**
 	 * @used-by report()
 	 * @see \Df\Qa\Message\Failure\Error::main()
 	 * @see \Df\Qa\Message\Failure\Exception::main()
-	 * @see \Df\Qa\Message\Notification::main()
 	 * @return string
 	 */
 	abstract protected function main();
