86dfb1f727aa9ffed0eb0ca3f76185eecb67de8a
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-17 00:39:34 +0600

    no message

--- Qa/Message/Failure/Error.php
+++ Qa/Message/Failure/Error.php
@@ -99,26 +99,22 @@ final class Error extends \Df\Qa\Message\Failure {
 		static $r; /** @var array(int => string) $r */
 		if (!$r) {
 			$r = [
-				E_ERROR => 'E_ERROR'
-				,E_WARNING => 'E_WARNING'
-				,E_PARSE => 'E_PARSE'
-				,E_NOTICE => 'E_NOTICE'
+				E_COMPILE_ERROR => 'E_COMPILE_ERROR'
+				,E_COMPILE_WARNING => 'E_COMPILE_WARNING'
 				,E_CORE_ERROR => 'E_CORE_ERROR'
 				,E_CORE_WARNING => 'E_CORE_WARNING'
-				,E_COMPILE_ERROR => 'E_COMPILE_ERROR'
-				,E_COMPILE_WARNING => 'E_COMPILE_WARNING'
-				,E_USER_ERROR => 'E_USER_ERROR'
-				,E_USER_WARNING => 'E_USER_WARNING'
-				,E_USER_NOTICE => 'E_USER_NOTICE'
-				,E_STRICT => 'E_STRICT'
+				,E_DEPRECATED => 'E_DEPRECATED'
+				,E_ERROR => 'E_ERROR'
+				,E_NOTICE => 'E_NOTICE'
+				,E_PARSE => 'E_PARSE'
 				,E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR'
+				,E_STRICT => 'E_STRICT'
+				,E_USER_DEPRECATED => 'E_USER_DEPRECATED'
+				,E_USER_ERROR => 'E_USER_ERROR'
+				,E_USER_NOTICE => 'E_USER_NOTICE'
+				,E_USER_WARNING => 'E_USER_WARNING'
+				,E_WARNING => 'E_WARNING'
 			];
-			if (defined('E_DEPRECATED')) {
-				$r[E_DEPRECATED] = 'E_DEPRECATED';
-			}
-			if (defined('E_USER_DEPRECATED')) {
-				$r[E_USER_DEPRECATED] = 'E_USER_DEPRECATED';
-			}
 		}
 		return $r;
 	}
