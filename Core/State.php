f9226ffbb3facab507f68a16e5d84e2937d99ad1
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-18 21:03:53 +0600

    https://github.com/mage2pro/core/issues/124

--- Core/State.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Df\Core;
-// 2015-08-13
-final class State {
-	/** @return self */
-	static function s() {static $r; return $r ? $r : $r = new self;}
-}
--- Core/lib/array/first.php
+++ Core/lib/array/first.php
@@ -79,8 +79,6 @@ function df_head(array $a) {return array_slice($a, 0, -1);}
  * @used-by ikf_eti()
  * @used-by \Df\Config\Backend::value()
  * @used-by \Df\Core\OLegacy::_prop()
- * @used-by \Df\Core\State::block()
- * @used-by \Df\Core\State::component()
  * @used-by \Df\Core\Text\Regex::match()
  * @used-by \Df\Customer\Settings\BillingAddress::disabled()
  * @used-by \Df\Framework\Form\Element::uidSt()
