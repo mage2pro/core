80eb7313825ff0dcf9a14a9c0886e8f9ed6c23f8
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-18 20:57:09 +0600

    https://github.com/mage2pro/core/issues/124

--- Core/State.php
+++ Core/State.php
@@ -1,7 +1,5 @@
 <?php
 namespace Df\Core;
-use Magento\Eav\Model\Entity\Attribute\AbstractAttribute;
-use Magento\Framework\View\Element\BlockInterface;
 // 2015-08-13
 final class State {
 	/**
@@ -17,38 +15,6 @@ final class State {
 	function controllerSet(\Magento\Framework\App\Action\Action $controller) {
 		$this->_controller = $controller;
 	}
-
-	/**
-	 * 2015-09-20
-	 * @used-by State::attributeSet()
-	 * @used-by State::attributeUnset()
-	 * @var AbstractAttribute
-	 */
-	private $_attribute;
-
-	/**
-	 * 2015-08-13
-	 * @used-by State::block()
-	 * @used-by State::blockSet()
-	 * @used-by State::blockSetPrev()
-	 * @var array(BlockInterface|null)
-	 */
-	private $_blockStack = [];
-
-	/** @var bool */
-	private $_blocksGenerationStarted = false;
-
-	/** @var bool */
-	private $_blocksHasBeenGenerated = false;
-
-	/**
-	 * 2015-09-19
-	 * @used-by State::component()
-	 * @used-by State::componentSet()
-	 * @used-by State::componentSetPrev()
-	 * @var array(UiComponentInterface|AbstractComponent|null)
-	 */
-	private $_componentStack = [];
 
 	/**
 	 * 2015-09-02
