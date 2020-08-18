d0ca85e7e36ff50be381d24d590da94ff1c43fcd
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-18 21:18:19 +0600

    https://github.com/mage2pro/core/issues/135

--- Core/lib/domain.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Df\Core\GlobalSingletonDestructor as D;
-use Df\Core\O;
-
-/**
- * @used-by \Df\Core\OLegacy::_construct()
- * @param O $o
- */
-function df_destructable_sg(O $o) {D::s()->register($o);}
