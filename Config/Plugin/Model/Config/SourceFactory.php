7ba99697f24023b94694e1d8e2dbba8ba8ef67ec
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-22 12:34:06 +0600

    no message

--- Config/Plugin/Model/Config/SourceFactory.php
+++ Config/Plugin/Model/Config/SourceFactory.php
@@ -5,7 +5,7 @@ use Magento\Config\Model\Config\SourceFactory as Sb;
 final class SourceFactory {
 	/**
 	 * 2015-11-14
-	 * The Magento core treats `<source_model>` classes as singletons: @see \Magento\Config\Model\Config\SourceFactory::create()
+	 * Magento treats `<source_model>` classes as singletons: @see \Magento\Config\Model\Config\SourceFactory::create()
 	 * https://github.com/magento/magento2/blob/2.0.0/app/code/Magento/Config/Model/Config/SourceFactory.php#L33
 	 * The puprose of my plugin to create independent instances of my `<source_model>` classes
 	 * for each `<source_model>` occurence.
