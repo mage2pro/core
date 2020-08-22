74c2208b656fe7aef8973faec116d1c7b20ca1ec
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-22 10:56:45 +0600

    no message

--- Config/Source.php
+++ Config/Source.php
@@ -170,9 +170,7 @@ abstract class Source extends SourceBase {
 	 * @param array(string => string) $a
 	 * @return array(string => string)
 	 */
-	final protected static function addKeysToValues(array $a) {return df_map_k(
-		$a, function($k, $v) {return "$v: $k";}
-	);}
+	final protected static function addKeysToValues(array $a) {return df_map_k($a, function($k, $v) {return "$v: $k";});}
 
 	/**
 	 * 2017-03-28
