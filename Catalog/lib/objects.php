55910ce71864c5eec12c683f1594b815a4507836
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-24 23:29:23 +0600

    no message

--- Catalog/lib/objects.php
+++ Catalog/lib/objects.php
@@ -2,10 +2,8 @@
 use Df\Core\Exception as DFE;
 use Magento\Catalog\Model\Locator\LocatorInterface as ILocator;
 use Magento\Catalog\Model\Locator\RegistryLocator;
-
 /**
- * 2016-02-25
- * https://github.com/magento/magento2/blob/e0ed4bad/app/code/Magento/Catalog/etc/adminhtml/di.xml#L10-L10
+ * 2016-02-25 https://github.com/magento/magento2/blob/e0ed4bad/app/code/Magento/Catalog/etc/adminhtml/di.xml#L10-L10
  * @used-by df_product_current()
  * @return ILocator|RegistryLocator
  * @throws DFE
