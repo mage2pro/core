7dedb80f9d97003d922543e16992d4de28cc65aa
Dmitry Fedyuk <dfediuk@gmail.com>
2020-11-23 07:57:37 +0600

    no message

--- Catalog/lib/product/collection.php
+++ Catalog/lib/product/collection.php
@@ -12,6 +12,16 @@ use Magento\Catalog\Model\ResourceModel\Product\Collection as C;
 function df_pc() {return df_new_om(C::class);}
 
 /**
+ * 2020-11-23
+ * 1) "Add an ability to preserve disabled products in a collection
+ * despite of the `cataloginventory/options/show_out_of_stock` option's value": https://github.com/mage2pro/core/issues/148
+ * 2) Currently, it is unused here, but used in Justuno.
+ * @param C $c
+ * @return C
+ */
+function df_pc_preserve_disabled(C $c) {return $c->setFlag('mage2pro_preserve_disabled');}
+
+/**
  * 2019-09-18
  * 2020-11-23 @deprecated
  * @used-by \BlushMe\Checkout\Block\Extra::items()
