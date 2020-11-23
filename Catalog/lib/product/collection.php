b95624dd8d6c110765ea2511a6de421d74fcb1b4
Dmitry Fedyuk <dfediuk@gmail.com>
2020-11-23 07:21:30 +0600

    7.0.9

--- Catalog/lib/product/collection.php
+++ Catalog/lib/product/collection.php
@@ -1,10 +1,20 @@
 <?php
 use Magento\Catalog\Model\ResourceModel\Product\Collection as C;
+
 /**
- * 2019-09-18 
- * @used-by \BlushMe\Checkout\Block\Extra::items()
- * @used-by \Justuno\M2\Controller\Response\Catalog::execute()
- * @used-by \Justuno\M2\Controller\Response\Inventory::execute()
+ * 2019-09-18
+ * @used-by vendor/tradefurniturecompany/report/view/frontend/templates/index.phtml:16
+ * used-by df_product_c()
+ * @used-by \TFC\Image\Command\C3::pc()
+ * @used-by \TFC\Image\Command\C3::pcL()
  * @return C
  */
-function df_product_c() {return df_new_om(C::class);}
+function df_pc() {return df_new_om(C::class);}
+
+/**
+ * 2019-09-18
+ * 2020-11-23 @deprecated
+ * @used-by \BlushMe\Checkout\Block\Extra::items()
+ * @return C
+ */
+function df_product_c() {return df_pc();}
