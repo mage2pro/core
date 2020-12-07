0f2c204171fc727d3d827d6a6c5a3fe5255e8639
Dmitry Fedyuk <dfediuk@gmail.com>
2020-12-07 13:16:21 +0600

    7.1.2: https://github.com/mage2pro/core/issues/98

--- Ui/view/frontend/requirejs-config.js
+++ Ui/view/frontend/requirejs-config.js
@@ -3,5 +3,5 @@ var config = {config: {mixins: {
 	// 2020-04-30
 	// "Prevent error messages from being hidden in 5 seconds from the frontend checkout screen":
 	// https://github.com/mage2pro/core/issues/98
-	'Magento_UI/js/view/messages': {'Df_Ui/view/messages': true}
+	'Magento_Ui/js/view/messages': {'Df_Ui/view/messages': true}
 }}};
