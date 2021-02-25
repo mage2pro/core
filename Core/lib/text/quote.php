8fdd52c91eb259c9fc5980aaecccb426f3ee6b7b
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-25 06:36:01 +0600

    no message

--- Core/lib/text/quote.php
+++ Core/lib/text/quote.php
@@ -3,20 +3,20 @@ use Df\Core\Helper\Text as T;
 use Magento\Framework\Phrase as P;
 /**
  * 2015-11-22
- * @param string|string[]|P|P[] $text
+ * @param string|string[]|P|P[] $s
  * @return string|string[]
  */
-function df_quote_double($text) {return df_t()->quote($text, T::QUOTE__DOUBLE);}
+function df_quote_double($s) {return df_t()->quote($s, T::QUOTE__DOUBLE);}
 
 /**
- * @param string|string[]|P|P[] $text
+ * @param string|string[]|P|P[] $s
  * @return string|string[]
  */
-function df_quote_russian($text) {return df_t()->quote($text, T::QUOTE__RUSSIAN);}
+function df_quote_russian($s) {return df_t()->quote($s, T::QUOTE__RUSSIAN);}
 
 /**
  * @used-by df_ejs()
- * @param string|string[]|P|P[] $text
+ * @param string|string[]|P|P[] $s
  * @return string|string[]
  */
-function df_quote_single($text) {return df_t()->quote($text, T::QUOTE__SINGLE);}
+function df_quote_single($s) {return df_t()->quote($s, T::QUOTE__SINGLE);}
