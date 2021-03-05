4e27efd17c9916b1c2aee9f2e27d8162119f34ad
Dmitry Fedyuk <dfediuk@gmail.com>
2021-03-06 03:57:47 +0600

    no message

--- Qa/lib/exception.php
+++ Qa/lib/exception.php
@@ -2,7 +2,7 @@
 use Df\Core\Exception as DFE;
 use Exception as E;
 use Magento\Framework\Exception\LocalizedException as LE;
-use Magento\Framework\Phrase;
+use Magento\Framework\Phrase as P;
 /**
  * 2016-07-18
  * @used-by \Df\Qa\Message\Failure\Exception::trace()
@@ -32,8 +32,8 @@ function df_ef(E $e) {while ($e->getPrevious()) {$e = $e->getPrevious();} return
  * @used-by \Inkifi\Pwinty\Controller\Index\Index::execute()
  * @used-by \Mangoit\MediaclipHub\Controller\Index\GetPriceEndpoint::execute()
  * @used-by \Mangoit\MediaclipHub\Controller\Index\RenewMediaclipToken::execute()
- * @param E|string|Phrase $e
- * @return string|Phrase
+ * @param E|P|string $e
+ * @return P|string
  */
 function df_ets($e) {return df_adjust_paths_in_message(
 	!$e instanceof E ? $e : ($e instanceof DFE ? $e->message() : $e->getMessage())
@@ -72,7 +72,7 @@ function df_le(E $e) {return $e instanceof LE ? $e : new LE(__(df_ets($e)), $e);
  * @used-by \Df\Payment\W\Responder::defaultError()
  * @used-by \Dfe\AllPay\W\Responder::error()
  * @param E|string $e
- * @return string|Phrase
+ * @return P|string
  */
 function df_lets($e) {return !$e instanceof E ? __($e) : df_ets(df_le($e));}
 
@@ -80,10 +80,8 @@ function df_lets($e) {return !$e instanceof E ? __($e) : df_ets(df_le($e));}
  * 2016-03-17             
  * @used-by \Dfe\CheckoutCom\Controller\Index\Index::execute()
  * @used-by \Dfe\TwoCheckout\Controller\Index\Index::execute()
- * @param callable $function
+ * @param callable $f
  * @return mixed
  * @throws LE
  */
-function df_leh(callable $function) {/** @var mixed $r */
-	try {$r = $function();} catch (E $e) {throw df_le($e);} return $r;
-}
+function df_leh(callable $f) {/** @var mixed $r */try {$r = $f();} catch (E $e) {throw df_le($e);} return $r;}
