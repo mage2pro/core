32c287333050d0e3a65979ec31ad837cfd4a9a69
Dmitry Fedyuk <dfediuk@gmail.com>
2021-03-26 18:42:24 +0600

    https://github.com/canadasatellite-ca/site/issues/46

--- Catalog/lib/product/image.php
+++ Catalog/lib/product/image.php
@@ -34,6 +34,7 @@ function df_product_image_path(P $p, $type = null, $attrs = []) {return df_media
 /**
  * 2019-08-21
  * @used-by \Dfe\Color\Observer\ProductSaveBefore::execute()
+ * @used-by \MageWorx\OptionFeatures\Helper\Data::getThumbImageUrl(canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/46)
  * @used-by \TFC\Core\Plugin\Catalog\Block\Product\View\GalleryOptions::afterGetOptionsJson()
  * @see df_media_path_absolute()
  * @param string $rel
