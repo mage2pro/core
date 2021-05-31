0c185bc196841ec12a48aca878e167b6a2681d69
Dmitry Fedyuk <dfediuk@gmail.com>
2021-05-31 13:25:52 +0600

    no message

--- Qa/lib/validation/const.php
+++ Qa/lib/validation/const.php
@@ -2,13 +2,3 @@
 if (!defined ('PHP_INT_MIN')) {
 	define('PHP_INT_MIN', ~PHP_INT_MAX);
 }
-/**
- * 2016-09-02
- * К сожалению, конструкции типа
- * const DF_F_TRIM = \Df\Zf\Filter\StringT\Trim::class;
- * приводят к сбою дибильного компилятора Magento 2:
- * https://github.com/magento/magento2/issues/6179
- * https://code.dmitry-fedyuk.com/m2e/allpay/issues/25
- */
-const DF_F_TRIM = '\Df\Zf\Filter\StringT\Trim';
-const DF_V_STRING = '\Df\Zf\Validate\StringT';
