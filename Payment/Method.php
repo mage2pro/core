2f942673fd5db217b31ac5d09f8a7725c974937f
Dmitry Fedyuk <dfediuk@gmail.com>
2021-07-02 05:37:52 +0600

    no message

--- Payment/Method.php
+++ Payment/Method.php
@@ -1581,14 +1581,21 @@ abstract class Method implements ICached, INonInterceptable, MethodInterface {
 	 *	}
 	 * Пока решил с этим ничего не делать, потому что сталкиваюсь с этим впервые, и причины пока неясны.
 	 * 2017-04-13 Я у себя этот сбой воспроизвести не смог.
-	 *
+	 * 2021-07-02
+	 * $a is a string because it is a result of the @see \Magento\Sales\Model\Order\Payment::formatAmount() call:
+	 * 		$baseAmountToRefund = $this->formatAmount($creditmemo->getBaseGrandTotal());
+	 * https://github.com/magento/magento2/blob/2.3.5-p2/app/code/Magento/Sales/Model/Order/Payment.php#L655
 	 * @override
 	 * @see \Magento\Payment\Model\MethodInterface::refund()
 	 * https://github.com/magento/magento2/blob/6ce74b2/app/code/Magento/Payment/Model/MethodInterface.php#L269-L277
 	 * @see \Magento\Payment\Model\Method\AbstractMethod::refund()
 	 * https://github.com/magento/magento2/blob/6ce74b2/app/code/Magento/Payment/Model/Method/AbstractMethod.php#L640-L656
+	 * @used-by \Magento\Sales\Model\Order\Payment::refund()
+	 * 		$gateway->refund($this, $baseAmountToRefund);
+	 * https://github.com/magento/magento2/blob/2.3.5-p2/app/code/Magento/Sales/Model/Order/Payment.php#L684
+	 * https://github.com/magento/magento2/blob/2.3.5-p2/app/code/Magento/Sales/Model/Order/Payment.php#L701
 	 * @param II|I|OP $payment
-	 * @param float $a
+	 * @param string|float $a
 	 * @return $this
 	 */
 	final function refund(II $payment, $a) {
