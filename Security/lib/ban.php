fef980320670c1eca3dbc542cc01484189adf209
Dmitry Fedyuk <dfediuk@gmail.com>
2021-09-17 12:34:14 +0600

    7.7.3: https://github.com/mage2pro/core/issues/159

--- Security/lib/ban.php
+++ Security/lib/ban.php
@@ -3,6 +3,7 @@ use Df\Security\BlackList as B;
 /**
  * 2021-09-14
  * "Implement an ability to temporary ban visitors with a particular IP address": https://github.com/mage2pro/core/issues/159
+ * @used-by \Df\Framework\Plugin\App\Http::aroundLaunch()
  * @param string|null $ip [optional]
  */
 function df_ban($ip = null) {
