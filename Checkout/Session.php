b4449ea41669a9ef916b5aa5a52ac8ec09bbba92
Dmitry Fedyuk <dfediuk@gmail.com>
2021-10-26 23:22:48 +0600

    https://github.com/mage2pro/core/issues/163

--- Checkout/Session.php
+++ Checkout/Session.php
@@ -22,6 +22,13 @@ final class Session extends \Df\Core\Session {
 
 	/**
 	 * 2021-10-22
+	 * 2021-10-26
+	 *	<virtualType name="Magento\Checkout\Model\Session\Storage" type="Magento\Framework\Session\Storage">
+	 *		<arguments>
+	 *			<argument name="namespace" xsi:type="string">checkout</argument>
+	 *		</arguments>
+	 *	</virtualType>
+	 * https://github.com/magento/magento2/blob/2.4.3-p1/app/code/Magento/Checkout/etc/di.xml#L9-L13
 	 * @override
 	 * @see \Df\Core\Session::c()
 	 * @used-by \Df\Core\Session::__construct()
