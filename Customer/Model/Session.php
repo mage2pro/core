c1b7216524c60a919efd15b63cd938ef9b5356c8
Dmitry Fedyuk <dfediuk@gmail.com>
2021-10-28 21:22:35 +0600

    no message

--- Customer/Model/Session.php
+++ Customer/Model/Session.php
@@ -8,8 +8,5 @@ namespace Df\Customer\Model;
  * 2016-06-04
  * @method int|null getLastCustomerId(bool $clear = false)
  * @method Session setLastCustomerId($v)
- *
- * @method bool|null getDfeFrugueRedirectStarted(bool $clear = false)
- * @method void setDfeFrugueRedirectStarted(bool $v)
  */
 class Session extends \Magento\Customer\Model\Session {}
