2547ccb83b27fe72b7d8cec123a51e81096f7740
Dmitry Fedyuk <dfediuk@gmail.com>
2021-11-08 04:55:02 +0600

    no message

--- Ui/view/base/web/validator.js
+++ Ui/view/base/web/validator.js
@@ -2,8 +2,7 @@
 define(['df', 'jquery', 'mage/validation'], function(df, $) {return {
 	/**
 	 * 2017-07-09
-	 * «How to implement a custom JavaScript validator for a backend configuration field?»
-	 * https://mage2.pro/t/4154
+	 * «How to implement a custom JavaScript validator for a backend configuration field?»: https://mage2.pro/t/4154
 	 * @used-by https://github.com/mage2pro/core/blob/5.3.2/Customer/view/base/web/validator/cpf.js#L34
 	 * @used-by https://github.com/mage2pro/core/blob/5.3.2/Ui/view/base/web/validator/popular.js#L7
 	 * @used-by https://github.com/mage2pro/phone/blob/1.0.13/view/base/web/validator.js#L11
@@ -11,9 +10,7 @@ define(['df', 'jquery', 'mage/validation'], function(df, $) {return {
 	 * @param {Function} f
 	 * @param {String} s
 	 */
-	add: function(id, f, s) {$.validator.addMethod(
-		'df-validate-' + id, function(v, e) {return this.optional(e) || f(v);}, s
-	);}
+	add: function(id, f, s) {$.validator.addMethod('df-validate-' + id, function(v, e) {return this.optional(e) || f(v);}, s);}
 	/**
 	 * 2017-07-12
 	 * @used-by https://github.com/mage2pro/core/blob/5.3.2/Customer/view/base/web/validator/cpf.js#L34
