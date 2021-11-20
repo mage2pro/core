37315db10fd796f993788f7033ac8a2fb5f9f38e
Dmitry Fedyuk <dfediuk@gmail.com>
2021-11-20 14:09:11 +0600

    no message

--- Framework/lib/fs/dir.php
+++ Framework/lib/fs/dir.php
@@ -5,7 +5,7 @@ use Magento\Framework\App\Filesystem\DirectoryList as DL;
  * 2019-08-23
  * @used-by df_google_init_service_account()
  * @param string $p [optional]
- * @return DL
+ * @return string
  */
 function df_fs_etc($p = '') {return df_cc_path(df_fs_dl()->getPath(DL::CONFIG), df_trim_ds_left($p));}
 
