4b2c814d6e23ed2db2ade6d3205e9ea820417adf
Dmitry Fedyuk <dfediuk@gmail.com>
2021-11-22 21:30:35 +0600

    https://github.com/tradefurniturecompany/google-shopping/issues/1

--- API/lib/google.php
+++ API/lib/google.php
@@ -2,6 +2,7 @@
 /**
  * 2020-10-25
  * @used-by \Dfe\Color\Image::__construct()
+ * @used-by \TFC\GoogleShopping\Command\C1::p()
  * @used-by \TFC\Image\Command\C1::p()
  * @used-by \TFC\Image\Command\C2::p()
  * @used-by \TFC\Image\Command\C3::p()
