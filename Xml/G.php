d22525248266e6985f86d5b4b89b6870fa8410ae
Dmitry Fedyuk <dfediuk@gmail.com>
2021-12-13 05:41:26 +0600

    no message

--- Xml/G.php
+++ Xml/G.php
@@ -47,7 +47,7 @@ final class G extends \Df\Core\O {
 		 * http://3v4l.org/Wiia2#v500
 		 */
 		$header = $this[self::P__SKIP_HEADER] ? '' : df_xml_header(); /** @var string $header */
-		$x = df_xml_parse(df_cc_n($header, sprintf('<%s/>', $this[self::$P__TAG]))); /** @var X $x */
+		$x = df_xml_parse("$header\n<{$this[self::$P__TAG]}/>"); /** @var X $x */
 		$x->addAttributes($this[self::P__ATTRIBUTES]);
 		$x->importArray($this[self::$P__CONTENTS]);
 		# Убеждаемся, что asXML вернуло строку, а не false.
