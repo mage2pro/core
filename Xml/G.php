af7f6b2717d4cd3ecea8c2b9c5b6c653e4b18f23
Dmitry Fedyuk <dfediuk@gmail.com>
2021-12-13 06:32:58 +0600

    no message

--- Xml/G.php
+++ Xml/G.php
@@ -3,48 +3,46 @@
 namespace Df\Xml;
 final class G extends \Df\Core\O {
 	/**
-	 * 2016-08-31  
+	 * 1) Метод ядра Magento CE @see \Magento\Framework\Simplexml\Element::asNiceXml()
+	 * не сохраняет в документе XML блоки CDATA,
+	 * а вместо этого заменяет недопустимые для XML символы их допустимыми кодами, например: & => &amp;
+	 * 2) @see \Magento\Framework\Simplexml\Element::asNiceXml()
+	 * не добавляет к документу заголовок XML: его надо добавить вручную.
+	 *
+	 * 2015-02-27
+	 * Для конвертации объекта класса @see SimpleXMLElement в строку
+	 * надо использовать именно метод @uses SimpleXMLElement::asXML(),
+	 * а не @see SimpleXMLElement::__toString() или оператор (string)$this.
+	 *
+	 * @see SimpleXMLElement::__toString() и (string)$this
+	 * возвращают непустую строку только для концевых узлов (листьев дерева XML).
+	 * Пример:
+	 *	<?xml version='1.0' encoding='utf-8'?>
+	 *		<menu>
+	 *			<product>
+	 *				<cms>
+	 *					<class>aaa</class>
+	 *					<weight>1</weight>
+	 *				</cms>
+	 *				<test>
+	 *					<class>bbb</class>
+	 *					<weight>2</weight>
+	 *				</test>
+	 *			</product>
+	 *		</menu>
+	 * Здесь для $e1 = $xml->{'product'}->{'cms'}->{'class'}
+	 * мы можем использовать $e1->__toString() и (string)$e1.
+	 * http://3v4l.org/rAq3F
+	 * Однако для $e2 = $xml->{'product'}->{'cms'}
+	 * мы не можем использовать $e2->__toString() и (string)$e2,
+	 * потому что узел «cms» не является концевым узлом (листом дерева XML): http://3v4l.org/Pkj37
+	 * Более того, метод @see SimpleXMLElement::__toString() отсутствует в PHP версий 5.2.17 и ниже:
+	 * http://3v4l.org/Wiia2#v500
+	 * 2016-08-31
 	 * @used-by p()
 	 * @return string
 	 */
 	private function _p() {
-		/**
-		 * 1) Метод ядра Magento CE @see \Magento\Framework\Simplexml\Element::asNiceXml()
-		 * не сохраняет в документе XML блоки CDATA,
-		 * а вместо этого заменяет недопустимые для XML символы их допустимыми кодами, например: & => &amp;
-		 * 2) @see \Magento\Framework\Simplexml\Element::asNiceXml()
-		 * не добавляет к документу заголовок XML: его надо добавить вручную.
-		 *
-		 * 2015-02-27
-		 * Для конвертации объекта класса @see SimpleXMLElement в строку
-		 * надо использовать именно метод @uses SimpleXMLElement::asXML(),
-		 * а не @see SimpleXMLElement::__toString() или оператор (string)$this.
-		 *
-		 * @see SimpleXMLElement::__toString() и (string)$this
-		 * возвращают непустую строку только для концевых узлов (листьев дерева XML).
-		 * Пример:
-		 *	<?xml version='1.0' encoding='utf-8'?>
-		 *		<menu>
-		 *			<product>
-		 *				<cms>
-		 *					<class>aaa</class>
-		 *					<weight>1</weight>
-		 *				</cms>
-		 *				<test>
-		 *					<class>bbb</class>
-		 *					<weight>2</weight>
-		 *				</test>
-		 *			</product>
-		 *		</menu>
-		 * Здесь для $e1 = $xml->{'product'}->{'cms'}->{'class'}
-		 * мы можем использовать $e1->__toString() и (string)$e1.
-		 * http://3v4l.org/rAq3F
-		 * Однако для $e2 = $xml->{'product'}->{'cms'}
-		 * мы не можем использовать $e2->__toString() и (string)$e2,
-		 * потому что узел «cms» не является концевым узлом (листом дерева XML): http://3v4l.org/Pkj37
-		 * Более того, метод @see SimpleXMLElement::__toString() отсутствует в PHP версий 5.2.17 и ниже:
-		 * http://3v4l.org/Wiia2#v500
-		 */
 		$header = $this[self::P__SKIP_HEADER] ? '' : df_xml_header(); /** @var string $header */
 		$x = df_xml_parse("$header\n<{$this[self::$P__TAG]}/>"); /** @var X $x */
 		$x->addAttributes($this[self::P__ATTRIBUTES]);
