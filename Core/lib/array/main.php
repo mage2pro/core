faa9da9941e61b9dcc37c141f22a1db673741e3a
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-15 22:56:25 +0300

    no message

--- Core/lib/array/main.php
+++ Core/lib/array/main.php
@@ -223,6 +223,12 @@ function dfa_ids($c) {return df_map('df_id', $c);}
  * Возвращает повторяющиеся элементы исходного массива (не повторяя их). https://3v4l.org/YEf5r
  * В алгоритме пользуемся тем, что @uses array_unique() сохраняет ключи исходного массива.
  * 2020-01-29 dfa_repeated([1,2,2,2,2,3,3,3,4]) => [2,3]
+ * 2022-10-15
+ * 1) The example above correctly works in 7.2 ≥ PHP ≤ 8.2: https://3v4l.org/Ds194
+ * 2) If flags is @see SORT_STRING (it is by default),
+ * formerly array has been copied and non-unique elements have been removed (without packing the array afterwards),
+ * but now a new array is built by adding the unique elements. This can result in different numeric indexes.
+ * https://www.php.net/manual/function.array-unique.php#refsect1-function.array-unique-changelog
  * @used-by \Df\Config\Backend\ArrayT::processI()
  * @param array $a
  * @return array
