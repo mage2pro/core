6b60ab850cec2bd54d2e7cc92d037ac4c47afe17
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-19 06:43:33 +0300

    no message

--- AdobeStockImageAdminUi/Plugin/Model/Listing/DataProvider.php
+++ AdobeStockImageAdminUi/Plugin/Model/Listing/DataProvider.php
@@ -1,6 +1,11 @@
 <?php
 namespace Df\AdobeStockImageAdminUi\Plugin\Model\Listing;
 use Closure as F;
+# 2022-10-19
+# If Magento is installed via Composer, then the `Magento_AdobeStockImageAdminUi` module is installed automatically.
+# If Magento is installed via Git, then the `Magento_AdobeStockImageAdminUi` module can be installed via Composer:
+# 	composer2 require magento/adobe-stock-integration:*
+# https://github.com/magento/adobe-stock-integration/wiki/Installation
 use Magento\AdobeStockImageAdminUi\Model\Listing\DataProvider as Sb;
 # 2021-05-04
 # «Failed to retrieve Adobe Stock search files results» / «Api Key is required»:
