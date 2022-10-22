161785a89b82958400a9f8f28d8623da06a437c9
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-22 17:45:47 +0300

    https://github.com/mage2pro/core/issues/168

--- Catalog/lib/product/image/path.php
+++ Catalog/lib/product/image/path.php
@@ -9,9 +9,8 @@ use Magento\Framework\App\Filesystem\DirectoryList as DL;
  * @param P $p
  * @param string|null $type [optional]
  * @param array(string => string) $attrs [optional]
- * @return string
  */
-function df_product_image_path(P $p, $type = null, $attrs = []) {return df_media_url2path(df_product_image_url(
+function df_product_image_path(P $p, $type = null, $attrs = []):string {return df_media_url2path(df_product_image_url(
 	$p, $type, $attrs
 ));}
 
@@ -22,18 +21,16 @@ function df_product_image_path(P $p, $type = null, $attrs = []) {return df_media
  * @used-by \TFC\Core\Plugin\Catalog\Block\Product\View\GalleryOptions::afterGetOptionsJson()
  * @see df_media_path_absolute()
  * @param string $rel
- * @return string
  */
-function df_product_image_path2abs($rel) {return df_cc_path(df_product_images_path(), df_trim_ds_left($rel));}
+function df_product_image_path2abs($rel):string {return df_cc_path(df_product_images_path(), df_trim_ds_left($rel));}
 
 /**
  * 2020-10-26
  * @used-by \TFC\Image\Command\C1::image()
  * @used-by \TFC\Image\Command\C1::p()
  * @param string $abs
- * @return string
  */
-function df_product_image_path2rel($abs) {return df_trim_text_left($abs, df_product_images_path() . '/');}
+function df_product_image_path2rel($abs):string {return df_trim_text_left($abs, df_product_images_path() . '/');}
 
 /**
  * 2020-10-26
@@ -44,24 +41,21 @@ function df_product_image_path2rel($abs) {return df_trim_text_left($abs, df_prod
  * @used-by \TFC\Image\Command\C1::image()
  * @used-by \TFC\Image\Command\C1::images()
  * @used-by \TFC\Image\Command\C3::p()
- * @return string
  */
-function df_product_images_path() {return df_path_absolute(DL::MEDIA, 'catalog/product');}
+function df_product_images_path():string {return df_path_absolute(DL::MEDIA, 'catalog/product');}
 
 /**
  * 2020-11-22 «pub/media/catalog/product»
  * @used-by \TFC\Image\Command\C3::p()
- * @return string
  */
-function df_product_images_path_rel() {return dfcf(function() {return df_path_relative(df_product_images_path());});}
+function df_product_images_path_rel():string {return dfcf(function() {return df_path_relative(df_product_images_path());});}
 
 /**
  * 2019-08-23
  * @used-by \Dfe\Color\Observer\ProductSaveBefore::execute()
  * @see df_media_path_absolute()
  * @param string $rel
- * @return string
  */
-function df_product_image_tmp_path2abs($rel) {return df_path_absolute(
+function df_product_image_tmp_path2abs($rel):string {return df_path_absolute(
 	DL::MEDIA, 'tmp/catalog/product/' . df_trim_ds_left($rel)
 );}
