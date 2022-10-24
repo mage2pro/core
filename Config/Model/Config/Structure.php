2c02ebf6f759b983e4eea036448b8097f583f77c
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-24 04:12:17 +0300

    no message

--- Config/Model/Config/Structure.php
+++ Config/Model/Config/Structure.php
@@ -3,13 +3,13 @@ namespace Df\Config\Model\Config;
 class Structure extends \Magento\Config\Model\Config\Structure {
 	/**
 	 * 2016-08-02
+	 * @used-by df_config_tab_label()
 	 * @param string $tabName
 	 * @param string $tabProperty
 	 * @return string|null
 	 */
 	static function tab($tabName, $tabProperty) {
-		/** @var Structure $s */
-		$s = df_config_structure();
+		$s = df_config_structure(); /** @var Structure $s */
 		if (!isset($s->_data['tabs'])) {
 			$s->getTabs();
 		}
