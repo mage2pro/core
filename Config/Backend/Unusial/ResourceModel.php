37e947c8c4bb232b0d917c133ea59f8cfaab4cba
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-24 03:54:51 +0300

    https://github.com/mage2pro/core/issues/168

--- Config/Backend/Unusial/ResourceModel.php
+++ Config/Backend/Unusial/ResourceModel.php
@@ -1,5 +1,6 @@
 <?php
 namespace Df\Config\Backend\Unusial;
+use Magento\Framework\DB\Adapter\AdapterInterface as IAdapter;
 use Magento\Framework\Model\ResourceModel\AbstractResource;
 /**
  * 2016-01-26
@@ -9,18 +10,20 @@ class ResourceModel extends AbstractResource {
 	/**
 	 * 2016-01-28
 	 * @override
-	 * @see \Magento\Framework\Model\ResourceModel\AbstractResource::getConnection()
-	 * @return \Magento\Framework\DB\Adapter\AdapterInterface
+	 * @see AbstractResource::getConnection()
 	 */
-	function getConnection() {return df_conn();}
+	function getConnection():IAdapter {return df_conn();}
 	
 	/**
 	 * 2016-01-28
 	 * @override
-	 * @see \Magento\Framework\Model\ResourceModel\AbstractResource::_construct()
+	 * @see AbstractResource::_construct()
 	 */
 	protected function _construct() {}
 
-	/** @return self */
-	static function s() {static $r; return $r ? $r : $r = new self;}
+	/**
+	 * 2016-01-26
+	 * @used-by Model::getResource()
+	 */
+	static function s():self {static $r; return $r ? $r : $r = new self;}
 }
