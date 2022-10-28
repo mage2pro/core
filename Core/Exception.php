0d4cf19381897ab13d5a407865fb68b22409081a
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-28 22:21:55 +0300

    https://github.com/mage2pro/core/issues/168

--- Core/Exception.php
+++ Core/Exception.php
@@ -5,8 +5,8 @@ use Magento\Framework\Exception\LocalizedException as LE;
 use Magento\Framework\Phrase;
 /**
  * 2016-07-31
- * Унаследовал наш класс от @see \Magento\Framework\Exception\LocalizedException вместо @see \Exception,
- * потому что во многих местах ядро анализирует тип исключительной ситуации,
+ * Унаследовал наш класс от @see \Magento\Framework\Exception\LocalizedException
+ * вместо @see E, потому что во многих местах ядро анализирует тип исключительной ситуации,
  * и по разному её обрабатывает, в зависимости от типа.
  * В частности, если исключительная ситуация имеет тип
  * @see \Magento\Framework\Exception\LocalizedException
@@ -99,19 +99,19 @@ class Exception extends LE implements \ArrayAccess {
 	final function markMessageAsHtml():self {$this->_messageIsHtml = true; return $this;}
 
 	/**
-	 * Стандартный метод @see \Exception::getMessage() объявлен как final.
+	 * Стандартный метод @see E::getMessage() объявлен как final.
 	 * Чтобы метод для получения диагностического сообщения
 	 * можно было переопределять — добавляем свой.
 	 *
 	 * 2015-02-22
 	 * Конечно, наша архитектура обладает тем недостатком,
 	 * что пользователи нашего класса и его потомков должны для извлечения диагностического сообщения
-	 * вместо стандартного интерфейса @see \Exception::getMessage()
+	 * вместо стандартного интерфейса @see E::getMessage()
 	 * использовать функцию @see df_ets()
 	 *
 	 * Однако неочевидно, как обойти этот недостаток.
 	 * В частности, способ, когда диагностическое сообщение формируется прямо в конструкторе
-	 * и передается первым параметром родительскому конструктору @see \Exception::__construct()
+	 * и передается первым параметром родительскому конструктору @see E::__construct()
 	 * не всегда подходит, потому что полный текст диагностического сообщения
 	 * не всегда известен в момент вызова конструктора @see __construct().
 	 * Пример, когда неизвестен: @see \Df\Core\Exception_Batch::message()
@@ -227,19 +227,16 @@ class Exception extends LE implements \ArrayAccess {
 	 * то вызываем метод @see \Df\Core\Exception::standard().
 	 * Этот метод конвертирует исключительную ситуацию в стандартную,
 	 * и стандартная среда её успешно обработает.
-	 * @return \Exception
 	 */
-	function standard() {return dfc($this, function() {return new \Exception($this->message(), 0, $this);});}
+	function standard():E {return dfc($this, function() {return new E($this->message(), 0, $this);});}
 
 	/**
 	 * Цель этого метода — предоставить потомкам возможность
 	 * указывать тип предыдущей исключительной ситуации в комментарии PHPDoc для потомка.
-	 * Метод @uses \Exception::getPrevious() объявлен как final,
+	 * Метод @uses E::getPrevious() объявлен как final,
 	 * поэтому потомки не могут в комментариях PHPDoc указывать его тип: IntelliJ IDEA ругается.
-	 * 2016-08-19
-	 * @return E
 	 */
-	protected function prev() {return $this->getPrevious();}
+	protected function prev():E {return $this->getPrevious();}
 
 	/**
 	 * @used-by self::comments()
@@ -278,7 +275,7 @@ class Exception extends LE implements \ArrayAccess {
 	/**
 	 * @used-by df_error_create()
 	 * @used-by \Df\Qa\Failure\Exception::i()
-	 * @param \Exception $e
+	 * @param E $e
 	 */
 	final static function wrap(E $e):self {return $e instanceof self ? $e : new self($e);}
 }
