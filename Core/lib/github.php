bd55e74dd6b1281fb245afbd8d1f53183ed00998
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-29 21:24:48 +0300

    https://github.com/mage2pro/core/issues/168

--- Core/lib/github.php
+++ Core/lib/github.php
@@ -2,28 +2,24 @@
 use Zend_Http_Client as C;
 /**
  * 2017-05-10
- * @return string
+ * @used-by df_github_request()
  */
-function df_github_token() {return df_credentials('github');}
+function df_github_token():string {return df_credentials('github');}
 
 /**
- * 2017-05-10
- * https://developer.github.com/v3/repos/releases/#get-the-latest-release
- * @used-by df_github_request()
+ * 2017-05-10 https://developer.github.com/v3/repos/releases#get-the-latest-release
+ * @used-by \Dfe\Portal\Test\Basic::t02()
  * @param string $repo
- * @return string
  */
-function df_github_repo_version($repo) {return df_github_request("repos/$repo/releases/latest", 'tag_name');}
+function df_github_repo_version($repo):string {return df_github_request("repos/$repo/releases/latest", 'tag_name');}
 
 /**
  * 2017-05-10 https://developer.github.com/v3/repos/releases/#get-the-latest-release
  * @used-by df_github_repo_version()
  * @param string $path
  * @param string|null $k [optional]
- * @param array(string => mixed) $params [optional]
- * @return string
  */
-function df_github_request($path, $k = null, $params = []) {
+function df_github_request($path, $k = null, $params = []):string {
 	$c = df_zf_http("https://api.github.com/$path")
 		# 2017-06-28
 		# «Difference between the Accept and Content-Type HTTP headers»
