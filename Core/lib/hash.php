b7b265531553c373f5264c06a424a4bfd7348bb8
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-29 21:30:43 +0300

    https://github.com/mage2pro/core/issues/168

--- Core/lib/hash.php
+++ Core/lib/hash.php
@@ -16,12 +16,10 @@ use Magento\Framework\Model\AbstractModel as M;
  * @used-by dfcf()
  * @used-by \Df\API\Client::p()
  * @used-by \Df\Payment\Block\Info::getCacheKeyInfo()
- * @return string
  */
-function df_hash_a(array $a) {
+function df_hash_a(array $a):string {
 	$resultA = []; /** @var string[] $resultA */
-	foreach ($a as $k => $v) {
-		/** @var int|string $k */ /** @var mixed $v */
+	foreach ($a as $k => $v) {/** @var int|string $k */ /** @var mixed $v */
 		$resultA[]= "$k=>" . (is_object($v) ? df_hash_o($v) : (is_array($v) ? df_hash_a($v) : $v));
 	}
 	return implode('::', $resultA);
@@ -35,21 +33,13 @@ function df_hash_a(array $a) {
  * https://php.net/manual/function.spl-object-hash.php#76220
  * Но мы сознательно идём на этот небольшой риск :-)
  * Этот риск совсем мал, потому что для моделей мы не используем spl_object_hash(), а используем getId().
- * 2016-10-26
- * @see \Closure является объектом, и к ней можно применять @see spl_object_hash():
- * https://3v4l.org/Ok2k8
- * 2018-08-11
- * It would better to use a more robust hashing solution for closures:
- * https://stackoverflow.com/a/14620643
+ * 2016-10-26 @see \Closure является объектом, и к ней можно применять @see spl_object_hash(): https://3v4l.org/Ok2k8
+ * 2018-08-11 It would be better to use a more robust hashing solution for closures: https://stackoverflow.com/a/14620643
+ * @used-by df_hash_a()
  * @param object $o
- * @return string
  */
-function df_hash_o($o) {
-	/**
-	 * 2016-09-05
-	 * Для ускорения заменил вызов df_id($o, true) на инлайновыый код.
-	 * @see df_id()
-	 */
+function df_hash_o($o):string {
+	/** 2016-09-05 Для ускорения заменил вызов df_id($o, true) на инлайновыый код. @see df_id() */
 	/** @var string $r */
 	$r = $o instanceof M || method_exists($o, 'getId') ? $o->getId() : ($o instanceof AI ? $o->id() : null);
 	return $r ? get_class($o) . "::$r" : spl_object_hash($o);
