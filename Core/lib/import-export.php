ad03e85dbca57223f2f0fb764545945b1ba93909
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-29 21:36:55 +0300

    https://github.com/mage2pro/core/issues/168

--- Core/lib/import-export.php
+++ Core/lib/import-export.php
@@ -1,8 +1,7 @@
 <?php
-use Magento\ImportExport\Model\ResourceModel\Helper;
+use Magento\ImportExport\Model\ResourceModel\Helper as H;
 /**
  * 2016-01-11
- * @return Helper
+ * @used-by df_next_increment()
  */
-function df_ie_helper() {return df_o(Helper::class);}
-
+function df_ie_helper():H {return df_o(H::class);}
