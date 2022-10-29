3bf1171cfa46df9ffecccd158762a48166616f04
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-29 12:46:49 +0300

    no message

--- Core/Visitor.php
+++ Core/Visitor.php
@@ -3,18 +3,21 @@ namespace Df\Core;
 final class Visitor extends O {
 	/**
 	 * На английском языке. Например: «Moscow».
+	 * @used-by \Dfe\TwoCheckout\Address::city()
 	 * @return string|null
 	 */
 	function city() {return $this->r('city');}
 
 	/**
 	 * На английском языке. Например: «Russia».
+	 * 2022-10-29 @deprecated It is unused.
 	 * @return string
 	 */
 	function countryName() {return $this->r('country_name');}
 
 	/**
 	 * @used-by \Df\Payment\Settings\_3DS::countries()
+	 * @used-by \Dfe\TwoCheckout\Address::countryIso3()
 	 * @used-by \Frugue\Shipping\Header::_toHtml()
 	 * @used-by \Frugue\Store\Plugin\Framework\App\FrontControllerInterface::aroundDispatch()
 	 * @return string|null
@@ -23,41 +26,56 @@ final class Visitor extends O {
 
 	/**
 	 * Например: «55.752».
+	 * 2022-10-29 @deprecated It is unused.
 	 * @return string|null
 	 */
 	function latitude() {return $this->r('latitude');}
 
 	/**
 	 * Например: «37.616».
+	 * 2022-10-29 @deprecated It is unused.
 	 * @return string|null
 	 */
 	function longitude() {return $this->r('longitude');}
 
 	/**
 	 * Например: «101194».
+	 * @used-by \Dfe\TwoCheckout\Address::postcode()
 	 * @return string|null
 	 */
 	function postCode() {return $this->r('zip_code');}
 
 	/**
 	 * Например: «MOW».
+	 * 2022-10-29 @deprecated It is unused.
 	 * @return string|null
 	 */
 	function regionCode() {return $this->r('region_code');}
 
 	/**
 	 * На английском языке. Например: «Moscow».
+	 * @used-by \Dfe\TwoCheckout\Address::region()
 	 * @return string|null
 	 */
 	function regionName() {return $this->r('region_name');}
 
 	/**
 	 * Например: «Europe/Moscow».
+	 * 2022-10-29 @deprecated It is unused.
 	 * @return string|null
 	 */
 	function timeZone() {return $this->r('time_zone');}
 
 	/**
+	 * @used-by self::city()
+	 * @used-by self::countryName()
+	 * @used-by self::iso2()
+	 * @used-by self::latitude()
+	 * @used-by self::longitude()
+	 * @used-by self::postCode()
+	 * @used-by self::regionCode()
+	 * @used-by self::regionName()
+	 * @used-by self::timeZone()
 	 * @param string $key
 	 * @return string|null
 	 */
@@ -91,21 +109,28 @@ final class Visitor extends O {
 	 * It is enough for us here.
 	 * 2) @see https://github.com/mage2pro/core/blob/5.6.6/Checkout/view/frontend/web/data.js#L93-L98
 	 *
+	 * @used-by self::r()
 	 * @return array(string => mixed)
 	 */
 	private function responseA() {return dfc($this, function() {return df_try(function() {return
 		df_http_json('http://geoip.mage2.pro/json/' . $this[self::$P__IP], [], 5);
 	}, []);});}
 
-	/** @var string */
-	private static $P__IP = 'ip';
-
 	/**
 	 * 2016-05-20
+	 * @used-by df_visitor()
+	 * @used-by \Dfe\TwoCheckout\Address::visitor()
 	 * @param string|null $ip [optional]
 	 * @return self
 	 */
-	final static function sp($ip = null):self {return dfcf(function($ip = null) {return new self([
+	static function sp($ip = null):self {return dfcf(function($ip = null) {return new self([
 		self::$P__IP => $ip ?: df_visitor_ip()
 	]);}, [$ip]);}
+
+	/**
+	 * @used-by self::responseA()
+	 * @used-by self::sp()
+	 * @var string
+	 */
+	private static $P__IP = 'ip';
 }
