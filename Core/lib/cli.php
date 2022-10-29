4b17886e14c4f5cd51d323329257d58f8c3046fe
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-29 13:22:38 +0300

    https://github.com/mage2pro/core/issues/168

--- Core/lib/cli.php
+++ Core/lib/cli.php
@@ -12,33 +12,29 @@ function df_cli_argv($i = null) {return dfa(dfa($_SERVER, 'argv', []), $i);}
 /**
  * 2020-05-24
  * @used-by df_context()
- * @return string
  */
-function df_cli_cmd() {return df_cc_s(df_cli_argv());}
+function df_cli_cmd():string {return df_cc_s(df_cli_argv());}
 
 /**
  * 2020-02-15
  * @used-by df_is_bin_magento()
- * @return string
  */
-function df_cli_script() {return df_cli_argv(0);}
+function df_cli_script():string {return df_cli_argv(0);}
 
 /**
  * 2016-12-23 http://stackoverflow.com/a/7771601
  * @see \Magento\Framework\Shell::execute()
  * @used-by df_sentry_m()
- * @return string
  */
-function df_cli_user() {return dfcf(function() {return exec('whoami');});}
+function df_cli_user():string {return dfcf(function() {return exec('whoami');});}
 
 /**
  * 2020-02-15
  * 1) `bin/magento` can be called with a path prefix, so I use @uses df_ends_with()
  * 2) df_cli_script() returns «bin/magento» even in the `php bin/magento ...` case.
  * @used-by df_is_cron()
- * @return bool
  */
-function df_is_bin_magento() {return df_ends_with(df_cli_script(), 'bin/magento');}
+function df_is_bin_magento():bool {return df_ends_with(df_cli_script(), 'bin/magento');}
 
 /**
  * 2016-10-25 http://stackoverflow.com/a/1042533
@@ -49,6 +45,5 @@ function df_is_bin_magento() {return df_ends_with(df_cli_script(), 'bin/magento'
  * @used-by df_webserver()
  * @used-by \Df\Sentry\Client::__construct()
  * @used-by \Df\Sentry\Client::capture()
- * @return bool
  */
-function df_is_cli() {return 'cli' === php_sapi_name();}
+function df_is_cli():bool {return 'cli' === php_sapi_name();}
