b55919fa6d03f12ad20d50d42b62f01de3cdad71
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 00:14:20 +0100

    https://github.com/mage2pro/core/issues/168

--- Core/lib/cache/clean-types.php
+++ Core/lib/cache/clean-types.php
@@ -7,19 +7,19 @@ use \Magento\Framework\App\Cache\Type\Config as TConfig;
  * 2021-09-22
  * @used-by \Df\Directory\Plugin\Model\Currency::afterSaveRates()
  */
-function df_cache_clean_blocks() {df_cache_clean_type(TBlock::TYPE_IDENTIFIER);}
+function df_cache_clean_blocks():void {df_cache_clean_type(TBlock::TYPE_IDENTIFIER);}
 
 /**
  * 2021-09-22
  * @used-by df_cfg_save_cc()
  */
-function df_cache_clean_cfg() {df_cache_clean_type(TConfig::TYPE_IDENTIFIER);}
+function df_cache_clean_cfg():void {df_cache_clean_type(TConfig::TYPE_IDENTIFIER);}
 
 /**
  * 2021-09-22
  * @used-by \Df\Directory\Plugin\Model\Currency::afterSaveRates()
  */
-function df_cache_clean_pages() {df_cache_clean_type(TPage::TYPE_IDENTIFIER);}
+function df_cache_clean_pages():void {df_cache_clean_type(TPage::TYPE_IDENTIFIER);}
 
 /**
  * 2017-06-30 «How does `Flush Cache Storage` work?» https://mage2.pro/t/4118
@@ -30,4 +30,4 @@ function df_cache_clean_pages() {df_cache_clean_type(TPage::TYPE_IDENTIFIER);}
  * @used-by df_cache_clean_pages()
  * @param string $t
  */
-function df_cache_clean_type($t) {df_cache_type_list()->cleanType($t);}
+function df_cache_clean_type($t):void {df_cache_type_list()->cleanType($t);}
