494f274a3465e744dcdc86b8017c41f619226b7a
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 00:51:26 +0100

    https://github.com/mage2pro/core/issues/168

--- Core/lib/text/camel.php
+++ Core/lib/text/camel.php
@@ -14,7 +14,6 @@ function df_camel_to_underscore(...$args) {return df_call_a(function($s) {return
  * «YandexMarket» => array(«Yandex», «Market»)
  * «NewNASAModule» => array(«New», «NASA», «Module»)
  * http://stackoverflow.com/a/17122207
- *
  * 2016-08-24
  * http://php.net/manual/reference.pcre.pattern.modifiers.php
  * x (PCRE_EXTENDED)
@@ -29,36 +28,31 @@ function df_camel_to_underscore(...$args) {return df_call_a(function($s) {return
  * 		Note, however, that this applies only to data characters.
  * 		Whitespace characters may never appear within special character sequences in a pattern,
  * 		for example within the sequence (?( which introduces a conditional subpattern.
- *
  * 2017-07-09
  * Note 1: ?<=
  * «Zero-width positive lookbehind assertion.
  * Continues match only if the subexpression matches at this position on the left.
  * For example, (?<=19)99 matches instances of 99 that follow 19.
  * This construct does not backtrack.»
- *
  * Note 2: ?=
  * «Zero-width positive lookahead assertion.
  * Continues match only if the subexpression matches at this position on the right.
  * For example, \w+(?=\d) matches a word followed by a digit, without matching the digit.
  * This construct does not backtrack.»
- *
  * I have extracted this explanation from Rad Software Regular Expression Designer
  * (it is a discontinued software, google for it),
  * and it get it from the .NET Framework 3.0 documentation:
  * https://msdn.microsoft.com/en-us/library/bs2twtah(v=vs.85).aspx
- *
  * Note 3.
  * Today I have changed «?=[A-Z0-9]» => «?=[A-Z0-9]», so now it handles the cases with digits, e.g.:
  * «Dynamics365» => [«Dynamics», «365»]
- *
  * @used-by df_api_name()
  * @used-by df_camel_to_underscore()
  * @used-by df_explode_class_camel()
  * @param string|string[] ...$args
  * @return string[]|string[][]
  */
-function df_explode_camel(...$args) {return df_call_a(function($name) {return preg_split(
+function df_explode_camel(...$args):array {return df_call_a(function($name) {return preg_split(
 	'#(?<=[a-z])(?=[A-Z0-9])#x', $name
 );}, $args);}
 
