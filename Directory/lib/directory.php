47e524de454df15e3d9fd90a91fc48b30b887bfe
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 16:12:14 +0100

    https://github.com/mage2pro/core/issues/168

--- Directory/lib/directory.php
+++ Directory/lib/directory.php
@@ -7,16 +7,13 @@ use Magento\Directory\Helper\Data as H;
  * @used-by \KingPalm\B2B\Block\RegionJS\Backend::_toHtml()
  * @used-by \KingPalm\B2B\Block\RegionJS\Frontend::_toHtml()
  * @used-by vendor/mage2pro/customer/view/frontend/templates/address/edit.phtml
- * @return H
  */
-function df_directory() {return df_o(H::class);}
+function df_directory():H {return df_o(H::class);}
 
 /**
  * 2015-10-12
  * @used-by \Df\Sso\CustomerReturn::register()
  * @used-by \KingPalm\B2B\Setup\V140\MoveDataToAddress::p()
  * @param string $iso2
- * @return bool
  */
-function df_is_postcode_required($iso2) {return !df_directory()->isZipCodeOptional($iso2);}
-
+function df_is_postcode_required($iso2):bool {return !df_directory()->isZipCodeOptional($iso2);}
