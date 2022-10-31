a646603206a95bbbfe29ad0511d713a7faf251bd
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 16:37:11 +0100

    https://github.com/mage2pro/core/issues/168

--- Eav/lib/objects.php
+++ Eav/lib/objects.php
@@ -8,22 +8,19 @@ use Magento\Framework\EntityManager\MetadataPool;
  * @used-by df_eav_ca()
  * @used-by df_eav_customer()
  * @used-by \Df\Customer\AddAttribute\Address::p()
- * @return Config
  */
-function df_eav_config() {return df_o(Config::class);}
+function df_eav_config():Config {return df_o(Config::class);}
 
 /**
  * 2015-10-06
  * @used-by \Df\Customer\AddAttribute\Address::p()
  * @used-by \Df\Customer\AddAttribute\Customer::p()
  * @used-by \KingPalm\B2B\Setup\UpgradeSchema::_process()
- * @return EavSetup
  */
-function df_eav_setup() {return df_o(EavSetup::class);}
+function df_eav_setup():EavSetup {return df_o(EavSetup::class);}
 
 /**
  * 2021-03-26
  * @used-by \CanadaSatellite\Theme\Plugin\Model\LinkManagement::aroundSaveChild(canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/44)
- * @return MetadataPool
  */
-function df_metadata_pool() {return df_o(MetadataPool::class);}
+function df_metadata_pool():MetadataPool {return df_o(MetadataPool::class);}
