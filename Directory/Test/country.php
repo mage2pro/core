74d38e7969454975fd64122177b071e207c4fcb3
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 16:33:11 +0100

    https://github.com/mage2pro/core/issues/168

--- Directory/Test/country.php
+++ Directory/Test/country.php
@@ -3,8 +3,8 @@
 namespace Df\Directory\Test;
 class country extends \Df\Core\TestCase {
 	/** 2017-09-07 @test */
-	function t00() {}
+	function t00():void {}
 
 	/** 2017-09-07 */
-	function t01() {print_r(df_json_encode(df_country_codes_allowed()));}
+	function t01():void {print_r(df_json_encode(df_country_codes_allowed()));}
 }
