49c04822dfcfbf5ec04f91df73775356a23dbed9
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 16:36:23 +0100

    https://github.com/mage2pro/core/issues/168

--- Eav/lib/customer.php
+++ Eav/lib/customer.php
@@ -3,6 +3,6 @@ use Magento\Eav\Model\Entity\Type as EntityType;
 
 /**
  * 2015-10-12
- * @return EntityType
+ * @used-by df_customer_att()
  */
-function df_eav_customer() {return df_eav_config()->getEntityType('customer');}
+function df_eav_customer():EntityType {return df_eav_config()->getEntityType('customer');}
