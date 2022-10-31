b53c4c4c3273b7b06e6d1fead28bcef013ee13f5
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 16:01:13 +0100

    https://github.com/mage2pro/core/issues/168

--- Customer/lib/url.php
+++ Customer/lib/url.php
@@ -5,13 +5,6 @@ use Magento\Customer\Model\Url;
  * 2016-12-01                             
  * @used-by df_customer_url_h()
  * @used-by \Df\Sso\CustomerReturn::redirectUrl()
- * @return Url
- */
-function df_customer_url() {return df_o(Url::class);}
-
-/**                            
- * 2018-09-11    
  * @used-by \Mangoit\MediaclipHub\Controller\Index\RenewMediaclipToken::execute()
- * @return Url
  */
-function df_customer_url_h() {return df_customer_url();}
+function df_customer_url():Url {return df_o(Url::class);}
