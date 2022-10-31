95be5e4f7d6906222000f902e00d1312414e019b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 15:51:55 +0100

    https://github.com/mage2pro/core/issues/168

--- Customer/lib/group.php
+++ Customer/lib/group.php
@@ -9,10 +9,9 @@ use Magento\Framework\Exception\NoSuchEntityException as NSE;
  * 2020-02-06     
  * @used-by df_customer_group_name()
  * @param C|G|int $v
- * @return G
  * @throws NSE
  */
-function df_customer_group($v) {return $v instanceof G ? $v : df_customer_group_reg()->retrieve(
+function df_customer_group($v):G {return $v instanceof G ? $v : df_customer_group_reg()->retrieve(
 	$v instanceof C ? $v->getGroupId() : $v
 );}
 
@@ -21,16 +20,14 @@ function df_customer_group($v) {return $v instanceof G ? $v : df_customer_group_
  * @used-by \Dfe\Sift\Payload\LoginOrRegister::p()
  * @param C|G|int $v 
  * @param F|bool|mixed $onE [optional]
- * @return G
  * @throws NSE
  */
-function df_customer_group_name($v, $onE = null) {return df_try(function() use($v) {return 
+function df_customer_group_name($v, $onE = null):G {return df_try(function() use($v) {return
 	df_customer_group($v)->getCode()
 ;}, $onE);}
 
 /**
  * 2020-02-06
  * @used-by df_customer_group()
- * @return Registry
  */
-function df_customer_group_reg() {return df_o(Registry::class);}
+function df_customer_group_reg():Registry {return df_o(Registry::class);}
