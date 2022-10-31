51f43d4ba7f344fe6899248b73e320ba6a81470b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 16:33:22 +0100

    https://github.com/mage2pro/core/issues/168

--- Directory/Test/currency.php
+++ Directory/Test/currency.php
@@ -3,8 +3,8 @@
 namespace Df\Directory\Test;
 class currency extends \Df\Core\TestCase {
 	/** 2017-01-29 @test */
-	function t00() {}
+	function t00():void {}
 
 	/** 2017-01-29 */
-	function t01() {print_r(df_currency_by_country_c('FI'));}
+	function t01():void {print_r(df_currency_by_country_c('FI'));}
 }
