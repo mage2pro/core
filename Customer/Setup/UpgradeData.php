614c51d7aa07263b5cf55a3cbafc486254e983a5
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 16:06:12 +0100

    https://github.com/mage2pro/core/issues/168

--- Customer/Setup/UpgradeData.php
+++ Customer/Setup/UpgradeData.php
@@ -16,17 +16,14 @@ class UpgradeData extends \Df\Framework\Upgrade\Data {
 	 * @see \Df\Framework\Upgrade::_process()
 	 * @used-by \Df\Framework\Upgrade::process()
 	 */
-	final protected function _process() {
-		/**
-		 * 2016-08-22
-		 * Помимо добавления поля в таблицу «customer_entity» надо ещё добавить атрибут,
-		 * иначе данные не будут сохраняться:
-		 *		$attribute = $this->getAttribute($k);
-		 *		if (empty($attribute)) {
-		 *			continue;
-		 *		}
-		 * https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Eav/Model/Entity/AbstractEntity.php#L1262-L1265
-		 */
+	final protected function _process():void {
+		# 2016-08-22
+		# Помимо добавления поля в таблицу «customer_entity» надо ещё добавить атрибут, иначе данные не будут сохраняться:
+		#		$attribute = $this->getAttribute($k);
+		#		if (empty($attribute)) {
+		#			continue;
+		#		}
+		# https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Eav/Model/Entity/AbstractEntity.php#L1262-L1265
 		if ($this->v('1.7.4')) {
 			Add::text(UpgradeSchema::F__DF, 'Mage2.PRO', [], [
 				Add::VISIBLE_IN_BACKEND => false, Add::VISIBLE_ON_FRONTEND => false
