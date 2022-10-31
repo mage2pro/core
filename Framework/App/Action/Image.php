801864c09d4819783185c3bdcd2095b1c2a52856
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 16:49:17 +0100

    https://github.com/mage2pro/core/issues/168

--- Framework/App/Action/Image.php
+++ Framework/App/Action/Image.php
@@ -7,26 +7,26 @@ abstract class Image extends \Magento\Framework\App\Action\Action {
 	 * 2015-11-29 Содержимое файла картинки. Например, его можно получить посредством @see file_get_contents()
 	 * @see \Df\GoogleFont\Controller\Index\Preview::contents()
 	 * @used-by self::execute()
-	 * @return string
 	 */
-	abstract protected function contents();
+	abstract protected function contents():string;
+
 	/**
 	 * 2015-11-29 E.g.: 'png'
 	 * @see \Df\GoogleFont\Controller\Index\Preview::type()
 	 * @used-by self::execute()
-	 * @return string
 	 */
-	abstract protected function type();
+	abstract protected function type():string;
 
 	/**
 	 * 2015-11-29
+	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
 	 * @override
 	 * @see \Magento\Framework\App\Action\Action::execute()
 	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
 	 * 		$result = $this->execute();
 	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
 	 */
-	function execute() {
+	function execute():void {
 		/**
 		 * 2015-11-29
 		 * @see \Magento\Framework\App\Response\Http::setNoCacheHeaders()
