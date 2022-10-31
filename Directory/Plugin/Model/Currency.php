2b68cf3ed0eb9feb65489086654cad0a44f37c64
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 16:32:38 +0100

    https://github.com/mage2pro/core/issues/168

--- Directory/Plugin/Model/Currency.php
+++ Directory/Plugin/Model/Currency.php
@@ -1,8 +1,6 @@
 <?php
 namespace Df\Directory\Plugin\Model;
 use Magento\Directory\Model\Currency as Sb;
-use Magento\Framework\App\Cache\Type\Block as B;
-use Magento\PageCache\Model\Cache\Type as F;
 # 2018-07-03
 final class Currency {
 	/**
@@ -11,9 +9,8 @@ final class Currency {
 	 * (Magento 2, PlazaThemes Grand theme)": https://github.com/sayitwithagift/core/issues/5
 	 * @see \Magento\Directory\Model\Currency::saveRates()
 	 * @param Sb $sb
-	 * @return $sb
 	 */
-	function afterSaveRates(Sb $sb) {
+	function afterSaveRates(Sb $sb):Sb {
 		df_cache_clean_blocks();
 		df_cache_clean_pages();
 		return $sb;
