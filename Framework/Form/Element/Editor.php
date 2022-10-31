96c0815ecae5084f9799584c4772a3c5182d9f97
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 17:12:49 +0100

    https://github.com/mage2pro/core/issues/168

--- Framework/Form/Element/Editor.php
+++ Framework/Form/Element/Editor.php
@@ -1,14 +1,14 @@
 <?php
 namespace Df\Framework\Form\Element;
-use Magento\Framework\Data\Form\Element\Editor as _Editor;
+use Magento\Framework\Data\Form\Element\Editor as _P;
 # 2016-01-06
-class Editor extends _Editor {
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+class Editor extends _P {
 	/**
 	 * 2016-01-06
 	 * @used-by \Dfe\Markdown\Plugin\Ui\Component\Form\Element\Wysiwyg::beforePrepare()
-	 * @param _Editor $editor
+	 * @param _P $p
 	 * @param string $html
-	 * @return string
 	 */
-	final static function wrapIntoContainerSt(_Editor $editor, $html) {return $editor->_wrapIntoContainer($html);}
+	final static function wrapIntoContainerSt(_P $p, $html):string {return $p->_wrapIntoContainer($html);}
 }
