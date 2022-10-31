39e59bf6118106eb037afb9fca78fd99bdeb03b1
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 15:04:32 +0100

    https://github.com/mage2pro/core/issues/168

--- Core/Test/lib/array.php
+++ Core/Test/lib/array.php
@@ -3,10 +3,10 @@ namespace Df\Core\Test\lib;
 # 2017-07-13
 class arrayT extends \Df\Core\TestCase {
 	/** 2017-07-13 */
-	function t00() {}
+	function t00():void {}
 
 	/** @test 2020-02-05 */
-	function t01_dfak_transform() {
+	function t01_dfak_transform():void {
 		$a = ['promotions' => [['description' => 'Test']]];
 		echo df_dump(dfak_prefix($a, '$', true));
 	}
