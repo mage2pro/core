6a74b140fe4283bf77229845891c3a28f60386b8
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-31 16:34:40 +0100

    https://github.com/mage2pro/core/issues/168

--- Eav/lib/customer-address.php
+++ Eav/lib/customer-address.php
@@ -4,6 +4,5 @@ use Magento\Eav\Model\Entity\Type as EntityType;
 /**
  * 2019-03-06
  * @used-by \Df\Customer\AddAttribute\Address::p()
- * @return EntityType
  */
-function df_eav_ca() {return df_eav_config()->getEntityType('customer_address');}
+function df_eav_ca():EntityType {return df_eav_config()->getEntityType('customer_address');}
