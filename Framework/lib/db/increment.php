620530818e9ba857e5950509c3fc508b10dd84a8
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-03 11:32:00 +0100

    https://github.com/mage2pro/core/issues/168

--- Framework/lib/db/increment.php
+++ Framework/lib/db/increment.php
@@ -10,17 +10,15 @@ use Magento\Framework\DB\Select;
  * @used-by \Df\Sso\CustomerReturn::customerData()
  * @uses \Magento\ImportExport\Model\ResourceModel\Helper::getNextAutoincrement()
  * @param string $t
- * @return int
  */
-function df_next_increment($t) {return df_int(df_ie_helper()->getNextAutoincrement(df_table($t)));}
+function df_next_increment($t):int {return df_int(df_ie_helper()->getNextAutoincrement(df_table($t)));}
 
 /**
  * 2015-10-12
  * 2019-01-12 It is never used.
  * @param string $t
- * @return int
  */
-function df_next_increment_old($t) {
+function df_next_increment_old($t):int {
 	$s = df_select()->from('information_schema.tables', 'AUTO_INCREMENT'); /** @var Select $s */
 	$s->where('? = table_name', $t);
 	$s->where('? = table_schema', df_db_name());
@@ -35,7 +33,7 @@ function df_next_increment_old($t) {
  * @param string $t
  * @param int $v
  */
-function df_next_increment_set($t, $v) {
+function df_next_increment_set($t, $v):void {
 	df_conn()->query(sprintf('ALTER TABLE %s AUTO_INCREMENT = %d', df_db_quote(df_table($t)), $v));
 	df_conn()->resetDdlCache($t);
 }
