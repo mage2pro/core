590a85c455b2cb8c2726e091a973d52dd171279c
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-03 20:20:19 +0100

    https://github.com/mage2pro/core/issues/168

--- Framework/lib/fs/name.php
+++ Framework/lib/fs/name.php
@@ -7,11 +7,9 @@
  * @param string $directory
  * @param string $template
  * @param string $ds [optional]
- * @return string
  */
-function df_file_name($directory, $template, $ds = '-') { /** @var string $r */
-	# 2016-11-09
-	# If $template contains the file's path, when it will be removed from $template and added to $directory.
+function df_file_name($directory, $template, $ds = '-'):string { /** @var string $r */
+	# 2016-11-09 If $template contains the file's path, when it will be removed from $template and added to $directory.
 	$directory = df_path_n($directory);
 	$template = df_path_n($template);
 	if (df_contains($template, '/')) {
@@ -62,23 +60,18 @@ function df_file_name($directory, $template, $ds = '-') { /** @var string $r */
 		}
 		else {
 			$counter++;
-			/**
-			 * Если в шаблоне имени файла нет переменной «{ordering}» — значит, надо добавить её,
-			 * чтобы в следующей интерации имя файла стало уникальным.
-			 * Вставляем «{ordering}» непосредственно перед расширением файла.
-			 * Например, rm.shipping.log преобразуем в rm.shipping-{ordering}.log
-			 */
+			# Если в шаблоне имени файла нет переменной «{ordering}» — значит, надо добавить её,
+			# чтобы в следующей интерации имя файла стало уникальным.
+			# Вставляем «{ordering}» непосредственно перед расширением файла.
+			# Например, rm.shipping.log преобразуем в rm.shipping-{ordering}.log
 			if (!$hasOrderingPosition && (2 === $counter)) {
-				/** @var string[] $fileNameTemplateExploded */
-				$fileNameTemplateExploded = explode('.', $template);
+				$fileNameTemplateExploded = explode('.', $template); /** @var string[] $fileNameTemplateExploded */
 				/** @var int $secondFromLastPartIndex*/
 				$secondFromLastPartIndex =  max(0, count($fileNameTemplateExploded) - 2);
 				/** @var string $secondFromLastPart */
 				$secondFromLastPart = dfa($fileNameTemplateExploded, $secondFromLastPartIndex);
 				df_assert_sne($secondFromLastPart);
-				$fileNameTemplateExploded[$secondFromLastPartIndex] =
-					implode('--', [$secondFromLastPart, '{ordering}'])
-				;
+				$fileNameTemplateExploded[$secondFromLastPartIndex] = implode('--', [$secondFromLastPart, '{ordering}']);
 				$template = df_assert_ne($template, implode('.', $fileNameTemplateExploded));
 			}
 		}
@@ -88,37 +81,30 @@ function df_file_name($directory, $template, $ds = '-') { /** @var string $r */
 
 /**
  * 2015-11-29
- * Преобразует строку таким образом,
- * чтобы её было безопасно и удобно использовать в качестве имени файла или папки.
+ * Преобразует строку таким образом, чтобы её было безопасно и удобно использовать в качестве имени файла или папки.
  * http://stackoverflow.com/a/2021729
  * 2017-02-09
- * Сегодня заметил, что эта функция удаляет пробелы, но сохраняет символы Unicode:
- * '歐付寶 all/Pay' => '歐付寶-allPay'
-
- * Пример №1: '歐付寶 all/Pay':
- * @see df_fs_name => 歐付寶-allPay
- * @see df_translit =>  all/Pay
- * @see df_translit_url => all-Pay
- * @see df_translit_url_lc => all-pay
- *
- * Пример №2: '歐付寶 O'Pay (allPay)':
- * @see df_fs_name => 歐付寶-allPay
- * @see df_translit =>  allPay
- * @see df_translit_url => allPay
- * @see df_translit_url_lc => allpay
- *
+ * Сегодня заметил, что эта функция удаляет пробелы, но сохраняет символы Unicode: '歐付寶 all/Pay' => '歐付寶-allPay'
+ * Example #1: '歐付寶 all/Pay':
+ * 		@see df_fs_name => 歐付寶-allPay
+ * 		@see df_translit =>  all/Pay
+ * 		@see df_translit_url => all-Pay
+ * 		@see df_translit_url_lc => all-pay
+ * Example #2: '歐付寶 O'Pay (allPay)':
+ * 		@see df_fs_name => 歐付寶-allPay
+ * 		@see df_translit =>  allPay
+ * 		@see df_translit_url => allPay
+ * 		@see df_translit_url_lc => allpay
  * @param string $n
  * @param string $spaceSubstitute [optional]
- * @return string
  */
-function df_fs_name($n, $spaceSubstitute = '-') {
+function df_fs_name($n, $spaceSubstitute = '-'):string {
 	$n = str_replace(' ', $spaceSubstitute, $n);
-	# Remove anything which isn't a word, whitespace, number
-	# or any of the following caracters -_~,;:[]().
-	# If you don't need to handle multi-byte characters
-	# you can use preg_replace rather than mb_ereg_replace
-	# Thanks @Łukasz Rysiak!
+	# «Remove anything which isn't a word, whitespace, number or any of the following caracters -_~,;:[]().
+	# If you don't need to handle multi-byte characters you can use preg_replace rather than mb_ereg_replace
+	# Thanks @Łukasz Rysiak!»
+	# http://stackoverflow.com/a/2021729
 	$n = mb_ereg_replace("([^\w\s\d\-_~,;:\[\]\(\).])", '', $n);
-	# Remove any runs of periods (thanks falstro!)
+	# «Remove any runs of periods (thanks falstro!)» http://stackoverflow.com/a/2021729
 	return mb_ereg_replace("([\.]{2,})", '', $n);
 }
