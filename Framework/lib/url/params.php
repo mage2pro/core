50773c1a533f799de4557ee64e4c161229f9e7b6
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-03 20:55:45 +0100

    https://github.com/mage2pro/core/issues/168

--- Framework/lib/url/params.php
+++ Framework/lib/url/params.php
@@ -7,7 +7,7 @@ use Magento\Framework\App\ActionInterface as IA;
  * @used-by df_url_frontend()
  * @return array(string => bool)
  */
-function df_nosid() {return ['_nosid' => true];}
+function df_nosid():array {return ['_nosid' => true];}
 
 /**
  * 2020-01-19
@@ -16,4 +16,4 @@ function df_nosid() {return ['_nosid' => true];}
  * @param string|null $u [optional]
  * @return array(string => string)
  */
-function df_url_param_redirect($u = null) {return [IA::PARAM_NAME_URL_ENCODED => df_url_h()->getEncodedUrl($u)];}
+function df_url_param_redirect($u = null):array {return [IA::PARAM_NAME_URL_ENCODED => df_url_h()->getEncodedUrl($u)];}
