5afb16782ddd5d7e22056cd6b6547049785d4633
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-03 11:00:03 +0100

    https://github.com/mage2pro/core/issues/168

--- Framework/lib/translation.php
+++ Framework/lib/translation.php
@@ -2,10 +2,11 @@
 use Magento\Framework\Phrase as P;
 /**
  * 2016-07-14
+ * @used-by df_checkout_message()
+ * @used-by df_message_add()
  * @param P|string $s
- * @return P
  */
-function df_phrase($s) {return $s instanceof P ? $s : __($s);}
+function df_phrase($s):P {return $s instanceof P ? $s : __($s);}
 
 /**
  * 2015-09-29
