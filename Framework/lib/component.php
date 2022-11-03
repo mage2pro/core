d8914b41ed91a29d7017664c1dfb9a97f6fcc2fc
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-03 10:14:16 +0100

    https://github.com/mage2pro/core/issues/168

--- Framework/lib/component.php
+++ Framework/lib/component.php
@@ -5,9 +5,8 @@ use Magento\Framework\Component\ComponentRegistrar as R;
  * 2019-12-31
  * @used-by df_lib_path()
  * @used-by \Df\SampleData\Model\Dependency::getSuggestsFromModules()
- * @return R
  */
-function df_component_r() {return df_o(R::class);}
+function df_component_r():R {return df_o(R::class);}
 
 /**
  * 2019-12-31
@@ -15,9 +14,8 @@ function df_component_r() {return df_o(R::class);}
  * «C:/work/clients/royalwholesalecandy.com-2019-12-08/code/vendor/magento/framework»
  * or «C:/work/clients/royalwholesalecandy.com-2019-12-08/code/lib/internal/magento/framework»
  * @used-by df_module_dir()
- * @return string
  */
-function df_framework_path() {return df_lib_path('magento/framework');}
+function df_framework_path():string {return df_lib_path('magento/framework');}
 
 /**
  * 2019-12-31
@@ -26,6 +24,5 @@ function df_framework_path() {return df_lib_path('magento/framework');}
  * or «C:/work/clients/royalwholesalecandy.com-2019-12-08/code/lib/internal/magento/framework»
  * @used-by df_framework_path()
  * @param string $l
- * @return string
  */
-function df_lib_path($l) {return df_component_r()->getPath(R::LIBRARY, $l);}
+function df_lib_path($l):string {return df_component_r()->getPath(R::LIBRARY, $l);}
