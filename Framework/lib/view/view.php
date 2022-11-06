e4c7871659f317e0c509cd8e20ede808b56c6b1b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-06 12:14:15 +0000

    https://github.com/mage2pro/core/issues/168

--- Framework/lib/view/view.php
+++ Framework/lib/view/view.php
@@ -7,9 +7,8 @@ use Magento\Framework\View\ConfigInterface as ViewConfigI;
  * By analogy with @see \Magento\Catalog\Helper\Image::getConfigView()
  * https://github.com/magento/magento2/blob/958164/app/code/Magento/Catalog/Helper/Image.php#L781-L792
  * @used-by df_product_image_url()
- * @return ConfigView
  */
-function df_view_config() {
+function df_view_config():ConfigView {
 	/**
 	 * 2021-11-25
 	 * 1) It fixes the «Area code is not set» error in @see \Magento\Framework\App\State::getAreaCode()
@@ -47,5 +46,3 @@ function df_view_config() {
 	$o = df_o(ViewConfigI::class); /** @var ViewConfig|ViewConfigI $o */
 	return $o->getViewConfig();
 }
-
-
