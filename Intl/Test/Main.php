f3e2878d125937685af21868336fef212a64ef08
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-06 13:53:37 +0000

    https://github.com/mage2pro/core/issues/168

--- Intl/Test/Main.php
+++ Intl/Test/Main.php
@@ -3,10 +3,8 @@
 namespace Df\Intl\Test;
 class Main extends \Df\Core\TestCase {
 	/** 2017-06-17 */
-	function t00() {}
+	function t00():void {}
 
 	/** 2017-06-17 @test */
-	function t01() {print_r(df_json_encode([
-		__('Dashboard')->__toString()
-	]));}
+	function t01():void {print_r(df_json_encode([__('Dashboard')->__toString()]));}
 }
