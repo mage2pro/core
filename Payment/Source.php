e49da3408561b00044f49259c93144010c07e6f0
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-07 19:43:51 +0000

    https://github.com/mage2pro/core/issues/168

--- Payment/Source.php
+++ Payment/Source.php
@@ -6,7 +6,6 @@ abstract class Source extends \Df\Config\Source {
 	/**
 	 * 2017-04-10
 	 * @used-by \Df\Payment\Source\Options\Location::map()
-	 * @return string
 	 */
-	final protected function titleB() {return $this->sibling('title_backend');}
+	final protected function titleB():string {return $this->sibling('title_backend');}
 }
