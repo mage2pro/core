0b1e70f382b4e6a9fd7845d00f4e3f9de27775aa
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-07 19:45:13 +0000

    https://github.com/mage2pro/core/issues/168

--- Payment/TID.php
+++ Payment/TID.php
@@ -10,7 +10,6 @@ final class TID {
 	 * 2) Paymill: tran_5390b8a15831cadd1e45dd8c5cd1, refund_2c9cd9a13357f2454522
 	 * 3) Spryng: 58a37e16b7f62b5161856629 (24 символа)
 	 * 4) Stripe: ch111_19ZMqvFzKb8aMux1iY2kx2xo, re_19xVUBFzKb8aMux1O9Qzn6ov
-	 *
 	 * 2017-03-23
 	 * Ещё вчера я думал:
 	 * «У всех вроде идентификаторы не короче 16 символов,
@@ -20,7 +19,6 @@ final class TID {
 	 * отсечь один тип и добавить другой.»
 	 * Однако ЭТО НЕВЕРНО и приводит к некорректной работе при внешних идентификаторах с дефисами,
 	 * например: «ORD-2017/03-00807».
-	 *
 	 * @used-by \Df\Payment\Init\Action::e2i()
 	 * @used-by \Df\PaypalClone\W\Nav::e2i()
 	 * @used-by \Df\StripeClone\Method::e2i()
@@ -29,12 +27,10 @@ final class TID {
 	 * @used-by \Dfe\AlphaCommerceHub\Method::charge()
 	 * @used-by \Dfe\SecurePay\Method::_refund()
 	 * @used-by \Dfe\TwoCheckout\Method::charge()
-	 *
 	 * @param string $id
 	 * @param string|null $t [optional]
-	 * @return string
 	 */
-	function e2i($id, $t = null) {return df_ccc(self::$S, 15 < strlen($id) ? $id : $this->md5($id), $t);}
+	function e2i($id, $t = null):string {return df_ccc(self::$S, 15 < strlen($id) ? $id : $this->md5($id), $t);}
 
 	/**
 	 * 2017-03-22
@@ -47,9 +43,8 @@ final class TID {
 	 * @used-by \Dfe\AlphaCommerceHub\Method::charge()
 	 * @param string $id
 	 * @param bool $force [optional]
-	 * @return string
 	 */
-	function i2e($id, $force = false) {
+	function i2e($id, $force = false):string {
 		/**
 		 * 2017-12-07
 		 * @todo Unfortunately, I forgot why I did this prohibition.
@@ -79,11 +74,8 @@ final class TID {
 	 * (гарантированном не содержащему дефисов и других неожиданных символов).
 	 * @used-by self::e2i()
 	 * @param string $id
-	 * @return string
 	 */
-	private function md5($id) {return self::$MD5 . substr(
-		md5($this->_m->getCode() . $id), 0, -strlen(self::$MD5)
-	);}
+	private function md5($id):string {return self::$MD5 . substr(md5($this->_m->getCode() . $id), 0, -strlen(self::$MD5));}
 
 	/**
 	 * 2017-03-22
@@ -98,9 +90,8 @@ final class TID {
 	 * 2017-03-22
 	 * @used-by \Df\Payment\Method::tid()
 	 * @param string|object $m
-	 * @return self
 	 */
-	static function s($m) {return dfcf(function(M $m) {return new self($m);}, [dfpm($m)]);}
+	static function s($m):self {return dfcf(function(M $m) {return new self($m);}, [dfpm($m)]);}
 
 	/**
 	 * 2017-03-22
