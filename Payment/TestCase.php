75a8362524c5ba1be0b4fdc5472d9292a5128801
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-07 19:44:08 +0000

    https://github.com/mage2pro/core/issues/168

--- Payment/TestCase.php
+++ Payment/TestCase.php
@@ -19,9 +19,8 @@ class TestCase extends \Df\Core\TestCase {
 	 * 2017-02-28
 	 * @final I do not use the PHP «final» keyword here to allow refine the return type using PHPDoc.
 	 * @param object|string|null $m [optional]
-	 * @return Method
 	 */
-    protected function m($m = null) {return dfc($this, function($m) {return dfpm($m ?: $this);}, [$m]);}
+    protected function m($m = null):Method {return dfc($this, function($m) {return dfpm($m ?: $this);}, [$m]);}
 
 	/**
 	 * 2017-03-27
