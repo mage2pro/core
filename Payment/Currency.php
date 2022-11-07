b897be4ad56bbf4990c13fb5f9a3a8b9878c2dc2
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-07 17:21:52 +0000

    https://github.com/mage2pro/core/issues/168

--- Payment/Currency.php
+++ Payment/Currency.php
@@ -23,9 +23,8 @@ class Currency {
 	 * @used-by \Df\Payment\Method::isAvailable()
 	 * @param float $a
 	 * @param O|Q $oq
-	 * @return float
 	 */
-	final function fromBase($a, $oq) {return $this->convert($a, df_currency_base_c($oq), $oq);}
+	final function fromBase($a, $oq):float {return $this->convert($a, df_currency_base_c($oq), $oq);}
 
 	/**
 	 * 2016-09-05
@@ -36,9 +35,8 @@ class Currency {
 	 * @used-by \Df\Payment\ConfigProvider::amount()
 	 * @param float $a
 	 * @param O|Q $oq
-	 * @return float
 	 */
-	final function fromOrder($a, $oq) {return $this->convert($a, df_oq_currency_c($oq), $oq);}
+	final function fromOrder($a, $oq):float {return $this->convert($a, df_oq_currency_c($oq), $oq);}
 
 	/**
 	 * 2016-09-05 «Mage2.PRO» → «Payment» → <...> → «Payment Currency».
@@ -49,9 +47,8 @@ class Currency {
 	 * @used-by \Df\Payment\ConfigProvider::config()
 	 * @param null|string|int|IScope|Store $s [optional]
 	 * @param string|null $oc [optional]
-	 * @return string
 	 */
-	final function iso3($s = null, $oc = null) {return dfc($this, function($s, $oc) {return CurrencyFE::v(
+	final function iso3($s = null, $oc = null):string {return dfc($this, function($s, $oc) {return CurrencyFE::v(
 		$this->_iso3($s), $s, $oc
 	);}, [$s, $oc]);}
 
@@ -64,16 +61,14 @@ class Currency {
 	 * @used-by \Df\Payment\Method::isAvailable()
 	 * @used-by \Df\Payment\Operation\Source::currencyC()
 	 * @param O|Q $oq
-	 * @return string
 	 */
-	final function oq($oq) {return $this->iso3($oq->getStore(), df_oq_currency_c($oq));}
+	final function oq($oq):string {return $this->iso3($oq->getStore(), df_oq_currency_c($oq));}
 
 	/**
 	 * 2016-09-06 Курс обмена учётной валюты на платёжную.
 	 * @used-by \Df\Payment\ConfigProvider::config()
-	 * @return float
 	 */
-	final function rateToPayment() {return df_currency_base()->getRate($this->iso3());}
+	final function rateToPayment():float {return df_currency_base()->getRate($this->iso3());}
 
 	/**
 	 * 2016-09-08
@@ -82,18 +77,16 @@ class Currency {
 	 * @used-by \Df\Payment\Method::convert()
 	 * @param float $a
 	 * @param O|Q $oq
-	 * @return float
 	 */
-	final function toBase($a, $oq) {return df_currency_convert($a, $this->oq($oq), df_currency_base($oq));}
+	final function toBase($a, $oq):float {return df_currency_convert($a, $this->oq($oq), df_currency_base($oq));}
 
 	/**
 	 * 2016-09-07 Конвертирует $a из валюты платежа в валюту заказа $o.
 	 * @used-by \Df\Payment\Method::convert()
 	 * @param float $a
 	 * @param O $o
-	 * @return float
 	 */
-	final function toOrder($a, O $o) {return df_currency_convert($a, $this->oq($o), $o->getOrderCurrencyCode());}
+	final function toOrder($a, O $o):float {return df_currency_convert($a, $this->oq($o), $o->getOrderCurrencyCode());}
 
 	/**
 	 * 2017-10-12
@@ -101,25 +94,22 @@ class Currency {
 	 * @see \Dfe\Spryng\Currency::_iso3()
 	 * @see \Dfe\Stripe\Currency::_iso3()
 	 * @param null|string|int|IScope|Store $s [optional]
-	 * @return string
 	 */
-	protected function _iso3($s = null) {return $this->s()->v('currency', $s);}
+	protected function _iso3($s = null):string {return $this->s()->v('currency', $s);}
 
 	/**
 	 * 2017-10-12
 	 * @final I do not use the PHP «final» keyword here to allow refine the return type using PHPDoc.
 	 * @used-by \Dfe\Stripe\Method::cardType()
-	 * @return Method
 	 */
-	protected function m() {return $this->_m;}
+	protected function m():Method {return $this->_m;}
 
 	/**
 	 * 2017-10-12
 	 * @final I do not use the PHP «final» keyword here to allow refine the return type using PHPDoc.
 	 * @used-by self::_iso3()
-	 * @return Settings
 	 */
-	protected function s() {return $this->_m->s();}
+	protected function s():Settings {return $this->_m->s();}
 
 	/**
 	 * 2016-09-05 Конвертирует денежную величину в валюту «Mage2.PRO» → «Payment» → <...> → «Payment Currency».
@@ -128,9 +118,8 @@ class Currency {
 	 * @param float $a
 	 * @param string $from
 	 * @param O|Q $oq
-	 * @return float
 	 */
-	private function convert($a, $from, $oq) {return df_currency_convert($a, $from, $this->oq($oq));}
+	private function convert($a, $from, $oq):float {return df_currency_convert($a, $from, $this->oq($oq));}
 
 	/**
 	 * 2017-10-12
@@ -143,9 +132,8 @@ class Currency {
 	 * 2017-10-12
 	 * @used-by dfp_currency()
 	 * @param object|string $m
-	 * @return self
 	 */
-	final static function f($m) {return dfcf(function(M $m) { /** @var self $i */
+	final static function f($m):self {return dfcf(function(M $m) { /** @var self $i */
 		$i = df_new(df_con_heir($m, __CLASS__)); $i->_m = $m; return $i;
 	}, [dfpm($m)]);}
 }
