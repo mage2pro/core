e983501b02f10f644ab083fca8abcb3c45cae82d
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-09 19:50:44 +0000

    https://github.com/mage2pro/core/issues/168

--- Payment/Test/trans.php
+++ Payment/Test/trans.php
@@ -8,13 +8,10 @@ class trans extends \Df\Core\TestCase {
 	 * @test
 	 * 2016-11-17
 	 */
-	function t01() {
-		/** @var T $t */
-		$t = df_trans(310);
-		/** @var OP $op */
-		$op = dfp($t);
-		/** @var bool $isTest */
-		$isTest = dfp_is_test($op);
+	function t01():void {
+		$t = df_trans(310); /** @var T $t */
+		$op = dfp($t); /** @var OP $op */
+		$isTest = dfp_is_test($op); /** @var bool $isTest */
 		$this->assertTrue(is_bool($isTest));
 	}
 }
