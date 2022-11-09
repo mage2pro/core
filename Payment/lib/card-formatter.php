339edca1309c3a6cd52b8029e3774b19111869ef
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-09 19:17:26 +0000

    https://github.com/mage2pro/core/issues/168

--- Payment/lib/card-formatter.php
+++ Payment/lib/card-formatter.php
@@ -5,6 +5,5 @@
  * @used-by \Dfe\PostFinance\W\Event::cardNumber()
  * @param string $last4
  * @param string $brand
- * @return string
  */
-function dfp_card_format_last4($last4, $brand) {return df_desc("···· $last4", $brand);}
+function dfp_card_format_last4($last4, $brand):string {return df_desc("···· $last4", $brand);}
