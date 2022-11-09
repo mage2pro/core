64e082add545982d00b8897de35812c7bccc923b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-09 19:29:22 +0000

    https://github.com/mage2pro/core/issues/168

--- Payment/lib/qa.php
+++ Payment/lib/qa.php
@@ -6,7 +6,7 @@ use Magento\Payment\Model\MethodInterface as IM;
  * 2016-07-14 Поддержка тегов HTML обеспечивается шаблоном Df_Checkout/messages
  * @param string|null $m [optional]
  */
-function dfp_error($m = null) {df_checkout_error(dfp_error_message($m));}
+function dfp_error($m = null):void {df_checkout_error(dfp_error_message($m));}
 
 /**
  * 2016-08-19
@@ -14,9 +14,8 @@ function dfp_error($m = null) {df_checkout_error(dfp_error_message($m));}
  * @used-by \Df\Payment\PlaceOrderInternal::message()
  * @used-by \Dfe\Stripe\Exception::messageC()
  * @param string|null $m [optional]
- * @return string
  */
-function dfp_error_message($m = null) {return nl2br(df_cc_n(
+function dfp_error_message($m = null):string {return nl2br(df_cc_n(
 	__("Sorry, the payment attempt is failed.")
 	, $m ? __("The payment service's message is «<b>%1</b>».", $m) : null
 	,__("Please try again, or try another payment method.")
@@ -41,7 +40,7 @@ function dfp_error_message($m = null) {return nl2br(df_cc_n(
  * @param string|null $title [optional]
  * @param string|null $suffix [optional]
  */
-function dfp_report($m, $d, $title = null, $suffix = null) {
+function dfp_report($m, $d, $title = null, $suffix = null):void {
 	dfp_sentry_tags($m);
 	df_sentry($m, $title ?: dfpm_title($m), ['extra' => is_array($d) ? $d : ['Payment Data' => $d]]);
 	df_log_l(df_module_name_c($m), $d, $suffix);
@@ -55,7 +54,7 @@ function dfp_report($m, $d, $title = null, $suffix = null) {
  * @used-by \Df\Payment\W\Handler::log()
  * @param string|object $c
  */
-function dfp_sentry_tags($c) {
+function dfp_sentry_tags($c):void {
 	$m = dfpm($c); /** @var IM|M $m */
 	df_sentry_tags($m,
 		[dfpm_title($m) => df_package_version($c)]
