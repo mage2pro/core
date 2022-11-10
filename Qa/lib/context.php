476b38be8d085ad7eec23c577eb66646197cf99e
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-10 13:11:55 +0000

    https://github.com/mage2pro/core/issues/168

--- Qa/lib/context.php
+++ Qa/lib/context.php
@@ -5,7 +5,7 @@
  * @used-by \Df\Qa\Failure\Error::preface()
  * @return array(string => mixed)
  */
-function df_context() {return [
+function df_context():array {return [
 	['mage2pro/core' => df_core_version(), 'Magento' => df_magento_version(), 'PHP' => phpversion()]
 	+ (df_is_cli()
 		? ['Command' => df_cli_cmd()]
