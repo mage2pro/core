941710224e34e1694f197f4152b29e150677c751
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-11 12:58:36 +0000

    https://github.com/mage2pro/core/issues/168

--- Sales/Plugin/Helper/Admin.php
+++ Sales/Plugin/Helper/Admin.php
@@ -7,16 +7,14 @@ final class Admin {
 	 * 2016-08-20
 	 * Цель плагина — сохранение в ссылках атрибута «target = "blank"» на страницах транзакций.
 	 * @see \Df\Payment\Method::tidFormat()
-	 *
 	 * @see \Magento\Sales\Helper\Admin::escapeHtmlWithLinks()
 	 * @used-by \Magento\Sales\Block\Adminhtml\Transactions\Detail::_toHtml()
 	 * @param Sb $sb
 	 * @param \Closure $f
 	 * @param string $data
 	 * @param string[]|null $allowedTags [optional]
-	 * @return string
 	 */
-	function aroundEscapeHtmlWithLinks(Sb $sb, \Closure $f, $data, $allowedTags = null) {return
+	function aroundEscapeHtmlWithLinks(Sb $sb, \Closure $f, $data, $allowedTags = null):string {return
 		df_trans_is_my() ? $data : $f($data, $allowedTags)
 	;}
 }
