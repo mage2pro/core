2674176876b5d59a64c6aea6082f0cdfe43c57ee
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-11 12:31:17 +0000

    https://github.com/mage2pro/core/issues/168

--- Review/lib/main.php
+++ Review/lib/main.php
@@ -5,9 +5,8 @@ use Magento\Review\Model\Review\Summary as RS;
  * 2019-11-20
  * 2022-10-22 @deprecated It is unused.
  * @param P|int $p
- * @return RS
  */
-function df_review_summary($p) {
+function df_review_summary($p):RS {
 	$r = df_new_om(RS::class); /** @var RS $r */
 	$r->load(df_product_id($p));
 	return $r;
