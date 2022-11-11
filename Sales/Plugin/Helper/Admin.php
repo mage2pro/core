4c1de8515eb86225c6c260a0f211e0d2794a1566
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-11 12:59:54 +0000

    https://github.com/mage2pro/core/issues/168

--- Sales/Plugin/Helper/Admin.php
+++ Sales/Plugin/Helper/Admin.php
@@ -4,8 +4,7 @@ use Magento\Sales\Helper\Admin as Sb;
 # 2016-08-20
 final class Admin {
 	/**
-	 * 2016-08-20
-	 * Цель плагина — сохранение в ссылках атрибута «target = "blank"» на страницах транзакций.
+	 * 2016-08-20 Цель плагина — сохранение в ссылках атрибута «target = "blank"» на страницах транзакций.
 	 * @see \Df\Payment\Method::tidFormat()
 	 * @see \Magento\Sales\Helper\Admin::escapeHtmlWithLinks()
 	 * @used-by \Magento\Sales\Block\Adminhtml\Transactions\Detail::_toHtml()
