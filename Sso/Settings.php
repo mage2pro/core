fae6f3bb285ed68603b0bfd9361c2faa07540bc2
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-11 19:17:09 +0000

    https://github.com/mage2pro/core/issues/168

--- Sso/Settings.php
+++ Sso/Settings.php
@@ -1,12 +1,17 @@
 <?php
 # 2016-12-02
 namespace Df\Sso;
-/** @method static Settings s() */
+/**
+ * 2016-12-02
+ * @see \Dfe\AmazonLogin\Settings
+ * @see \Dfe\BlackbaudNetCommunity\Settings
+ * @see \Dfe\FacebookLogin\Settings
+ * @method static Settings s()
+ */
 abstract class Settings extends \Df\Config\Settings {
 	/**
 	 * 2016-12-02
 	 * @used-by \Df\Sso\CustomerReturn::execute()
-	 * @return string
 	 */
-	function regCompletionMessage() {return $this->v();}
+	final function regCompletionMessage():string {return $this->v();}
 }
