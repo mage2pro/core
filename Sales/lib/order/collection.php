2fa451d7258e7a5fe7d43d68683b40ecec1735fb
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-11 12:55:33 +0000

    https://github.com/mage2pro/core/issues/168

--- Sales/lib/order/collection.php
+++ Sales/lib/order/collection.php
@@ -3,6 +3,5 @@ use Magento\Sales\Model\ResourceModel\Order\Collection as C;
 /**
  * 2019-11-20
  * 2022-10-22 @deprecated It is unused.
- * @return C
  */
-function df_order_c() {return df_new_om(C::class);}
+function df_order_c():C {return df_new_om(C::class);}
