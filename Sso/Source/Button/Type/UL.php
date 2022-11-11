0420aa36e801327adea3d806b5758fd4d5099ea8
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-11 19:25:15 +0000

    https://github.com/mage2pro/core/issues/168

--- Sso/Source/Button/Type/UL.php
+++ Sso/Source/Button/Type/UL.php
@@ -4,6 +4,7 @@ namespace Df\Sso\Source\Button\Type;
  * 2016-11-23
  * Не предоставляющие собственный дизайн для кнопок сервисы (например, «Blackbaud NetCommunity»)
  * используют этот класс, а предоставляющие — класс @see UNL.
+ * @see \Df\Sso\Source\Button\Type\UNL
  */
 class UL extends \Df\Config\Source {
 	/**
@@ -19,22 +20,28 @@ class UL extends \Df\Config\Source {
 	/**
 	 * 2016-11-30
 	 * @used-by \Df\Sso\Button::loggedOut()
-	 * @param string $type
-	 * @return bool
+	 * @param string $t
 	 */
-	static function isLink($type) {return self::$L === $type;}
+	final static function isLink($t):bool {return self::$L === $t;}
 
 	/**
 	 * 2016-11-30
 	 * @used-by \Df\Sso\Button::loggedOut()
-	 * @param string $type
-	 * @return bool
+	 * @param string $t
 	 */
-	static function isUnified($type) {return self::$U === $type;}
+	final static function isUnified($t):bool {return self::$U === $t;}
 
-	/** 2016-11-30 */
+	/**
+	 * 2016-11-30
+	 * @used-by self::isLink()
+	 * @used-by self::map()
+	 */
 	private static $L = 'L';
 
-	/** 2016-11-30 */
+	/**
+	 * 2016-11-30
+	 * @used-by self::isUnified()
+	 * @used-by self::map()
+	 */
 	private static $U = 'U';
 }
