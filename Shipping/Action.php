7270545a3472926450f9bbc9716f65db2d41ea2e
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-11 18:17:16 +0000

    https://github.com/mage2pro/core/issues/168

--- Shipping/Action.php
+++ Shipping/Action.php
@@ -8,7 +8,6 @@ abstract class Action extends \Df\Framework\Action {
 	 * 2018-04-24
 	 * @final I do not use the PHP «final» keyword here to allow refine the return type using PHPDoc.
 	 * @used-by \Doormall\Shipping\Controller\Index\Index::execute()
-	 * @return S
 	 */
-	protected function s() {return dfss($this->module());}
+	protected function s():S {return dfss($this->module());}
 }
