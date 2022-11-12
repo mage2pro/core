49a753e9602d5d2571c5b6b94028cda9d0d19e21
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-12 12:54:49 +0000

    https://github.com/mage2pro/core/issues/168

--- Core/lib/empty.php
+++ Core/lib/empty.php
@@ -98,6 +98,7 @@ function df_nes($v):bool {return is_null($v) || '' === $v;}
  * @used-by \Dfe\TBCBank\API\Validator::long()
  * @used-by \Dfe\TwoCheckout\Address::city()
  * @used-by \Dfe\TwoCheckout\Address::postcode()
+ * @used-by \Dfe\TwoCheckout\Address::region()
  * @used-by \Dfe\Vantiv\API\Validator::long()
  * @param mixed|null $v
  * @return mixed
