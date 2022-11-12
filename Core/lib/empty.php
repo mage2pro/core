dc9dd7a367cb8db9743e8620d847ca8bdf92a050
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-12 12:51:47 +0000

    https://github.com/mage2pro/core/issues/168

--- Core/lib/empty.php
+++ Core/lib/empty.php
@@ -96,6 +96,7 @@ function df_nes($v):bool {return is_null($v) || '' === $v;}
  * @used-by \Dfe\Sift\Payload\Payment\PayPal::p()
  * @used-by \Dfe\Sift\Test\CaseT\PayPal::t01()
  * @used-by \Dfe\TBCBank\API\Validator::long()
+ * @used-by \Dfe\TwoCheckout\Address::city()
  * @used-by \Dfe\Vantiv\API\Validator::long()
  * @param mixed|null $v
  * @return mixed
