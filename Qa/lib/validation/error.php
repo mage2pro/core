3e445d1e7802974b61883a46cc893401542330f4
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-13 15:29:16 +0000

    https://github.com/mage2pro/core/issues/174

--- Qa/lib/validation/error.php
+++ Qa/lib/validation/error.php
@@ -231,6 +231,7 @@ function df_not_implemented($m):void {df_error("The method «{$m}» is not imple
  * @used-by \Dfe\Dynamics365\API\Facade::GetDefaultPriceLevel()
  * @used-by \Dfe\Qiwi\W\Event::k_idE()
  * @used-by \Dfe\TBCBank\Facade\Customer::cardAdd()
+ * @used-by \Dfe\Vantiv\Facade\Customer::cardAdd()
  * @throws DFE
  */
 function df_should_not_be_here():void {df_error_html('The method %s is not allowed to call.', df_caller_mh());}
