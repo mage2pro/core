9d3d9db4c4c6290d893dee8f07d673d1da0c8ad7
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-13 17:23:42 +0000

    https://github.com/mage2pro/core/issues/168

--- Tax/lib/main.php
+++ Tax/lib/main.php
@@ -8,18 +8,13 @@ use Magento\Sales\Model\Order as O;
  * It is normal for an order position to have zero price: e.g., in case of free shipping.
  * @used-by df_oqi_tax_rate()
  * @used-by df_tax_rate_shipping()
- * @param float $withTax
- * @param float $withoutTax
- * @return float
  */
-function df_tax_rate($withTax, $withoutTax) {return !$withoutTax ? 0 :
+function df_tax_rate(float $withTax, float $withoutTax):float {return !$withoutTax ? 0 :
 	100 * ($withTax - $withoutTax) / $withoutTax
 ;}
 
 /**
  * 2017-09-30
  * @used-by \Dfe\YandexKassa\Charge::pTaxLeafs()
- * @param O $o
- * @return float
  */
-function df_tax_rate_shipping(O $o) {return df_tax_rate($o->getShippingInclTax(), $o->getShippingAmount());}
+function df_tax_rate_shipping(O $o):float {return df_tax_rate($o->getShippingInclTax(), $o->getShippingAmount());}
