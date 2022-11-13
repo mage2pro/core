93ff54dc87ecd238d37164c6cc4baa76302c7bf7
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-13 15:10:24 +0000

    https://github.com/mage2pro/core/issues/174

--- Config/Model/Config/Structure.php
+++ Config/Model/Config/Structure.php
@@ -4,11 +4,9 @@ class Structure extends \Magento\Config\Model\Config\Structure {
 	/**
 	 * 2016-08-02
 	 * @used-by df_config_tab_label()
-	 * @param string $tabName
-	 * @param string $tabProperty
 	 * @return string|null
 	 */
-	static function tab($tabName, $tabProperty) {
+	static function tab(string $tabName, string $tabProperty) {
 		$s = df_config_structure(); /** @var Structure $s */
 		if (!isset($s->_data['tabs'])) {
 			$s->getTabs();
