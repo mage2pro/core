131b01a7afd45232a3ab15bce7b1c839a9022f55
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-13 17:23:08 +0000

    https://github.com/mage2pro/core/issues/168

--- Swatches/lib/main.php
+++ Swatches/lib/main.php
@@ -4,6 +4,5 @@ use Magento\Swatches\Helper\Data as SwatchesH;
  * 2019-08-21
  * @used-by \Dfe\Color\Image::palette()
  * @used-by \Dfe\Color\Plugin\Catalog\Model\ResourceModel\Eav\Attribute::beforeBeforeSave()
- * @return SwatchesH
  */
-function df_swatches_h() {return df_o(SwatchesH::class);}
+function df_swatches_h():SwatchesH {return df_o(SwatchesH::class);}
