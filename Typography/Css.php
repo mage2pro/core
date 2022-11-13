c9f69fede36230646966c0d7d0db329be35bf8ac
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-13 17:28:06 +0000

    https://github.com/mage2pro/core/issues/174

--- Typography/Css.php
+++ Typography/Css.php
@@ -1,24 +1,26 @@
 <?php
 namespace Df\Typography;
+# 2015-12-16
 final class Css extends \Df\Core\O {
 	/**
 	 * 2015-12-16
-	 * @return string
+	 * @used-by \Df\Typography\Font::css()
+	 * @used-by \Dfe\Frontend\Block\ProductView\Css::customCss()
 	 */
-	function render() {return df_cc_n(df_map_k($this->_blocks, function($selector, array $rules) {
+	function render():string {return df_cc_n(df_map_k($this->_blocks, function($selector, array $rules) {
 		/** @var string $selector */ /** @var string[] $rules */
 		$rulesS = df_tab_multiline(df_cc_n($rules)); /** @var string $rulesS */
 		return "{$selector} {\n{$rulesS}\n}";
 	}));}
 
 	/**
-	 * @param string $name
-	 * @param string $value
-	 * @param string $selector [optional]
+	 * 2015-12-16
+	 * @used-by \Df\Typography\Font::css()
+	 * @used-by \Dfe\Frontend\Block\ProductView\Css::customCss()
 	 */
-	function rule($name, $value, $selector = '') {
-		if ('' !== $value && false !== $value) {
-			$this->_blocks[$this->prefix() . $selector][]= "{$name}: {$value} !important;";
+	function rule(string $n, string $v, string $selector = ''):void {
+		if ('' !== $v && false !== $v) {
+			$this->_blocks[$this->prefix() . $selector][]= "$n: $v !important;";
 		}
 	}
 
