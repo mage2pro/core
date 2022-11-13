ad5d2b81a65c65e19487e59e0ea1e893e637faf1
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-13 17:01:33 +0000

    https://github.com/mage2pro/core/issues/168

--- StripeClone/P/Preorder.php
+++ StripeClone/P/Preorder.php
@@ -18,10 +18,9 @@ abstract class Preorder extends \Df\Payment\Operation {
 	/**
 	 * 2017-06-12
 	 * @used-by \Df\StripeClone\Method::chargeNew()
-	 * @param M $m
 	 * @return array(string => mixed)
 	 */
-	final static function request(M $m) {
+	final static function request(M $m):array {
 		$i = df_new(df_con_heir($m, __CLASS__), $m); /** @var self $i */
 		return $i->p();
 	}
