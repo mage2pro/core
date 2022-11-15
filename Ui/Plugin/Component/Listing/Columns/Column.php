58f9a35dca55a1ca5b7399880caed9e685a52597
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-15 14:23:03 +0000

    https://github.com/mage2pro/core/issues/168

--- Ui/Plugin/Component/Listing/Columns/Column.php
+++ Ui/Plugin/Component/Listing/Columns/Column.php
@@ -6,9 +6,8 @@ class Column {
 	/**
 	 * 2016-07-28 Цель плагина — возбуждение события «df_column__prepare».
 	 * @see \Magento\Ui\Component\Listing\Columns\Column::prepare()
-	 * @param Sb $sb
 	 */
-	function beforePrepare(Sb $sb) {df_dispatch('df_column__prepare', [self::COLUMN => $sb]);}
+	function beforePrepare(Sb $sb):void {df_dispatch('df_column__prepare', [self::COLUMN => $sb]);}
 
 	/**
 	 * 2018-07-28
