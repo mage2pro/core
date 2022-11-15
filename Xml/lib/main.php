a751e332658025b2e98d65ae1afb7bf9fd7c8f4f
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-15 16:08:06 +0000

    https://github.com/mage2pro/core/issues/168

--- Xml/lib/main.php
+++ Xml/lib/main.php
@@ -29,36 +29,27 @@ function df_cdata_raw_if_needed(string $s):string {return !df_needs_cdata($s) ? 
  * 2015-02-27
  * 1) Метод @see \SimpleXMLElement::count() появился только в PHP 5.3,
  * поэтому мы его не используем: https://php.net/manual/simplexmlelement.count.php
- * Также обратите внимание, что count($e->children())
- * некорректно возвращает 1 для листов в PHP 5.1: http://3v4l.org/PT6Pt
+ * 2) `count($e->children())` некорректно возвращает 1 для листов в PHP 5.1: http://3v4l.org/PT6Pt
  * Однако нам не нужно поддерживать PHP 5.1.
- *
- * Обратите внимание, что для несуществующего узла попытка вызова @uses count()
- * приведёт к сбою: «Warning: count(): Node no longer exists»
+ * 3) Для несуществующего узла попытка вызова @uses count() приведёт к сбою: «Warning: count(): Node no longer exists»
  * http://3v4l.org/PsIPe#v512
- *
- * Текущий алгоритм проверен на работоспособность здесь: http://3v4l.org/VldTN
- *
+ * 4) Текущий алгоритм проверен на работоспособность здесь: http://3v4l.org/VldTN
+ * 2015-08-15
+ * Нельзя здесь использовать `count($e->children())`,
+ * потому что класс @see SimpleXmlElement не реализует интерфейс @see Iterator,
+ * а реализует только интерфейс @see Traversable.
+ * https://php.net/manual/class.iterator.php
+ * https://php.net/manual/class.traversable.php
+ * https://php.net/manual/simplexmlelement.count.php
  * 2015-08-16
- *
- * Как ни странно, написанное выше действительно верно: http://3v4l.org/covo1
- *
- * Обратите внимение, что класс @see \SimpleXMLElement не реализует интерфейс @see Iterator,
+ * 1) Как ни странно, написанное выше действительно верно: http://3v4l.org/covo1
+ * 2) Класс @see \SimpleXMLElement не реализует интерфейс @see Iterator,
  * а реализует только интерфейс @see Traversable.
  * https://php.net/manual/class.iterator.php
  * https://php.net/manual/class.traversable.php
  * https://php.net/manual/simplexmlelement.count.php
  * Однако @uses count() почему-то работает для него.
  * @see \SimpleXMLElement — самый загадочный класс PHP.
- *
- * 2015-08-15
- * Нельзя здесь использовать count($e->children()),
- * потому что класс @see SimpleXmlElement не реализует интерфейс @see Iterator,
- * а реализует только интерфейс @see Traversable.
- * https://php.net/manual/class.iterator.php
- * https://php.net/manual/class.traversable.php
- * https://php.net/manual/simplexmlelement.count.php
- *
  * @used-by df_assert_leaf()
  */
 function df_check_leaf(CX $e):bool {return !df_xml_exists($e) || !$e->children()->count();}
