174da08c3dd97abd816b2bafff54af2f299997c0
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-15 14:32:55 +0000

    https://github.com/mage2pro/core/issues/174

--- Webapi/Plugin/Model/ServiceMetadata.php
+++ Webapi/Plugin/Model/ServiceMetadata.php
@@ -1,19 +1,18 @@
 <?php
-# 2016-10-06
 namespace Df\Webapi\Plugin\Model;
 use Magento\Webapi\Model\ServiceMetadata as Sb;
+# 2016-10-06
 class ServiceMetadata extends Sb {
 	/** @override */
 	function __construct() {}
 
 	/**
 	 * 2016-10-06
-	 * When browsing the default soap url that should return xml soap services,
+	 * 1) When browsing the default soap url that should return xml soap services,
 	 * instead there is an exception with the following:
 	 * «The service interface name "Df\Payment\PlaceOrder" is invalid»
 	 * https://code.dmitry-fedyuk.com/m2e/stripe/issues/7
-	 *
-	 * Magento 2 накладывает ограничения на имена классов-вебсервисов:
+	 * 2) Magento 2 накладывает ограничения на имена классов-вебсервисов:
 	 * https://github.com/magento/magento2/blob/2.1.1/app/code/Magento/Webapi/Model/ServiceMetadata.php#L188-L230
 	 * Однако, как я понял, моего веб-сервиса @see \Df\Payment\PlaceOrder эти ограничения касаются
 	 * только в сценарии генерации документа WSDL /soap/default?wsdl_list=1
@@ -25,23 +24,14 @@ class ServiceMetadata extends Sb {
 	 * и мне удобнее иметь для себя свои имена.
 	 * Поэтому я и написал этот плагин: чтобы возвращать ядру имя своего сервиса
 	 * (и других моих сервисов, если они потом будут), обходя ограничения ядра на имена классов сервисов.
-	 *
 	 * @see \Magento\Webapi\Model\ServiceMetadata::getServiceName()
 	 * https://github.com/magento/magento2/blob/2.1.1/app/code/Magento/Webapi/Model/ServiceMetadata.php#L188-L230
-	 *
-	 * @param Sb $sb
-	 * @param \Closure $f
-	 * @param string $interface
-	 * @param string $version
-	 * @param bool $preserveVersion Should version be preserved during interface name conversion into service name
-	 * @return string
 	 */
-	function aroundGetServiceName(Sb $sb, \Closure $f, $interface, $version, $preserveVersion = true) {return
-		df_starts_with($interface, 'Df\\')
-		# 2016-10-06
-		# Df\Payment\PlaceOrder => dfPaymentPlaceOrder
-		? lcfirst(implode(df_explode_class($interface))) . (!$preserveVersion ? '' : $version)
-		: $f($interface, $version, $preserveVersion)
+	function aroundGetServiceName(Sb $sb, \Closure $f, string $i, string $v, bool $preserveV = true):string {return
+		df_starts_with($i, 'Df\\')
+		# 2016-10-06 Df\Payment\PlaceOrder => dfPaymentPlaceOrder
+		? lcfirst(implode(df_explode_class($i))) . (!$preserveV ? '' : $v)
+		: $f($i, $v, $preserveV)
 	;}
 }
 
