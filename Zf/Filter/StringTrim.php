45ed4004f6ad5908380aa262ed68394981564365
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-16 15:37:15 +0000

    https://github.com/mage2pro/core/issues/168

--- Zf/Filter/StringTrim.php
+++ Zf/Filter/StringTrim.php
@@ -1,29 +1,24 @@
 <?php
-/**
- * Обратите внимание, что класс Zend_Filter_StringTrim может работать некорректно
- * для строк, заканчивающихся заглавной кириллической буквой «Р».
- * http://framework.zend.com/issues/browse/ZF-11223
- * Однако решение, которое предложено по ссылке выше
- * (http://framework.zend.com/issues/browse/ZF-11223)
- * может приводить к падению интерпретатора PHP
- * для строк, начинающихся с заглавной кириллической буквы «Р».
- * Такое у меня происходило в методе @see Df_Autotrading_Model_Request_Locations::parseLocation()
- * Кто виноват: решение или исходный класс Zend_Filter_StringTrim — не знаю
- * (скорее, решение).
- * Поэтому мой класс \Df\Zf\Filter\StringTrim дополняет решение по ссылке выше
- * программным кодом из Zend Framework 2.0.
- */
 namespace Df\Zf\Filter;
-class StringTrim extends \Zend_Filter_StringTrim {
+/**
+ * Класс Zend_Filter_StringTrim может работать некорректно для строк, заканчивающихся заглавной кириллической буквой «Р».
+ * http://framework.zend.com/issues/browse/ZF-11223
+ * Однако решение, которое предложено по ссылке выше (http://framework.zend.com/issues/browse/ZF-11223)
+ * может приводить к падению интерпретатора PHP для строк, начинающихся с заглавной кириллической буквы «Р».
+ * Такое у меня происходило в методе @see Df_Autotrading_Model_Request_Locations::parseLocation()
+ * Кто виноват: решение или исходный класс Zend_Filter_StringTrim — не знаю (скорее, решение).
+ * Поэтому мой класс \Df\Zf\Filter\StringTrim дополняет решение по ссылке выше программным кодом из Zend Framework 2.0.
+ */
+final class StringTrim extends \Zend_Filter_StringTrim {
 	/**
 	 * @override
+	 * @see \Zend_Filter_StringTrim::_unicodeTrim()
 	 * @param string $value
 	 * @param string $charlist
-	 * @return string
 	 */
-	protected function _unicodeTrim($value, $charlist = '\\\\s') {
+	protected function _unicodeTrim($value, $charlist = '\\\\s'):string {/** @var string $r */
 		if ('' === $value) {
-			$result = $value;
+			$r = $value;
 		}
 		else {
 			# Начало кода из Zend Framework 2.0
@@ -33,9 +28,9 @@ class StringTrim extends \Zend_Filter_StringTrim {
 				$charlist
 			);
   			$pattern = '/^[' . $chars . ']+|[' . $chars . ']+$/usSD';
-			$result = preg_replace($pattern, '', $value);
+			$r = preg_replace($pattern, '', $value);
 			# Конец кода из Zend Framework 2.0
-			if (null === $result) {
+			if (null === $r) {
 				/**
 				 * Раньше тут происходил вызов @see df_notify_me().
 				 * Заменил вызов @see df_notify_me() на @see df_notify_exception(),
@@ -48,10 +43,10 @@ class StringTrim extends \Zend_Filter_StringTrim {
 				 * «/contacts/index/++++++++++++++++++++++++++++++++++++++++++Result:+�å+������ü+����û+��ÿ+�������è;»
 				 * Неверность кодировки объясняется, видимо, функциями ядра для работы с веб-адресами.
 				 */
-				$result = $this->_slowUnicodeTrim($value, $charlist);
+				$r = $this->_slowUnicodeTrim($value, $charlist);
 			}
 		}
-		return $result;
+		return $r;
 	}
 
 	/**
