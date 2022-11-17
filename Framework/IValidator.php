a690649954120cd39bd3c1e8b6e441d8bfa507db
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-17 00:20:09 +0000

    https://github.com/mage2pro/core/issues/174

--- Framework/IValidator.php
+++ Framework/IValidator.php
@@ -7,7 +7,6 @@ interface IValidator {
 	 * 2016-06-30
 	 * @used-by \Df\Framework\Plugin\Data\Form\Element\AbstractElement::afterGetComment()
 	 * @used-by \Df\Framework\Validator\Composite::check()
-	 * @param AE $e
 	 * @return true|Phrase|Phrase[]
 	 */
 	function check(AE $e);
