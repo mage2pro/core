89129573adc15019296dd282e6288bd5ca817636
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 17:55:09 +0000

    refactoring

--- Core/lib/cms.php
+++ Core/lib/cms.php
@@ -3,6 +3,8 @@ use Magento\Cms\Model\Wysiwyg\Config;
 use Magento\Ui\Component\Wysiwyg\ConfigInterface;
 /**
  * 2016-01-06
+ * @used-by \Dfe\Markdown\FormElement::config()
+ * @used-by \Dfe\Markdown\FormElement::enabled()
  * @return Config|ConfigInterface
  */
 function df_wysiwyg_config() {return df_o(ConfigInterface::class);}
