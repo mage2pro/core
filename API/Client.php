8521c10035e43dc14475b78bd62b6714768f5d31
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 06:12:11 +0000

    https://github.com/mage2pro/core/issues/174

--- API/Client.php
+++ API/Client.php
@@ -173,9 +173,8 @@ abstract class Client {
 	 * @used-by \Dfe\Qiwi\API\Client::_construct()
 	 * @used-by \Dfe\Vantiv\API\Client::_construct()
 	 * @param callable|IFilter $f
-	 * @param int $p
 	 */
-	final protected function addFilterResBV($f, $p = FilterChain::DEFAULT_PRIORITY):void {$this->_filtersResBV->attach($f, $p);}
+	final protected function addFilterResBV($f, int $p = FilterChain::DEFAULT_PRIORITY):void {$this->_filtersResBV->attach($f, $p);}
 
 	/**
 	 * 2017-07-08
