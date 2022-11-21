aa47ac2aad954f80ba9b74323af5b078dfaed211
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 06:46:50 +0000

    refactoring

--- Catalog/lib/product/image/url.php
+++ Catalog/lib/product/image/url.php
@@ -6,6 +6,7 @@ use Magento\Catalog\Model\Product as P;
  * 2021-11-30
  * @see df_product_image_url()
  * @used-by \TFC\GoogleShopping\Att\AdditionalImageLink::v() (tradefurniturecompany.co.uk, https://github.com/tradefurniturecompany/google-shopping/issues/1)
+ * @used-by \TFC\GoogleShopping\Command\C1::gp() (tradefurniturecompany.co.uk, https://github.com/tradefurniturecompany/google-shopping/issues/1)
  * @param int|null $limit [optionaL]
  */
 function df_product_images_additional(P $p, $limit = null):array {
