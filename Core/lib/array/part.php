1817f0fa9ea26c51d050e77d344cf7ec67dd5a3b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 06:49:53 +0000

    refactoring

--- Core/lib/array/part.php
+++ Core/lib/array/part.php
@@ -13,18 +13,13 @@
 function df_head(array $a):array {return array_slice($a, 0, -1);}
 
 /**
- * 2021-11-30
+ * 2021-10-05, 2021-11-30
+ * @uses array_slice() returns an empty array if `$limit` is `0`, and returns all elements if `$limit` is `null`,
+ * so I convert `0` and other empty values to `null`.
  * @used-by df_bt()
  * @used-by df_product_images_additional()
  */
-function df_slice(array $a, int $offset, int $length = 0):array {return array_slice(
-	/**
-	 * 2021-10-05
-	 * @uses array_slice() returns an empty array if `$limit` is `0`, and returns all elements if `$limit` is `null`,
-	 * so I convert `0` and other empty values to `null`;
-	 */
-	$a, $offset, df_etn($length)
-);}
+function df_slice(array $a, int $offset, int $length = 0):array {return array_slice($a, $offset, df_etn($length));}
 
 /**
  * Отсекает первый элемент массива и возвращает хвост (аналог CDR в Lisp).
