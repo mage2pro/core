1b17f74bbbf1f41b7e0d92035605a11f94e162c6
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 06:37:27 +0000

    https://github.com/mage2pro/core/issues/174

--- Catalog/lib/product/attribute.php
+++ Catalog/lib/product/attribute.php
@@ -41,10 +41,9 @@ function df_product_att_options(string $c):array {return dfcf(function($c) {retu
  * 2019-10-22
  * @used-by \Dfe\Color\Image::opts()
  * @used-by \PPCs\Core\Plugin\Iksanika\Stockmanage\Block\Adminhtml\Product\Grid::aroundAddColumn()
- * @param string $c
  * @return array(array(string => int|string))
  */
-function df_product_att_options_m($c):array {return df_options_to_map(df_product_att_options($c));}
+function df_product_att_options_m(string $c):array {return df_options_to_map(df_product_att_options($c));}
 
 /**              
  * 2019-09-22
