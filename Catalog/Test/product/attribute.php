0b26a35d1b26a1ba3c5d6889f6f7d4c8d8bfe9b6
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 15:55:31 +0000

    https://github.com/mage2pro/core/issues/168

--- Catalog/Test/product/attribute.php
+++ Catalog/Test/product/attribute.php
@@ -4,10 +4,10 @@ use Magento\Catalog\Model\Product as P;
 # 2020-01-31
 class attribute extends \Df\Core\TestCase {
 	/** 2020-01-31 @test */
-	function t00() {}
+	function t00():void {}
 
 	/** 2020-01-31 @test */
-	function df_product_att_val_s() {
+	function df_product_att_val_s():void {
 		$p = df_product(45); /** @var P $p */
 		echo df_product_att_val_s($p, 'description');
 		echo df_product_att_val_s($p, 'color');
