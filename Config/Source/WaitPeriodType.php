b7fae8881aa23da7e80a7c3af45a719b55b88f5d
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 16:48:13 +0000

    https://github.com/mage2pro/core/issues/174

--- Config/Source/WaitPeriodType.php
+++ Config/Source/WaitPeriodType.php
@@ -19,15 +19,15 @@ final class WaitPeriodType extends \Df\Config\Source {
 	 * @used-by \Dfe\AllPay\Settings::waitPeriodATM()
 	 * @used-by \Dfe\Moip\Settings\Boleto::waitPeriod()
 	 * @used-by \Dfe\Qiwi\Settings::waitPeriod()
-	 * @param string|null $k [optional]
-	 * @param string $kType [optional]
 	 */
-	static function calculate(S $s, $k = null, $kType = 'waitPeriodType'):int {return dfcf(function(S $s, $k, $kType) {
-		$r = $s->nat($k); /** @var int $r */
-		return self::$WORKING_DAYS === $s->v($kType) ? $r :
-			df_num_calendar_days_by_num_working_days(ZD::now(), $r, $s->scope())
-		;
-	}, [$s, $k ?: df_caller_f(), $kType]);}
+	static function calculate(S $s, string $k = '', string $kType = 'waitPeriodType'):int {return dfcf(
+		function(S $s, string $k, string $kType):int {
+			$r = $s->nat($k); /** @var int $r */
+			return self::$WORKING_DAYS === $s->v($kType) ? $r : df_num_calendar_days_by_num_working_days(
+				ZD::now(), $r, $s->scope()
+			);
+		}, [$s, $k ?: df_caller_f(), $kType]
+	);}
 	
 	/**
 	 * 2017-07-30
