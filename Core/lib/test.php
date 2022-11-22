91e7d57f27cd0432db758219323efff0f9d2a953
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-22 02:01:12 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/test.php
+++ Core/lib/test.php
@@ -8,9 +8,8 @@
  * 4) `null`: it comes down to the case 1 with the «Df_Core» module name.
  * @used-by df_test_file_l()
  * @param string|object|null $m
- * @param string $localPath [optional]
  */
-function df_test_file($m, $localPath = ''):string {return df_cc_path(df_module_dir($m), 'T/data', $localPath);}
+function df_test_file($m, string $localPath = ''):string {return df_cc_path(df_module_dir($m), 'T/data', $localPath);}
 
 /**
  * 2016-11-21
