67ade9d74349b002304ec3524d984a801ebc90f5
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-22 02:00:52 +0000

    https://github.com/mage2pro/core/issues/168

--- Core/lib/state.php
+++ Core/lib/state.php
@@ -111,8 +111,7 @@ function df_my():bool {return isset($_SERVER['DF_DEVELOPER']);}
  * @used-by \Inkifi\Pwinty\Event::s()
  * @used-by \Magento\Framework\View\Asset\Bundle::fillContent() (Frugue)
  * @used-by \Mangoit\MediaclipHub\Model\Orders::byOId()
- * @return bool
  */
-function df_my_local() {return dfcf(function() {return
+function df_my_local():bool {return dfcf(function() {return
 	df_my() && (df_is_localhost() || 'dfediuk' === dfa($_SERVER, 'USERNAME'))
 ;});}
