eecc38bb121ec381e679541cb627b3847cf67d36
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-22 01:48:38 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/js.php
+++ Core/lib/js.php
@@ -103,21 +103,20 @@ function df_js_inline_url(string $res, bool $async = false):string {return df_re
 
 /**
  * 2019-06-01
- * @used-by df_js()
- * @used-by \KingPalm\B2B\Block\RegionJS\Frontend::_toHtml()
- * @used-by vendor/kingpalm/adult/view/frontend/templates/popup.phtml
- * @see df_widget()
- * @param string $selector
- * @param string|object|null $m
  * $m could be:
  * 1) A module name: «A_B»
  * 2) A class name: «A\B\C».
  * 3) An object: it comes down to the case 2 via @see get_class()
  * 4) `null`.
+ * @used-by df_js()
+ * @used-by \KingPalm\B2B\Block\RegionJS\Frontend::_toHtml()
+ * @used-by vendor/kingpalm/adult/view/frontend/templates/popup.phtml
+ * @see df_widget()
+ * @param string|object|null $m
  * @param string|null $s [optional]
  * @param array(string => mixed) $p [optional]
  */
-function df_js_x($selector, $m, $s = null, array $p = []):string {return df_tag(
+function df_js_x(string $selector, $m, $s = null, array $p = []):string {return df_tag(
 	'script', ['type' => 'text/x-magento-init'], df_json_encode([$selector => [
 		df_cc_path(is_null($m) ? null : df_module_name($m), $s ?: 'main') => $p
 	]])
