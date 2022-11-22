0de7b09874f755f615816f2e8b866138f955d944
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-22 01:50:21 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/model.php
+++ Core/lib/model.php
@@ -36,7 +36,6 @@ function df_load($m, $id, bool $throw = true, $field = null) {/** @var M|null $r
 /**
  * 2016-05-23
  * @see \Df\Framework\Plugin\Model\AbstractModel::afterSave()
- * @param M $m
  * @param callable|array $callback
  */
 function df_on_save(M $m, $callback) {CallbackPool::attach(spl_object_hash($m), $callback);}
