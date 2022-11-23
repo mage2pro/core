d4ce7618257dcf9d689c3ed7a079a41dec7d10b3
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 03:50:13 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/text/explode.php
+++ Core/lib/text/explode.php
@@ -2,10 +2,9 @@
 /**
  * 2016-03-25 «charge.dispute.funds_reinstated» => [charge, dispute, funds, reinstated]
  * @param string[] $delimiters
- * @param string $s
  * @return string[]
  */
-function df_explode_multiple(array $delimiters, $s):array {
+function df_explode_multiple(array $delimiters, string $s):array {
 	$main = array_shift($delimiters); /** @var string $main */
 	# 2016-03-25
 	# «If search is an array and replace is a string, then this replacement string is used for every value of search.»
