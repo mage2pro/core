5d7ecae81b6016257f248bc8ffd5685b0f16d062
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 02:17:16 +0000

    https://github.com/mage2pro/core/issues/174

--- Sales/Plugin/Model/Order/CreditmemoRepository.php
+++ Sales/Plugin/Model/Order/CreditmemoRepository.php
@@ -10,14 +10,9 @@ final class CreditmemoRepository {
 	 * 2016-03-18
 	 * Цель плагина — устранения дефекта:
 	 * «Bug: the @see \Magento\Sales\Model\Order\CreditmemoRepository::save() method
-	 * misses (does not log and does not show) the actual exception message
-	 * on a credit memo saving falure».
+	 * misses (does not log and does not show) the actual exception message on a credit memo saving falure».
 	 * https://mage2.pro/t/973
-	 *
 	 * @see \Magento\Sales\Model\Order\CreditmemoRepository::save()
-	 * @param Sb $sb
-	 * @param \Closure $f
-	 * @param CM $element
 	 * @throws CouldNotSaveException|LE;
 	 */
 	function aroundSave(Sb $sb, \Closure $f, CM $element):CM {/** @var CM $r */
