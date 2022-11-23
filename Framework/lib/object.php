150c8ea2d1610cfde6662c242eb06a12c3761a19
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 14:03:26 +0000

    refactoring

--- Framework/lib/object.php
+++ Framework/lib/object.php
@@ -3,8 +3,6 @@ use Df\Framework\Api\AbstractSimpleObject as DFO;
 use Magento\Framework\Api\AbstractSimpleObject as O;
 /**
  * 2017-05-22
- * @param O $o
- * @param string $k
  * @return mixed|null
  */
-function df_api_object_get(O $o, $k) {return DFO::get($o, $k);}
+function df_api_object_get(O $o, string $k) {return DFO::get($o, $k);}
