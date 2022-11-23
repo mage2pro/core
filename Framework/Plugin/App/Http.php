145c0257d536662957845563060d9afb33f62444
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 01:58:59 +0000

    https://github.com/mage2pro/core/issues/174

--- Framework/Plugin/App/Http.php
+++ Framework/Plugin/App/Http.php
@@ -6,10 +6,6 @@ use Magento\Framework\App\ResponseInterface as IResponse;
 # 2021-09-16
 # "Implement an ability to temporary ban visitors with a particular IP address": https://github.com/mage2pro/core/issues/159
 final class Http {
-	/**
-	 * 2021-04-19
-	 * @param Sb $sb
-	 * @param \Closure $f
-	 */
+	/** 2021-04-19 */
 	function aroundLaunch(Sb $sb, \Closure $f):IResponse {return B::has() ? df_403() : $f();}
 }
