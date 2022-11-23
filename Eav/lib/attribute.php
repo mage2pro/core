3684e08cc5314e3abf7225ff410d98322108c678
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 05:06:24 +0000

    https://github.com/mage2pro/core/issues/174

--- Eav/lib/attribute.php
+++ Eav/lib/attribute.php
@@ -4,6 +4,5 @@
  * @used-by df_customer_att_pos_set()
  * @used-by \KingPalm\B2B\Setup\UpgradeData::_process()
  * @see df_product_attrs_r()
- * @param string $c
  */
-function df_att_code2id($c):int {return df_first(df_fetch_col_int('eav_attribute', 'attribute_id', 'attribute_code', $c));}
+function df_att_code2id(string $c):int {return df_first(df_fetch_col_int('eav_attribute', 'attribute_id', 'attribute_code', $c));}
