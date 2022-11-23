c12e694244c73e8cef4180d63ad684978cc2929a
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 23:47:53 +0000

    refactoring

--- Framework/lib/fs/ds.php
+++ Framework/lib/fs/ds.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * 2022-11-24
+ * The @see DS constant exists in Magento 1: https://github.com/OpenMage/magento-mirror/blob/1.9.4.5/app/Mage.php#L27
+ * It is absent in Magento 2.
+ */
 if (!defined('DS')) {
 	define('DS', DIRECTORY_SEPARATOR);
 }
