ef07223da08cec41b7ed1f19cdb02cf8e86af86b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 02:00:36 +0000

    refactoring

--- Framework/Plugin/App/Router/ActionList.php
+++ Framework/Plugin/App/Router/ActionList.php
@@ -14,18 +14,14 @@ class ActionList extends Sb {
 	 *	}
 	 * @see \Magento\Framework\App\Router\Base::matchAction()
 	 * https://github.com/magento/magento2/blob/2.1.5/lib/internal/Magento/Framework/App/Router/Base.php#L296-L298
-	 * @param Sb $sb
-	 * @param \Closure $f
-     * @param string $m
-     * @param string $area
-     * @param string $ns
-     * @param string $action
 	 * @return string|null
 	 */
-	function aroundGet(Sb $sb, \Closure $f, $m, $area, $ns, $action) {return $f($m, $area, $ns, $action) ?: (
-		'Df' !== substr($m, 0, 2) || !df_is_virtual(
-			$c = df_cc_class_uc(df_module_name_c($m), 'Controller', 'adminhtml' === $area ? $area : null, $ns, $action)
+	function aroundGet(Sb $sb, \Closure $f, string $m, string $area, string $ns, string $action) {return
+		$f($m, $area, $ns, $action) ?: (
+			'Df' !== substr($m, 0, 2) || !df_is_virtual(
+				$c = df_cc_class_uc(df_module_name_c($m), 'Controller', 'adminhtml' === $area ? $area : null, $ns, $action)
+			)
+			? null : df_vtr($c)
 		)
-		? null : df_vtr($c)
-	);}
+	;}
 }
