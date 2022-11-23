e8effac46857606f355efd7195ec39c7e693c242
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 03:39:26 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/reflection/main.php
+++ Core/lib/reflection/main.php
@@ -38,9 +38,8 @@ function df_cc_method($a1, $a2 = null):string {return df_ccc('::',
  * 2017-01-11
  * http://stackoverflow.com/a/666701
  * @used-by \Df\Payment\W\F::i()
- * @param string $c
  */
-function df_class_check_abstract($c):bool {df_param_sne($c, 0); return (new RC(df_ctr($c)))->isAbstract();}
+function df_class_check_abstract(string $c):bool {df_param_sne($c, 0); return (new RC(df_ctr($c)))->isAbstract();}
 
 /**
  * 2016-05-06
