38745a16353ffd2b466c22407a971dc811e3e3bf
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 14:45:30 +0000

    https://github.com/mage2pro/core/issues/174

--- Framework/lib/fs/ext.php
+++ Framework/lib/fs/ext.php
@@ -13,10 +13,9 @@ function df_file_ext(string $f):string {return pathinfo($f, PATHINFO_EXTENSION);
  * @see df_strip_ext()
  * @used-by df_block()
  * @used-by df_module_file()
- * @param string $f
  * @param string|null $ext
  */
-function df_file_ext_add($f, $ext):string {return !$ext ? $f : df_append($f, ".$ext");}
+function df_file_ext_add(string $f, $ext):string {return !$ext ? $f : df_append($f, ".$ext");}
 
 /**
  * 2018-07-06
