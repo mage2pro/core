eaa89617b9905912d62b36b55ac793d2967261ca
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 05:07:29 +0000

    https://github.com/mage2pro/core/issues/174

--- Eav/lib/attribute.php
+++ Eav/lib/attribute.php
@@ -5,4 +5,6 @@
  * @used-by \KingPalm\B2B\Setup\UpgradeData::_process()
  * @see df_product_attrs_r()
  */
-function df_att_code2id(string $c):int {return df_first(df_fetch_col_int('eav_attribute', 'attribute_id', 'attribute_code', $c));}
+function df_att_code2id(string $c):int {return df_first(df_fetch_col_int(
+	'eav_attribute', 'attribute_id', 'attribute_code', $c
+));}
--- Eav/lib/custom.php
+++ Eav/lib/custom.php
@@ -5,11 +5,9 @@ use Magento\Framework\Api\AttributeValue as A;
 /**
  * 2019-03-06
  * @used-by \Verdepieno\Core\CustomerAddressForm::f()
- * @param O $o
- * @param string $k
  * @return mixed|null
  */
-function df_cav(O $o, $k) {
+function df_cav(O $o, string $k) {
 	$a = $o->getCustomAttribute($k); /** @var IA|A $a */
 	return $a ? $a->getValue() : null;
 }
