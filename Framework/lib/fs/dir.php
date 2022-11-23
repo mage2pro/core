0cd52ef78abbae9b4aa133a40ec8611f639c0099
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 14:44:06 +0000

    https://github.com/mage2pro/core/issues/174

--- Framework/lib/fs/dir.php
+++ Framework/lib/fs/dir.php
@@ -4,9 +4,8 @@ use Magento\Framework\App\Filesystem\DirectoryList as DL;
 /**
  * 2019-08-23
  * @used-by df_google_init_service_account()
- * @param string $p [optional]
  */
-function df_fs_etc($p = ''):string {return df_cc_path(df_fs_dl()->getPath(DL::CONFIG), df_trim_ds_left($p));}
+function df_fs_etc(string $p = ''):string {return df_cc_path(df_fs_dl()->getPath(DL::CONFIG), df_trim_ds_left($p));}
 
 /**
  * 2021-03-20
