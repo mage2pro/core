3c9d1bbd42c3741a54dc8aa5abd4c4908b53073b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 03:28:12 +0000

    refactoring

--- Core/lib/lang/main.php
+++ Core/lib/lang/main.php
@@ -46,6 +46,7 @@ function df_if1(bool $cond, $onTrue, $onFalse = null) {return $cond ? df_call_if
 
 /**
  * 2016-02-09 Осуществляет ленивое ветвление только для второй ветки.
+ * @used-by \Df\Config\Settings::p()
  * @param mixed $onTrue
  * @param mixed|null|callable $onFalse [optional]
  * @return mixed
