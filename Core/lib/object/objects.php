67305abb4df932a19cd033ff99bd4d825a3bbfbc
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 03:32:21 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/object/objects.php
+++ Core/lib/object/objects.php
@@ -114,11 +114,10 @@ function df_new(string $c, ...$args) {return new $c(...$args);}
  * @used-by \Df\API\Facade::p()
  * @used-by \Df\Payment\W\F::aspect()
  * @used-by \Df\Zf\Validate\StringT\Parser::getZendValidator()
- * @param string|null $expected [optional]
  * @param mixed ...$args
  * @return object
  */
-function df_newa(string $c, $expected = null, ...$args) {return df_ar(df_new($c, ...$args), $expected);}
+function df_newa(string $c, string $expected = '', ...$args) {return df_ar(df_new($c, ...$args), $expected);}
 
 /**
  * 2016-01-06
