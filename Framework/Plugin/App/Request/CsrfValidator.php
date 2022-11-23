62df8be9f31f1b758ffb61ab7d291c4b840dbc90
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 01:59:44 +0000

    https://github.com/mage2pro/core/issues/174

--- Framework/Plugin/App/Request/CsrfValidator.php
+++ Framework/Plugin/App/Request/CsrfValidator.php
@@ -3,7 +3,6 @@ namespace Df\Framework\Plugin\App\Request;
 use Df\Framework\Action as DfA;
 use Magento\Framework\App\ActionInterface as IA;
 use Magento\Framework\App\Request\CsrfValidator as Sb;
-use Magento\Framework\App\Request\Http as R;
 use Magento\Framework\App\RequestInterface as IR;
 # 2020-02-25
 /**
@@ -53,10 +52,6 @@ final class CsrfValidator {
 	 *			}
 	 *		}
 	 * https://github.com/magento/magento2/blob/2.3.4/lib/internal/Magento/Framework/App/Request/CsrfValidator.php#L111-L135
-	 * @param Sb $sb
-	 * @param \Closure $f
-	 * @param IR|R $r
-	 * @param IA $a
 	 */
 	function aroundValidate(Sb $sb, \Closure $f, IR $r, IA $a):bool {return $a instanceof DfA || $f($r, $a);}
 }
