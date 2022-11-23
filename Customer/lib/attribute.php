a7787b5224e5d2aa42e6b57aac609ef40f4509bd
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 04:29:43 +0000

    https://github.com/mage2pro/core/issues/174

--- Customer/lib/attribute.php
+++ Customer/lib/attribute.php
@@ -3,9 +3,8 @@ use Magento\Eav\Model\Entity\Attribute\AbstractAttribute as Att;
 /**       
  * 2016-06-04
  * @used-by df_customer_att_is_required()
- * @param string $c
  */
-function df_customer_att($c):Att {return df_eav_config()->getAttribute(df_eav_customer(), $c);}
+function df_customer_att(string $c):Att {return df_eav_config()->getAttribute(df_eav_customer(), $c);}
 
 /**      
  * 2016-06-04
