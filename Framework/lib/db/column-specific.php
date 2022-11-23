68233d709a7448bd0fa05a6a4667fb83d49568e9
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 14:17:45 +0000

    https://github.com/mage2pro/core/issues/168

--- Framework/lib/db/column-specific.php
+++ Framework/lib/db/column-specific.php
@@ -33,7 +33,7 @@ function df_dbc_o(string $name, $dfn = null):void {df_db_column_add('sales_order
  * @param string $name
  * @param string|null|array(string => mixed) $dfn [optional]
  */
-function df_dbc_oa($name, $dfn = null) {df_db_column_add('sales_order_address', ...func_get_args());}
+function df_dbc_oa($name, $dfn = null):void {df_db_column_add('sales_order_address', ...func_get_args());}
 
 /**
  * 2019-06-04 @todo Support df_call_a()
