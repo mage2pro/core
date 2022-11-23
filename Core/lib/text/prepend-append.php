8b7bdcf85908617ce9b050f1ad28507b5846fa8a
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 03:57:51 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/text/prepend-append.php
+++ Core/lib/text/prepend-append.php
@@ -3,10 +3,8 @@
  * 2016-03-08 It adds the $tail suffix to the $s string if the suffix is absent in $s.
  * @used-by df_cc_path_t()
  * @used-by df_file_ext_add()
- * @param string $s
- * @param string $tail
  */
-function df_append($s, $tail):string {return df_ends_with($s, $tail) ? $s : $s . $tail;}
+function df_append(string $s, string $tail):string {return df_ends_with($s, $tail) ? $s : $s . $tail;}
 
 /**
  * 2015-12-25
