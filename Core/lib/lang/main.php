a4d44b89ca9fb8b223e444c0b0d324c49c2aea74
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-23 03:13:24 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/lang/main.php
+++ Core/lib/lang/main.php
@@ -67,12 +67,12 @@ function df_nop($v) {return $v;}
 
 /**
  * 2015-12-06
+ * 2022-11-23 `callable` as an argument type is supported by PHP ≥ 5.4:
+ * https://github.com/mage2pro/core/issues/174#user-content-callable
  * @param string|object $id
- * @param callable $job
- * @param float $interval [optional]
  * @return mixed
  */
-function df_sync($id, callable $job, $interval = 0.1) { /** @var mixed $r */
+function df_sync($id, callable $job, float $interval = 0.1) { /** @var mixed $r */
 	$intervalI = round(1000000 * $interval); /** @var int $intervalI */
 	$nameShort = 'df-core-sync-' . md5(is_object($id) ? get_class($id) : $id) . '.lock'; /** @var string $nameShort */
 	$name = df_path_absolute(DirectoryList::TMP, $nameShort); /** @var string $name */
