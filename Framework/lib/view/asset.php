2f045b0dfd26404ad92e6e5bb1190ca7dd70a7e7
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-24 02:33:50 +0000

    https://github.com/mage2pro/core/issues/174

--- Framework/lib/view/asset.php
+++ Framework/lib/view/asset.php
@@ -52,11 +52,10 @@ function df_asset_exists(string $name, string $m = '', string $ext = ''):bool {r
  * @used-by \Dfe\Portal\Block::m()
  * @used-by \Dfe\Stripe\Block\Multishipping::_toHtml()
  * @used-by \SayItWithAGift\Options\Frontend::_toHtml()
- * @param string|null $name [optional]
  * @param string|object|null $m [optional]
  * @param string|null $extension [optional]
  */
-function df_asset_name($name = null, $m = null, $extension = null):string {return df_ccc(
+function df_asset_name(string $name = '', $m = null, $extension = null):string {return df_ccc(
 	'.', df_ccc('::', $m ? df_module_name($m) : null, $name ?: 'main'), $extension
 );}
 
