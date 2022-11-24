ad14d3f43b2d3af55004b420ff74dc80e3103405
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-24 02:14:19 +0000

    https://github.com/mage2pro/core/issues/174

--- Framework/lib/module/my.php
+++ Framework/lib/module/my.php
@@ -6,10 +6,10 @@
  * @return string[]
  */
 function df_modules_my():array {return dfcf(function() {return array_keys(array_filter(df_map_k(
-	df_map_r(df_module_list()->getNames(), function($m) {return [$m,
+	df_map_r(df_module_list()->getNames(), function(string $m):array {return [$m,
 		df_starts_with($m, 'Magento_') ? [] : df_package($m, 'authors', [])
 	];})
-	,function($m, array $authors) {return
+	,function(string $m, array $authors):bool {return
 		df_starts_with($m, ['Df_', 'Dfe_']) || df_find($authors, function(array $a) {return
 			'admin@mage2.pro' === dfa($a, 'email')
 		;})
