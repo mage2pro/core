2934f716ba8a34d1571d1da169aa25f3f2d8a2e2
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-24 02:32:02 +0000

    https://github.com/mage2pro/core/issues/174

--- Framework/lib/view/asset.php
+++ Framework/lib/view/asset.php
@@ -33,11 +33,10 @@ function df_asset_create(string $u):File {$a = df_asset(); return !df_check_url_
  * 1) a short name;
  * 2) a full name composed with @see df_asset_name()
  * @used-by df_fe_init()
- * @param string $name
  * @param string|null $m [optional]
  * @param string|null $ext [optional]
  */
-function df_asset_exists($name, $m = null, $ext = null):bool {return dfcf(
+function df_asset_exists(string $name, $m = null, $ext = null):bool {return dfcf(
 	function($name, $m = null, $ext = null) {return
 		!!df_asset_source()->findSource(df_asset_create(df_asset_name($name, $m, $ext)))
 	;}
