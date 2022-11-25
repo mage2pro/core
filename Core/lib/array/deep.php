d9b14923a473b185848a13fb3b345598c56ffb3b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-25 22:44:13 +0000

    refactoring

--- Core/lib/array/deep.php
+++ Core/lib/array/deep.php
@@ -32,6 +32,8 @@ use Df\Core\Exception as DFE;
  * @used-by \Dfe\Moip\API\Validator::short()
  * @used-by \Dfe\Moip\Facade\Card::country()
  * @used-by \Dfe\Moip\Facade\Card::owner()
+ * @used-by \Dfe\Paymill\Facade\O::toArray()
+ * @used-by \Dfe\Paymill\Test\CaseT::token()
  * @used-by \Dfe\Stripe\Facade\Card::__construct()
  * @used-by \Dfe\TwoCheckout\Method::charge()
  * @used-by \Dfe\YandexKassa\Source\Option::map()
