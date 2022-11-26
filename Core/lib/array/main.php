bc504116ca2ab97d7f0e57a01fdfd82005ff73d2
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-26 01:58:45 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/array/main.php
+++ Core/lib/array/main.php
@@ -268,14 +268,17 @@ function dfa_unique_fast(array $a):array {return array_keys(@array_flip($a));}
  * 		$this->_data = dfa_unset($this->_data, 'can_use_default_value', 'can_use_website_value', 'scope');
  * I replaced it with:
  * 		$this->unsetData(['can_use_default_value', 'can_use_website_value', 'scope']);
+ * 2022-11-26
+ * We can not declare the argument $k as `string ...$a` because such a syntax will reject arrays: https://3v4l.org/jFdPm
  * @used-by \Df\Config\Backend::value()
  * @used-by \Df\Config\Backend\ArrayT::processI()
  * @used-by \Df\Framework\Request::clean()
  * @used-by \Dfe\Markdown\Observer\Catalog\ControllerAction::processPost()
  * @param array(string => mixed) $a
+ * @param string|string[] $k
  * @return array(string => mixed)
  */
-function dfa_unset(array $a, string ...$k):array {return array_diff_key($a, array_flip(df_args($k)));}
+function dfa_unset(array $a, ...$k):array {return array_diff_key($a, array_flip(df_args($k)));}
 
 /**
  * Алгоритм взят отсюда: https://php.net/manual/function.array-unshift.php#106570
