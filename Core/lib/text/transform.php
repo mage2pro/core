548252589c3a60597a1c924ad249edd5c36666fb
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-26 02:29:03 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/text/transform.php
+++ Core/lib/text/transform.php
@@ -53,10 +53,12 @@ function df_ucfirst(...$a) {return df_call_a(function(string $s):string {return
  * Эта функция умеет работать с UTF-8, в отличие от стандартной функции @see ucwords()
  * https://php.net/manual/function.mb-convert-case.php
  * https://php.net/manual/function.mb-convert-case.php#refsect1-function.mb-convert-case-parameters
+ * 2022-11-26 We can not declare the argument as `string ...$a` because such a syntax will reject arrays: https://3v4l.org/jFdPm
  * @see df_ucfirst
  * @used-by \Df\Config\Source\LetterCase::apply()
+ * @param string|string[] $a
  * @return string|string[]
  */
-function df_ucwords(string ...$a) {return df_call_a(function(string $s):string {return mb_convert_case(
+function df_ucwords(...$a) {return df_call_a(function(string $s):string {return mb_convert_case(
 	$s, MB_CASE_TITLE, 'UTF-8'
 );}, $a);}
