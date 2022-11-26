1f83559d14abef797934d67f471594b1b636995d
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-26 07:21:08 +0000

    refactoring

--- Core/lib/text/parse.php
+++ Core/lib/text/parse.php
@@ -3,6 +3,8 @@
  * 2018-09-27
  * @used-by \Dfe\TBCBank\API\Client::_construct()
  * @used-by \Dfe\TBCBank\Test\CaseT\CheckResult::t01()
- * @return string|string[]
+ * @return string[][]
  */
-function df_parse_colon(string $s) {return df_map_r(df_explode_n($s), function($s) {return df_trim(explode(':', $s));});}
+function df_parse_colon(string $s):array {return df_map_r(df_explode_n($s), function(string $s):array {return df_trim(
+	explode(':', $s)
+);});}
