323670d5e2bfa8b55295ea2008f6805fa114c50a
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-26 07:49:40 +0000

    https://github.com/mage2pro/core/issues/174

--- Core/lib/credentials.php
+++ Core/lib/credentials.php
@@ -2,9 +2,8 @@
 /**
  * 2017-05-10 $_SERVER['HOME'] is defined only in the CLI mode.
  * @used-by df_github_token()
- * @param string|null $k [optional]
  * @return string|null|array(string => mixed)
  */
-function df_credentials($k = null) {return dfac(function() {return df_json_file_read(
+function df_credentials(string $k = '') {return dfac(function() {return df_json_file_read(
 	(df_my_local() ? 'C:/tools/shell/home' : '/var/www') . '/.credentials/credentials.json'
 );}, $k);}
