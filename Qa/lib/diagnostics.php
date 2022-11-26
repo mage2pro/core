b6c3e8416860e8d065188953ec04020fde069e9d
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-26 01:19:38 +0000

    https://github.com/mage2pro/core/issues/174

--- Qa/lib/diagnostics.php
+++ Qa/lib/diagnostics.php
@@ -11,11 +11,10 @@ function df_break($cond = true) {$cond && function_exists('xdebug_break') ? xdeb
  * @used-by \Dfe\Klarna\Api\Checkout\V3\UserAgent::__construct()
  * «How to detect the current web server programmatically?» https://mage2.pro/t/2523
  * http://serverfault.com/a/164159
- * @param bool $asArray [optional]
  * @return string|string[]
  * An example of result: «Apache/2.4.20» or ['Apache', '2.4.20'].
  */
-function df_webserver($asArray = false) {return dfcf(function($asArray = false) { /** @var string $r */
+function df_webserver(bool $asArray = false) {return dfcf(function($asArray = false) { /** @var string $r */
 	# 2017-01-25 «Apache/2.4.20 (Win64) OpenSSL/1.0.2h PHP/5.6.22»
 	$s = dfa($_SERVER, 'SERVER_SOFTWARE'); /** @var string|null $s */
 	$r = $s ? df_first(df_explode_space($s)) : (df_is_cli() ? 'PHP CLI/' . phpversion() : 'Unknown/Unknown');
