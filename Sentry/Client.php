53eaf9f13a05ca025ecf2c6669aa6e43be8ec9d5
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-08 20:49:58 +0000

    refactoring

--- Sentry/Client.php
+++ Sentry/Client.php
@@ -346,26 +346,23 @@ final class Client {
 	}
 
 	/** @used-by self::captureException() */
-	private function translateSeverity(string $s):string {
-		switch ($s) {
-			case E_COMPILE_ERROR:      return self::ERROR;
-			case E_COMPILE_WARNING:    return self::WARN;
-			case E_CORE_ERROR:         return self::ERROR;
-			case E_CORE_WARNING:       return self::WARN;
-			case E_ERROR:              return self::ERROR;
-			case E_NOTICE:             return self::INFO;
-			case E_PARSE:              return self::ERROR;
-			case E_RECOVERABLE_ERROR:  return self::ERROR;
-			case E_STRICT:             return self::INFO;
-			case E_USER_ERROR:         return self::ERROR;
-			case E_USER_NOTICE:        return self::INFO;
-			case E_USER_WARNING:       return self::WARN;
-			case E_WARNING:            return self::WARN;
-			case E_DEPRECATED:         return self::WARN;
-			case E_USER_DEPRECATED:    return self::WARN;
-		}
-		return self::ERROR;
-	}
+	private function translateSeverity(string $s):string {return dfa([
+		E_COMPILE_ERROR => self::ERROR
+		,E_COMPILE_WARNING => self::WARN
+		,E_CORE_ERROR => self::ERROR
+		,E_CORE_WARNING => self::WARN
+		,E_DEPRECATED => self::WARN
+		,E_ERROR => self::ERROR
+		,E_NOTICE => self::INFO
+		,E_PARSE => self::ERROR
+		,E_RECOVERABLE_ERROR => self::ERROR
+		,E_STRICT => self::INFO
+		,E_USER_DEPRECATED => self::WARN
+		,E_USER_ERROR => self::ERROR
+		,E_USER_NOTICE => self::INFO
+		,E_USER_WARNING => self::WARN
+		,E_WARNING => self::WARN
+	], $s, self::ERROR);}
 
 	/**
 	 * 2016-12-23
