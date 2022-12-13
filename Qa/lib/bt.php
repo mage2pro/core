96deb400718c29e48ee3537c01ceb731011b7a72
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-13 23:29:02 +0000

    refactoring

--- Qa/lib/bt.php
+++ Qa/lib/bt.php
@@ -38,7 +38,7 @@ function df_bt_inc($p, int $o = 1) {return is_array($p) || $p instanceof E ? $p 
 
 /**
  * $p позволяет при записи стека вызовов пропустить несколько последних вызовов функций,
- * которые и так очевидны (например, вызов данной функции, вызов df_bt_log() и т.п.)
+ * которые и так очевидны (например, вызов данной функции, вызов df_bt_s() и т.п.)
  * @used-by \Df\Core\Exception::__construct()
  * @used-by \Df\Store\Plugin\Model\App\Emulation::beforeStartEnvironmentEmulation()
  * @param int|E|array(array(string => string|int)) $p
@@ -47,11 +47,11 @@ function df_bt_log($p = 0):void {df_report('bt-{date}-{time}.log', df_bt_s(df_bt
 
 /**
  * 2019-12-16
+ * $p позволяет при записи стека вызовов пропустить несколько последних вызовов функций,
+ * которые и так очевидны (например, вызов данной функции, вызов df_bt_log() и т.п.)
  * @used-by df_bt_log()
  * @used-by df_log_l()
  * @used-by \Dfe\Sift\Test\CaseT\API\Account\Decisions::t01()
  * @param int|E|array(array(string => string|int)) $p
- * Позволяет при записи стека вызовов пропустить несколько последних вызовов функций,
- * которые и так очевидны (например, вызов данной функции, вызов df_bt_log() и т.п.)
  */
 function df_bt_s($p = 0):string {return Formatter::p(new Trace(df_bt(df_bt_inc($p))));}
