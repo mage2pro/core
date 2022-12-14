b7f6a90b9c36fea0acd140c8cd0dc67f61c29a70
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-14 00:53:21 +0000

    https://github.com/mage2pro/core/issues/174

--- Sales/lib/invoice.php
+++ Sales/lib/invoice.php
@@ -6,9 +6,8 @@ use Magento\Sales\Model\Order\Invoice as I;
 /**
  * 2016-03-27
  * @used-by dfp_refund()
- * @return I|null
  * @throws DFE
  */
-function df_invoice_by_trans(O $o, string $tid) {return DfOP::getInvoiceForTransactionId($o, $tid) ?: df_error(
+function df_invoice_by_trans(O $o, string $tid):I {return DfOP::getInvoiceForTransactionId($o, $tid) ?: df_error(
 	"No invoice found for the transaction {$tid}."
 );}
