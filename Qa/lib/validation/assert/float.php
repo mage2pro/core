5baf3a3e6d85bbd517ce0a9f712dd5109afab0a0
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-14 00:13:38 +0000

    refactoring

--- Qa/lib/validation/assert/float.php
+++ Qa/lib/validation/assert/float.php
@@ -63,6 +63,7 @@ function df_float($v, bool $allowNull = true) {/** @var int|int[] $r */
 }
 
 /**
+ * 2022-12-14 @deprecated It is unused.
  * @param mixed $v
  * @return float|null
  * @throws DFE
