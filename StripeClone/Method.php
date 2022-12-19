9cadb03794831c33664c2b1b4612d2e01a37c888
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-19 19:28:12 +0000

    https://github.com/mage2pro/core/issues/174

--- StripeClone/Method.php
+++ StripeClone/Method.php
@@ -756,6 +756,9 @@ abstract class Method extends \Df\Payment\Method {
 
 	/**
 	 * 2016-12-27
+	 * 2022-12-19
+	 * `object` as an argument type is not supported by PHP < 7.2:
+	 * https://github.com/mage2pro/core/issues/174#user-content-object
 	 * @used-by self::_refund()
 	 * @used-by self::charge()
 	 * @used-by self::chargeNew()
