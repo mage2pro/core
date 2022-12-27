96b285cdc68520ca841ada6e2e1816939afff17d
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-27 01:10:29 +0000

    refactoring

--- Core/lib/html.php
+++ Core/lib/html.php
@@ -102,6 +102,7 @@ function df_style_inline_hide(string ...$s):string {return !$s ? '' : df_style_i
  * @used-by \Df\Framework\Plugin\Data\Form\Element\AbstractElement::aroundGetLabelHtml()
  * @used-by \Df\Payment\Block\Info::msgUnconfirmed()
  * @used-by \Df\Payment\Block\Info::rCustomerAccount()
+ * @used-by \Df\Payment\Block\Info::rCheckoutSuccess()
  * @used-by \Dfe\Klarna\Button::_toHtml()
  * @used-by \Dfe\PostFinance\Block\Info::prepare()
  * @used-by \Dfe\Stripe\Block\Form::_toHtml()
