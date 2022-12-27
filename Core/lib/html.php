d46fe0e8883b2bf73c960bdc78c49e0ea7fbd753
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-27 01:14:18 +0000

    refactoring

--- Core/lib/html.php
+++ Core/lib/html.php
@@ -112,6 +112,7 @@ function df_style_inline_hide(string ...$s):string {return !$s ? '' : df_style_i
  * @used-by \Dfe\Klarna\Button::_toHtml()
  * @used-by \Dfe\Logo\Frontend::_toHtml()
  * @used-by \Dfe\Logo\Plugin\Catalog\Block\Product\View\Options::aroundGetOptionHtml()
+ * @used-by \Dfe\Markdown\FormElement::componentHtml()
  * @used-by \Dfe\PostFinance\Block\Info::prepare()
  * @used-by \Dfe\Stripe\Block\Form::_toHtml()
  * @used-by \Frugue\Shipping\Header::_toHtml()
