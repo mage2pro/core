410e7373142c394e2a4610d189363b394e6cce0f
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-27 01:12:12 +0000

    refactoring

--- Core/lib/html.php
+++ Core/lib/html.php
@@ -76,6 +76,7 @@ function df_style_inline_hide(string ...$s):string {return !$s ? '' : df_style_i
  * 2016-05-30 From now on $attrs could be a string. It is the same as ['class' => $attrs].
  * @used-by cs_quote_description() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/107)
  * @used-by df_caller_mh()
+ * @used-by df_flag_tag()
  * @used-by df_html_b()
  * @used-by df_js_data()
  * @used-by df_js_inline_r()
