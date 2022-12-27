155eda254e3020b6dde295cdfa063c70d41b6d3d
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-27 01:03:55 +0000

    refactoring

--- Core/lib/html.php
+++ Core/lib/html.php
@@ -82,6 +82,7 @@ function df_style_inline_hide(string ...$s):string {return !$s ? '' : df_style_i
  * @used-by df_kv_table()
  * @used-by df_link_inline()
  * @used-by df_style_inline()
+ * @used-by df_tag_ab()
  * @used-by df_tag_list()
  * @used-by \AlbumEnvy\Popup\Content::_toHtml()
  * @used-by \Df\Backend\Block\Widget\Form\Renderer\Fieldset\Element::_render()
