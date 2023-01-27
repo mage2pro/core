a3f990ee03fd067be89abbe3781592450dfe36e1
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-27 05:22:33 +0300

    9.0.6: https://github.com/justuno-com/core/issues/384

--- Core/lib/cli.php
+++ Core/lib/cli.php
@@ -2,13 +2,17 @@
 /**
  * 2017-03-15 Нулевой параметр argv — это имя текущего скрипта.
  * 2022-11-23 With $i = null the function returns all `argv` data: @see df_cli_cmd().
+ * 2023-01-27
+ * The previous code was: `dfa_deep($_SERVER, ['argv', $i])`.
+ * It did not handle correctly the non-CLI case (returned `null`):
+ * «ju_cli_script(): Return value must be of type string»: https://github.com/justuno-com/core/issues/384
  * @used-by df_cli_cmd()
  * @used-by df_cli_script()
  * @used-by df_is_cron()
  * @param int|null $i [optional]
  * @return string|string[]
  */
-function df_cli_argv($i = null) {return dfa_deep($_SERVER, ['argv', $i]);}
+function df_cli_argv($i = null) {return dfa(dfa($_SERVER, 'argv', []), $i, '');}
 
 /**
  * 2020-05-24
