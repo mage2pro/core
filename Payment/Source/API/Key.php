2fdb261c102a070deaae6fbedbb31cd46350f325
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-28 02:33:02 +0300

    9.1.2: https://github.com/mage2pro/core/issues/181

--- Payment/Source/API/Key.php
+++ Payment/Source/API/Key.php
@@ -7,9 +7,14 @@ abstract class Key extends \Df\Config\Source\API\Key {
 	/**
 	 * 2017-07-02
 	 * @final I do not use the PHP «final» keyword here to allow refine the return type using PHPDoc.
+	 * 2023-01-28
+	 * «Declaration of Df\Payment\Source\API\Key::ss(): Df\Payment\Settings
+	 * must be compatible with Df\Config\Source\API\Key::ss(): Df\Config\Settings»:
+	 * https://github.com/mage2pro/core/issues/181
 	 * @override
 	 * @see \Df\Config\Source\API\Key::ss()
 	 * @used-by \Df\Config\Source\API\Key::isRequirementMet()
+	 * @return S
 	 */
-	final protected function ss():S {return dfps($this);}
+	final protected function ss():\Df\Config\Settings {return dfps($this);}
 }
