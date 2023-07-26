b993c8fe892a5961e9a62d8775bc67e20235e9a2
Dmitrii Fediuk <dfediuk@gmail.com>
2023-07-26 22:54:43 +0300

    https://github.com/mage2pro/core/issues/279

--- Qa/lib/bt/entry.php
+++ Qa/lib/bt/entry.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * 2023-07-26 "Implement `df_bt_entry_file()`": https://github.com/mage2pro/core/issues/279
+ * @used-by df_bt_entry_is_phtml()
+ */
+function df_bt_entry_file(array $e):string {return $e['file'];}
+
+/**
  * 2023-07-26
  * @used-by df_caller_m()
  * @used-by df_caller_module()
@@ -8,6 +14,7 @@ function df_bt_entry_is_method(array $e):bool {return dfa_has_keys($e, ['class',
 
 /**
  * 2023-07-26
+ * @see dfa()
  * @used-by df_caller_module()
  */
-function df_bt_entry_is_phtml(array $e):bool {return df_ends_with(dfa($e, 'file'), '.phtml');}
+function df_bt_entry_is_phtml(array $e):bool {return df_ends_with(df_bt_entry_file($e), '.phtml');}
