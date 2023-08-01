d96f46f95eb27f36c671c4f390529a1947edfb84
Dmitrii Fediuk <dfediuk@gmail.com>
2023-08-02 00:34:17 +0300

    https://github.com/mage2pro/core/issues/305

--- Framework/Log/Handler/BrokenReference.php
+++ Framework/Log/Handler/BrokenReference.php
@@ -2,8 +2,7 @@
 namespace Df\Framework\Log\Handler;
 use Monolog\Logger as L;
 # 2023-08-02
-# "Prevent logging of
-# «Broken reference: the '…' tries to reorder itself towards '…', but their parents are different: '…' and '…' respectively»":
+# "Prevent logging of «Broken reference»  `Monolog\Logger::INFO`-level records":
 # https://github.com/mage2pro/core/issues/305
 final class BrokenReference extends \Df\Framework\Log\Handler {
 	/**
@@ -12,9 +11,7 @@ final class BrokenReference extends \Df\Framework\Log\Handler {
 	 * @see \Df\Framework\Log\Handler::_p()
 	 * @used-by \Df\Framework\Log\Handler::p()
 	 */
-	protected function _p():bool {$m = $this->r()->msg(); /** @var string  $m */return
-		L::INFO === $this->r()->level()
-		&& df_starts_with($m, 'Broken reference:')
-		&& df_contains($m, 'tries to reorder itself towards')
+	protected function _p():bool {/** @var string  $m */return
+		L::INFO === $this->r()->level() && $this->r()->msg('Broken reference:')
 	;}
 }
