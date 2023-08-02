f20621519e9d5c18f67fbea7c82cdbaad51bdf56
Dmitrii Fediuk <dfediuk@gmail.com>
2023-08-03 00:15:20 +0100

    https://github.com/mage2pro/core/issues/311

--- Qa/lib/validation/assert/eq.php
+++ Qa/lib/validation/assert/eq.php
@@ -1,13 +1,13 @@
 <?php
 use Df\Core\Exception as DFE;
-use Exception as E;
+use Throwable as Th; # 2023-08-03 "Treat `\Throwable` similar to `\Exception`": https://github.com/mage2pro/core/issues/311
 /**
  * @used-by df_module_name_by_path()
  * @used-by df_vector_sum()
  * @used-by \Df\Qa\Trace\Frame::url()
  * @param string|int|float|bool $expected
  * @param string|int|float|bool $v
- * @param string|E|null $m [optional]
+ * @param string|Th|null $m [optional]
  * @return string|int|float|bool
  * @throws DFE
  */
@@ -19,7 +19,7 @@ function df_assert_eq($expected, $v, $m = null) {return $expected === $v ? $v : 
  * @used-by df_nat()
  * @param int|float $lowBound
  * @param int|float $v
- * @param string|E|null $m [optional]
+ * @param string|Th|null $m [optional]
  * @return int|float
  * @throws DFE
  */
@@ -31,7 +31,7 @@ function df_assert_ge($lowBound, $v, $m = null) {return $lowBound <= $v ? $v : d
  * 2017-01-15 В настоящее время никем не используется.
  * @param int|float $lowBound
  * @param int|float $v
- * @param string|E|null $m [optional]
+ * @param string|Th|null $m [optional]
  * @return int|float
  * @throws DFE
  */
@@ -45,7 +45,7 @@ function df_assert_gt($lowBound, $v, $m = null) {return $lowBound <= $v ? $v : d
  * @used-by \Df\Customer\Settings\BillingAddress::restore()
  * @used-by \Dfe\CurrencyFormat\FE::onFormInitialized()
  * @param int|float|string $v
- * @param string|E|null $m [optional]
+ * @param string|Th|null $m [optional]
  * @return int|float
  * @throws DFE
  */
@@ -55,7 +55,7 @@ function df_assert_gt0($v, $m = null) {return 0 <= $v ? $v : df_error($m ?: "A p
  * @used-by \Mangoit\MediaclipHub\Model\Orders::byOId()
  * @param int|float $highBound
  * @param int|float $v
- * @param string|E|null $m [optional]
+ * @param string|Th|null $m [optional]
  * @return int|float
  * @throws DFE
  */
@@ -67,7 +67,7 @@ function df_assert_le($highBound, $v, $m = null) {return $highBound >= $v ? $v :
  * @used-by \RWCandy\Captcha\Assert::name()
  * @param int|float $highBound
  * @param int|float $v
- * @param string|E|null $m [optional]
+ * @param string|Th|null $m [optional]
  * @return int|float
  * @throws DFE
  */
@@ -85,7 +85,7 @@ function df_assert_lt($highBound, $v, $m = null) {return $highBound >= $v ? $v :
  * @used-by \Dfe\Stripe\Init\Action::redirectUrl()
  * @param string|int|float|bool $neResult
  * @param string|int|float|bool $v
- * @param string|E|null $m [optional]
+ * @param string|Th|null $m [optional]
  * @return string|int|float|bool
  * @throws DFE
  */
