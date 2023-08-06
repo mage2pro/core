52db0a469d5129206f2d5fb5c1a9f12aa8af79b8
Dmitrii Fediuk <dfediuk@gmail.com>
2023-08-06 07:02:32 +0100

    https://github.com/mage2pro/core/issues/327

--- Framework/Plugin/Data/Form/Element/AbstractElement.php
+++ Framework/Plugin/Data/Form/Element/AbstractElement.php
@@ -8,7 +8,10 @@ use Magento\Framework\Phrase;
 # 2015-12-13
 # Хитрая идея, которая уже давно пришла мне в голову: наследуясь от модифицируемого класса,
 # мы получаем возможность вызывать методы с областью доступа protected у переменной $s.
-class AbstractElement extends Sb {
+# 2023-08-06
+# "Prevent interceptors generation for the plugins extended from interceptable classes":
+#https://github.com/mage2pro/core/issues/327
+class AbstractElement extends Sb implements \Magento\Framework\ObjectManager\NoninterceptableInterface {
 	/**
 	 * 2016-01-01
 	 * An empty constructor allows us to skip the parent's one.
