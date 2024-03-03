c34216b97c971405a60b78a57d83e6a33bf9ed6c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-03 19:39:26 +0000

    refactoring

--- Core/lib/lang/other.php
+++ Core/lib/lang/other.php
@@ -1,6 +1,4 @@
 <?php
-use Magento\Framework\App\Filesystem\DirectoryList;
-
 /**
  * 2017-02-07
  * @used-by df_lang_zh()
@@ -25,28 +23,3 @@ function df_b(array $args, bool $r) {return !$args ? $r : $args[intval(!$r)];}
  * @return mixed
  */
 function df_nop($v) {return $v;}
-
-/**
- * 2015-12-06
- * 2022-11-23 `callable` as an argument type is supported by PHP ≥ 5.4:
- * https://github.com/mage2pro/core/issues/174#user-content-callable
- * @used-by \Dfe\GoogleFont\Controller\Index\Index::execute()
- * @param string|object $id
- * @return mixed
- */
-function df_sync($id, callable $job, float $interval = 0.1) { /** @var mixed $r */
-	$intervalI = round(1000000 * $interval); /** @var int $intervalI */
-	$nameShort = 'df-core-sync-' . md5(is_object($id) ? get_class($id) : $id) . '.lock'; /** @var string $nameShort */
-	$name = df_sys_path_abs(DirectoryList::TMP, $nameShort); /** @var string $name */
-	while(file_exists($name)) {
-		usleep($intervalI);
-	}
-	try {
-		df_file_write($name, '');
-		$r = $job();
-	}
-	finally {
-		df_fs_w(DirectoryList::TMP)->delete($nameShort);
-	}
-	return $r;
-}
---
+++ Core/lib/lang/sync.php
@@ -0,0 +1,26 @@
+<?php
+use Magento\Framework\App\Filesystem\DirectoryList as DL;
+/**
+ * 2015-12-06
+ * 2022-11-23 `callable` as an argument type is supported by PHP ≥ 5.4:
+ * https://github.com/mage2pro/core/issues/174#user-content-callable
+ * @used-by \Dfe\GoogleFont\Controller\Index\Index::execute()
+ * @param string|object $id
+ * @return mixed
+ */
+function df_sync($id, callable $job, float $interval = 0.1) { /** @var mixed $r */
+	$intervalI = round(1000000 * $interval); /** @var int $intervalI */
+	$nameShort = 'df-core-sync-' . md5(is_object($id) ? get_class($id) : $id) . '.lock'; /** @var string $nameShort */
+	$name = df_sys_path_abs(DL::TMP, $nameShort); /** @var string $name */
+	while(file_exists($name)) {
+		usleep($intervalI);
+	}
+	try {
+		df_file_write($name, '');
+		$r = $job();
+	}
+	finally {
+		df_fs_w(DL::TMP)->delete($nameShort);
+	}
+	return $r;
+}
