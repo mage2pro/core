34321b88fce5c170852801823152b71e9fe9a51a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-10 16:38:56 +0100

    https://github.com/cabinetsbay/site/issues/98

--- Catalog/lib/category/main.php
+++ Catalog/lib/category/main.php
@@ -33,6 +33,7 @@ function df_category_id($c):int {return df_int($c instanceof C ? $c->getId() : $
 /**
  * 2024-03-10
  * @uses \Magento\Catalog\Model\Category::getLevel() can return a string (e.g., "3").
+ * @used-by cb_category_is_top() (https://github.com/cabinetsbay/site/issues/98)
  * @used-by \Sharapov\Cabinetsbay\Block\Category\View::getRootCategoryName() (https://github.com/cabinetsbay/site/issues/98)
  * @used-by \Sharapov\Cabinetsbay\Block\Category\View::level() (https://github.com/cabinetsbay/site/issues/98)
  * @used-by \Sharapov\Cabinetsbay\Block\Category\View::parent() (https://github.com/cabinetsbay/site/issues/98)
