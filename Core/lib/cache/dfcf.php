e35b2795337bcfa9fbe18b58589e11d02d2df9b2
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-23 10:57:36 +0100

    refactoring

--- Core/lib/cache/dfcf.php
+++ Core/lib/cache/dfcf.php
@@ -88,6 +88,7 @@ use Df\Core\RAM;
  * @used-by \Df\OAuth\App::state()
  * @used-by \Df\Payment\Choice::f()
  * @used-by \Df\Payment\Currency::f()
+ * @used-by \Df\Payment\Facade::s()
  * @used-by \Df\Payment\Init\Action::sg()
  * @used-by \Df\Payment\Url::f()
  * @used-by \Df\Payment\W\F::s()
