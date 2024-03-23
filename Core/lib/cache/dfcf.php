49da943795a2a240cd9d810e39ceb8e00d7a7a91
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-23 10:52:37 +0100

    refactoring

--- Core/lib/cache/dfcf.php
+++ Core/lib/cache/dfcf.php
@@ -41,6 +41,7 @@ use Df\Core\RAM;
  * @used-by df_currency_nums()
  * @used-by df_days_off()
  * @used-by df_db_version()
+ * @used-by df_domain_current()
  * @used-by df_google_init_service_account()
  * @used-by df_is_windows()
  * @used-by df_magento_version()
