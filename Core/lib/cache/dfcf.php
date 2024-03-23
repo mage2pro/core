c0b074be4328f0977d1d1e8618a79101f2f831b2
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-23 11:01:18 +0100

    refactoring

--- Core/lib/cache/dfcf.php
+++ Core/lib/cache/dfcf.php
@@ -104,6 +104,7 @@ use Df\Core\RAM;
  * @used-by \Df\Qa\Trace\Formatter::p()
  * @used-by \Df\Shipping\Method::codeS()
  * @used-by \Df\Shipping\Method::sg()
+ * @used-by \Df\Sso\Button::sModule()
  * @used-by \Dfe\AllPay\W\Event::time()
  * @used-by \Dfe\Color\Image::optsM()
  * @used-by \Dfe\Color\Image::palette()
