9f1b5ca821dfbfccdf871d8a4dc5c571431183e0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-23 10:16:40 +0100

    refactoring

--- Framework/lib/db/o.php
+++ Framework/lib/db/o.php
@@ -1,7 +1,7 @@
 <?php
 use Magento\Framework\App\ResourceConnection as R;
 use Magento\Framework\DB\Ddl\Trigger;
-use Magento\Framework\DB\Select;
+use Magento\Framework\DB\Select as S;
 use Magento\Framework\DB\Transaction;
 
 /**
@@ -22,13 +22,13 @@ function df_db_transaction():Transaction {return df_new_om(Transaction::class);}
 /**
  * 2015-09-29
  * 2016-12-01
- * The function always returns @see Select
- * I added @see \Zend_Db_Select to the PHPDoc return type declaration just for my IDE convenience.
+ * The function always returns @see S
+ * I added @see Zend_Db_Select to the PHPDoc return type declaration just for my IDE convenience.
  * @used-by df_db_from()
  * @used-by df_next_increment_old()
- * @return Select|Zend_Db_Select
+ * @return S|Zend_Db_Select
  */
-function df_select() {return df_conn()->select();}
+function df_select():S {return df_conn()->select();}
 
 /**
  * 2019-11-22
