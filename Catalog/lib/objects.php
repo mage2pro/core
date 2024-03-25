6b54cb00c6b1f8647fffb0419c78981746bfb675
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-25 07:42:13 +0100

    https://github.com/cabinetsbay/site/issues/105

--- Catalog/lib/objects.php
+++ Catalog/lib/objects.php
@@ -22,7 +22,7 @@ function df_catalog_locator() {
 
 /**
  * 2020-10-30
- * @used-by app/design/frontend/Cabinetsbay/cabinetsbay_default/Magento_Catalog/templates/category/header.phtml (https://github.com/cabinetsbay/site/issues/98)
+ * @used-by vendor/cabinetsbay/core/view/frontend/templates/catalog/category/tabs.phtml (https://github.com/cabinetsbay/site/issues/105)
  * @used-by app/design/frontend/TradeFurnitureCompany/default/Magento_Catalog/templates/category/description.phtml
  */
 function df_catalog_output():OutputH {return df_o(OutputH::class);}
