ad8132a33ac603ed995ef7eda224a6cc6f1b2bf4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-25 10:28:43 +0300

    10.7.9

--- Cms/lib/filter.php
+++ Cms/lib/filter.php
@@ -1,7 +1,14 @@
 <?php
 use Magento\Cms\Model\Template\FilterProvider as FP;
+use Magento\Framework\Filter\Template as F;
+/**
+ * 2024-03-25
+ * @used-by df_cms_filter_page()
+ */
+function df_cms_filter_provider():FP {return df_o(FP::class);}
+
 /**
  * 2024-03-25
  * @used-by \Sharapov\Cabinetsbay\Block\Category\View::getCabinetAssembly() (https://github.com/cabinetsbay/site/issues/98)
  */
-function df_cms_fp():FP {return df_o(FP::class);}
+function df_cms_filter_page(string $s):string {return df_cms_filter_provider()->getPageFilter()->filter($s);}
