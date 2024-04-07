d25889d2c351ee6637ef9aff98189ebf6868fca0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-08 02:20:44 +0300

    10.8.4: https://github.com/thehcginstitute-com/m1/issues/551

--- Core/lib/array/main.php
+++ Core/lib/array/main.php
@@ -97,21 +97,32 @@ function dfaf($a, $b):array {
 
 /**
  * 2018-04-24
+ * 2024-04-08 I added the `is_null($k)` case: https://github.com/thehcginstitute-com/m1/issues/551
  * @used-by \Doormall\Shipping\Partner\Entity::locations()
  * @param array(int|string => mixed) $a
- * @param string|int $k
+ * @param string|int|null $k
  * @return array(int|string => array(int|string => mixed))
  */
-function dfa_group(array $a, $k):array {
+function dfa_group(array $a, $k = null):array {
 	$r = []; /** @var array(int|string => array(int|string => mixed)) $r */
-	$isInt = is_int($k); /** @var bool $isInt */
-	foreach ($a as $v) { /** @var mixed $v */
-		$index = $v[$k]; /** @var string $index */
-		if (!isset($r[$index])) {
-			$r[$index] = [];
+	if (is_null($k)) {
+		foreach ($a as $k => $index) { /** @var int|string $index */
+			if (!isset($r[$index])) {
+				$r[$index] = [];
+			}
+			$r[$index][] = $k;
 		}
-		unset($v[$k]);
-		$r[$index][] = 1 === count($v) ? df_first($v) : (!$isInt ? $v : array_values($v));
+	}
+	else {
+		$isInt = is_int($k); /** @var bool $isInt */
+		foreach ($a as $v) { /** @var mixed $v */
+			$index = $v[$k]; /** @var string $index */
+			if (!isset($r[$index])) {
+				$r[$index] = [];
+			}
+			unset($v[$k]);
+			$r[$index][] = 1 === count($v) ? df_first($v) : (!$isInt ? $v : array_values($v));
+		}
 	}
 	return $r;
 }
