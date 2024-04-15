3d359a13975fc1db9cd74d5ed990bb7552a064ce
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-15 01:57:33 +0100

    refactoring

--- Catalog/lib/category/current.php
+++ Catalog/lib/category/current.php
@@ -1,1 +1,12 @@
 <?php
+use Magento\Catalog\Model\Category as C;
+use Magento\Framework\Exception\NotFoundException as NotFound;
+/**
+ * 2024-04-15
+ * @param Closure|bool|mixed $onError
+ * @return C|null
+ * @throws NotFound|Exception
+ */
+function df_category_current($onError = null) {return df_try(function() {return
+	null
+;}, $onError);}
