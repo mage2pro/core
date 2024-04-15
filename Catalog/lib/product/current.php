d14ac4f2e61f902eb4c23df6cbfc69dcadb0c53b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-15 01:35:41 +0100

    refactoring

---
+++ Catalog/lib/product/current.php
@@ -0,0 +1,22 @@
+<?php
+use Magento\Catalog\Model\Product as P;
+use Magento\Framework\Exception\NotFoundException as NotFound;
+/**
+ * 2018-09-27
+ * @used-by df_product_current_id()
+ * @param Closure|bool|mixed $onError
+ * @return P|null
+ * @throws NotFound|\Exception
+ */
+function df_product_current($onError = null) {return df_try(function() {return
+	# 2019-08-01 `df_catalog_locator()` is available only in the backend.
+	# 2024-04-15 https://github.com/magento/magento2/blob/2.4.7/app/code/Magento/Catalog/etc/adminhtml/di.xml#L10
+	df_is_backend() ? df_catalog_locator()->getProduct() : (df_registry('current_product') ?: df_error())
+;}, $onError);}
+
+/**
+ * 2019-11-15
+ * @used-by \Dfe\Markdown\Modifier::modifyData()
+ * @return int|null
+ */
+function df_product_current_id() {return !($p = df_product_current() /** @var P $p */) ? null : $p->getId();}
--- Catalog/lib/product/main.php
+++ Catalog/lib/product/main.php
@@ -1,12 +1,10 @@
 <?php
 use Magento\Catalog\Api\ProductRepositoryInterface as IProductRepository;
 use Magento\Catalog\Helper\Product as ProductH;
-use Magento\Catalog\Model\Product as P;
 use Magento\Catalog\Model\ProductRepository;
 use Magento\Catalog\Model\ResourceModel\Product as Res;
 use Magento\Catalog\Model\ResourceModel\Product\Action;
 use Magento\Framework\Exception\NoSuchEntityException as NSE;
-use Magento\Framework\Exception\NotFoundException as NotFound;
 use Magento\Quote\Model\Quote\Item as QI;
 use Magento\Sales\Model\Order\Item as OI;
 use Magento\Store\Api\Data\StoreInterface as IStore;
@@ -58,26 +56,6 @@ function df_product($p, $s = false):P {return $p instanceof P ? $p : df_product_
  * @used-by \Dfe\Color\Observer\ProductImportBunchSaveAfter::execute()
  */
 function df_product_action():Action {return df_o(Action::class);}
-
-/**
- * 2018-09-27
- * @used-by df_product_current_id()
- * @param Closure|bool|mixed $onError
- * @return P|null
- * @throws NotFound|\Exception
- */
-function df_product_current($onError = null) {return df_try(function() {return
-	# 2019-08-01 `df_catalog_locator()` is available only in the backend.
-	# 2024-04-15 https://github.com/magento/magento2/blob/2.4.7/app/code/Magento/Catalog/etc/adminhtml/di.xml#L10
-	df_is_backend() ? df_catalog_locator()->getProduct() : (df_registry('current_product') ?: df_error())
-;}, $onError);}
-
-/**
- * 2019-11-15
- * @used-by \Dfe\Markdown\Modifier::modifyData()
- * @return int|null
- */
-function df_product_current_id() {return !($p = df_product_current() /** @var P $p */) ? null : $p->getId();}
 
 /**
  * 2018-06-04
