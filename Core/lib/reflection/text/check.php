f45ff86368e439bdef8e17445c84f16ee7eb74a0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-15 01:48:46 +0100

    refactoring

--- Core/lib/reflection/text/check.php
+++ Core/lib/reflection/text/check.php
@@ -8,6 +8,7 @@ use ReflectionClass as RC;
  * Намеренно не объединяем строки в единное выражение, чтобы собака @ не подавляла сбои первой строки.
  * Такие сбои могут произойти при синтаксических ошибках в проверяемом классе
  * (похоже, getInstanceType как-то загружает код класса).
+ * @used-by df_assert_class_exists()
  * @used-by df_con_hier_suf()
  * @used-by \Df\Core\R\ConT::generic()
  * @used-by \Df\Payment\Block\Info::checkoutSuccess()
