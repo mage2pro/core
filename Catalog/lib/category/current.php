b69afd8031792e08a518ca8b2cc99e912af4b369
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-20 00:49:34 +0100

    https://github.com/mage2pro/core/issues/363

--- Catalog/lib/category/current.php
+++ Catalog/lib/category/current.php
@@ -3,6 +3,7 @@ use Magento\Catalog\Model\Category as C;
 use Magento\Framework\Exception\NotFoundException as NotFound;
 /**
  * 2024-04-15
+ * 2024-04-20 "Implement `df_category_current()`" https://github.com/mage2pro/core/issues/363
  * @param Closure|bool|mixed $onError
  * @return C|null
  * @throws NotFound|Exception
