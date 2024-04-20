2448074043dc4f83af64afd1435b7819b1afd3a0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-20 01:25:37 +0100

    ⠀

--- Catalog/lib/category/current.php
+++ Catalog/lib/category/current.php
@@ -2,7 +2,9 @@
 use Magento\Catalog\Model\Category as C;
 /**
  * 2024-04-15
- * 2024-04-20 "Implement `df_category_current()`" https://github.com/mage2pro/core/issues/363
+ * 2024-04-20
+ * 1) "Implement `df_category_current()`" https://github.com/mage2pro/core/issues/363
+ * 2) https://stackoverflow.com/a/46414822
  * @used-by df_category_level()
  */
 function df_category_current():C {return df_catalog_layer()->getCurrentCategory();}
