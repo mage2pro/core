bbbe4c4c4938167446cfaf4ad3dd007f58439c3f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-06 19:02:02 +0100

    https://github.com/mage2pro/core/issues/365

--- Cms/lib/block/get.php
+++ Cms/lib/block/get.php
@@ -10,12 +10,13 @@ use Magento\Framework\Exception\NoSuchEntityException as NSE;
  * @see \Magento\Cms\Model\ResourceModel\Block::load()
  * @see \Magento\Cms\Model\ResourceModel\Block::getBlockId()
  * 2024-05-06
- * 1) @uses \Magento\Cms\Model\BlockRepository::getById() works identically to
+ * 1) "Improve `df_cms_block*` functions": https://github.com/mage2pro/core/issues/365
+ * 2.1) @uses \Magento\Cms\Model\BlockRepository::getById() works identically to
  * @see \Magento\Cms\Model\GetBlockByIdentifier::execute()
  * because the `$storeId` parameter of @see \Magento\Cms\Model\GetBlockByIdentifier::execute() is ignored:
  * https://github.com/magento/magento2/blob/2.4.7/app/code/Magento/Cms/Model/BlockRepository.php#L142-L157
  * https://github.com/magento/magento2/blob/2.4.7/app/code/Magento/Cms/Model/GetBlockByIdentifier.php#L40-L54
- * 2) @see \Magento\Cms\Model\GetBlockByIdentifier is available in Magento ≥ 2.3.
+ * 2.2) @see \Magento\Cms\Model\GetBlockByIdentifier is available in Magento ≥ 2.3.
  * @used-by df_cms_block_content()
  * @param B|int|string $b
  * @param Closure|bool|string $onE [optional]
