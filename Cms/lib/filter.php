6333606c0391bd7c06a92cf60d9c82c8e0039e08
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-06 18:34:57 +0100

    refactoring

--- Cms/lib/filter.php
+++ Cms/lib/filter.php
@@ -2,6 +2,7 @@
 use Magento\Cms\Model\Template\FilterProvider as FP;
 /**
  * 2024-03-25
+ * 2024-05-06 https://github.com/magento/magento2/blob/2.0.0/app/code/Magento/Cms/Model/Template/FilterProvider.php
  * @used-by df_cms_filter_page()
  */
 function df_cms_filter_provider():FP {return df_o(FP::class);}
