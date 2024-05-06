360edec1bc84521faa0e26a0050c96765c11ca46
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-06 18:37:22 +0100

    refactoring

---
+++ Cms/lib/block/o.php
@@ -0,0 +1,15 @@
+<?php
+use Magento\Cms\Model\BlockRepository as BR;
+use Magento\Cms\Model\ResourceModel\Block\Collection as C;
+
+/**
+ * 2018-05-21
+ * @used-by df_cms_block()
+ */
+function df_cms_block_r():BR {return df_o(BR::class);}
+
+/**
+ * 2018-05-21
+ * @used-by \Df\Config\Source\Block::map()
+ */
+function df_cms_blocks():C {return df_new_om(C::class);}
--- Cms/lib/block/other.php
+++ Cms/lib/block/other.php
@@ -1,7 +1,5 @@
 <?php
 use Magento\Cms\Model\Block as B;
-use Magento\Cms\Model\BlockRepository as BR;
-use Magento\Cms\Model\ResourceModel\Block\Collection as C;
 use Magento\Framework\Exception\NoSuchEntityException as NSE;
 
 /**
@@ -35,15 +33,3 @@ function df_cms_block($b):B {return $b instanceof B ? $b : df_cms_block_r()->get
 function df_cms_block_content($id, $onE = ''):string {return df_try(function() use($id):string {return
 	df_cms_block($id)->getContent()
 ;}, $onE);}
-
-/**
- * 2018-05-21
- * @used-by df_cms_block()
- */
-function df_cms_block_r():BR {return df_o(BR::class);}
-
-/**
- * 2018-05-21
- * @used-by \Df\Config\Source\Block::map()
- */
-function df_cms_blocks():C {return df_new_om(C::class);}
