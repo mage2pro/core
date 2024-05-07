1e7f149be659bdd53bf77b82a8608f432b4d1404
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-07 14:59:15 +0100

    https://github.com/cabinetsbay/core/issues/6

--- Framework/lib/view/block.php
+++ Framework/lib/view/block.php
@@ -68,7 +68,7 @@ function df_block($c, $data = [], string $t = '', array $vars = []) {
  * @used-by \Dfe\Stripe\Block\Multishipping::_toHtml()
  * @used-by \Inkifi\Map\HTML::tiles()
  * @used-by \KingPalm\B2B\Block\Registration::_toHtml()
- * @used-by app/design/frontend/Cabinetsbay/cabinetsbay_default/Magento_Catalog/templates/category/header.phtml (https://github.com/cabinetsbay/site/issues/105)
+ * @used-by vendor/cabinetsbay/catalog/view/frontend/templates/category/header.phtml (https://github.com/cabinetsbay/site/issues/105)
  * @used-by app/design/frontend/Cabinetsbay/cabinetsbay_default/Magento_Catalog/templates/category/view.phtml (https://github.com/cabinetsbay/site/issues/112)
  * @used-by vendor/cabinetsbay/catalog/view/frontend/templates/category/l2/l3.phtml (https://github.com/cabinetsbay/site/issues/112)
  * @used-by vendor/cabinetsbay/catalog/view/frontend/templates/category/l2/l3/items.phtml (https://github.com/cabinetsbay/site/issues/112)
