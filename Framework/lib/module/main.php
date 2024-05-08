fb5c891f1e9f71a98005cc01df6d1799a9245be3
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-08 03:37:42 +0100

    https://github.com/cabinetsbay/site/issues/98

--- Framework/lib/module/main.php
+++ Framework/lib/module/main.php
@@ -6,6 +6,7 @@ use Magento\Framework\Module\ModuleListInterface as IML;
 /**
  * 2024-05-08 "Implement `df_assert_module_enabled()`": https://github.com/mage2pro/core/issues/367
  * @used-by df_module_name_by_path()
+ * @used-by \Sharapov\Cabinetsbay\Setup\InstallData::install() (https://github.com/cabinetsbay/site/issues/98)
  */
 function df_assert_module_enabled(string $m):string {return df_assert(df_module_enabled($m), "The `{$m}` module must be enabled.");}
 
@@ -14,7 +15,6 @@ function df_assert_module_enabled(string $m):string {return df_assert(df_module_
  * @used-by df_caller_entry_m()
  * @used-by df_log_l()
  * @used-by df_msi()
- * @used-by \Sharapov\Cabinetsbay\Setup\InstallData::install() (https://github.com/cabinetsbay/site/issues/98)
  */
 function df_module_enabled(string $m):bool {return df_module_m()->isEnabled(df_module_name($m));}
 
