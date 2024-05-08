5fee3de8da51b3435c05f2bbdbf9915d5a38804f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-08 03:22:31 +0100

    refactoring

--- Qa/lib/validation/assert/other.php
+++ Qa/lib/validation/assert/other.php
@@ -4,41 +4,6 @@ use Df\Qa\Method as Q;
 use Throwable as Th; # 2023-08-03 "Treat `\Throwable` similar to `\Exception`": https://github.com/mage2pro/core/issues/311
 
 /**
- * 2016-11-10
- * @used-by df_con_heir()
- * @used-by df_con_hier()
- * @used-by df_eav_update()
- * @used-by df_load()
- * @used-by df_newa()
- * @used-by df_trans()
- * @used-by dfpex_args()
- * @used-by \Df\Payment\Choice::f()
- * @used-by \Df\Payment\Operation\Source\Creditmemo::cm()
- * @used-by \Df\Payment\Operation\Source\Order::ii()
- * @used-by \Df\Payment\Operation\Source\Quote::ii()
- * @used-by \Df\Payment\W\Strategy::handle()
- * @used-by \Df\Payment\W\Strategy::m()
- * @used-by \Df\Payment\W\Strategy\Refund::_handle()
- * @param string|object $v
- * @param string|object|null $c [optional]
- * @param string|Th|null $m [optional]
- * @return string|object
- * @throws DFE
- */
-function df_ar($v, $c = null, $m = null) {return dfcf(function($v, $c = null, $m = null) {
-	if ($c) {
-		$c = df_cts($c);
-		!is_null($v) ?: df_error($m ?: "Expected class: «{$c}», given `null`.");
-		is_object($v) || is_string($v) ?: df_error($m ?: "Expected class: «{$c}», given: %s.", df_type($v));
-		$cv = df_assert_class_exists(df_cts($v)); /** @var string $cv */
-		if (!is_a($cv, $c, true)) {
-			df_error($m ?: "Expected class: «{$c}», given class: «{$cv}».");
-		}
-	}
-	return $v;
-}, func_get_args());}
-
-/**
  * 2021-03-22
  * @used-by df_date_create()
  * @param int|float $v
