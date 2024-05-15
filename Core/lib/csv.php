2a941f68e4739c72b54df35b0b60a88c66900a91
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-16 00:32:59 +0100

    refactoring

--- Core/lib/csv.php
+++ Core/lib/csv.php
@@ -57,6 +57,7 @@ function df_csv_parse_int($s):array {return df_int(df_csv_parse($s));}
  * 		df_csv_pretty(['aaa', 'bbb']) → 'aaa, bbb, ccc'
  * 2022-11-26 We can not declare the argument as `string ...$a` because such a syntax will reject arrays: https://3v4l.org/jFdPm
  * @see df_csv()
+ * @used-by df_assert_in()
  * @used-by dfe_modules_log()
  * @used-by \Df\Framework\Validator\Currency::message()
  * @used-by \Df\Sentry\Client::send()
