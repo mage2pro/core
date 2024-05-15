5c50c70ec49e38856cb2deacc1ee243916797065
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-15 03:15:26 +0100

    refactoring

--- Eav/lib/o.php
+++ Eav/lib/o.php
@@ -9,6 +9,7 @@ use Magento\Framework\EntityManager\MetadataPool;
  * @used-by df_eav_ca()
  * @used-by df_eav_customer()
  * @used-by \Df\Customer\AddAttribute\Address::p()
+ * @used-by \Dfe\Markdown\DbRecord::__construct()
  * @used-by \Sharapov\Cabinetsbay\Setup\InstallData::install() (https://github.com/cabinetsbay/site/issues/98)
  */
 function df_eav_config():Config {return df_o(Config::class);}
