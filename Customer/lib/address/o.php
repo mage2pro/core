55bcec9598fb09339e72c56844ea10803d8b346f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-16 00:57:00 +0100

    refactoring

--- Customer/lib/address/o.php
+++ Customer/lib/address/o.php
@@ -1,25 +1,25 @@
 <?php
-use Magento\Customer\Api\AddressRepositoryInterface as IAddressRep;
-use Magento\Customer\Helper\Address as AddressH;
-use Magento\Customer\Model\AddressRegistry;
-use Magento\Customer\Model\ResourceModel\AddressRepository as AddressRep;
+use Magento\Customer\Api\AddressRepositoryInterface as IRep;
+use Magento\Customer\Helper\Address as H;
+use Magento\Customer\Model\AddressRegistry as Reg;
+use Magento\Customer\Model\ResourceModel\AddressRepository as Rep;
 
 /**
  * 2019-06-01
  * @used-by \KingPalm\B2B\Block\Registration::region()
  */
-function df_address_h():AddressH {return df_o(AddressH::class);}
+function df_address_h():H {return df_o(H::class);}
 
 /**
  * 2016-04-05
  * @used-by \Df\Customer\Plugin\Model\ResourceModel\AddressRepository::aroundSave()
  * @used-by \Dfe\Customer\Plugin\Customer\Model\ResourceModel\AddressRepository::aroundSave()
  */
-function df_address_registry():AddressRegistry {return df_o(AddressRegistry::class);}
+function df_address_registry():Reg {return df_o(Reg::class);}
 
 /**
  * 2021-05-07
  * @used-by \Df\Quote\Plugin\Model\QuoteAddressValidator::doValidate()
- * @return IAddressRep|AddressRep
+ * @return IRep|Rep
  */
-function df_customer_address_rep() {return df_o(IAddressRep::class);}
+function df_customer_address_rep() {return df_o(IRep::class);}
