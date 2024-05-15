78dfe1dfd475f721bf507130404400562b76552c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-15 03:17:05 +0100

    refactoring

--- Eav/lib/customer.php
+++ Eav/lib/customer.php
@@ -1,8 +1,8 @@
 <?php
-use Magento\Eav\Model\Entity\Type as EntityType;
+use Magento\Eav\Model\Entity\Type as T;
 
 /**
  * 2015-10-12
  * @used-by df_customer_att()
  */
-function df_eav_customer():EntityType {return df_eav_config()->getEntityType('customer');}
+function df_eav_customer():T {return df_eav_config()->getEntityType('customer');}
