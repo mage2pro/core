deccb7134d7d8ef2a4d7e3dd4737c39bc58a1408
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-16 03:23:52 +0100

    https://github.com/mage2pro/core/issues/373

--- Eav/lib/attribute.php
+++ Eav/lib/attribute.php
@@ -15,7 +15,7 @@ function df_att_code2id(string $c):int {return df_first(df_fetch_col_int(
  * 2024-05-16 "Implement `df_att_val_s()`": https://github.com/mage2pro/core/issues/373
  * @uses \Magento\Eav\Model\Entity\Attribute\Source\AbstractSource::getOptionText()
  */
-function df_att_val(M $m, A $a, string $k, string $d = ''):string {return df_fnes($r = $m[$a->getAttributeCode()]) ? $d : (
+function df_att_val(M $m, A $a, string $d = ''):string {return df_fnes($r = $m[$a->getAttributeCode()]) ? $d : (
 	!$a->usesSource() ? $r : (
 		/**
 		 * 2020-01-31
