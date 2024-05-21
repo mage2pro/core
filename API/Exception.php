3ed9e94fd9d97e592536a72039034563c4627dd8
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-22 00:41:03 +0100

    refactoring

--- API/Exception.php
+++ API/Exception.php
@@ -29,10 +29,11 @@ abstract class Exception extends \Df\Core\Exception {
 	/**
 	 * 2017-07-09
 	 * @used-by \Df\API\Client::_p()
-	 * @see \Dfe\ZohoBI\API\Validator::short()
 	 * @see \Dfe\AlphaCommerceHub\API\Validator::short()
+	 * @see \Dfe\Dynamics365\API\Validator\JSON::short()
 	 * @see \Dfe\Sift\API\Validator\GetDecisions::short()
 	 * @see \Dfe\Square\API\Validator::short()
+	 * @see \Dfe\ZohoBI\API\Validator::short()
 	 */
 	function short():string {return $this->long();}
 }
