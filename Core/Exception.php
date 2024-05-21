d77950f006d40a7a02673d2eeb3f952ad17fbc16
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-21 23:22:33 +0100

    https://github.com/mage2pro/core/issues/376

--- Core/Exception.php
+++ Core/Exception.php
@@ -21,7 +21,7 @@ use \Throwable as Th; # 2023-08-02 "Treat `\Throwable` similar to `\Exception`":
  */
 class Exception extends LE implements \ArrayAccess {
 	/**
-	 * Обратите внимание, что PHP разрешает сигнатуре конструктора класса-потомка
+	 * PHP разрешает сигнатуре конструктора класса-потомка
 	 * отличаться от сигнатуры конструктора класса родителя: http://3v4l.org/qQdJ3
 	 * @see \Df\API\Exception\HTTP::__construct()
 	 * @see \Df\API\Response\Validator::__construct()
