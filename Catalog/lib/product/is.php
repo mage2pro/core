5df5ccb275723f715a65b7b1e69415a08ce876b0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-21 23:57:19 +0100

    https://github.com/mage2pro/core/issues/383

--- Catalog/lib/product/is.php
+++ Catalog/lib/product/is.php
@@ -4,6 +4,7 @@ use Magento\Catalog\Model\Product as P;
 /**
  * 2024-05-22 "Implement `df_is_p()`": https://github.com/mage2pro/core/issues/383
  * @used-by df_product()
+ * @used-by df_product_id()
  * @param mixed $v
  */
 function df_is_p($v):bool {return $v instanceof P;}
