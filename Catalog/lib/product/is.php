e395c5ee1346f12242a6e1bc90daeb522b296710
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-21 23:57:48 +0100

    https://github.com/mage2pro/core/issues/383

--- Catalog/lib/product/is.php
+++ Catalog/lib/product/is.php
@@ -3,6 +3,7 @@ use Magento\Catalog\Model\Product as P;
 
 /**
  * 2024-05-22 "Implement `df_is_p()`": https://github.com/mage2pro/core/issues/383
+ * @used-by df_att_set()
  * @used-by df_product()
  * @used-by df_product_id()
  * @param mixed $v
