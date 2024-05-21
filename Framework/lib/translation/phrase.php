f10c5d88aea1fab8f6ac95cb520cc3e7e27b19ef
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-21 23:40:04 +0100

    https://github.com/mage2pro/core/issues/381

--- Framework/lib/translation/phrase.php
+++ Framework/lib/translation/phrase.php
@@ -1,6 +1,12 @@
 <?php
 use Magento\Framework\Phrase as P;
 /**
+ * 2024-05-22 "Implement `df_is_phrase()`": https://github.com/mage2pro/core/issues/381
+ * @param mixed $v
+ */
+function df_is_phrase($v):bool {return $v instanceof P;}
+
+/**
  * 2016-07-14
  * @used-by df_checkout_message()
  * @used-by df_message_add()
