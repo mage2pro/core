033d4d601fec42a55602c654ce3157243e2400be
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-23 20:18:44 +0100

    https://github.com/mage2pro/core/issues/390

--- Catalog/lib/category/data-provider.php
+++ Catalog/lib/category/data-provider.php
@@ -1,6 +1,8 @@
 <?php
 use Magento\Catalog\Model\Category\DataProvider as DP;
 /**
- * 2024-05-23 "Implement `df_category_data_provider()`": https://github.com/mage2pro/core/issues/390
+ * 2024-05-23
+ * 1) "Implement `df_category_data_provider()`": https://github.com/mage2pro/core/issues/390
+ * 2) It exists in Magento ≥ 2.1: https://github.com/magento/magento2/commit/73d75d9e
  */
-function df_category_data_provider():DP {return df_o(DP::class);}
+function df_category_dp():DP {return df_o(DP::class);}
