e41030d3baa08cb9db1d44fbd9321954b0598b1b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-23 18:49:22 +0100

    https://github.com/mage2pro/core/issues/389

--- Eav/lib/type.php
+++ Eav/lib/type.php
@@ -13,6 +13,11 @@ use Magento\Eav\Model\Entity\Type as T;
 function df_eav_ca():T {return df_eav_type(ICustomerAddressMetadata::ENTITY_TYPE_ADDRESS);}
 
 /**
+ * 2024-05-23 "Implement `df_eav_category()`": https://github.com/mage2pro/core/issues/389
+ */
+function df_eav_category():T {return df_eav_type('catalog_category');}
+
+/**
  * 2015-10-12
  * 2024-05-23
  * The 'customer' constant is also duplicated here:
@@ -25,6 +30,7 @@ function df_eav_customer():T {return df_eav_type(C::ENTITY);}
 /**
  * 2024-05-23 "Implement `df_eav_type()`": https://github.com/mage2pro/core/issues/388
  * @used-by df_eav_ca()
+ * @used-by df_eav_category()
  * @used-by df_eav_customer()
  * @used-by \Dfe\Markdown\DbRecord::__construct()
  */
