1f2dec8dc6ad2b3b198a56ccb847e0e17df3565e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-23 18:42:29 +0100

    https://github.com/mage2pro/core/issues/388

--- Eav/lib/type.php
+++ Eav/lib/type.php
@@ -1,7 +1,8 @@
 <?php
-use Magento\Eav\Model\Entity\Type as T;
 use Magento\Customer\Api\AddressMetadataInterface as ICustomerAddressMetadata;
 use Magento\Customer\Api\CustomerMetadataInterface as ICustomerMetadata;
+use Magento\Customer\Model\Customer as C;
+use Magento\Eav\Model\Entity\Type as T;
 /**
  * 2019-03-06
  * @used-by \Df\Customer\AddAttribute\Address::p()
@@ -14,9 +15,13 @@ function df_eav_ca():T {return df_eav_type(ICustomerAddressMetadata::ENTITY_TYPE
 
 /**
  * 2015-10-12
+ * 2024-05-23
+ * The 'customer' constant is also duplicated here:
+ * @see \Magento\Customer\Api\CustomerMetadataInterface::ENTITY_TYPE_CUSTOMER
+ * @see \Magento\Customer\Api\CustomerMetadataManagementInterface::ENTITY_TYPE_CUSTOMER
  * @used-by df_customer_att()
  */
-function df_eav_customer():T {return df_eav_type(ICustomerMetadata::ENTITY_TYPE_CUSTOMER);}
+function df_eav_customer():T {return df_eav_type(C::ENTITY);}
 
 /**
  * 2024-05-23 "Implement `df_eav_type()`": https://github.com/mage2pro/core/issues/388
