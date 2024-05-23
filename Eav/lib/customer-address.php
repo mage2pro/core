12359adcc55b0dd2c739c9666201224aefbd6e29
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-23 18:30:22 +0100

    https://github.com/mage2pro/core/issues/388

--- Eav/lib/customer-address.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Magento\Eav\Model\Entity\Type as EntityType;
-
-/**
- * 2019-03-06
- * @used-by \Df\Customer\AddAttribute\Address::p()
- */
-function df_eav_ca():EntityType {return df_eav_config()->getEntityType('customer_address');}
--- Eav/lib/type.php
+++ Eav/lib/type.php
@@ -1,6 +1,13 @@
 <?php
 use Magento\Eav\Model\Entity\Type as T;
 /**
+ * 2019-03-06
+ * @used-by \Df\Customer\AddAttribute\Address::p()
+ */
+function df_eav_ca():T {return df_eav_type('customer_address');}
+
+/**
  * 2024-05-23 "Implement `df_eav_type()`": https://github.com/mage2pro/core/issues/388
+ * @used-by df_eav_ca()
  */
 function df_eav_type(string $t):T {return df_eav_config()->getEntityType($t);}
