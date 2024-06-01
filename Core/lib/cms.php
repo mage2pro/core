04bb4aa97994018d06dfbe91bec48d6f6044163b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-01 16:41:16 +0100

    https://github.com/mage2pro/core/issues/392

--- Core/lib/cms.php
+++ Core/lib/cms.php
@@ -3,6 +3,7 @@ use Magento\Cms\Model\Wysiwyg\Config;
 use Magento\Ui\Component\Wysiwyg\ConfigInterface;
 /**
  * 2016-01-06
+ * @used-by \Df\Widget\P\Wysiwyg::prepareElementHtml() (https://github.com/mage2pro/core/issues/392)
  * @used-by \Dfe\Markdown\FormElement::config()
  * @used-by \Dfe\Markdown\FormElement::enabled()
  * @return Config|ConfigInterface
