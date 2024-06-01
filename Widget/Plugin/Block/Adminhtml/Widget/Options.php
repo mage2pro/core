8d0c7307003127dab374b64fadefb1c699411863
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-01 13:51:17 +0100

    https://github.com/mage2pro/core/issues/397

--- Widget/Plugin/Block/Adminhtml/Widget/Options.php
+++ Widget/Plugin/Block/Adminhtml/Widget/Options.php
@@ -15,6 +15,20 @@ final class Options {
 			df_log($t);
 			/** 2024-06-01 Similar to @see \Df\Cron\Plugin\Console\Command\CronCommand::aroundRun() */
 			Latest::register($t);
+			/**
+			 * 2024-06-01
+			 * 1) It will be caught by @see \Magento\Widget\Controller\Adminhtml\Widget\LoadOptions::execute():
+			 * 		catch (\Magento\Framework\Exception\LocalizedException $e) {
+			 * 			$result = ['error' => true, 'message' => $e->getMessage()];
+			 * 			$this->getResponse()->representJson(
+			 * 				$this->_objectManager->get(\Magento\Framework\Json\Helper\Data::class)->jsonEncode($result)
+			 * 			);
+			 * 		}
+			 * https://github.com/magento/magento2/blob/2.4.7/app/code/Magento/Widget/Controller/Adminhtml/Widget/LoadOptions.php#L53-L58
+			 * 2) As you can see,
+			 * @see \Magento\Widget\Controller\Adminhtml\Widget\LoadOptions::execute() does not log the exception,
+			 * that is why we need the plugin.
+			 */
 			throw $t;
 		}
 	}
