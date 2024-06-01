505853b7d5bdc87d310afecddfe4f1f6fb513859
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-01 12:51:41 +0100

    https://github.com/mage2pro/core/issues/392

--- Widget/P/Wysiwyg.php
+++ Widget/P/Wysiwyg.php
@@ -1,4 +1,6 @@
 <?php
+namespace Df\Widget\P;
 # 2024-06-01
 # "Implement an ability to use the built-in WYSIWYG editor as a CMS widget parameter":
 # https://github.com/mage2pro/core/issues/392
+final class Wysiwyg {}
