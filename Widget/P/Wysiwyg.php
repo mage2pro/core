de3b9b0bd0a93c330170efaf9dcb0166ed5e1bb7
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-01 13:05:53 +0100

    https://github.com/mage2pro/core/issues/392

--- Widget/P/Wysiwyg.php
+++ Widget/P/Wysiwyg.php
@@ -1,6 +1,11 @@
 <?php
 namespace Df\Widget\P;
-# 2024-06-01
-# "Implement an ability to use the built-in WYSIWYG editor as a CMS widget parameter":
-# https://github.com/mage2pro/core/issues/392
+/**
+ * 2024-06-01
+ * "Implement an ability to use the built-in WYSIWYG editor as a CMS widget parameter":
+ * https://github.com/mage2pro/core/issues/392
+ * @used-by \Magento\Framework\View\Element\BlockFactory::createBlock():
+ * 		$block = $this->objectManager->create($blockName, $arguments);
+ * https://github.com/magento/magento2/blob/2.4.7/lib/internal/Magento/Framework/View/Element/BlockFactory.php#L44
+ */
 final class Wysiwyg {}
