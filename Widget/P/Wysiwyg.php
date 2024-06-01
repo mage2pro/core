d0864d5e5bb8bdea03aac1b8f372d08808afadf9
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-01 13:22:33 +0100

    https://github.com/mage2pro/core/issues/395

--- Widget/P/Wysiwyg.php
+++ Widget/P/Wysiwyg.php
@@ -3,13 +3,16 @@ namespace Df\Widget\P;
 use Magento\Framework\View\Element\AbstractBlock;
 /**
  * 2024-06-01
- * "Implement an ability to use the built-in WYSIWYG editor as a CMS widget parameter":
+ * 1) "Implement an ability to use the built-in WYSIWYG editor as a CMS widget parameter":
  * https://github.com/mage2pro/core/issues/392
  * @used-by \Magento\Framework\View\Element\BlockFactory::createBlock():
  * 		$block = $this->objectManager->create($blockName, $arguments);
  * https://github.com/magento/magento2/blob/2.4.7/lib/internal/Magento/Framework/View/Element/BlockFactory.php#L44
+ * 2.1) @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+ * 2.2) «Class Df\Widget\P\Wysiwyg\Interceptor may not inherit from final class (Df\Widget\P\Wysiwyg)»:
+ * https://github.com/mage2pro/core/issues/395
  */
-final class Wysiwyg
+class Wysiwyg
 	/**
 	 * 2024-06-01
 	 * 1.1) «Invalid block type: Df\Widget\P\Wysiwyg»:
