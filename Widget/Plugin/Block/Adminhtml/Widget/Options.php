d86b1ba179329428d7315311ce8a755654070e79
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-01 13:41:05 +0100

    https://github.com/mage2pro/core/issues/397

---
+++ Widget/Plugin/Block/Adminhtml/Widget/Options.php
@@ -0,0 +1,5 @@
+<?php
+namespace Df\Widget\Plugin\Block\Adminhtml\Widget;
+use Magento\Widget\Block\Adminhtml\Widget\Options as Sb;
+# 2024-06-01 "Log widget creation errors": https://github.com/mage2pro/core/issues/397
+final class Options {}
