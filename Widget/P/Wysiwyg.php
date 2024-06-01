5dd41125dc710d4f5bdff9dfd7e8348dcd7b3faf
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-01 13:18:21 +0100

    https://github.com/mage2pro/core/issues/394

--- Widget/P/Wysiwyg.php
+++ Widget/P/Wysiwyg.php
@@ -1,6 +1,6 @@
 <?php
 namespace Df\Widget\P;
-use Magento\Framework\View\Element\BlockInterface as IBlock;
+use Magento\Framework\View\Element\AbstractBlock;
 /**
  * 2024-06-01
  * "Implement an ability to use the built-in WYSIWYG editor as a CMS widget parameter":
@@ -12,18 +12,32 @@ use Magento\Framework\View\Element\BlockInterface as IBlock;
 final class Wysiwyg
 	/**
 	 * 2024-06-01
-	 * «Df\Widget\P\Wysiwyg does not implement BlockInterface»: https://github.com/mage2pro/core/issues/393
-	 * @see \Magento\Framework\View\Element\BlockFactory::createBlock():
+	 * 1.1) «Invalid block type: Df\Widget\P\Wysiwyg»:
+	 * https://github.com/mage2pro/core/issues/394
+	 * 1.2) @see \Magento\Framework\View\Layout\Generator\Block::getBlockInstance():
+	 * 		if (!$block instanceof \Magento\Framework\View\Element\AbstractBlock) {
+	 * 			throw new LocalizedException(
+	 * 				new \Magento\Framework\Phrase(
+	 * 					'Invalid block type: %1',
+	 * 					[is_object($block) ? get_class($block) : (string) $block]
+	 * 				),
+	 * 				$e
+	 * 			);
+	 *		}
+	 * https://github.com/magento/magento2/blob/2.4.7/lib/internal/Magento/Framework/View/Layout/Generator/Block.php#L277-L285
+	 * 2.1) «Df\Widget\P\Wysiwyg does not implement BlockInterface»: https://github.com/mage2pro/core/issues/393
+	 * 2.2) @see \Magento\Framework\View\Element\BlockFactory::createBlock():
 	 * 		if (!$block instanceof BlockInterface) {
 	 * 			throw new \LogicException($blockName . ' does not implement BlockInterface');
 	 * 		}
 	 * https://github.com/magento/magento2/blob/2.4.7/lib/internal/Magento/Framework/View/Element/BlockFactory.php#L45-L47
 	 */
-	implements IBlock {
+	extends AbstractBlock {
 	/**
 	 * 2024-06-01
 	 * @override
-	 * @see \Magento\Framework\View\Element\BlockInterface::toHtml()
+	 * @see \Magento\Framework\View\Element\AbstractBlock::_toHtml()
+	 * @used-by \Magento\Framework\View\Element\AbstractBlock::_loadCache()
 	 */
-	function toHtml():string {return __METHOD__;}
+	final protected function _toHtml():string {return __METHOD__;}
 }
