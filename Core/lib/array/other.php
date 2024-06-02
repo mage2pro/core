a91c5c304fae78438f826f60b79cc377419aedc6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 00:31:09 +0100

    refactoring

--- Core/lib/array/other.php
+++ Core/lib/array/other.php
@@ -83,27 +83,6 @@ function dfa_fill(int $startIndex, int $length, $v):array {return !$length ? [] 
 function dfa_repeated(array $a):array {return array_values(array_unique(array_diff_key($a, array_unique($a))));}
 
 /**
- * 2016-09-02
- * @see dfa_deep_unset()
- * @uses array_flip() correctly handles empty arrays.
- * 2019-11-15
- * Previously, it was used as:
- * 		$this->_data = dfa_unset($this->_data, 'can_use_default_value', 'can_use_website_value', 'scope');
- * I replaced it with:
- * 		$this->unsetData(['can_use_default_value', 'can_use_website_value', 'scope']);
- * 2022-11-26
- * We can not declare the argument $k as `string ...$a` because such a syntax will reject arrays: https://3v4l.org/jFdPm
- * @used-by \Df\Config\Backend::value()
- * @used-by \Df\Config\Backend\ArrayT::processI()
- * @used-by \Df\Framework\Request::clean()
- * @used-by \Dfe\Markdown\Observer\Catalog\ControllerAction::processPost()
- * @param array(string => mixed) $a
- * @param string|string[] $k
- * @return array(string => mixed)
- */
-function dfa_unset(array $a, ...$k):array {return array_diff_key($a, array_flip(df_args($k)));}
-
-/**
  * Алгоритм взят отсюда: https://php.net/manual/function.array-unshift.php#106570
  * 2022-10-31 @deprecated It is unused.
  * @param array(string => mixed) $a
---
+++ Core/lib/array/other/unset.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 2016-09-02
+ * @see dfa_deep_unset()
+ * @uses array_flip() correctly handles empty arrays.
+ * 2019-11-15
+ * Previously, it was used as:
+ * 		$this->_data = dfa_unset($this->_data, 'can_use_default_value', 'can_use_website_value', 'scope');
+ * I replaced it with:
+ * 		$this->unsetData(['can_use_default_value', 'can_use_website_value', 'scope']);
+ * 2022-11-26
+ * We can not declare the argument $k as `string ...$a` because such a syntax will reject arrays: https://3v4l.org/jFdPm
+ * @used-by \Df\Config\Backend::value()
+ * @used-by \Df\Config\Backend\ArrayT::processI()
+ * @used-by \Df\Framework\Request::clean()
+ * @used-by \Dfe\Markdown\Observer\Catalog\ControllerAction::processPost()
+ * @param array(string => mixed) $a
+ * @param string|string[] $k
+ * @return array(string => mixed)
+ */
+function dfa_unset(array $a, ...$k):array {return array_diff_key($a, array_flip(df_args($k)));}
