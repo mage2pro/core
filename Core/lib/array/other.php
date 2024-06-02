473c3281997217af7872c61a4dc5e00e5d21481a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 00:33:40 +0100

    refactoring

--- Core/lib/array/other.php
+++ Core/lib/array/other.php
@@ -16,20 +16,6 @@ use Magento\Framework\DataObject as _DO;
  */
 function df_array($v):array {return is_array($v) ? $v : [$v];}
 
-/**
- * 2016-09-07
- * 2017-03-06 @uses mb_substr() корректно работает с $length = null.
- * 2022-11-23
- * If $length is 0, then @uses mb_substr() returns an empty string: https://3v4l.org/ijD3V
- * If $length is NULL, then @uses mb_substr() returns all characters to the end of the string.
- * https://3v4l.org/ijD3V
- * 2022-11-26 That is why I use @uses df_etn().
- * @used-by \Df\Payment\Charge::metadata()
- * @param string[] $a
- * @return string[]
- */
-function dfa_chop(array $a, int $length = 0):array {return df_map('mb_substr', $a, [0, df_etn($length)]);}
-
 /**               
  * 2016-11-25
  * @used-by \Df\Config\Source\SizeUnit::map()
