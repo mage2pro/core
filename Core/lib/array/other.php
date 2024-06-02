5a6b1a7212da9ec5dc5b1469b741f313cd91ed0c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 00:46:30 +0100

    refactoring

--- Core/lib/array/other.php
+++ Core/lib/array/other.php
@@ -15,17 +15,3 @@ use Magento\Framework\DataObject as _DO;
  * @param mixed|mixed[] $v
  */
 function df_array($v):array {return is_array($v) ? $v : [$v];}
-
-/**               
- * 2016-11-25
- * @used-by \Df\Config\Source\SizeUnit::map()
- * @used-by \Dfe\AmazonLogin\Source\Button\Native\Size::map()
- * @used-by \Dfe\CheckoutCom\Source\Prefill::map()
- * @used-by \Dfe\FacebookLogin\Source\Button\Size::map()
- * @used-by \Dfe\ZohoCRM\Source\Domain::map() 
- * @used-by \KingPalm\B2B\Source\Type::map()
- * @used-by df_a_to_options()
- * @param string|int|int[]|string[] ...$a
- * @return array(int|string => int|string)
- */
-function dfa_combine_self(...$a):array {$a = df_args($a); return array_combine($a, $a);}
---
+++ Core/lib/array/other/combine_self.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 2016-11-25
+ * @used-by \Df\Config\Source\SizeUnit::map()
+ * @used-by \Dfe\AmazonLogin\Source\Button\Native\Size::map()
+ * @used-by \Dfe\CheckoutCom\Source\Prefill::map()
+ * @used-by \Dfe\FacebookLogin\Source\Button\Size::map()
+ * @used-by \Dfe\ZohoCRM\Source\Domain::map()
+ * @used-by \KingPalm\B2B\Source\Type::map()
+ * @used-by df_a_to_options()
+ * @param string|int|int[]|string[] ...$a
+ * @return array(int|string => int|string)
+ */
+function dfa_combine_self(...$a):array {$a = df_args($a); return array_combine($a, $a);}
