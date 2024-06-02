fa626662d1f67aebcaa362d416f6ac548e17bec8
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 09:49:33 +0100

    11.0.8

--- Store/lib/website.php
+++ Store/lib/website.php
@@ -1,6 +1,7 @@
 <?php
 use Magento\Framework\Exception\NoSuchEntityException as NSE;
-use Magento\Store\Model\Store;
+use Magento\Sales\Model\Order as O;
+use Magento\Store\Model\Store as S;
 use Magento\Store\Model\Website as W;
 /**
  * 2019-11-22
@@ -12,10 +13,10 @@ use Magento\Store\Model\Website as W;
  * 		*) `null` or absert: the current website
  * 		*) `true`: the default website
  * @used-by df_website_code()
- * @param W|Store|int|string|null|bool $v [optional]
+ * @param W|S|int|string|null|bool $v [optional]
  * @throws NSE|Exception
  */
-function df_website($v = null):W {return $v instanceof Store ? $v->getWebsite() : df_store_m()->getWebsite($v);}
+function df_website($v = null):W {return $v instanceof S ? $v->getWebsite() : df_store_m()->getWebsite($v);}
 
 /**
  * 2019-11-22
@@ -27,7 +28,7 @@ function df_website($v = null):W {return $v instanceof Store ? $v->getWebsite() 
  * 		*) `null` or absent: the current website
  * 		*) `true`: the default website
  * @used-by df_msi_website2stockId()
- * @param W|Store|int|string|null|bool $v [optional]
+ * @param W|S|int|string|null|bool $v [optional]
  * @throws Exception
  * @throws NSE
  */
@@ -43,7 +44,7 @@ function df_website_code($v = null):string {return df_website($v)->getCode();}
  * 		*) `null` or absent: the current website
  * 		*) `true`: the default website
  * @used-by df_subscriber()
- * @param W|Store|int|string|null|bool $v [optional]
+ * @param W|S|int|string|null|bool $v [optional]
  * @throws Exception
  * @throws NSE
  */
