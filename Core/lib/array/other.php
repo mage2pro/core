eb8d11b20d1a5af382c7cce0925e3b88b24afae9
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 00:26:51 +0100

    refactoring

--- Core/lib/array/other.php
+++ Core/lib/array/other.php
@@ -83,38 +83,6 @@ function dfa_fill(int $startIndex, int $length, $v):array {return !$length ? [] 
 function dfa_repeated(array $a):array {return array_values(array_unique(array_diff_key($a, array_unique($a))));}
 
 /**
- * Работает в разы быстрее, чем @see array_unique()
- * «Just found that array_keys(array_flip($array)); is amazingly faster than array_unique();.
-  * About 80% faster on 100 element array,
-  * 95% faster on 1000 element array
-  * and 99% faster on 10000+ element array.»
- * http://stackoverflow.com/questions/5036504/php-performance-question-faster-to-leave-duplicates-in-array-that-will-be-searc#comment19991540_5036538
- * http://www.php.net/manual/en/function.array-unique.php#70786
- * 2015-02-06
- * Обратите внимание, что т.к. алгоритм @see dfa_unique_fast() использует @uses array_flip(),
- * то @see dfa_unique_fast() можно применять только в тех ситуациях,
- * когда массив содержит только строки и целые числа,
- * иначе вызов @uses array_flip() завершится сбоем уровня E_WARNING:
- * «array_flip(): Can only flip STRING and INTEGER values»
- * http://magento-forum.ru/topic/4695/
- * В реальной практике сбой случается, например, когда массив содержит значение null:
- * http://3v4l.org/bat52
- * Пример кода, приводящего к сбою: dfa_unique_fast(array(1, 2, 2, 3, null))
- * В то же время, несмотря на E_WARNING, метод всё-таки возвращает результат,
- * правда, без недопустимых значений:
- * при подавлении E_WARNING dfa_unique_fast(array(1, 2, 2, 3, null)) вернёт:
- * array(1, 2, 3).
- * Более того, даже если сбойный элемент содержится в середине исходного массива,
- * то результат при подавлении сбоя E_WARNING будет корректным (без недопустимых элементов):
- * dfa_unique_fast(array(1, 2, null,  2, 3)) вернёт тот же результат array(1, 2, 3).
- * http://3v4l.org/uvJoI
- * По этой причине добавил оператор @ перед @uses array_flip()
- * @param array(int|string => int|string) $a
- * @return array(int|string => int|string)
- */
-function dfa_unique_fast(array $a):array {return array_keys(@array_flip($a));}
-
-/**
  * 2016-09-02
  * @see dfa_deep_unset()
  * @uses array_flip() correctly handles empty arrays.
