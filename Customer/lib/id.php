eaf968c58f10da0785a71e2cdab1f3f087ba161a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 11:57:55 +0100

    https://github.com/mage2pro/core/issues/402

--- Customer/lib/id.php
+++ Customer/lib/id.php
@@ -5,7 +5,6 @@ use Magento\Customer\Model\Data\Customer as DC;
 /**
  * 2016-12-04
  * 2024-06-02 "Improve `df_customer_id()`": https://github.com/mage2pro/core/issues/402
- * @used-by df_customer()
  * @used-by df_customer_is_need_confirm()
  * @used-by df_subscriber()
  * @used-by \Df\Customer\Plugin\Js\CustomerId::afterGetSectionData()
