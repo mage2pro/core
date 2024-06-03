7b300283e60f48070ab14ba3302839b609687aff
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 01:04:50 +0100

    https://github.com/mage2pro/core/issues/403

--- Core/lib/array/other/ita.php
+++ Core/lib/array/other/ita.php
@@ -8,8 +8,9 @@
  * https://php.net/manual/migration82.other-changes.php#migration82.other-changes.functions.spl
  * 2023-07-26 "Replace `array|Traversable` with `iterable`": https://github.com/mage2pro/core/issues/255
  * 2024-06-03
- * 1) "Use `iterable` as an argument type": https://github.com/mage2pro/core/issues/403
- * 2) `iterable` as an argument type is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
+ * 1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
+ * 2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
+ * 3) https://php.net/manual/en/language.types.iterable.php
  * @used-by df_filter_f()
  * @used-by df_index()
  * @used-by df_map()
