4b763e67c41bf389be3ece2a6df489868a997b38
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 01:38:45 +0100

    https://github.com/mage2pro/core/issues/403

--- Core/lib/array/clean/filter.php
+++ Core/lib/array/clean/filter.php
@@ -94,9 +94,16 @@ function df_filter_head($a1, $a2):array {return df_filter_f($a1, $a2, function(a
 
 /**
  * 2023-07-26 "Implement `df_filter_tail()`": https://github.com/mage2pro/core/issues/263
+ * 2024-06-03
+ * 1.1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
+ * 1.2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
+ * 1.3) https://php.net/manual/en/language.types.iterable.php
+ * 2) We still can not use «Union Types» (e.g. `callable|iterable`) because they require PHP ≥ 8 (we need to support PHP ≥ 7.1):
+ * 2.1) https://php.watch/versions/8.0/union-types
+ * 2.2) https://3v4l.org/AOWmO
  * @used-by \Df\Qa\Trace::__construct()
- * @param callable|array(int|string => mixed)|array[]Traversable $a1
- * @param callable|array(int|string => mixed)|array[]|Traversable $a2
+ * @param callable|iterable $a1
+ * @param callable|iterable $a2
  * @return array(int|string => mixed)
  */
 function df_filter_tail($a1, $a2):array {return df_filter_f($a1, $a2, function(array $a, $f):array {
