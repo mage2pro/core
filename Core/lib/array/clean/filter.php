d479baf1dd44e1c8db0e6c6bd0c99bd9f586dc21
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 01:36:24 +0100

    https://github.com/mage2pro/core/issues/403

--- Core/lib/array/clean/filter.php
+++ Core/lib/array/clean/filter.php
@@ -32,8 +32,8 @@ function df_filter($a1, $a2):array {return df_filter_f($a1, $a2, 'array_filter')
  * 1.2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
  * 1.3) https://php.net/manual/en/language.types.iterable.php
  * 2) We still can not use «Union Types» (e.g. `callable|iterable`) because they require PHP ≥ 8 (we need to support PHP ≥ 7.1):
- * https://php.watch/versions/8.0/union-types
- * https://3v4l.org/AOWmO
+ * 2.1) https://php.watch/versions/8.0/union-types
+ * 2.2) https://3v4l.org/AOWmO
  * 3) "Use the `callable` type": https://github.com/mage2pro/core/issues/404
  * @used-by df_filter()
  * @used-by df_filter_head()
