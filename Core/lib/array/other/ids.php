be1b19bb5e196f4f8b1f4e369a9b95d85b61a601
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 01:43:34 +0100

    https://github.com/mage2pro/core/issues/403

--- Core/lib/array/other/ids.php
+++ Core/lib/array/other/ids.php
@@ -5,12 +5,12 @@ use Magento\Framework\DataObject as _DO;
 /**
  * 2016-07-31
  * 2024-06-03
- * We still can not use «Union Types» (e.g. `callable|iterable`) because they require PHP ≥ 8 (we need to support PHP ≥ 7.1):
- * https://php.watch/versions/8.0/union-types
- * https://3v4l.org/AOWmO
+ * 1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
+ * 2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
+ * 3) https://php.net/manual/en/language.types.iterable.php
  * @uses df_id()
  * @used-by \Df\Config\Backend\ArrayT::processI()
  * @param Traversable|array(int|string => _DO|AI) $c
  * @return int[]|string[]
  */
-function dfa_ids($c):array {return df_map('df_id', $c);}
+function dfa_ids(iterable $c):array {return df_map('df_id', $c);}
--- Core/lib/array/other/index.php
+++ Core/lib/array/other/index.php
@@ -2,9 +2,14 @@
 use Magento\Framework\DataObject as _DO;
 /**
  * 2015-12-30 Преобразует коллекцию или массив в карту.
+ * 2024-06-03
+ * 1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
+ * 2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
+ * 3) https://php.net/manual/en/language.types.iterable.php
+ * https://3v4l.org/AOWmO
  * @used-by df_mvars()
  * @used-by \Df\Config\A::get()
  * @param string|Closure $k
  * @param Traversable|array(int|string => _DO) $a
  */
-function df_index($k, $a):array {return array_combine(df_column($a, $k), df_ita($a));}
+function df_index($k, iterable $a):array {return array_combine(df_column($a, $k), df_ita($a));}
