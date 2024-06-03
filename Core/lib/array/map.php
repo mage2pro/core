48700616f08f19cea11864daf158c891743eaba5
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 02:17:39 +0100

    https://github.com/mage2pro/core/issues/403

--- Core/lib/array/map.php
+++ Core/lib/array/map.php
@@ -199,6 +199,13 @@ function df_map_kr($a1, $a2):array {return df_map($a1, $a2, [], [], DF_BEFORE, t
 
 /**
  * 2016-11-08 Функция принимает аргументы в любом порядке.
+ * 2024-06-03
+ * 1.1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
+ * 1.2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
+ * 1.3) https://php.net/manual/en/language.types.iterable.php
+ * 2) We still can not use «Union Types» (e.g. `callable|iterable`) because they require PHP ≥ 8 (we need to support PHP ≥ 7.1):
+ * 2.1) https://php.watch/versions/8.0/union-types
+ * 2.2) https://3v4l.org/AOWmO
  * @used-by df_category_children_map()
  * @used-by df_modules_my()
  * @used-by df_parse_colon()
@@ -214,8 +221,8 @@ function df_map_kr($a1, $a2):array {return df_map($a1, $a2, [], [], DF_BEFORE, t
  * @used-by \Dfe\Spryng\Source\Account::fetch()
  * @used-by \Mineralair\Core\Controller\Modal\Index::execute()
  * @used-by \TFC\GoogleShopping\Products::atts() (tradefurniturecompany.co.uk, https://github.com/tradefurniturecompany/google-shopping/issues/1)
- * @param array|callable|Traversable $a1
- * @param array|callable|Traversable $a2
+ * @param callable|iterable $a1
+ * @param callable|iterable $a2
  * @return array(int|string => mixed)
  */
 function df_map_r($a1, $a2):array {return df_map($a1, $a2, [], [], 0, true);}
