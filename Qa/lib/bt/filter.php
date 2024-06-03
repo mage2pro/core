38e28815c0043cc55e0d9a27f1cd705c50c195e3
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 05:10:28 +0100

    https://github.com/mage2pro/core/issues/404

--- Qa/lib/bt/filter.php
+++ Qa/lib/bt/filter.php
@@ -1,12 +1,14 @@
 <?php
 /**
  * 2024-03-04 "Implement `df_bt_filter_head()`": https://github.com/mage2pro/core/issues/356
+ * 2024-06-03
+ * 1) "Use the `callable` type": https://github.com/mage2pro/core/issues/404
+ * 2) `callable` can be nullable: https://github.com/mage2pro/core/issues/174#user-content-callable	 
  * @used-by df_caller_entry()
  * @used-by \Df\Qa\Trace::__construct()
  * @param array(array(string => string|int)) $r
- * @param callable|null $f [optional]
  */
-function df_bt_filter_head(array $bt, array $skip = [], $f = null):array {
+function df_bt_filter_head(array $bt, array $skip = [], ?callable $f = null):array {
 	$skip = array_merge($skip, ['dfc', 'dfcf' ,'unknown']);
 	return df_filter_head($bt, function(array $e) use ($f, $skip) {/** @var array(string => string|int)|null $e */
 		$f2 = df_bt_entry_func($e); /** @var string $f2 */
