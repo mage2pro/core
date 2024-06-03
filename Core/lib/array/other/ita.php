b23b06ae01c0b5b5a01d5403f9214a6764c1ba90
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 01:00:10 +0100

    https://github.com/mage2pro/core/issues/403

--- Core/lib/array/other/ita.php
+++ Core/lib/array/other/ita.php
@@ -7,12 +7,14 @@
  * @uses iterator_to_array() allows an array as the first argument since PHP 8.2:
  * https://php.net/manual/migration82.other-changes.php#migration82.other-changes.functions.spl
  * 2023-07-26 "Replace `array|Traversable` with `iterable`": https://github.com/mage2pro/core/issues/255
+ * 2024-06-03
+ * 1) "Use `iterable` as an argument type": https://github.com/mage2pro/core/issues/403
+ * 2) `iterable` as an argument type is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
  * @used-by df_filter_f()
  * @used-by df_index()
  * @used-by df_map()
  * @used-by dfa_select_ordered()
  * @used-by dfak_transform()
  * @used-by \Df\Qa\Dumper::dumpObject()
- * @param iterable $i
  */
-function df_ita($i):array {return is_array($i) ? $i : iterator_to_array($i);}
+function df_ita(iterable $i):array {return is_array($i) ? $i : iterator_to_array($i);}
