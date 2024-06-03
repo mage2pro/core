e37153b415173a09ef9f9b1a21ec202cd12bc287
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 01:41:27 +0100

    https://github.com/mage2pro/core/issues/403

--- Core/lib/array/other/ids.php
+++ Core/lib/array/other/ids.php
@@ -4,6 +4,10 @@ use Magento\Framework\DataObject as _DO;
 
 /**
  * 2016-07-31
+ * 2024-06-03
+ * We still can not use «Union Types» (e.g. `callable|iterable`) because they require PHP ≥ 8 (we need to support PHP ≥ 7.1):
+ * https://php.watch/versions/8.0/union-types
+ * https://3v4l.org/AOWmO
  * @uses df_id()
  * @used-by \Df\Config\Backend\ArrayT::processI()
  * @param Traversable|array(int|string => _DO|AI) $c
