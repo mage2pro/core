848982b7011f815480149018adcb2fa9fd96c37a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-09 22:55:05 +0100

    https://github.com/mage2pro/core/issues/416

--- Core/lib/text/camel.php
+++ Core/lib/text/camel.php
@@ -65,7 +65,7 @@ function df_explode_camel(...$a):array {return df_call_a(function(string $n):arr
  * 2022-11-26 We can not declare the argument as `string ...$a` because such a syntax will reject arrays: https://3v4l.org/jFdPm
  * @see df_camel_to_underscore()
  * @used-by \Dfe\TwoCheckout\Handler::p()
- * @param string|string[] $a
+ * @param string|string[] ...$a
  * @return string|string[]
  */
 function df_underscore_to_camel(...$a) {return df_call_a(function(string $s):string {return implode(df_ucfirst(explode(
