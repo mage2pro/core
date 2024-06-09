1b70b43dbbef92dd63e9f6d5b3176a87add80dcf
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-09 11:31:39 +0100

    refactoring

--- Framework/lib/fs/delete.php
+++ Framework/lib/fs/delete.php
@@ -6,6 +6,3 @@ use Magento\Framework\Filesystem\Io\File as File;
  * 2022-11-03 @deprecated It is unused.
  */
 function df_fs_delete(string $p):void {File::rmdirRecursive(df_param_sne($p, 0));}
-
-
-
