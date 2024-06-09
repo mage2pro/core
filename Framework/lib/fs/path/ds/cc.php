a6091950c5617ab355d8c4dc4ab55d0d7d238f99
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-09 20:24:50 +0100

    refactoring

--- Framework/lib/fs/path/ds/cc.php
+++ Framework/lib/fs/path/ds/cc.php
@@ -2,7 +2,7 @@
 /**
  * 2015-12-01 Отныне всегда используем `/` вместо @see DIRECTORY_SEPARATOR
  * 2022-11-26 We can not declare the argument as `string ...$a` because such a syntax will reject arrays: https://3v4l.org/jFdPm
- * 2024-06-09 "`df_cc_path()` should trim `/` and `DS` for arguments": https://github.com/mage2pro/core/issues/406
+ * 2024-06-09 "`df_cc_path()` should trim internal `/` and `DS` for arguments": https://github.com/mage2pro/core/issues/406
  * @used-by df_config_e()
  * @used-by df_db_credentials()
  * @used-by df_fs_etc()
@@ -28,14 +28,16 @@
  * @used-by \KingPalm\Core\Plugin\Aitoc\OrdersExportImport\Model\Processor\Config\ExportConfigMapper::aroundToConfig()
  * @used-by \TFC\Image\Command\C1::image()
  * @used-by \Wolf\Filter\Observer\ControllerActionPredispatch::execute()
- * @param string|string[] $a
+ * @param string|string[] ...$a
  */
-function df_cc_path(...$a):string {return implode('/', df_clean(dfa_flatten($a)));}
+function df_cc_path(...$a):string {
+	return implode('/', df_clean(dfa_flatten($a)));
+}
 
 /**
  * 2016-05-31
  * 2022-11-26 We can not declare the argument as `string ...$a` because such a syntax will reject arrays: https://3v4l.org/jFdPm
  * 2023-01-01 @deprecated It is unused.
- * @param string|string[] $a
+ * @param string|string[] ...$a
  */
 function df_cc_path_t(...$a):string {return df_append(df_cc_path(dfa_flatten($a)), '/');}
