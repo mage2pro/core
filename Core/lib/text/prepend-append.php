ceb998b07b9d59e2800bc127731e04bc8499faef
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-09 22:57:28 +0100

    https://github.com/mage2pro/core/issues/416

--- Core/lib/text/prepend-append.php
+++ Core/lib/text/prepend-append.php
@@ -75,7 +75,7 @@ function df_prepend(string $s, string $head):string {return df_starts_with($s, $
 /**
  * 2022-11-26 We can not declare the argument as `string ...$a` because such a syntax will reject arrays: https://3v4l.org/jFdPm
  * @used-by df_tab_multiline()
- * @param string|string[] $a
+ * @param string|string[] ...$a
  * @return string|string[]|array(string => string)
  */
 function df_tab(...$a) {return df_call_a(function(string $s):string {return "\t" . $s;}, $a);}
