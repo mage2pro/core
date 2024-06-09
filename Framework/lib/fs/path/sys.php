c5fc1ba1bba3e6d91f10ff5e4bb2c6de1d379f5e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-09 17:52:37 +0100

    refactoring

--- Framework/lib/fs/path/sys.php
+++ Framework/lib/fs/path/sys.php
@@ -24,7 +24,15 @@ function df_sys_path_abs(string $p, string $suf = ''):string {return df_prepend(
  * 2023-07-26
  * "`df_sys_reader()` can not be used with an arbitrary path
  *  because of @see \Magento\Framework\Filesystem\DirectoryList::assertCode()": https://github.com/mage2pro/core/issues/271
- * @see \Magento\Framework\Filesystem\DirectoryList::assertCode()
+ * 2024-06-09
+ * 		private function assertCode($code) {
+ * 			if (!isset($this->directories[$code])) {
+ * 				throw new \Magento\Framework\Exception\FileSystemException(
+ * 					new \Magento\Framework\Phrase('Unknown directory type: \'%1\'', [$code])
+ * 				);
+ * 			}
+ * 		}
+ * https://github.com/magento/magento2/blob/2.4.7/lib/internal/Magento/Framework/Filesystem/DirectoryList.php#L233-L247
  * @used-by df_media_reader()
  * @used-by df_path_rel()
  * @used-by df_sys_path_abs()
