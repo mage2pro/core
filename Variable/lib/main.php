cbe1e050202600d95042364f2178ba7ab921a9ce
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-10 00:49:09 +0100

    11.2.0

--- Variable/lib/main.php
+++ Variable/lib/main.php
@@ -11,14 +11,16 @@ function df_mvar(string $c) {return dfa(df_mvars(), $c);}
 
 /**
  * 2024-01-02
+ * 2024-06-10 If $c is an array, then the `df_mvar_name()` result uses $c as keys.
  * @used-by df_mvar_n()
  * @used-by \CabinetsBay\Catalog\B\Category::images() (https://github.com/cabinetsbay/catalog/issues/2)
  * @param string|string[] ...$c
  * @return string|string[]
  */
-function df_mvar_name(...$c) {return df_call_a($c, function(string $c):string {return
-	!($v = df_mvar($c)) ? '' : $v->getName()
-;});}
+function df_mvar_name(...$c) {
+	$r = df_call_a($c, function(string $c):string {return !($v = df_mvar($c)) ? '' : $v->getName();});
+	return !is_array($r) ? $r : array_combine($c, $r);
+}
 
 /**
  * 2024-01-02
