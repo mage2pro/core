87cbd05e4f13bfb1e53c9d81fa062d77e626b347
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-11 14:41:21 +0100

    refactoring

--- Framework/lib/view/page/page.php
+++ Framework/lib/view/page/page.php
@@ -1,6 +1,4 @@
 <?php
-use Magento\Framework\View\Result\Page as ResultPage;
-use Magento\Framework\View\Result\PageFactory;
 /**
  * 2015-10-05
  * @used-by \Dfe\GoogleBackendLogin\Block\Metadata::_construct()
@@ -18,21 +16,3 @@ function df_metadata(string $k, string $v):void {
  * @used-by \Dfe\Portal\Controller\Index\Index::execute()
  */
 function df_page_title(string $s):void {df_page_config()->getTitle()->set($s);}
-
-/**
- * 2017-05-05
- * 2017-05-07
- * $template is a custom root template instead of «Magento_Theme::root.phtml».
- * https://github.com/magento/magento2/blob/2.1.6/app/etc/di.xml#L559-L565
- * «How is the root HTML template (Magento_Theme::root.phtml) declared and implemented?»
- * https://mage2.pro/t/3900
- * @used-by \Dfe\Portal\Controller\Index\Index::execute()
- */
-function df_page_result(string $template = '', string ...$handles):ResultPage {
-	$f = df_o(PageFactory::class);/** @var PageFactory $f */
-	$r = $f->create(false, df_clean(['template' => $template])); /** @var ResultPage $r */
-	foreach ($handles as $h) {
-		$r->addHandle($h);
-	}
-	return $r;
-}
---
+++ Framework/lib/view/page/result.php
@@ -0,0 +1,21 @@
+<?php
+use Magento\Framework\View\Result\Page as ResultPage;
+use Magento\Framework\View\Result\PageFactory as F;
+
+/**
+ * 2017-05-05
+ * 2017-05-07
+ * $template is a custom root template instead of «Magento_Theme::root.phtml».
+ * https://github.com/magento/magento2/blob/2.1.6/app/etc/di.xml#L559-L565
+ * «How is the root HTML template (Magento_Theme::root.phtml) declared and implemented?»
+ * https://mage2.pro/t/3900
+ * @used-by \Dfe\Portal\Controller\Index\Index::execute()
+ */
+function df_page_result(string $template = '', string ...$handles):ResultPage {
+	$f = df_o(F::class);/** @var F $f */
+	$r = $f->create(false, df_clean(['template' => $template])); /** @var ResultPage $r */
+	foreach ($handles as $h) {
+		$r->addHandle($h);
+	}
+	return $r;
+}
