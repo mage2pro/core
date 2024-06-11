a8d6a87905f6c394d5f7f41d986cf815e5f16921
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-11 14:45:44 +0100

    refactoring

--- Framework/lib/view/page/result.php
+++ Framework/lib/view/page/result.php
@@ -4,10 +4,9 @@ use Magento\Framework\View\Result\Page as R;
 /**
  * 2017-05-05
  * 2017-05-07
- * $template is a custom root template instead of «Magento_Theme::root.phtml».
+ * 1) $template is a custom root template instead of «Magento_Theme::root.phtml».
  * https://github.com/magento/magento2/blob/2.1.6/app/etc/di.xml#L559-L565
- * «How is the root HTML template (Magento_Theme::root.phtml) declared and implemented?»
- * https://mage2.pro/t/3900
+ * 2) «How is the root HTML template (Magento_Theme::root.phtml) declared and implemented?»: https://mage2.pro/t/3900
  * @used-by \Dfe\Portal\Controller\Index\Index::execute()
  */
 function df_page_result(string $t = '', string ...$handles):R {
