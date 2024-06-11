c3d7ba4f6ad8ad1ea744eedec27793baa8ebf0ec
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-11 16:52:15 +0100

    https://github.com/mage2pro/core/issues/424

--- Framework/lib/fs/contents.php
+++ Framework/lib/fs/contents.php
@@ -39,30 +39,35 @@ function df_contents(string $f, $onE = true, $rs = null):string {
 		$f = df_path_abs($f);
 	}
 	return df_try(
-		/**
-		 * 2015-11-27
-		 * Обратите внимание, что для использования @uses file_get_contents
-		 * с адресами https требуется расширение php_openssl интерпретатора PHP,
-		 * однако оно является системным требованием Magento 2:
-		 * http://devdocs.magento.com/guides/v2.0/install-gde/system-requirements.html#required-php-extensions
-		 * Поэтому мы вправе использовать здесь @uses file_get_contents
-		 * 2016-05-31, 2022-10-14
-		 * file_get_contents() could generate @see E_WARNING: e.g.:
-		 * 	*) if the file is absent
-		 * 	*)  in the case of network errors:
-		 * 			«failed to open stream: A connection attempt failed
-		 * 			because the connected party did not properly respond after a period of time,
-		 * 			or established connection failed because connected host has failed to respond.»
-		 * https://php.net/manual/function.file-get-contents.php#refsect1-function.file-get-contents-errors
-		 * That is why I use the silence operator.
-		 * 2023-07-16
-		 * «file_get_contents(): Passing null to parameter #2 ($use_include_path) of type bool is deprecated
-		 * in vendor/mage2pro/core/Framework/lib/fs/contents.php on line 36»:
-		 * https://github.com/mage2pro/core/issues/230
-		 */
-		function() use ($f, $rs):string {
-			return df_assert_ne(false, @file_get_contents($f, false, $rs));
-		}
+		function() use ($f, $isFile, $rs):string {return df_assert_ne(false,
+			/**
+			 * 2015-11-27
+			 * Обратите внимание, что для использования @uses file_get_contents()
+			 * с адресами https требуется расширение php_openssl интерпретатора PHP,
+			 * однако оно является системным требованием Magento 2:
+			 * http://devdocs.magento.com/guides/v2.0/install-gde/system-requirements.html#required-php-extensions
+			 * Поэтому мы вправе использовать здесь @uses file_get_contents
+			 * 2016-05-31, 2022-10-14
+			 * file_get_contents() could generate @see E_WARNING: e.g.:
+			 * 	*) if the file is absent
+			 * 	*)  in the case of network errors:
+			 * 			«failed to open stream: A connection attempt failed
+			 * 			because the connected party did not properly respond after a period of time,
+			 * 			or established connection failed because connected host has failed to respond.»
+			 * https://php.net/manual/function.file-get-contents.php#refsect1-function.file-get-contents-errors
+			 * That is why I use the silence operator.
+			 * 2023-07-16
+			 * «file_get_contents(): Passing null to parameter #2 ($use_include_path) of type bool is deprecated
+			 * in vendor/mage2pro/core/Framework/lib/fs/contents.php on line 36»:
+			 * https://github.com/mage2pro/core/issues/230
+			 * 2024-06-11
+			 * 1) "Improve `df_contents()`": https://github.com/mage2pro/core/issues/425
+			 * 2) "«PHP Warning: file_get_contents(vendor/mage2pro/core/<…>/composer.json):
+			 * failed to open stream: No such file or directory in vendor/mage2pro/core/Framework/lib/fs/contents.php on line 55»
+			 * in the IntellliJ IDEA's console if xDebug is enabled": https://github.com/mage2pro/core/issues/424
+			 */
+			$isFile && (!file_exists($f) || !is_file($f) || !is_readable($f)) ? false : @file_get_contents($f, false, $rs)
+		);}
 		,true !== $onE ? $onE : function() use ($f, $isFile) {df_error(
 			'Unable to read the %s «%s».', $isFile ? 'file' : 'URL', $f
 		);}
