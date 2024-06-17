7c006bb7479698992ffb9cd2c1cd5744b9fc3841
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-17 20:34:27 +0100

    refactoring

--- Catalog/lib/product/main.php
+++ Catalog/lib/product/main.php
@@ -1,15 +1,6 @@
 <?php
 use Magento\Catalog\Model\Product as P;
 
-/**             
- * 2019-11-18
- * @see df_category_id()
- * @used-by df_qty()
- * @used-by df_review_summary()
- * @param P|int $p
- */
-function df_product_id($p):int {return df_int(df_is_p($p) ? $p->getId() : $p);}
-
 /**
  * 2018-06-04
  * @see df_product()
