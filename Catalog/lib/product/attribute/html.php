c25c6a2cee39289c2454b1c4f61a62f4210f548a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-17 20:54:40 +0100

    https://github.com/mage2pro/core/issues/426

---
+++ Catalog/lib/product/attribute/html.php
@@ -0,0 +1,6 @@
+<?php
+use Magento\Catalog\Model\Product as P;
+/**
+ * 2024-06-17 "Implement `df_product_att_html()`": https://github.com/mage2pro/core/issues/426
+ */
+function df_product_att_html(P $p, string $v, string $ac):string {return df_catalog_output()->productAttribute($p, $v, $ac);}
