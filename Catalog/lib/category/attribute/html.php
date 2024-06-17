45c3efd570a5be07d41061c8bec9e6b71cae6dd6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-17 21:01:49 +0100

    https://github.com/mage2pro/core/issues/427

--- Catalog/lib/category/attribute/html.php
+++ Catalog/lib/category/attribute/html.php
@@ -3,5 +3,6 @@ use Magento\Catalog\Model\Category as C;
 /**
  * 2024-06-17 "Implement `df_category_att_html()`": https://github.com/mage2pro/core/issues/427
  * @see df_product_att_html()
+ * @used-by vendor/cabinetsbay/catalog/view/frontend/templates/category/l3/tabs/overview.phtml (https://github.com/cabinetsbay/catalog/issues/22)
  */
 function df_category_att_html(C $c, string $v, string $ac):string {return df_catalog_output()->categoryAttribute($c, $v, $ac);}
