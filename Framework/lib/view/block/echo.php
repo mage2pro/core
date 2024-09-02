18555942715b9fff24ba4dde12df846915fc40f1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-02 03:26:12 +0100

    https://github.com/mage2pro/core/issues/432

--- Framework/lib/view/block/echo.php
+++ Framework/lib/view/block/echo.php
@@ -4,10 +4,10 @@ use Closure as F;
  * 2024-09-02
  * 1) "Implement `df_block_echo()`": https://github.com/mage2pro/core/issues/432
  * 2) A short syntax:
- *		df_block_echo('CabinetsBay_Catalog', 'category/l2/l3', [
+ *		df_block_echo($b, 'category/l2/l3', [
  *			'cc' => df_category_children($b->getCurrentCategory())->setOrder('position', 'ASC')
  *		])('filters', 'items')
- * https://github.com/cabinetsbay/catalog/blob/0.1.6/view/frontend/templates/category/l2/l3.phtml#L7-L10
+ * https://github.com/cabinetsbay/catalog/blob/0.1.7/view/frontend/templates/category/l2/l3.phtml#L7-L10
  * https://3v4l.org/NNHbU
  * @used-by vendor/cabinetsbay/catalog/view/frontend/templates/category/l2/l3.phtml (https://github.com/cabinetsbay/catalog/issues/18)
  * @used-by vendor/cabinetsbay/catalog/view/frontend/templates/category/view.phtml (https://github.com/cabinetsbay/catalog/issues/18)
