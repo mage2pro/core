38a4e5ee6118837a95e0c84e784625f435f37645
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-07 10:47:20 +0100

    https://github.com/thehcginstitute-com/m1/issues/678

--- Core/lib/lang/call/.php
+++ Core/lib/lang/call/.php
@@ -65,11 +65,11 @@ function df_call($o, $m, array $p = []) {/** @var mixed $r */
 						# 2024-09-07
 						# "`df_call()`: «Failed to retrieve the default value»":
 						# https://github.com/thehcginstitute-com/m1/issues/678
-						function() use($p, $rp, $rfa, $isMethod) {return $rp->isOptional() ? $rp->getDefaultValue() :
+						function(string $n) use($p, $rp, $rfa, $isMethod) {return $rp->isOptional() ? $rp->getDefaultValue() :
 							df_error(
 								sprintf(
 									"The `df_call()` call is invalid because the %s `{$rfa->getName()}`"
-									. " requires the `{$rp->getName()}` argument but it is not provided."
+									. " requires the `{$n}` argument but it is not provided."
 									,$isMethod ? 'method' : 'function'
 								)
 								,$p
