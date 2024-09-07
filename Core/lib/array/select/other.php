f925dd2aca7140482e47ad026d334b09467853b9
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-07 03:53:45 +0100

    refactoring

--- Core/lib/array/select/other.php
+++ Core/lib/array/select/other.php
@@ -47,14 +47,3 @@ function dfa_select_ordered($a, array $k):array {
 	 */
 	return df_clean_null(array_intersect_key($resultWithGarbage, $resultKeys));
 }
-
-/**
- * 2019-01-28
- * @used-by \Dfe\Vantiv\API\Client::_construct()
- * 2024-03-05
- * 1) https://3v4l.org/C3qrh
- * 2) The previous code (`dfa_seq`): https://github.com/mage2pro/core/blob/10.6.9/Core/lib/array/main.php#L214-L231
- * @param array(int|string => mixed) $a
- * @return mixed|null
- */
-function dfa_try(array $a, string ...$k) {return df_first(dfa($a, $k));}
---
+++ Core/lib/array/select/try.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 2019-01-28
+ * @used-by \Dfe\Vantiv\API\Client::_construct()
+ * 2024-03-05
+ * 1) https://3v4l.org/C3qrh
+ * 2) The previous code (`dfa_seq`): https://github.com/mage2pro/core/blob/10.6.9/Core/lib/array/main.php#L214-L231
+ * @param array(int|string => mixed) $a
+ * @return mixed|null
+ */
+function dfa_try(array $a, string ...$k) {return df_first(dfa($a, $k));}
