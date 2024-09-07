fcdfde31b804452b24b8e9b6a422d41a94ddadc0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-07 01:28:26 +0100

    refactoring

--- Core/lib/array/map/other.php
+++ Core/lib/array/map/other.php
@@ -2,25 +2,6 @@
 use Df\Core\Exception as DFE;
 
 /**
- * PHP supports global constants since 5.3:
- * http://www.codingforums.com/php/303927-unexpected-t_const-php-version-5-2-17-a.html#post1363452
- * @used-by df_find()
- * @used-by df_map()
- */
-const DF_AFTER = 1;
-
-/**
- * PHP supports global constants since 5.3:
- * http://www.codingforums.com/php/303927-unexpected-t_const-php-version-5-2-17-a.html#post1363452
- * @used-by df_find()
- * @used-by df_map()
- * @used-by df_map_k()
- * @used-by df_map_kr()
- * @used-by \Df\Payment\Method::amountFactor()
- */
-const DF_BEFORE = -1;
-
-/**
  * 2016-08-09 Функция принимает аргументы в любом порядке.
  * 2024-06-03
  * 1.1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
