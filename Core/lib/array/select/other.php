dbda22a8d33fe761d42105e52c056fcaa0e0ddcd
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-07 03:06:32 +0100

    refactoring

--- Core/lib/array/select/other.php
+++ Core/lib/array/select/other.php
@@ -1,20 +1,6 @@
 <?php
-use Closure as F;
 use Magento\Config\Model\Config\Structure\AbstractElement as AE;
 use Magento\Framework\DataObject as _DO;
-
-/**
- * 2020-01-29
- * @see dfaoc()
- * @used-by df_credentials()
- * @used-by dfe_portal_module()
- * @used-by \Df\Framework\Request::extra()
- * @used-by \Df\OAuth\App::state()
- * @param string|string[] $k [optional]
- * @param mixed|callable|null $d [optional]
- * @return mixed
- */
-function dfac(F $f, $k = '', $d = null) {return dfa(dfcf($f, [], [], false, 1), $k, $d);}
 
 /**
  * 2020-01-29     
@@ -28,26 +14,6 @@ function dfac(F $f, $k = '', $d = null) {return dfa(dfcf($f, [], [], false, 1), 
  * @return _DO|AE|mixed
  */
 function dfad($o, $k = '', $d = null) {return df_nes($k) ? $o : dfa(df_gd($o), $k, $d);}
-
-/**
- * 2020-01-29
- * 2022-11-17
- * `object` as an argument type is not supported by PHP < 7.2: https://github.com/mage2pro/core/issues/174#user-content-object
- * 2024-06-03 We need to support PHP ≥ 7.1: https://github.com/mage2pro/core/issues/368
- * @see dfac()
- * @used-by \Df\Config\A::get()
- * @used-by \Df\Core\Visitor::r()
- * @used-by \Df\Framework\Form\Element\Fieldset::v()
- * @used-by \Df\Payment\TM::req()
- * @used-by \Df\Payment\TM::res0()
- * @used-by \Dfe\CheckoutCom\Response::a()
- * @used-by \Dfe\FacebookLogin\Customer::r()
- * @param object $o
- * @param string|string[] $k [optional]
- * @param mixed|callable|null $d [optional]
- * @return mixed
- */
-function dfaoc($o, F $f, $k = '', $d = null) {return dfa(dfc($o, $f, [], false, 1), $k, $d);}
 
 /**
  * 2015-02-08
