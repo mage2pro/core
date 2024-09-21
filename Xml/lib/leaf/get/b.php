ec485363782578c9b29a7e05d85286d163119d07
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 23:28:22 +0100

    refactoring

--- Xml/lib/leaf/get/b.php
+++ Xml/lib/leaf/get/b.php
@@ -5,4 +5,4 @@ use SimpleXMLElement as CX;
  * @deprecated It is unused.
  * @param bool|callable $d [optional]
  */
-function df_leaf_b(CX $e = null, $d = false):bool {return df_bool(df_leaf($e, $d));}
+function df_leaf_b(?CX $e = null, $d = false):bool {return df_bool(df_leaf($e, $d));}
