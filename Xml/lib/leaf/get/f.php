9551b973ba42ad0b3e3f307a460d638c1ba94fe2
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 23:28:38 +0100

    refactoring

--- Xml/lib/leaf/get/f.php
+++ Xml/lib/leaf/get/f.php
@@ -4,4 +4,4 @@ use SimpleXMLElement as CX;
  * 2015-08-16 Намеренно убрал параметр $d.
  * 2022-11-15 @deprecated It is unused.
  */
-function df_leaf_f(CX $e = null):float {return df_float(df_leaf($e));}
+function df_leaf_f(?CX $e = null):float {return df_float(df_leaf($e));}
