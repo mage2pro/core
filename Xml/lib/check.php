6cf1e5f60d9408ceeecbfbc50cd9dfdd7a3a44bf
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 22:43:07 +0100

    refactoring

--- Xml/lib/check.php
+++ Xml/lib/check.php
@@ -1,34 +1,4 @@
 <?php
-use SimpleXMLElement as CX;
-
-/**
- * 2015-02-27
- * 1) Метод @see \SimpleXMLElement::count() появился только в PHP 5.3,
- * поэтому мы его не используем: https://php.net/manual/simplexmlelement.count.php
- * 2) `count($e->children())` некорректно возвращает 1 для листов в PHP 5.1: http://3v4l.org/PT6Pt
- * Однако нам не нужно поддерживать PHP 5.1.
- * 3) Для несуществующего узла попытка вызова @uses count() приведёт к сбою: «Warning: count(): Node no longer exists»
- * http://3v4l.org/PsIPe#v512
- * 4) Текущий алгоритм проверен на работоспособность здесь: http://3v4l.org/VldTN
- * 2015-08-15
- * Нельзя здесь использовать `count($e->children())`,
- * потому что класс @see SimpleXmlElement не реализует интерфейс @see Iterator,
- * а реализует только интерфейс @see Traversable.
- * https://php.net/manual/class.iterator.php
- * https://php.net/manual/class.traversable.php
- * https://php.net/manual/simplexmlelement.count.php
- * 2015-08-16
- * 1) Как ни странно, написанное выше действительно верно: http://3v4l.org/covo1
- * 2) Класс @see \SimpleXMLElement не реализует интерфейс @see Iterator,
- * а реализует только интерфейс @see Traversable.
- * https://php.net/manual/class.iterator.php
- * https://php.net/manual/class.traversable.php
- * https://php.net/manual/simplexmlelement.count.php
- * Однако @uses count() почему-то работает для него.
- * @see \SimpleXMLElement — самый загадочный класс PHP.
- * @used-by df_assert_leaf()
- */
-function df_check_leaf(CX $e):bool {return !df_xml_exists($e) || !$e->children()->count();}
 
 /**
  * 2016-09-01
--- Xml/lib/leaf.php
+++ Xml/lib/leaf.php
@@ -1,6 +1,44 @@
 <?php
+use Df\Core\Exception as E;
 use Df\Xml\X;
 use SimpleXMLElement as CX;
+
+/**
+ * @used-by df_leaf()
+ * @throws E
+ */
+function df_assert_leaf(CX $e):CX {return df_check_leaf($e) ? $e : df_error(
+	"Требуется лист XML, однако получена ветка XML:\n%s.", df_xml_report($e)
+);}
+
+/**
+ * 2015-02-27
+ * 1) Метод @see \SimpleXMLElement::count() появился только в PHP 5.3,
+ * поэтому мы его не используем: https://php.net/manual/simplexmlelement.count.php
+ * 2) `count($e->children())` некорректно возвращает 1 для листов в PHP 5.1: http://3v4l.org/PT6Pt
+ * Однако нам не нужно поддерживать PHP 5.1.
+ * 3) Для несуществующего узла попытка вызова @uses count() приведёт к сбою: «Warning: count(): Node no longer exists»
+ * http://3v4l.org/PsIPe#v512
+ * 4) Текущий алгоритм проверен на работоспособность здесь: http://3v4l.org/VldTN
+ * 2015-08-15
+ * Нельзя здесь использовать `count($e->children())`,
+ * потому что класс @see SimpleXmlElement не реализует интерфейс @see Iterator,
+ * а реализует только интерфейс @see Traversable.
+ * https://php.net/manual/class.iterator.php
+ * https://php.net/manual/class.traversable.php
+ * https://php.net/manual/simplexmlelement.count.php
+ * 2015-08-16
+ * 1) Как ни странно, написанное выше действительно верно: http://3v4l.org/covo1
+ * 2) Класс @see \SimpleXMLElement не реализует интерфейс @see Iterator,
+ * а реализует только интерфейс @see Traversable.
+ * https://php.net/manual/class.iterator.php
+ * https://php.net/manual/class.traversable.php
+ * https://php.net/manual/simplexmlelement.count.php
+ * Однако @uses count() почему-то работает для него.
+ * @see \SimpleXMLElement — самый загадочный класс PHP.
+ * @used-by df_assert_leaf()
+ */
+function df_check_leaf(CX $e):bool {return !df_xml_exists($e) || !$e->children()->count();}
 
 /**
  * 2015-02-27
