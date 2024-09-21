007ac7f4f4e4eaf4ef7c7ab6711d450e314c2e19
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 23:11:25 +0100

    refactoring

--- Xml/lib/leaf/get/other.php
+++ Xml/lib/leaf/get/other.php
@@ -13,12 +13,6 @@ function df_leaf_child(CX $e, string $child, $d = null) {return df_leaf($e->{$ch
  * 2015-08-16 Намеренно убрал параметр $default.
  * 2022-11-15 @deprecated It is unused.
  */
-function df_leaf_f(CX $e = null):float {return df_float(df_leaf($e));}
-
-/**
- * 2015-08-16 Намеренно убрал параметр $default.
- * 2022-11-15 @deprecated It is unused.
- */
 function df_leaf_i(CX $e = null):int {return df_int(df_leaf($e));}
 
 /**
