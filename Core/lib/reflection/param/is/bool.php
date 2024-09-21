c35fbbbc4d5fd158dc1822dbfd565d855bcc345e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 20:32:55 +0100

    https://github.com/mage2pro/core/issues/435

---
+++ Core/lib/reflection/param/is/bool.php
@@ -0,0 +1,9 @@
+<?php
+use ReflectionNamedType as NT; # 2024-09-21 https://www.php.net/manual/en/class.reflectionnamedtype.php
+use ReflectionParameter as P;
+use ReflectionType as T; # 2024-09-21 https://www.php.net/manual/en/class.reflectiontype.php
+
+/**
+ * 2024-09-21
+ */
+function dfr_param_is_bool(P $p):bool {return false;}
