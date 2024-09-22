72d8c65cee20066c40ccb22a87ce44489c20ffcf
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 21:50:15 +0100

    refactoring

--- Core/lib/text/filter.php
+++ Core/lib/text/filter.php
@@ -22,15 +22,6 @@ function df_e(...$a) {return df_call_a(function(string $s):string {return htmlsp
 );}, $a);}
 
 /**
- * 2019-01-11
- * 2019-11-15 https://stackoverflow.com/a/1253417
- * @used-by \Inkifi\Consolidation\Controller\Adminhtml\Index\Index::execute()
- */
-function df_is_guid(string $s):bool {return 36 === strlen($s) && preg_match(
-	'#^\{?[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}\}?$#', $s
-);}
-
-/**
  * http://darklaunch.com/2009/05/06/php-normalize-newlines-line-endings-crlf-cr-lf-unix-windows-mac
  * @used-by df_explode_n()
  */
