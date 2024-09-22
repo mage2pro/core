c12ddde766c512a3e842ff95299a6a3a37bf6a4f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-23 00:24:45 +0100

    https://github.com/mage2pro/core/issues/436

--- Xml/lib/cdata.php
+++ Xml/lib/cdata.php
@@ -18,7 +18,7 @@ function df_cdata_raw_if_needed(string $s):string {return !df_needs_cdata($s) ? 
  * 2021-12-12 https://3v4l.org/3SDsT
  * @see df_cdata()
  * @used-by df_cdata_raw_if_needed()
- * @used-by \Df\Xml\G::importString()
+ * @used-by \Df\Xml\G2::importString()
  */
 function df_needs_cdata(string $s):bool {
 	$s1 = htmlspecialchars_decode($s, ENT_XML1 | ENT_QUOTES); /** @var string $s1 */
