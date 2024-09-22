d6780dd5156cc5b7630b5cbcfaf3eaf7f9a5e3b9
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 21:52:01 +0100

    refactoring

--- Core/lib/text/explode.php
+++ Core/lib/text/explode.php
@@ -16,22 +16,6 @@ function df_explode_multiple(array $delimiters, string $s):array {
 }
 
 /**
- * 2018-04-24 I have added @uses trim() today.
- * @used-by df_module_enum()
- * @used-by df_parse_colon()
- * @used-by df_tab_multiline()
- * @used-by df_zf_http_last_req()
- * @used-by \Df\Core\Text\Regex::getSubjectSplitted()
- * @used-by \Dfe\AllPay\Charge::descriptionOnKiosk()
- * @used-by \Dfe\Moip\P\Charge::pInstructionLines()
- * @used-by \Dfe\TBCBank\W\Reader::reqFilter()
- * @used-by \Doormall\Shipping\Partner\Entity::locations()
- * @used-by \Inkifi\Core\Plugin\Catalog\Block\Product\View::afterSetLayout()
- * @return string[]
- */
-function df_explode_n(string $s):array {return explode("\n", df_normalize(df_trim($s)));}
-
-/**
  * 2016-09-03 Another implementation: df_explode_multiple(['/', DS], $path)
  * @used-by df_store_code_from_url()
  * @used-by df_url_trim_index()
---
+++ Core/lib/text/n/explode.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 2018-04-24 I have added @uses trim() today.
+ * @used-by df_module_enum()
+ * @used-by df_parse_colon()
+ * @used-by df_tab_multiline()
+ * @used-by df_zf_http_last_req()
+ * @used-by \Df\Core\Text\Regex::getSubjectSplitted()
+ * @used-by \Dfe\AllPay\Charge::descriptionOnKiosk()
+ * @used-by \Dfe\Moip\P\Charge::pInstructionLines()
+ * @used-by \Dfe\TBCBank\W\Reader::reqFilter()
+ * @used-by \Doormall\Shipping\Partner\Entity::locations()
+ * @used-by \Inkifi\Core\Plugin\Catalog\Block\Product\View::afterSetLayout()
+ * @return string[]
+ */
+function df_explode_n(string $s):array {return explode("\n", df_normalize(df_trim($s)));}
