72a476b9a896821ae4bbca036bc14963891407dd
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-23 00:06:59 +0100

    refactoring

--- Xml/G2.php
+++ Xml/G2.php
@@ -29,10 +29,9 @@ final class G2 {
 	 */
 	function addAttributes(array $aa):void {
 		foreach ($aa as $k => $v) {/** @var string $k */ /** @var mixed $v */
-			df_assert_stringable(
+			$this->addAttribute(df_assert_sne($k), df_assert_stringable(
 				$v, sprintf("The attribute «{$k}» has a non-`Strinable` type %s.", df_type($v)), ['attributes' => $aa]
-			);
-			$this->addAttribute(df_assert_sne($k), $v);
+			));
 		}
 	}
 
