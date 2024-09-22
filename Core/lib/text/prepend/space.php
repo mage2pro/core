c8ee021d0864e7e4f763784edb6af74f39c467aa
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 22:10:15 +0100

    refactoring

--- Core/lib/text/prepend/space.php
+++ Core/lib/text/prepend/space.php
@@ -41,12 +41,3 @@ function df_pad(string $phrase, int $length, string $pattern = ' ', int $positio
 	}
 	return $r;
 }
-
-/**
- * 2015-11-29 Добавляет к строковому представлению целого числа нули слева.
- * 2015-12-01
- * Строковое представление может быть 16-ричным (код цвета), поэтому убрал @see df_int()
- * http://stackoverflow.com/a/1699980
- * @used-by df_rgb2hex()
- */
-function df_pad0(int $length, string $number):string {return str_pad($number, $length, '0', STR_PAD_LEFT);}
