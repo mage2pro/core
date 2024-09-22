cbca42ef2e6fd6550526cf60a157f01f611ff5b4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 02:20:01 +0100

    https://github.com/mage2pro/core/issues/436

--- Xml/lib/parse/other.php
+++ Xml/lib/parse/other.php
@@ -13,17 +13,17 @@ use Throwable as T;
  * @used-by df_xml_x()
  * @used-by \Dfe\Robokassa\Api\Options::p()
  * @used-by \Dfe\SecurePay\Refund::process()
- * @param X|string $x
+ * @param G|string $x
  * @throws E
  */
-function df_xml_parse($x, bool $throw = true):?X {/** @var ?X $r */
-	if ($x instanceof X) {
+function df_xml_parse($x, bool $throw = true):?G {/** @var ?G $r */
+	if ($x instanceof G) {
 		$r = $x;
 	}
 	else {
 		df_param_sne($x, 0);
 		$r = null;
-		try {$r = new X($x);}
+		try {$r = new G($x);}
 		catch (T $t) {
 			if ($throw) {
 				df_error("Failed to parse XML: «%s».\nXML:\n%s", df_xts($t), df_trim($x));
