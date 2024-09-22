defa30b223064f3a87b7dcebfc9d6bfe46d9d3a4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 22:13:27 +0100

    refactoring

--- Core/lib/text/append.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * 2016-03-08 It adds the $tail suffix to the $s string if the suffix is absent in $s.
- * @used-by df_cc_path_t()
- * @used-by df_file_ext_add()
- */
-function df_append(string $s, string $tail):string {return df_ends_with($s, $tail) ? $s : $s . $tail;}
--- Core/lib/text/prepend-append/.php
+++ Core/lib/text/prepend-append/.php
@@ -1,5 +1,12 @@
 <?php
 /**
+ * 2016-03-08 It adds the $tail suffix to the $s string if the suffix is absent in $s.
+ * @used-by df_cc_path_t()
+ * @used-by df_file_ext_add()
+ */
+function df_append(string $s, string $tail):string {return df_ends_with($s, $tail) ? $s : $s . $tail;}
+
+/**
  * 2016-03-08 It adds the $head prefix to the $s string if the prefix is absent in $s.
  * @used-by df_sys_path_abs()
  * @used-by ikf_ite()
