6cf34ecac7474ae7125d96e5731e81504170b655
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 17:49:30 +0100

    https://github.com/mage2pro/core/issues/436

--- Xml/G2.php
+++ Xml/G2.php
@@ -12,8 +12,16 @@ final class G2 {
 	function __construct($x) {$this->_x = df_xml_x($x);}
 
 	/**
+	 * 2021-12-13
+	 * @used-by self::addAttributes()
+	 * @used-by self::addChildX()
+	 */
+	function addAttribute(string $k, string $v = '', string $ns = ''):void {$this->_x->addAttribute($this->k($k), $v, $ns);}
+
+	/**
 	 * 2024-09-22
-	 * @used-by __construct()
+	 * @used-by self::__construct()
+	 * @used-by self::addAttribute()
 	 * @var X
 	 */
 	private $_x;
