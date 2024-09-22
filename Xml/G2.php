8748875d2f1b4484c21530c46cf5f4c3c4b45bb8
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 23:39:06 +0100

    refactoring

--- Xml/G2.php
+++ Xml/G2.php
@@ -1,6 +1,7 @@
 <?php
 namespace Df\Xml;
 use \SimpleXMLElement as X;
+use \Throwable as T; # 2023-08-03 "Treat `\Throwable` similar to `\Exception`": https://github.com/mage2pro/core/issues/311
 # 2024-09-22
 # 1) "Refactor `Df\Xml\X`": https://github.com/mage2pro/core/issues/436
 # 2) "Refactor the `Df_Xml` module": https://github.com/mage2pro/core/issues/437
@@ -32,6 +33,60 @@ final class G2 {
 			);
 			$this->addAttribute(df_assert_sne($k), $v);
 		}
+	}
+
+	/**
+	 * @used-by self::importArray()
+	 * @param mixed $v
+	 * @param string[]|bool $wrapInCData [optional]
+	 */
+	private function importString(string $k, $v, $wrapInCData = []):void {
+		$needWrapInCData = !is_array($wrapInCData) && !!$wrapInCData; /** @var bool $needWrapInCData */
+		$wrapInCData = df_eta($wrapInCData);
+		# '' означает, что метод `importString` не должен создавать дочерний тэг `$k`,
+		# а должен добавить текст в качестве единственного содержимого текущего тэга.
+		$kIsEmpty = df_es($k); /** @var bool $kIsEmpty */
+		$kAsString = $kIsEmpty ? $this->getName() : $k; /** @var string $kAsString */
+		$vIsString = is_string($v); /** @var bool $vIsString */
+		$vAsString = ''; /** @var string $vAsString */
+		try {$vAsString = $vIsString ? $v : df_string($v);}
+		catch (T $t) {df_error("Unable to convert the value of the key «{$kAsString}» to a string.\n%s", df_xts($t));}
+		if ($vIsString && $vAsString) {
+			/**
+			 * Поддержка синтаксиса
+			 *	 [
+			 *		'Представление' =>
+			 *			df_cdata($this->getAddress()->format(Mage_Customer_Model_Attribute_Data::OUTPUT_FORMAT_TEXT))
+			 *	 ]
+			 * Обратите внимание, что проверка на синтаксис[[]] должна предшествовать
+			 * проверке на принадлежность ключа $kAsString в массиве $wrapInCData,
+			 * потому что при соответствии синтаксису[[]] нам надо удалить из значения символы[[]].
+			 * Обратите внимание, что нам нужно выполнить проверку на синтаксис df_cdata ([[]])
+			 * даже при $needWrapInCData = true, потому что маркеры [[ и ]] из данных надо удалять.
+			 */
+			if (self::marker()->marked($vAsString)) {
+				$vAsString = self::marker()->unmark($vAsString);
+				$needWrapInCData = true;
+			}
+			$needWrapInCData = $needWrapInCData || in_array($kAsString, $wrapInCData) || df_needs_cdata($vAsString);
+		}
+		$needWrapInCData
+			? ($kIsEmpty ? $this->cdata($vAsString) : $this->addChildText($kAsString, $vAsString))
+			: (
+				$kIsEmpty
+				? $this->setValue($vAsString)
+				: $this->addChild(
+					$kAsString
+					/**
+					 * Обратите внимание, что мы намеренно не добавляем htmlspecialchars:
+					 * пусть вместо этого источник данных помечает те даннные, которые
+					 * могут содержать неразрешённые в качестве содержимого тегов XML
+					 * значения посредством @see df_cdata()
+					 */
+					,$vAsString
+				)
+			)
+		;
 	}
 
 	/**
