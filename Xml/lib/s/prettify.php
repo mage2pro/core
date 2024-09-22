7854e3b2ab36d961387ecfb0c03938ca8f1a40fc
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 01:37:35 +0100

    https://github.com/mage2pro/core/issues/436

--- Xml/lib/s/prettify.php
+++ Xml/lib/s/prettify.php
@@ -7,4 +7,4 @@ use Df\Xml\G;
  * @uses \Df\Xml\G::asNiceXml() не сохраняет заголовок XML.
  * @param G|string $x
  */
-function df_xml_prettify($x):string {return df_cc_n(df_xml_parse_header($x), df_xml_parse($x)->asNiceXml());}
+function df_xml_prettify($x):string {return df_cc_n(df_xml_parse_header($x), df_xml_s(df_xml_parse($x)));}
