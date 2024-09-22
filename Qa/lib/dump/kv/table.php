16aa3564f430a5d76125d90d92bb058c612e15c6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 19:54:51 +0100

    refactoring

--- Qa/lib/dump/kv/table.php
+++ Qa/lib/dump/kv/table.php
@@ -9,10 +9,7 @@ function df_kv_table(array $a):string {return df_tag('table', [], df_map_k(
 	df_clean($a), function($k, $v) {return
 		df_tag('tr', [], [
 			df_tag('td', [], $k)
-			,df_tag('td', [],
-				is_array($v) || (is_object($v) && !method_exists($v, '__toString'))
-					? "\n" . df_json_encode($v) : $v
-			)
+			,df_tag('td', [], df_is_stringable($v) ? $v : "\n" . df_json_encode($v))
 		])
 	;}
 ));}
