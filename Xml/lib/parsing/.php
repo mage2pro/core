04fe8c909efead4c86410e964a084f9131f1ef93
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-23 00:33:40 +0100

    https://github.com/mage2pro/core/issues/437

--- Xml/lib/parsing/.php
+++ Xml/lib/parsing/.php
@@ -1,16 +1,11 @@
 <?php
 use Closure as F;
 use Df\Core\Exception as E;
-use Df\Xml\G;
 use SimpleXMLElement as X;
 use Throwable as T;
 
 /**
- * 2024-09-22
- * 1) It returns:
- * 		@see G if $x is a string
- * 		$x unchanged if $x is @see X
- * 2) "Refactor the `Df_Xml` module": https://github.com/mage2pro/core/issues/437
+ * 2024-09-22 "Refactor the `Df_Xml` module": https://github.com/mage2pro/core/issues/437
  * @used-by df_module_name_by_path()
  * @used-by df_xml_node()
  * @used-by df_xml_parse_a()
@@ -21,11 +16,10 @@ use Throwable as T;
  * @used-by \Dfe\SecurePay\Refund::process()
  * @param X|string $x
  * @param F|T|bool|mixed $onE
- * @return G|X
  * @throws E
  */
 function df_xml_x($x, $onE = true):X {return $x instanceof X ? $x : df_try(
-	function() use($x) {return new G(df_assert_sne($x, 0));}
+	function() use($x) {return new X(df_assert_sne($x, 0));}
 	, true !== $onE ? $onE : function(T $t) use($x):E {return df_error_create(
 		"Failed to parse XML: «%s».\nXML:\n%s", df_xts($t), df_trim($x)
 	);}
