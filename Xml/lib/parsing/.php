082c871c4b5e97eb273591769d95a6840a75633a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 16:58:16 +0100

    https://github.com/mage2pro/core/issues/437

--- Xml/lib/parsing/.php
+++ Xml/lib/parsing/.php
@@ -6,9 +6,10 @@ use Throwable as T;
 
 /**
  * 2024-09-22
- * It returns:
+ * 1) It returns:
  * 		@see G if $x is a string
  * 		$x unchanged if $x is @see X
+ * 2) "Refactor the `Df_Xml` module": https://github.com/mage2pro/core/issues/437
  * @used-by df_module_name_by_path()
  * @used-by df_xml_g()
  * @used-by df_xml_node()
