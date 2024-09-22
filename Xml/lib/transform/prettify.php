18eaf77b164c3590cc54380f2684f4ef9718216b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 16:35:32 +0100

    https://github.com/mage2pro/core/issues/437

--- Xml/lib/transform/prettify.php
+++ Xml/lib/transform/prettify.php
@@ -4,7 +4,7 @@ use SimpleXMLElement as X;
 /**
  * 2016-09-01
  * @used-by \Dfe\SecurePay\Refund::process()
- * A result of @uses df_xml_s() does not include the XML header.
+ * The output of @uses df_xml_s() does not include the XML header.
  * @param X|string $x
  */
 function df_xml_prettify($x):string {return df_cc_n(df_xml_parse_header($x), df_xml_s(df_xml_parse($x)));}
