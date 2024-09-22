491833cfd5700651fbee13cd5af8f95ccf980dca
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 22:08:08 +0100

    refactoring

---
+++ Core/lib/text/prepend-append/pad.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Аналог @see str_pad() для Unicode: http://stackoverflow.com/a/14773638
+ * @used-by df_kv()
+ * @used-by \Dfe\Moip\CardFormatter::label()
+ */
+function df_pad(string $phrase, int $length, string $pattern = ' ', int $position = STR_PAD_RIGHT):string {/** @var string $r */
+	$encoding = 'UTF-8'; /** @var string $encoding */
+	$input_length = mb_strlen($phrase, $encoding); /** @var int $input_length */
+	$pad_string_length = mb_strlen($pattern, $encoding); /** @var int $pad_string_length */
+	if ($length <= 0 || $length - $input_length <= 0) {
+		$r = $phrase;
+	}
+	else {
+		$num_pad_chars = $length - $input_length; /** @var int $num_pad_chars */
+		/** @var int $left_pad */ /** @var int $right_pad */
+		switch ($position) {
+			case STR_PAD_RIGHT:
+				# 2024-06-06 "Use the «Symmetric array destructuring» PHP 7.1 feature": https://github.com/mage2pro/core/issues/379
+				[$left_pad, $right_pad] = [0, $num_pad_chars];
+				break;
+			case STR_PAD_LEFT:
+				# 2024-06-06 "Use the «Symmetric array destructuring» PHP 7.1 feature": https://github.com/mage2pro/core/issues/379
+				[$left_pad, $right_pad] = [$num_pad_chars, 0];
+				break;
+			case STR_PAD_BOTH:
+				$left_pad = floor($num_pad_chars / 2);
+				$right_pad = $num_pad_chars - $left_pad;
+				break;
+			default:
+				df_error();
+		}
+		$r = '';
+		for ($i = 0; $i < $left_pad; ++$i) {
+			$r .= mb_substr($pattern, $i % $pad_string_length, 1, $encoding);
+		}
+		$r .= $phrase;
+		for ($i = 0; $i < $right_pad; ++$i) {
+			$r .= mb_substr($pattern, $i % $pad_string_length, 1, $encoding);
+		}
+	}
+	return $r;
+}
+
+/**
+ * 2015-11-29 Добавляет к строковому представлению целого числа нули слева.
+ * 2015-12-01
+ * Строковое представление может быть 16-ричным (код цвета), поэтому убрал @see df_int()
+ * http://stackoverflow.com/a/1699980
+ * @used-by df_rgb2hex()
+ */
+function df_pad0(int $length, string $number):string {return str_pad($number, $length, '0', STR_PAD_LEFT);}
