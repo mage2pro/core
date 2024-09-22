5b806b670ecdc3cd836ef69abc7859817e935c30
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 01:16:18 +0100

    https://github.com/mage2pro/core/issues/436

--- Xml/lib/s/.php
+++ Xml/lib/s/.php
@@ -4,10 +4,69 @@ use SimpleXMLElement as CX;
 
 /**
  * 2016-09-01
- * @see df_xml_x()
+ * @see \Magento\Framework\Simplexml\Element::asNiceXml()
  * @used-by df_assert_leaf()
  * @used-by df_xml_children()
+ * @used-by df_xml_g()
  * @used-by df_xml_parse_header()
+ * @used-by df_xml_prettify()
+ * @used-by df_xml_s()
  * @param CX|MX|string $x
  */
-function df_xml_s($x):string {return is_string($x) ? $x : ($x instanceof MX ? $x->asNiceXml() : $x->asXML());}
+function df_xml_s($x, int $level = 0):string {/** @var string $r */
+	if (is_string($x)) {
+		$r = $x;
+	}
+	else {
+		/** @var string $nl */ /** @var string $pad */
+		[$nl, $pad] = !$level ? ['', ''] : ["\n", str_pad('', $level * 1, "\t", STR_PAD_LEFT)];
+		$r = $pad . '<' . $x->getName(); /** @var string $r */
+		$attributes = $x->attributes();
+		if ($attributes) {
+			foreach ($attributes as $key => $value) {
+				$r .= ' ' . $key . '="' . str_replace('"', '\"', (string)$value) . '"';
+			}
+		}
+		$attributes = $x->attributes('xsi', true);
+		if ($attributes) {
+			foreach ($attributes as $key => $value) {
+				$r .= ' xsi:' . $key . '="' . str_replace('"', '\"', (string)$value) . '"';
+			}
+		}
+		if ($x->hasChildren()) {
+			$r .= '>';
+			$value = trim((string)$x);
+			if (strlen($value)) {
+				/**
+				 * 2021-12-16
+				 * The previous code was: `$this->xmlentities($value)`
+				 * @see \Magento\Framework\Simplexml\Element::xmlentities()
+				 */
+				$r .= df_cdata_raw_if_needed($value);
+			}
+			$r .= $nl;
+			foreach ($x->children() as $child) {/** @var CX $child */
+				$r .= $child->asNiceXml('', is_numeric($level) ? $level + 1 : true);
+			}
+			$r .= $pad . '</' . $x->getName() . '>' . $nl;
+		}
+		else {
+			$value = (string)$x;
+			if (strlen($value)) {
+				/**
+				 * 2021-12-16
+				 * The previous code was: `$this->xmlentities($value)`
+				 * @see \Magento\Framework\Simplexml\Element::xmlentities()
+				 */
+				$r .= '>' . df_cdata_raw_if_needed($value) . '</' . $x->getName() . '>' . $nl;
+			}
+			else {
+				$r .= '/>' . $nl;
+			}
+		}
+		if ((0 === $level || false === $level) && !empty($filename)) {
+			file_put_contents($filename, $r);
+		}
+	}
+	return $r;
+}
