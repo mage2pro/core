628e25e17edf5267af0c1e9f82ee2bff9d8e3145
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 22:15:39 +0100

    refactoring

--- Core/lib/text/prepend-append/0.php
+++ Core/lib/text/prepend-append/0.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * 2015-11-29 Добавляет к строковому представлению целого числа $number нули слева в количестве $length.
+ * 2015-11-29 It prepends $s with $count zeros.
  * 2015-12-01
  * Строковое представление может быть 16-ричным (код цвета), поэтому убрал @see df_int()
  * http://stackoverflow.com/a/1699980
  * @used-by df_rgb2hex()
  */
-function df_pad0(int $length, string $number):string {return str_pad($number, $length, '0', STR_PAD_LEFT);}
+function df_pad0(int $count, string $s):string {return str_pad($s, $count, '0', STR_PAD_LEFT);}
