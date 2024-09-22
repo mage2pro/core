878c7bef489491fded6077db72eaedcb8277fc5c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-22 01:02:40 +0100

    https://github.com/mage2pro/core/issues/436

--- Xml/lib/s/other.php
+++ Xml/lib/s/other.php
@@ -1,20 +1,11 @@
 <?php
-use Df\Xml\G;
 use Magento\Framework\Simplexml\Element as MX;
 use SimpleXMLElement as CX;
 
 /**
- * 2016-09-01
- * @used-by \Dfe\SecurePay\Refund::process()
- * @uses \Df\Xml\G::asNiceXml() не сохраняет заголовок XML.
- * @param string|X $x
- */
-function df_xml_prettify($x):string {return df_cc_n(df_xml_parse_header($x), df_xml_parse($x)->asNiceXml());}
-
-/**
  * @used-by df_assert_leaf()
  * @used-by df_xml_children()
- * @param CX|MX|X $e
+ * @param CX|MX $e
  */
 function df_xml_report(CX $e):string {return $e instanceof MX ? $e->asNiceXml() : $e->asXML();}
 
@@ -22,6 +13,6 @@ function df_xml_report(CX $e):string {return $e instanceof MX ? $e->asNiceXml() 
  * 2016-09-01
  * @see df_xml_x()
  * @used-by df_xml_parse_header()
- * @param string|X $x
+ * @param CX|string $x
  */
 function df_xml_s($x):string {return is_string($x) ? $x : $x->asXML();}
