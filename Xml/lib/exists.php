9be5a15c33c19eeedb004eada5d70f7ff1991d18
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 00:54:34 +0100

    https://github.com/mage2pro/core/issues/437

--- Xml/lib/exists.php
+++ Xml/lib/exists.php
@@ -5,4 +5,4 @@ use SimpleXMLElement as X;
  * http://stackoverflow.com/questions/1560827#comment20135428_1562158
  * @used-by df_xml_children()
  */
-function df_xml_exists_child(X $e, string $child):bool {return isset($e->{$child});}
+function df_xml_exists(X $e, string $child):bool {return isset($e->{$child});}
