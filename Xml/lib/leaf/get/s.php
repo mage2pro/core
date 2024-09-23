d574037a795837e759841f5f941adaa288357604
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-23 01:03:07 +0100

    https://github.com/mage2pro/core/issues/437

--- Xml/lib/leaf/get/s.php
+++ Xml/lib/leaf/get/s.php
@@ -1,19 +1,18 @@
 <?php
-use Df\Xml\G;
-use SimpleXMLElement as CX;
+use SimpleXMLElement as X;
 
 /**
  * @used-by df_leaf_sne()
  * @used-by \Dfe\Robokassa\Api\Options::p()
  * @param string|callable $d [optional]
  */
-function df_leaf_s(?CX $e = null, $d = ''):string {return (string)df_leaf($e, $d);}
+function df_leaf_s(?X $e = null, $d = ''):string {return (string)df_leaf($e, $d);}
 
 /**
  * @used-by \Dfe\SecurePay\Refund::process()
  * @param string|callable $d [optional]
  */
-function df_leaf_sne(?CX $e = null, $d = ''):string {/** @var string $r */
+function df_leaf_sne(?X $e = null, $d = ''):string {/** @var string $r */
 	if (df_es($r = df_leaf_s($e, $d))) {
 		df_error('Лист дерева XML должен быть непуст, однако он пуст.');
 	}
