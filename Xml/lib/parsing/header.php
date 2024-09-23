d7759f648c543583d7e9fa5b2115c5dcbda0a9ec
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-23 04:24:50 +0100

    https://github.com/mage2pro/core/issues/437

--- Xml/lib/parsing/header.php
+++ Xml/lib/parsing/header.php
@@ -1,14 +1,11 @@
 <?php
-use Df\Core\Exception as E;
-use Df\Xml\G;
-use SimpleXMLElement as SX;
-use Throwable as T;
+use SimpleXMLElement as X;
 
 /**
  * 2016-09-01
  * Если XML не отформатирован, то после его заголовка перенос строки идти не обязан: http://stackoverflow.com/a/8384602
  * @used-by df_xml_prettify()
- * @param string|X $x
+ * @param X|string $x
  * @return string|null
  */
 function df_xml_parse_header($x) {return df_preg_match('#^<\?xml.*\?>#', df_xml_s($x));}
