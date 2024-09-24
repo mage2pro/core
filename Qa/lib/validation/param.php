121a37ce9ee4c71c1a92eaf2401771e97db30601
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:50:24 +0100

    https://github.com/mage2pro/core/issues/440

--- Qa/lib/validation/param.php
+++ Qa/lib/validation/param.php
@@ -16,10 +16,10 @@ function df_param_iso2(string $v, int $ord, int $sl = 0):string {return Q::asser
  * @used-by df_fs_delete()
  * @used-by dfa_deep()
  * @used-by dfa_deep_unset()
- * @used-by \Df\Core\Html\Tag::openTagWithAttributesAsText()
- * @used-by \Df\Payment\W\Nav::e2i()
- * @used-by \Dfe\AllPay\W\Event::tl_()
- * @used-by \Dfe\Markdown\DbRecord::__construct()
+ * @used-by Df\Core\Html\Tag::openTagWithAttributesAsText()
+ * @used-by Df\Payment\W\Nav::e2i()
+ * @used-by Dfe\AllPay\W\Event::tl_()
+ * @used-by Dfe\Markdown\DbRecord::__construct()
  * @throws DFE
  */
 function df_param_sne(string $v, int $ord, int $sl = 0):string {$sl++;
