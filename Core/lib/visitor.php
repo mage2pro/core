24a2bc2bca4de8a3989973e33c57a953d7662363
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:31:31 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/visitor.php
+++ Core/lib/visitor.php
@@ -4,11 +4,11 @@ use Magento\Sales\Model\Order as O;
 use Magento\Framework\HTTP\PhpEnvironment\RemoteAddress as RA;
 /**
  * 2016-05-20
- * @used-by \Dfe\TwoCheckout\Address::visitor()
- * @used-by \Df\Payment\Settings\_3DS::countries()
- * @used-by \Df\Sso\CustomerReturn::register()
- * @used-by \Frugue\Shipping\Header::_toHtml()
- * @used-by \Frugue\Store\Plugin\Framework\App\FrontControllerInterface::aroundDispatch()
+ * @used-by Dfe\TwoCheckout\Address::visitor()
+ * @used-by Df\Payment\Settings\_3DS::countries()
+ * @used-by Df\Sso\CustomerReturn::register()
+ * @used-by Frugue\Shipping\Header::_toHtml()
+ * @used-by Frugue\Store\Plugin\Framework\App\FrontControllerInterface::aroundDispatch()
  * @param string|null|O $ip [optional]
  */
 function df_visitor($ip = null):V {return V::sp(df_is_o($ip) ? $ip->getRemoteIp() : $ip);}
@@ -17,16 +17,16 @@ function df_visitor($ip = null):V {return V::sp(df_is_o($ip) ? $ip->getRemoteIp(
  * @used-by df_context()
  * @used-by df_sentry_m()
  * @used-by df_visitor_ip()
- * @used-by \Df\Core\Visitor::sp()
- * @used-by \Df\Security\BlackList::ip()
- * @used-by \Dfe\AlphaCommerceHub\Charge::pCharge()
- * @used-by \Dfe\Sift\API\B\Event::p()
- * @used-by \Dfe\TBCBank\API\Facade::check()
- * @used-by \Dfe\TBCBank\Charge::common()
- * @used-by \Dfe\TBCBank\Test\CaseT\Init::transId()
- * @used-by \Dfe\TBCBank\Test\CaseT\Regular::transId()
- * @used-by \CanadaSatellite\Bambora\Facade::api() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/175)
- * @used-by \Stock2Shop\OrderExport\Payload::visitor()
+ * @used-by Df\Core\Visitor::sp()
+ * @used-by Df\Security\BlackList::ip()
+ * @used-by Dfe\AlphaCommerceHub\Charge::pCharge()
+ * @used-by Dfe\Sift\API\B\Event::p()
+ * @used-by Dfe\TBCBank\API\Facade::check()
+ * @used-by Dfe\TBCBank\Charge::common()
+ * @used-by Dfe\TBCBank\Test\CaseT\Init::transId()
+ * @used-by Dfe\TBCBank\Test\CaseT\Regular::transId()
+ * @used-by CanadaSatellite\Bambora\Facade::api() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/175)
+ * @used-by Stock2Shop\OrderExport\Payload::visitor()
  */
 function df_visitor_ip():string {
 	/** @var RA $a */ $a = df_o(RA::class);
@@ -40,6 +40,6 @@ function df_visitor_ip():string {
 /**
  * 2017-11-01 It returns a string like «en_US».
  * https://stackoverflow.com/a/22334417
- * @used-by \Dfe\AlphaCommerceHub\Charge::pCharge()
+ * @used-by Dfe\AlphaCommerceHub\Charge::pCharge()
  */
 function df_visitor_locale():string {return \Locale::acceptFromHttp(dfa($_SERVER, 'HTTP_ACCEPT_LANGUAGE'));}
