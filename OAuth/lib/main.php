ff06bf47cc981e5d63d9a3a30b335b4d4dad4e47
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:44:40 +0100

    https://github.com/mage2pro/core/issues/440

--- OAuth/lib/main.php
+++ OAuth/lib/main.php
@@ -2,10 +2,10 @@
 use Df\OAuth\App;
 /**
  * 2017-07-10          
- * @used-by \Df\OAuth\FE\Button::app() 
- * @used-by \Df\OAuth\ReturnT\GeneralPurpose::_execute()  
- * @used-by \Dfe\Dynamics365\API\Client::headers()
- * @used-by \Dfe\Salesforce\API\Client::headers()
+ * @used-by Df\OAuth\FE\Button::app() 
+ * @used-by Df\OAuth\ReturnT\GeneralPurpose::_execute()  
+ * @used-by Dfe\Dynamics365\API\Client::headers()
+ * @used-by Dfe\Salesforce\API\Client::headers()
  * @param string|object $c
  */
 function df_oauth_app($c):App {return dfs_con($c, 'OAuth\\App');}
