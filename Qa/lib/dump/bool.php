3964e8a458b250bd8b8a6c16d2d04217e35a4ada
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:48:35 +0100

    https://github.com/mage2pro/core/issues/440

--- Qa/lib/dump/bool.php
+++ Qa/lib/dump/bool.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * @see df_bts_yn()
- * @used-by \Df\Qa\Dumper::dump()
- * @used-by \Dfe\AlphaCommerceHub\Method::charge()
- * @used-by \Dfe\Vantiv\Charge::pCharge()
- * @used-by \Inkifi\Pwinty\T\CaseT\V30\Order\Validate::t02()
+ * @used-by Df\Qa\Dumper::dump()
+ * @used-by Dfe\AlphaCommerceHub\Method::charge()
+ * @used-by Dfe\Vantiv\Charge::pCharge()
+ * @used-by Inkifi\Pwinty\T\CaseT\V30\Order\Validate::t02()
  */
 function df_bts(bool $v):string {return $v ? 'true' : 'false';}
 
 /**
  * 2017-11-08
- * @used-by \Dfe\Stripe\Init\Action::redirectUrl()
- * @used-by \Inkifi\Pwinty\Controller\Index\Index::execute()
+ * @used-by Dfe\Stripe\Init\Action::redirectUrl()
+ * @used-by Inkifi\Pwinty\Controller\Index\Index::execute()
  * @see df_bts()
  */
 function df_bts_yn(bool $v):string {return $v ? 'yes' : 'no';}
