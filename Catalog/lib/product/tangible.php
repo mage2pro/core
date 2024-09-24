d626424377232548e9074d51d028e05a11edfbc7
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:13:38 +0100

    https://github.com/mage2pro/core/issues/440

--- Catalog/lib/product/tangible.php
+++ Catalog/lib/product/tangible.php
@@ -17,9 +17,9 @@ use Magento\Quote\Model\Quote as Q;
  *			df_oqi_roots(df_assert_oq($v)), function($i) {return df_tangible($i->getProduct());}
  *		)
  * https://github.com/mage2pro/core/blob/7.5.2/Catalog/lib/product/tangible.php#L10-L17
- * @used-by \Amasty\Checkout\Model\QuoteManagement::saveInsertedInfo()
- * @used-by \Dfe\Frontend\ConfigSource\Visibility\Product\VD::needHideFor()
- * @used-by \Dfe\TwoCheckout\LineItem\Product::tangible()
+ * @used-by Amasty\Checkout\Model\QuoteManagement::saveInsertedInfo()
+ * @used-by Dfe\Frontend\ConfigSource\Visibility\Product\VD::needHideFor()
+ * @used-by Dfe\TwoCheckout\LineItem\Product::tangible()
  * @used-by app/code/Interactivated/Quotecheckout/view/frontend/templates/dashboard/onepage/billing.phtml (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/128)
  * @uses \Magento\Catalog\Model\Product::isVirtual()
  * @uses \Magento\Quote\Model\Quote::isVirtual()
