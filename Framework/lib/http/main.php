7674af69f3c6353dd856f1b4d58fb062e184dccc
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:38:54 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/http/main.php
+++ Framework/lib/http/main.php
@@ -19,7 +19,7 @@ function df_header_utf():void {df_is_cli() || headers_sent() ?: header('Content-
 /**
  * 2017-02-26      
  * @used-by df_response_sign()
- * @used-by \Dfe\GingerPaymentsBase\Api::__construct()
+ * @used-by Dfe\GingerPaymentsBase\Api::__construct()
  * @param array(string => string) $a [optional]
  * @return array(string => string)
  */
@@ -30,7 +30,7 @@ function df_headers(array $a = []):array {return dfak_transform($a + [
 /**             
  * 2016-12-04
  * @used-by df_customer_logged_in_2()
- * @used-by \Wolf\Filter\Block\Navigation::getCacheKeyInfo()
+ * @used-by Wolf\Filter\Block\Navigation::getCacheKeyInfo()
  */
 function df_http_context():Context {return df_o(Context::class);}
 
@@ -60,7 +60,7 @@ function df_http_context():Context {return df_o(Context::class);}
  * http://stackoverflow.com/a/10236480
  * https://amitabhkant.com/2011/08/21/using-timeouts-with-file_get_contents-in-php/
  * @used-by df_http_json()
- * @used-by \Dfe\Robokassa\Api\Options::p()
+ * @used-by Dfe\Robokassa\Api\Options::p()
  * @param array(string => string) $query [optional]
  * @param Closure|bool|mixed $onE [optional]
  */
@@ -73,13 +73,13 @@ function df_http_get(string $url, array $query = [], int $timeout = 0, $onE = tr
 /**
  * 2016-04-13
  * @see df_request_body_json()
- * @used-by \Df\Core\Visitor::r()
- * @used-by \Dfe\GoogleFont\Fonts::responseA()
- * @used-by \Dfe\AmazonLogin\Customer::res()
- * @used-by \Dfe\CurrencyConvert\Ecb::rates()
- * @used-by \Dfe\GoogleBackendLogin\Plugin\Backend\App\AbstractAction::beforeDispatch()
- * @used-by \Dfe\Paymill\Test\CaseT::token()
- * @used-by \Dfe\Salesforce\Test\Basic::t02_the_latest_version()
+ * @used-by Df\Core\Visitor::r()
+ * @used-by Dfe\GoogleFont\Fonts::responseA()
+ * @used-by Dfe\AmazonLogin\Customer::res()
+ * @used-by Dfe\CurrencyConvert\Ecb::rates()
+ * @used-by Dfe\GoogleBackendLogin\Plugin\Backend\App\AbstractAction::beforeDispatch()
+ * @used-by Dfe\Paymill\Test\CaseT::token()
+ * @used-by Dfe\Salesforce\Test\Basic::t02_the_latest_version()
  * @param array(string => string) $query [optional]
  * @param Closure|bool|mixed $onE [optional]
  * @return array(string => mixed)
@@ -90,7 +90,7 @@ function df_http_json(string $url, array $query = [], int $timeout = 0, $onE = t
 
 /**
  * 2018-07-25
- * @used-by \Frugue\Store\Block\Switcher::post()
+ * @used-by Frugue\Store\Block\Switcher::post()
  * @used-by vendor/blushme/checkout/view/frontend/templates/extra-sell.phtml (blushme.se)
  */
 function df_post_h():PostHelper {return df_o(PostHelper::class);}
@@ -99,46 +99,46 @@ function df_post_h():PostHelper {return df_o(PostHelper::class);}
  * @used-by df_scope()
  * @used-by df_order()
  * @used-by df_store()
- * @used-by \Alignet\Paymecheckout\Plugin\Magento\Framework\Session\SidResolver::aroundGetSid() (innomuebles.com, https://github.com/innomuebles/m2/issues/11)
- * @used-by \CabinetsBay\Catalog\Observer\LayoutLoadBefore::execute() (https://github.com/cabinetsbay/site/issues/98)
- * @used-by \CanadaSatellite\Core\Plugin\Magento\Framework\App\Http::aroundLaunch() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/72)
- * @used-by \Df\Backend\Model\Auth::loginByEmail()
- * @used-by \Df\Framework\Plugin\Data\Form\Element\Fieldset::beforeAddField()
- * @used-by \Df\Framework\Request::clean()
- * @used-by \Df\Framework\Request::extra()
- * @used-by \Df\Framework\Request::extraKeysRaw()
- * @used-by \Dfe\GoogleFont\Controller\Index\Index::execute()
- * @used-by \Dfe\GoogleFont\Controller\Index\Preview::contents()
- * @used-by \Dfe\GoogleFont\Font\Variant\Preview\Params::fromRequest()
- * @used-by \Df\OAuth\App::getAndSaveTheRefreshToken()
- * @used-by \Df\OAuth\App::state()
- * @used-by \Df\OAuth\ReturnT::redirectUrl()
- * @used-by \Df\Payment\CustomerReturn::execute()
- * @used-by \Df\Payment\CustomerReturn::isSuccess()
- * @used-by \Df\Payment\W\Strategy\ConfirmPending::_handle()
- * @used-by \Dfe\AllPay\Controller\CustomerReturn\Index::message()
- * @used-by \Dfe\AmazonLogin\Customer::url()
- * @used-by \Dfe\AmazonLogin\Customer::validate()
- * @used-by \Dfe\BlackbaudNetCommunity\Customer::p()
- * @used-by \Dfe\CheckoutCom\Controller\Index\Index::execute()
- * @used-by \Dfe\FacebookLogin\Customer::req()
- * @used-by \Dfe\FacebookLogin\Customer::token()
- * @used-by \Dfe\IPay88\Controller\CustomerReturn\Index::isSuccess()
- * @used-by \Dfe\IPay88\Controller\CustomerReturn\Index::message()
- * @used-by \Dfe\Robokassa\Controller\CustomerReturn\Index::isSuccess()
- * @used-by \Dfe\Sift\Observer\Customer\SaveAfterDataObject::execute()
- * @used-by \Dfe\Stripe\Controller\CustomerReturn\Index::isSuccess()
- * @used-by \Doormall\Shipping\Controller\Index\Index::execute()
- * @used-by \Frugue\Store\Plugin\Framework\App\FrontControllerInterface::aroundDispatch()
- * @used-by \Inkifi\Consolidation\Controller\Adminhtml\Index\Index::execute()
- * @used-by \Inkifi\Consolidation\Processor::s()
- * @used-by \MageWorx\OptionInventory\Controller\StockMessage\Update::execute() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/125)
- * @used-by \PPCs\Core\Plugin\Iksanika\Stockmanage\Block\Adminhtml\Product\Grid::aroundAddColumn()
- * @used-by \PPCs\Core\Plugin\Iksanika\Stockmanage\Controller\Adminhtml\Product\MassUpdateProducts::beforeExecute()
- * @used-by \RWCandy\Captcha\Observer\CustomerAccountCreatePost::execute()
- * @used-by \Wolf\Filter\Block\Navigation::selectedPath()
- * @used-by \Wolf\Filter\Controller\Garage\Remove::execute()
- * @used-by \Wolf\Filter\Controller\Index\Change::execute()
+ * @used-by Alignet\Paymecheckout\Plugin\Magento\Framework\Session\SidResolver::aroundGetSid() (innomuebles.com, https://github.com/innomuebles/m2/issues/11)
+ * @used-by CabinetsBay\Catalog\Observer\LayoutLoadBefore::execute() (https://github.com/cabinetsbay/site/issues/98)
+ * @used-by CanadaSatellite\Core\Plugin\Magento\Framework\App\Http::aroundLaunch() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/72)
+ * @used-by Df\Backend\Model\Auth::loginByEmail()
+ * @used-by Df\Framework\Plugin\Data\Form\Element\Fieldset::beforeAddField()
+ * @used-by Df\Framework\Request::clean()
+ * @used-by Df\Framework\Request::extra()
+ * @used-by Df\Framework\Request::extraKeysRaw()
+ * @used-by Dfe\GoogleFont\Controller\Index\Index::execute()
+ * @used-by Dfe\GoogleFont\Controller\Index\Preview::contents()
+ * @used-by Dfe\GoogleFont\Font\Variant\Preview\Params::fromRequest()
+ * @used-by Df\OAuth\App::getAndSaveTheRefreshToken()
+ * @used-by Df\OAuth\App::state()
+ * @used-by Df\OAuth\ReturnT::redirectUrl()
+ * @used-by Df\Payment\CustomerReturn::execute()
+ * @used-by Df\Payment\CustomerReturn::isSuccess()
+ * @used-by Df\Payment\W\Strategy\ConfirmPending::_handle()
+ * @used-by Dfe\AllPay\Controller\CustomerReturn\Index::message()
+ * @used-by Dfe\AmazonLogin\Customer::url()
+ * @used-by Dfe\AmazonLogin\Customer::validate()
+ * @used-by Dfe\BlackbaudNetCommunity\Customer::p()
+ * @used-by Dfe\CheckoutCom\Controller\Index\Index::execute()
+ * @used-by Dfe\FacebookLogin\Customer::req()
+ * @used-by Dfe\FacebookLogin\Customer::token()
+ * @used-by Dfe\IPay88\Controller\CustomerReturn\Index::isSuccess()
+ * @used-by Dfe\IPay88\Controller\CustomerReturn\Index::message()
+ * @used-by Dfe\Robokassa\Controller\CustomerReturn\Index::isSuccess()
+ * @used-by Dfe\Sift\Observer\Customer\SaveAfterDataObject::execute()
+ * @used-by Dfe\Stripe\Controller\CustomerReturn\Index::isSuccess()
+ * @used-by Doormall\Shipping\Controller\Index\Index::execute()
+ * @used-by Frugue\Store\Plugin\Framework\App\FrontControllerInterface::aroundDispatch()
+ * @used-by Inkifi\Consolidation\Controller\Adminhtml\Index\Index::execute()
+ * @used-by Inkifi\Consolidation\Processor::s()
+ * @used-by MageWorx\OptionInventory\Controller\StockMessage\Update::execute() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/125)
+ * @used-by PPCs\Core\Plugin\Iksanika\Stockmanage\Block\Adminhtml\Product\Grid::aroundAddColumn()
+ * @used-by PPCs\Core\Plugin\Iksanika\Stockmanage\Controller\Adminhtml\Product\MassUpdateProducts::beforeExecute()
+ * @used-by RWCandy\Captcha\Observer\CustomerAccountCreatePost::execute()
+ * @used-by Wolf\Filter\Block\Navigation::selectedPath()
+ * @used-by Wolf\Filter\Controller\Garage\Remove::execute()
+ * @used-by Wolf\Filter\Controller\Index\Change::execute()
  * @param string|string[] $k [optional]
  * @param string|null|callable $d [optional]
  * @return string|array(string => string)
@@ -151,14 +151,14 @@ function df_request($k = '', $d = null) {$o = df_request_o(); return df_nes($k) 
  * 2017-03-09
  * @used-by df_context()
  * @used-by df_request_body_json()
- * @used-by \Dfe\Sift\Controller\Index\Index::checkSignature()
+ * @used-by Dfe\Sift\Controller\Index\Index::checkSignature()
  */
 function df_request_body():string {return df_contents('php://input');}
 
 /**
  * 2017-03-09
  * @see df_http_json()
- * @used-by \Df\Payment\W\Reader\Json::http()
+ * @used-by Df\Payment\W\Reader\Json::http()
  */
 function df_request_body_json():string {return !($j = df_request_body()) ? [] : df_json_decode($j);}
 
@@ -168,8 +168,8 @@ function df_request_body_json():string {return !($j = df_request_body()) ? [] : 
  * @see \Laminas\Http\Headers::createKey()
  * https://github.com/zendframework/zendframework/blob/release-2.4.6/library/Zend/Http/Headers.php#L462-L471
  * @used-by df_request_ua()
- * @used-by \Dfe\Qiwi\W\Reader::http()
- * @used-by \Dfe\Sift\Controller\Index\Index::checkSignature()
+ * @used-by Dfe\Qiwi\W\Reader::http()
+ * @used-by Dfe\Sift\Controller\Index\Index::checkSignature()
  * @return string|false
  */
 function df_request_header(string $k) {return df_request_o()->getHeader($k);}
@@ -177,7 +177,7 @@ function df_request_header(string $k) {return df_request_o()->getHeader($k);}
 /**
  * 2021-06-05
  * @used-by df_context()
- * @used-by \Df\Sentry\Client::get_http_data()
+ * @used-by Df\Sentry\Client::get_http_data()
  */
 function df_request_method():string {return dfa($_SERVER, 'REQUEST_METHOD');}
 
@@ -189,14 +189,14 @@ function df_request_method():string {return dfa($_SERVER, 'REQUEST_METHOD');}
  * @used-by df_request()
  * @used-by df_request_header()
  * @used-by df_rp_has()
- * @used-by \Alignet\Paymecheckout\Plugin\Magento\Framework\Session\SidResolver::aroundGetSid() (innomuebles.com, https://github.com/innomuebles/m2/issues/11)
- * @used-by \CanadaSatellite\Core\Plugin\Magento\Framework\App\Http::aroundLaunch() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/72)
- * @used-by \Df\Framework\Action::module()
- * @used-by \Df\Sentry\Client::get_http_data()
- * @used-by \Dfe\Portal\Block\Content::getTemplate()
- * @used-by \Dfe\Portal\Controller\Index\Index::execute()
- * @used-by \MageWorx\OptionInventory\Controller\StockMessage\Update::execute() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/125)
- * @used-by \Wolf\Filter\Observer\ControllerActionPredispatch::execute()
+ * @used-by Alignet\Paymecheckout\Plugin\Magento\Framework\Session\SidResolver::aroundGetSid() (innomuebles.com, https://github.com/innomuebles/m2/issues/11)
+ * @used-by CanadaSatellite\Core\Plugin\Magento\Framework\App\Http::aroundLaunch() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/72)
+ * @used-by Df\Framework\Action::module()
+ * @used-by Df\Sentry\Client::get_http_data()
+ * @used-by Dfe\Portal\Block\Content::getTemplate()
+ * @used-by Dfe\Portal\Controller\Index\Index::execute()
+ * @used-by MageWorx\OptionInventory\Controller\StockMessage\Update::execute() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/125)
+ * @used-by Wolf\Filter\Observer\ControllerActionPredispatch::execute()
  * @return IRequest|RequestHttp
  */
 function df_request_o() {return df_o(IRequest::class);}
@@ -215,6 +215,6 @@ function df_request_o() {return df_o(IRequest::class);}
  * df_contains(df_url(), $s)) does not work properly for some requests.
  * E.g.: df_url() for the `/us/stores/store/switch/___store/uk` request will return `<website>/us/`
  * @used-by df_url_path_contains()
- * @used-by \Alignet\Paymecheckout\Plugin\Magento\Framework\Session\SidResolver::aroundGetSid() (innomuebles.com)
+ * @used-by Alignet\Paymecheckout\Plugin\Magento\Framework\Session\SidResolver::aroundGetSid() (innomuebles.com)
  */
 function df_rp_has(string ...$s):bool {return df_contains(df_request_o()->getPathInfo(), ...$s);}
