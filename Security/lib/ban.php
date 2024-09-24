81d90ba036ba76223ad96fb8a7a90e6d1a915d68
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:52:12 +0100

    https://github.com/mage2pro/core/issues/440

--- Security/lib/ban.php
+++ Security/lib/ban.php
@@ -3,9 +3,9 @@ use Df\Security\BlackList as B;
 /**
  * 2021-09-14
  * "Implement an ability to temporary ban visitors with a particular IP address": https://github.com/mage2pro/core/issues/159
- * @used-by \CanadaSatellite\Bambora\Action::check()  (canadasatellite.ca, https://github.com/canadasatellite-ca/bambora/issues/14)
- * @used-by \CanadaSatellite\Core\Plugin\Magento\Customer\Api\AccountManagementInterface::aroundIsEmailAvailable() (canadasatellite.ca, https://github.com/canadasatellite-ca/core/issues/1)
- * @used-by \CanadaSatellite\Core\Plugin\Magento\Framework\App\Http::aroundLaunch() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/72)
+ * @used-by CanadaSatellite\Bambora\Action::check()  (canadasatellite.ca, https://github.com/canadasatellite-ca/bambora/issues/14)
+ * @used-by CanadaSatellite\Core\Plugin\Magento\Customer\Api\AccountManagementInterface::aroundIsEmailAvailable() (canadasatellite.ca, https://github.com/canadasatellite-ca/core/issues/1)
+ * @used-by CanadaSatellite\Core\Plugin\Magento\Framework\App\Http::aroundLaunch() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/72)
  */
 function df_ban(string $ip = ''):void {
 	if (!df_is_backend()) {
