e67d98f3da2d733d4c7be6db58f19b8ec617dea9
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:47:24 +0100

    https://github.com/mage2pro/core/issues/440

--- Payment/lib/url-my.php
+++ Payment/lib/url-my.php
@@ -2,21 +2,21 @@
 use Df\Payment\Operation as Op;
 /**
  * 2016-08-27
- * @used-by \Df\Payment\Charge::customerReturn()
+ * @used-by Df\Payment\Charge::customerReturn()
  * @param string|object $m
  */
 function dfp_url_customer_return($m):string {return df_url_frontend(df_route($m, 'customerReturn'));}
 
 /**
  * 2016-08-27
- * @used-by \Df\Payment\Charge::customerReturnRemote()
+ * @used-by Df\Payment\Charge::customerReturnRemote()
  * @param string|object $m
  */
 function dfp_url_customer_return_remote($m):string {return df_webhook($m, 'customerReturn');}
 
 /**
  * 2017-08-23
- * @used-by \Df\Payment\Operation::customerReturnRemoteWithFailure()
+ * @used-by Df\Payment\Operation::customerReturnRemoteWithFailure()
  * @param string|object $m
  */
 function dfp_url_customer_return_remote_f($m):string {return df_webhook($m, 'customerReturn', false, null, [
