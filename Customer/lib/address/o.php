1dfc726b4f5eb1244d55863f445f0207c7f8c47b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:32:02 +0100

    https://github.com/mage2pro/core/issues/440

--- Customer/lib/address/o.php
+++ Customer/lib/address/o.php
@@ -6,20 +6,20 @@ use Magento\Customer\Model\ResourceModel\AddressRepository as Rep;
 
 /**
  * 2019-06-01
- * @used-by \KingPalm\B2B\Block\Registration::region()
+ * @used-by KingPalm\B2B\Block\Registration::region()
  */
 function df_address_h():H {return df_o(H::class);}
 
 /**
  * 2016-04-05
- * @used-by \Df\Customer\Plugin\Model\ResourceModel\AddressRepository::aroundSave()
- * @used-by \Dfe\Customer\Plugin\Customer\Model\ResourceModel\AddressRepository::aroundSave()
+ * @used-by Df\Customer\Plugin\Model\ResourceModel\AddressRepository::aroundSave()
+ * @used-by Dfe\Customer\Plugin\Customer\Model\ResourceModel\AddressRepository::aroundSave()
  */
 function df_address_registry():Reg {return df_o(Reg::class);}
 
 /**
  * 2021-05-07
- * @used-by \Df\Quote\Plugin\Model\QuoteAddressValidator::doValidate()
+ * @used-by Df\Quote\Plugin\Model\QuoteAddressValidator::doValidate()
  * @return IRep|Rep
  */
 function df_address_rep() {return df_o(IRep::class);}
