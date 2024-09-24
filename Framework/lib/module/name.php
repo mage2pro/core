101dbc85db0d5cdc2705b3b2492181281843a1f4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:39:35 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/module/name.php
+++ Framework/lib/module/name.php
@@ -29,12 +29,12 @@
  * @used-by df_sentry_module()
  * @used-by df_widget()
  * @used-by dfpm_title()
- * @used-by \Df\Core\Session::__construct()
- * @used-by \Df\Framework\Plugin\View\Element\AbstractBlock::afterGetModuleName()
- * @used-by \Df\Payment\Method::s()
- * @used-by \Df\Shipping\Method::s()
- * @used-by \Df\Sso\CustomerReturn::execute()
- * @used-by \TFC\Blog\Plugin\Block\Post\ListPost::afterGetCustomBlogThemeVendor()
+ * @used-by Df\Core\Session::__construct()
+ * @used-by Df\Framework\Plugin\View\Element\AbstractBlock::afterGetModuleName()
+ * @used-by Df\Payment\Method::s()
+ * @used-by Df\Shipping\Method::s()
+ * @used-by Df\Sso\CustomerReturn::execute()
+ * @used-by TFC\Blog\Plugin\Block\Post\ListPost::afterGetCustomBlogThemeVendor()
  * @param string|object|null $c [optional]
  */
 function df_module_name($c = null, string $d = '_'):string {return dfcf(
@@ -52,8 +52,8 @@ function df_module_name($c = null, string $d = '_'):string {return dfcf(
  * @used-by dfp_report()
  * @used-by dfs()
  * @used-by dfs_con()
- * @used-by \Df\Framework\Action::module()
- * @used-by \Df\Payment\Block\Info::checkoutSuccess()
+ * @used-by Df\Framework\Action::module()
+ * @used-by Df\Payment\Block\Info::checkoutSuccess()
  * @param string|object|null $c [optional]
  */
 function df_module_name_c($c = null):string {return df_module_name($c, '\\');}
@@ -62,7 +62,7 @@ function df_module_name_c($c = null):string {return df_module_name($c, '\\');}
  * 2016-08-28 «Dfe\AllPay\W\Handler» => «AllPay»
  * 2016-10-20
  * Making $c optional leads to the error «get_class() called without object from outside a class»: https://3v4l.org/k6Hd5
- * @used-by \Df\Payment\W\Reader::testData()
+ * @used-by Df\Payment\W\Reader::testData()
  * @param string|object $c
  */
 function df_module_name_short($c):string {return df_explode_class($c)[1];}
@@ -78,9 +78,9 @@ function df_module_name_short($c):string {return df_explode_class($c)[1];}
  * 		3) an object. It will be treated as case 2 after @see get_class()
  * @see df_cts_lc_camel()
  * @used-by df_report_prefix()
- * @used-by \Df\Payment\Method::codeS()
- * @used-by \Df\Shipping\Method::getCarrierCode()
- * @used-by \Dfe\Stripe\Block\Multishipping::_toHtml()
+ * @used-by Df\Payment\Method::codeS()
+ * @used-by Df\Shipping\Method::getCarrierCode()
+ * @used-by Dfe\Stripe\Block\Multishipping::_toHtml()
  * @param string|object $c
  */
 function df_module_name_lc($c, string $d = '_'):string {return implode($d, df_explode_class_lc_camel(df_module_name_c($c)));}
