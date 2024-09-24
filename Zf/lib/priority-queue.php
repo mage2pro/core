fecc23de5774bacf84d5798aeff9c7ddcd8aee8f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:55:12 +0100

    https://github.com/mage2pro/core/issues/440

--- Zf/lib/priority-queue.php
+++ Zf/lib/priority-queue.php
@@ -13,20 +13,20 @@ function df_zf_pq_max(Q $q):int {return $q->isEmpty() ? 0 : max($q->toArray(Q::E
  * Returns the minimum priority from the queue.
  * A check for emptiness is required because of «min(): Array must contain at least one element».
  * @used-by df_zf_pq_add_lowest()
- * @used-by \Df\API\Client::appendFilter()
+ * @used-by Df\API\Client::appendFilter()
  */
 function df_zf_pq_min(Q $q):int {return $q->isEmpty() ? 0 : min($q->toArray(Q::EXTR_PRIORITY));}
 
 /**
  * 2017-07-07 Adds $i to the queue at the lowest priority.
- * @used-by \Df\Zf\Test\main::t01()
+ * @used-by Df\Zf\Test\main::t01()
  * @param mixed $i
  */
 function df_zf_pq_add_highest(Q $q, $i):void {$q->insert($i, df_zf_pq_max($q) + 1);}
 
 /**
  * 2017-07-07 Adds $i to the queue at the lowest priority.
- * @used-by \Df\Zf\Test\main::t01()
+ * @used-by Df\Zf\Test\main::t01()
  * @param mixed $i
  */
 function df_zf_pq_add_lowest(Q $q, $i):void {$q->insert($i, df_zf_pq_min($q) - 1);}
