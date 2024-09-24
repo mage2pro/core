aa8a5af3d575d3d21bee59c8f31f92bd616887cf
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:32:14 +0100

    https://github.com/mage2pro/core/issues/440

--- Customer/lib/attribute.php
+++ Customer/lib/attribute.php
@@ -14,14 +14,14 @@ function df_customer_att($a):IA {return df_eav_config()->getAttribute(df_eav_cus
 
 /**      
  * 2016-06-04
- * @used-by \Df\Sso\Customer::dob()
- * @used-by \Df\Sso\CustomerReturn::customerData()
+ * @used-by Df\Sso\Customer::dob()
+ * @used-by Df\Sso\CustomerReturn::customerData()
  */
 function df_customer_att_is_required(string $c):bool {return df_customer_att($c)->getIsRequired();}
 
 /**
  * 2019-06-15
- * @used-by \Df\Customer\AddAttribute\Customer::p()
+ * @used-by Df\Customer\AddAttribute\Customer::p()
  */
 function df_customer_att_pos_after(string $a):int {return 1 + (int)df_conn()->fetchOne(
 	df_db_from(['ca' => 'customer_eav_attribute'], 'sort_order')
@@ -31,15 +31,15 @@ function df_customer_att_pos_after(string $a):int {return 1 + (int)df_conn()->fe
 
 /**        
  * 2019-06-03 
- * @used-by \Df\Customer\AddAttribute\Address::p()
- * @used-by \Df\Customer\AddAttribute\Customer::p()
- * @used-by \KingPalm\B2B\Setup\UpgradeData::_process()
+ * @used-by Df\Customer\AddAttribute\Address::p()
+ * @used-by Df\Customer\AddAttribute\Customer::p()
+ * @used-by KingPalm\B2B\Setup\UpgradeData::_process()
  */
 function df_customer_att_pos_next():int {return 10 + df_fetch_col_max('customer_eav_attribute', 'sort_order');}
 
 /**
  * 2019-06-15
- * @used-by \KingPalm\B2B\Setup\UpgradeData::_process()
+ * @used-by KingPalm\B2B\Setup\UpgradeData::_process()
  */
 function df_customer_att_pos_set(string $a, int $pos):void {df_conn()->update(
 	df_table('customer_eav_attribute'), ['sort_order' => $pos], ['? = attribute_id' => df_att_code2id($a)]
