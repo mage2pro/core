aabfea34684b4ef3d1fb5623a8fc7d20eacadd65
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:16:28 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/array/other/array.php
+++ Core/lib/array/other/array.php
@@ -5,9 +5,9 @@
  * @used-by df_fe_init()
  * @used-by df_find()
  * @used-by df_map()
- * @used-by \Df\API\Facade::p()
- * @used-by \Df\Framework\Plugin\Data\Form\Element\AbstractElement::afterGetComment()
- * @used-by \TFC\Core\B\Home\Slider::p() (tradefurniturecompany.co.uk, https://github.com/tradefurniturecompany/core/issues/43)
+ * @used-by Df\API\Facade::p()
+ * @used-by Df\Framework\Plugin\Data\Form\Element\AbstractElement::afterGetComment()
+ * @used-by TFC\Core\B\Home\Slider::p() (tradefurniturecompany.co.uk, https://github.com/tradefurniturecompany/core/issues/43)
  * @param mixed|mixed[] $v
  */
 function df_array($v):array {return is_array($v) ? $v : [$v];}
