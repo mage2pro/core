303e8d3c35e89d4646a109dcfda56c5038a9b125
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:43:13 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/event.php
+++ Framework/lib/event.php
@@ -5,18 +5,18 @@ use Magento\Framework\Event\ManagerInterface as IManager;
  * 2015-08-16
  * https://mage2.ru/t/95
  * https://mage2.pro/t/60
- * @used-by \CanadaSatellite\Bambora\Method::isAvailable() (canadasatellite.ca, https://github.com/canadasatellite-ca/bambora/issues/1)
- * @used-by \Df\Backend\Model\Auth::loginByEmail()
- * @used-by \Df\Framework\Log\Dispatcher::handle()
- * @used-by \Df\Framework\Plugin\App\ResponseInterface::afterSendResponse()
- * @used-by \Df\Framework\Plugin\Mail\TransportInterfaceFactory::aroundCreate()
- * @used-by \Df\Framework\Plugin\View\Element\UiComponent\DataProvider\DataProvider::afterGetSearchResult()
- * @used-by \Df\Payment\Method::isAvailable()
- * @used-by \Df\Sales\Plugin\Model\Order\Address\Renderer::aroundFormat()
- * @used-by \Df\Sso\CustomerReturn::mc()
- * @used-by \Df\Sso\CustomerReturn::register()
- * @used-by \Df\Ui\Plugin\Component\Listing\Columns\Column::beforePrepare()
- * @used-by \Df\User\Plugin\Model\User::aroundAuthenticate()
+ * @used-by CanadaSatellite\Bambora\Method::isAvailable() (canadasatellite.ca, https://github.com/canadasatellite-ca/bambora/issues/1)
+ * @used-by Df\Backend\Model\Auth::loginByEmail()
+ * @used-by Df\Framework\Log\Dispatcher::handle()
+ * @used-by Df\Framework\Plugin\App\ResponseInterface::afterSendResponse()
+ * @used-by Df\Framework\Plugin\Mail\TransportInterfaceFactory::aroundCreate()
+ * @used-by Df\Framework\Plugin\View\Element\UiComponent\DataProvider\DataProvider::afterGetSearchResult()
+ * @used-by Df\Payment\Method::isAvailable()
+ * @used-by Df\Sales\Plugin\Model\Order\Address\Renderer::aroundFormat()
+ * @used-by Df\Sso\CustomerReturn::mc()
+ * @used-by Df\Sso\CustomerReturn::register()
+ * @used-by Df\Ui\Plugin\Component\Listing\Columns\Column::beforePrepare()
+ * @used-by Df\User\Plugin\Model\User::aroundAuthenticate()
  * @param array(string => mixed) $d
  */
 function df_dispatch(string $ev, array $d = []):void {
