79337b8689660c2dc9e4ad33c6653f2f4e1a83c6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:25:07 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/text/prepend-append/.php
+++ Core/lib/text/prepend-append/.php
@@ -10,6 +10,6 @@ function df_append(string $s, string $tail):string {return df_ends_with($s, $tai
  * 2016-03-08 It adds the $head prefix to the $s string if the prefix is absent in $s.
  * @used-by df_sys_path_abs()
  * @used-by ikf_ite()
- * @used-by \Df\Framework\Plugin\Data\Form\Element\AbstractElement::afterGetElementHtml()
+ * @used-by Df\Framework\Plugin\Data\Form\Element\AbstractElement::afterGetElementHtml()
  */
 function df_prepend(string $s, string $head):string {return df_starts_with($s, $head) ? $s : $head . $s;}
