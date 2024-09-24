7f0e1abd669db8b733bbf905b39ca284917fd044
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:18:47 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/array/select/try.php
+++ Core/lib/array/select/try.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 2019-01-28
- * @used-by \Dfe\Vantiv\API\Client::_construct()
+ * @used-by Dfe\Vantiv\API\Client::_construct()
  * 2024-03-05
  * 1) https://3v4l.org/C3qrh
  * 2) The previous code (`dfa_seq`): https://github.com/mage2pro/core/blob/10.6.9/Core/lib/array/main.php#L214-L231
