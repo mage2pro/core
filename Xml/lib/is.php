468727389c44570c5686e7b27679568a0372cfc1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:54:32 +0100

    https://github.com/mage2pro/core/issues/440

--- Xml/lib/is.php
+++ Xml/lib/is.php
@@ -5,7 +5,7 @@
  * Вообще говоря, заголовок у XML необязателен, но моя функция @see df_xml_prettify() его добавляет,
  * поэтому меня пока данный алгоритм устраивает.
  * Более качественный алгоритм будет более ресурсоёмким: нам надо будет разбирать весь XML.
- * @used-by \Df\Backend\Block\Widget\Grid\Column\Renderer\Text::render()
+ * @used-by Df\Backend\Block\Widget\Grid\Column\Renderer\Text::render()
  * @param mixed $v
  */
 function df_is_xml($v):bool {return is_string($v) && df_starts_with($v, '<?xml');}
