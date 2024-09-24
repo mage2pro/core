c782e356b54522e9e99bbd40487a959288934966
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:09:10 +0100

    https://github.com/mage2pro/core/issues/440

--- Catalog/lib/category/hierarchy.php
+++ Catalog/lib/category/hierarchy.php
@@ -5,7 +5,7 @@ use Magento\Catalog\Model\Category as C;
  * 2024-03-10
  * "The result of `Magento\Catalog\Model\Category::getParentCategories()` always includes the caller":
  * https://mage2.pro/t/6429
- * @used-by \CabinetsBay\Catalog\B\Category::l3() (https://github.com/cabinetsbay/site/issues/98)
+ * @used-by CabinetsBay\Catalog\B\Category::l3() (https://github.com/cabinetsbay/site/issues/98)
  * @return C|null
  */
 function df_category_ancestor_at_level(C $c, int $l) {return $l > ($l2 = df_category_level($c)) ? null : (
@@ -19,8 +19,8 @@ function df_category_ancestor_at_level(C $c, int $l) {return $l > ($l2 = df_cate
  * 2.2) Level 1: «Default Category» (undeletable, can be renamed).
  * @used-by cb_category_is_l2() (https://github.com/cabinetsbay/site/issues/98)
  * @used-by df_category_ancestor_at_level()
- * @used-by \CabinetsBay\Catalog\Observer\LayoutLoadBefore::execute() (https://github.com/cabinetsbay/catalog/issues/3)
- * @used-by \CabinetsBay\Catalog\B\Category::level() (https://github.com/cabinetsbay/site/issues/98)
+ * @used-by CabinetsBay\Catalog\Observer\LayoutLoadBefore::execute() (https://github.com/cabinetsbay/catalog/issues/3)
+ * @used-by CabinetsBay\Catalog\B\Category::level() (https://github.com/cabinetsbay/site/issues/98)
  * @used-by vendor/cabinetsbay/catalog/view/frontend/templates/products.phtml (https://github.com/cabinetsbay/site/issues/98)
  */
 function df_category_level(?C $c = null):int {return (int)($c ?: df_category_current())->getLevel();}
