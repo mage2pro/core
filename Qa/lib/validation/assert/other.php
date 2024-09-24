784ceef5afd3fbb64ac43d169e449b7896ebb013
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:49:53 +0100

    https://github.com/mage2pro/core/issues/440

--- Qa/lib/validation/assert/other.php
+++ Qa/lib/validation/assert/other.php
@@ -6,10 +6,10 @@ use Throwable as Th; # 2023-08-03 "Treat `\Throwable` similar to `\Exception`": 
 /**
  * 2017-01-14
  * @used-by df_xml_s_simple()
- * @used-by \Dfe\GoogleFont\Font\Variant\Preview::box()
- * @used-by \Dfe\GoogleFont\Fonts\Png::colorAllocateAlpha()
- * @used-by \Dfe\GoogleFont\Fonts\Png::image()
- * @used-by \Dfe\GoogleFont\Fonts\Sprite::draw()
+ * @used-by Dfe\GoogleFont\Font\Variant\Preview::box()
+ * @used-by Dfe\GoogleFont\Fonts\Png::colorAllocateAlpha()
+ * @used-by Dfe\GoogleFont\Fonts\Png::image()
+ * @used-by Dfe\GoogleFont\Fonts\Sprite::draw()
  * @param mixed $v
  * @param string|Th $m [optional]
  * @return mixed
@@ -24,14 +24,14 @@ function df_assert_nef($v, $m = null) {return false !== $v ? $v : df_error($m ?:
  * @used-by df_file_name()
  * @used-by df_json_decode()
  * @used-by df_xml_x()
- * @used-by \CanadaSatellite\Bambora\Action\Authorize::p() (canadasatellite.ca, https://github.com/canadasatellite-ca/bambora/issues/1)
- * @used-by \CanadaSatellite\Bambora\Action\_Void::p() (canadasatellite.ca, https://github.com/canadasatellite-ca/bambora/issues/1)
- * @used-by \Df\Payment\W\Event::pid()
- * @used-by \Df\PaypalClone\Charge::p()
- * @used-by \Df\StripeClone\Payer::newCard()
- * @used-by \Df\Xml\G::addAttributes()
- * @used-by \Dfe\Stripe\Controller\CustomerReturn\Index::isSuccess()
- * @used-by \Dfe\Stripe\Init\Action::redirectUrl()
+ * @used-by CanadaSatellite\Bambora\Action\Authorize::p() (canadasatellite.ca, https://github.com/canadasatellite-ca/bambora/issues/1)
+ * @used-by CanadaSatellite\Bambora\Action\_Void::p() (canadasatellite.ca, https://github.com/canadasatellite-ca/bambora/issues/1)
+ * @used-by Df\Payment\W\Event::pid()
+ * @used-by Df\PaypalClone\Charge::p()
+ * @used-by Df\StripeClone\Payer::newCard()
+ * @used-by Df\Xml\G::addAttributes()
+ * @used-by Dfe\Stripe\Controller\CustomerReturn\Index::isSuccess()
+ * @used-by Dfe\Stripe\Init\Action::redirectUrl()
  * @throws DFE
  */
 function df_assert_sne(string $v, int $sl = 0):string {
