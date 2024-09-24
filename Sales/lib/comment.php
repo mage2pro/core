914d1ab1d55b3c24af1132a54efafee016a27f7e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:51:17 +0100

    https://github.com/mage2pro/core/issues/440

--- Sales/lib/comment.php
+++ Sales/lib/comment.php
@@ -5,9 +5,9 @@ use Magento\Sales\Model\Order\Status\History;
 /**
  * 2017-04-13
  * @used-by df_mail_order()
- * @used-by \Df\Payment\CustomerReturn::execute()   
- * @used-by \Df\Payment\W\Strategy\ConfirmPending::_handle()
- * @used-by \Stock2Shop\OrderExport\Observer\OrderSaveAfter::execute()
+ * @used-by Df\Payment\CustomerReturn::execute()   
+ * @used-by Df\Payment\W\Strategy\ConfirmPending::_handle()
+ * @used-by Stock2Shop\OrderExport\Observer\OrderSaveAfter::execute()
  */
 function df_order_comment(O $o, string $text, bool $visibleOnFront = false, bool $customerNotified = false):void {
 	$h = $o->addStatusHistoryComment(__($text)); /** @var History|IHistory $h */
