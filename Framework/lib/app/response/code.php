2c1118549f1928dc1efd5f0dd3721621611d43ae
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:35:34 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/app/response/code.php
+++ Framework/lib/app/response/code.php
@@ -4,7 +4,7 @@ use Magento\Framework\App\Response\Http as HttpResponse;
 
 /**
  * 2021-05-24 https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400#content
- * @used-by \MageWorx\OptionInventory\Controller\StockMessage\Update::execute() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/125)
+ * @used-by MageWorx\OptionInventory\Controller\StockMessage\Update::execute() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/125)
  * @return IResponse|HttpResponse
  */
 function df_400() {return df_response_code(400);}
@@ -12,7 +12,7 @@ function df_400() {return df_response_code(400);}
 /**
  * 2021-04-19 https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/403#content
  * @used-by df_ban()
- * @used-by \CanadaSatellite\Core\Plugin\Magento\Framework\App\Http::aroundLaunch() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/72)
+ * @used-by CanadaSatellite\Core\Plugin\Magento\Framework\App\Http::aroundLaunch() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/72)
  * @return IResponse|HttpResponse
  */
 function df_403() {return df_response_code(403);}
@@ -20,9 +20,9 @@ function df_403() {return df_response_code(403);}
 /**
  * 2020-02-24 https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500#content
  * @used-by ikf_endpoint()	inkifi.com
- * @used-by \Dfe\CheckoutCom\Handler::p()
- * @used-by \Dfe\Sift\Controller\Index\Index::execute()
- * @used-by \Dfe\TwoCheckout\Handler::p()
+ * @used-by Dfe\CheckoutCom\Handler::p()
+ * @used-by Dfe\Sift\Controller\Index\Index::execute()
+ * @used-by Dfe\TwoCheckout\Handler::p()
  * @return IResponse|HttpResponse
  */
 function df_500() {return df_response_code(500);}
