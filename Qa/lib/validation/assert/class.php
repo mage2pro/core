0c86186e5915c3f0e6dae3519d5c7e66606fa18a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:49:29 +0100

    https://github.com/mage2pro/core/issues/440

--- Qa/lib/validation/assert/class.php
+++ Qa/lib/validation/assert/class.php
@@ -11,13 +11,13 @@ use Throwable as Th; # 2023-08-03 "Treat `\Throwable` similar to `\Exception`": 
  * @used-by df_newa()
  * @used-by df_trans()
  * @used-by dfpex_args()
- * @used-by \Df\Payment\Choice::f()
- * @used-by \Df\Payment\Operation\Source\Creditmemo::cm()
- * @used-by \Df\Payment\Operation\Source\Order::ii()
- * @used-by \Df\Payment\Operation\Source\Quote::ii()
- * @used-by \Df\Payment\W\Strategy::handle()
- * @used-by \Df\Payment\W\Strategy::m()
- * @used-by \Df\Payment\W\Strategy\Refund::_handle()
+ * @used-by Df\Payment\Choice::f()
+ * @used-by Df\Payment\Operation\Source\Creditmemo::cm()
+ * @used-by Df\Payment\Operation\Source\Order::ii()
+ * @used-by Df\Payment\Operation\Source\Quote::ii()
+ * @used-by Df\Payment\W\Strategy::handle()
+ * @used-by Df\Payment\W\Strategy::m()
+ * @used-by Df\Payment\W\Strategy\Refund::_handle()
  * @param string|object $v
  * @param string|object|null $c [optional]
  * @param string|Th|null $m [optional]
@@ -40,7 +40,7 @@ function df_ar($v, $c = null, $m = null) {return dfcf(function($v, $c = null, $m
 /**
  * 2016-08-03
  * @used-by df_ar()
- * @used-by \Df\Config\Backend\Serialized::entityC()
+ * @used-by Df\Config\Backend\Serialized::entityC()
  * @param string|Th $m [optional]
  * @throws DFE
  */
