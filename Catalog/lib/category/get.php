97c80e881c16b171cb31239adea446f01f30dda7
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:08:59 +0100

    https://github.com/mage2pro/core/issues/440

--- Catalog/lib/category/get.php
+++ Catalog/lib/category/get.php
@@ -8,10 +8,10 @@ use Magento\Store\Api\Data\StoreInterface as IStore;
  * @used-by df_category_ancestor_at_level()
  * @used-by df_category_children()
  * @used-by df_category_children_map()
- * @used-by \Wolf\Filter\Block\Navigation::hDropdowns()
- * @used-by \Wolf\Filter\Block\Navigation::selectedPath()
- * @used-by \Wolf\Filter\Controller\Index\Change::execute()
- * @used-by \CabinetsBay\Catalog\B\Category::l3p() (https://github.com/cabinetsbay/site/issues/98)
+ * @used-by CabinetsBay\Catalog\B\Category::l3p() (https://github.com/cabinetsbay/site/issues/98)
+ * @used-by Wolf\Filter\Block\Navigation::hDropdowns()
+ * @used-by Wolf\Filter\Block\Navigation::selectedPath()
+ * @used-by Wolf\Filter\Controller\Index\Change::execute()
  * @param int|string|C $c
  * @param int|string|null|bool|IStore $s [optional]
  */
