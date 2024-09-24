a75ff2197ae712b35f6ad1c7f80142778e610a57
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:15:26 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/array/clean/filter.php
+++ Core/lib/array/clean/filter.php
@@ -17,8 +17,8 @@
  * https://php.watch/versions/8.0/union-types
  * https://3v4l.org/AOWmO
  * @used-by df_clean_r()
- * @used-by \Frugue\Core\Plugin\Sales\Model\Quote::afterGetAddressesCollection()
- * @used-by \TFC\Core\Plugin\Sales\Model\Order::afterGetParentItemsRandomCollection()
+ * @used-by Frugue\Core\Plugin\Sales\Model\Quote::afterGetAddressesCollection()
+ * @used-by TFC\Core\Plugin\Sales\Model\Order::afterGetParentItemsRandomCollection()
  * @param callable|iterable $a1
  * @param callable|iterable $a2
  * @return array(int|string => mixed)
@@ -102,7 +102,7 @@ function df_filter_head($a1, $a2):array {return df_filter_f($a1, $a2, function(a
  * 2) We still can not use «Union Types» (e.g. `callable|iterable`) because they require PHP ≥ 8 (we need to support PHP ≥ 7.1):
  * 2.1) https://php.watch/versions/8.0/union-types
  * 2.2) https://3v4l.org/AOWmO
- * @used-by \Df\Qa\Trace::__construct()
+ * @used-by Df\Qa\Trace::__construct()
  * @param callable|iterable $a1
  * @param callable|iterable $a2
  * @return array(int|string => mixed)
