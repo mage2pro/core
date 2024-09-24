565560dc487b5efe08c06e4583b920cafa6372f4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:32:57 +0100

    https://github.com/mage2pro/core/issues/440

--- Customer/lib/state.php
+++ Customer/lib/state.php
@@ -7,7 +7,7 @@ use Magento\Sales\Model\Order as O;
 
 /**
  * 2016-12-04
- * @used-by \Df\Customer\Observer\RegisterSuccess::execute()
+ * @used-by Df\Customer\Observer\RegisterSuccess::execute()
  * @param C|DC|int $c
  */
 function df_customer_is_need_confirm($c):bool {return
@@ -16,10 +16,10 @@ function df_customer_is_need_confirm($c):bool {return
 
 /**
  * 2016-03-15 How to programmatically check whether a customer is new or returning? https://mage2.pro/t/1617
- * @used-by \Df\Payment\Init\Action::preconfigured()
- * @used-by \Df\Payment\Settings\_3DS::disable_()
- * @used-by \Df\Payment\Settings\_3DS::enable_()
- * @used-by \Dfe\CheckoutCom\Settings::actionDesired()
+ * @used-by Df\Payment\Init\Action::preconfigured()
+ * @used-by Df\Payment\Settings\_3DS::disable_()
+ * @used-by Df\Payment\Settings\_3DS::enable_()
+ * @used-by Dfe\CheckoutCom\Settings::actionDesired()
  * @param int|null $id
  */
 function df_customer_is_new($id):bool {return dfcf(function($id) {return !$id || !df_conn()->fetchOne(
@@ -32,14 +32,14 @@ function df_customer_is_new($id):bool {return dfcf(function($id) {return !$id ||
  * 2015-11-09
  * Сегодня заметил странную ситуацию, что метод @uses \Magento\Customer\Model\Session::isLoggedIn()
  * для авторизованных посетителей стал почему-то возвращать false
- * в контексте вызова из @used-by \Df\Sso\Button::_toHtml()().
+ * в контексте вызова из @used-by Df\Sso\Button::_toHtml()().
  * Также заметил, что стандартный блок авторизации в шапке страницы
  * определяет авторизованность посетителя совсем по-другому алгоритму:
  * @see \Magento\Customer\Block\Account\AuthorizationLink::isLoggedIn()
  * Вот именно этот алгоритм мы сейчас и задействуем.
  * https://github.com/sayitwithagift/core/issues/3
- * @used-by \Df\Sso\Button::_toHtml()
- * @used-by \Dfe\AmazonLogin\Button::jsOptions()
+ * @used-by Df\Sso\Button::_toHtml()
+ * @used-by Dfe\AmazonLogin\Button::jsOptions()
  * @used-by beverageuniverse.com: app/design/frontend/Magento/Beverage/Magento_Theme/templates/html/topmenu.phtml
  */
 function df_customer_logged_in():bool {return df_customer_session()->isLoggedIn() || df_customer_logged_in_2();}
