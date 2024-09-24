4ce77f5c8ea4ce80006948383c230b273f253a3f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:12:10 +0100

    https://github.com/mage2pro/core/issues/440

--- Catalog/lib/product/image/o.php
+++ Catalog/lib/product/image/o.php
@@ -10,12 +10,12 @@ use Magento\Catalog\Model\Product\Media\Config as MC;
  * @used-by app/design/frontend/MageBig/martfury/layout01/MageBig_QuickView/templates/product/view/gallery.phtml (innomuebles.com, https://github.com/innomuebles/m2/issues/7)
  * @used-by app/design/frontend/MageBig/martfury/layout01/MageBig_WidgetPlus/templates/widget/layout/view/gallery.phtml (innomuebles.com, https://github.com/innomuebles/m2/issues/7)
  * @used-by app/design/frontend/MageBig/martfury/layout01/Magento_Catalog/templates/product/view/gallery.phtml:25 (innomuebles.com, https://github.com/innomuebles/m2/issues/7)
- * @used-by \SayItWithAGift\Options\Frontend::_toHtml()
+ * @used-by SayItWithAGift\Options\Frontend::_toHtml()
  */
 function df_catalog_image_h():ImageH {return df_o(ImageH::class);}
 
 /**
  * 2018-07-16
- * @used-by \SayItWithAGift\Options\Frontend::_toHtml()
+ * @used-by SayItWithAGift\Options\Frontend::_toHtml()
  */
 function df_product_mc():MC {return df_o(MC::class);}
