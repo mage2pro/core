7d94aa0ce0d4a99512a2c94b812cfbfa8ac719d1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:37:18 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/fs/dir/create.php
+++ Framework/lib/fs/dir/create.php
@@ -13,7 +13,7 @@ function df_mkdir(string $f):void {df_file()->checkAndCreateFolder($f);}
  * 2021-03-20
  * It solves issues like «var/log/Magedelight_Firstdata_SOAPError.log" cannot be opened with mode "a"»:
  * https://github.com/canadasatellite-ca/site/issues/22
- * @used-by \Magedelight\Firstdata\Model\Api\AbstractInterface::__construct() (canadasatellite.ca)
+ * @used-by Magedelight\Firstdata\Model\Api\AbstractInterface::__construct() (canadasatellite.ca)
  * @throws Exception
  */
 function df_mkdir_log():void {df_mkdir(df_fs_dl()->getPath(DL::LOG));}
