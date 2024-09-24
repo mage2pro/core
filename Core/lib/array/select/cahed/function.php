ee3803b40326d27a25ebea8d298da0de61720fa8
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:17:59 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/array/select/cahed/function.php
+++ Core/lib/array/select/cahed/function.php
@@ -6,8 +6,8 @@ use Closure as F;
  * @see dfaoc()
  * @used-by df_credentials()
  * @used-by dfe_portal_module()
- * @used-by \Df\Framework\Request::extra()
- * @used-by \Df\OAuth\App::state()
+ * @used-by Df\Framework\Request::extra()
+ * @used-by Df\OAuth\App::state()
  * @param string|string[] $k [optional]
  * @param mixed|callable|null $d [optional]
  * @return mixed
