0b8672782710dcf3e565e25921bd94ebe01f0863
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:31:06 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/session.php
+++ Core/lib/session.php
@@ -4,7 +4,7 @@ use Magento\Framework\Session\Generic as GenericSession;
 use Magento\Framework\Session\SessionManagerInterface as ISessionManager;
 /**
  * 2018-05-14
- * @used-by \Df\Backend\Model\Auth::loginByEmail()
+ * @used-by Df\Backend\Model\Auth::loginByEmail()
  * @return ISessionManager|BackendSession|GenericSession
  */
 function df_session_manager() {return df_o(ISessionManager::class);}
