37f412b71ce0b8fdc13eeaf666dbc4b5fa8f8db6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:24:45 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/text/n/has.php
+++ Core/lib/text/n/has.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * 2024-09-22
- * @used-by \Df\Core\Html\Tag::content()
- * @used-by \Df\Core\Text\Regex::isSubjectMultiline()
- * @used-by \Df\Qa\Dumper::dumpObject()
+ * @used-by Df\Core\Html\Tag::content()
+ * @used-by Df\Core\Text\Regex::isSubjectMultiline()
+ * @used-by Df\Qa\Dumper::dumpObject()
  */
 function df_is_multiline(string $s):bool {return df_contains(df_normalize($s), "\n");}
