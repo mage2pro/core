4016f048ef06b0c47d1a217bca58e62842139870
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:18:31 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/array/select/getData.php
+++ Core/lib/array/select/getData.php
@@ -5,9 +5,9 @@ use Magento\Framework\DataObject as _DO;
 /**
  * 2020-01-29     
  * @used-by df_call()
- * @used-by \Df\Config\Backend::fc()
- * @used-by \Df\Payment\Block\Info::ii()
- * @used-by \Df\Payment\Method::ii()
+ * @used-by Df\Config\Backend::fc()
+ * @used-by Df\Payment\Block\Info::ii()
+ * @used-by Df\Payment\Method::ii()
  * @param _DO|AE $o
  * @param string|string[] $k [optional]
  * @param mixed|callable|null $d [optional]
