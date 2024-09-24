23dfa8adb7e1c4c26ee18448434987b542dc3422
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:48:16 +0100

    https://github.com/mage2pro/core/issues/440

--- Qa/lib/dump/kv/.php
+++ Qa/lib/dump/kv/.php
@@ -4,9 +4,9 @@
  * @see df_dump()
  * @see df_kv_table()
  * @used-by df_api_rr_failed()
- * @used-by \Df\API\Client::p()
- * @used-by \Df\Qa\Failure\Error::preface()
- * @used-by \Df\Sentry\Client::send_http()
+ * @used-by Df\API\Client::p()
+ * @used-by Df\Qa\Failure\Error::preface()
+ * @used-by Df\Sentry\Client::send_http()
  * @param array(string => string) $a
  */
 function df_kv(array $a, int $pad = 0):string {return df_cc_n(df_map_k(df_clean($a), function($k, $v) use($pad) {return
