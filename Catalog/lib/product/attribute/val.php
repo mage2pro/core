4bdd97ab33aa153a845e92c92b8530c03467db04
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:11:32 +0100

    https://github.com/mage2pro/core/issues/440

--- Catalog/lib/product/attribute/val.php
+++ Catalog/lib/product/attribute/val.php
@@ -7,8 +7,8 @@ use Magento\Framework\Exception\NoSuchEntityException as NSE;
  * @see df_product_att_html()
  * @see \Magento\Catalog\Model\Product::getAttributeText()
  * @uses \Magento\Eav\Model\Entity\Attribute\Source\AbstractSource::getOptionText()
- * @used-by \Df\Catalog\Test\product\attribute::df_product_att_val()
- * @used-by \Dfe\Sift\Payload\OQI::p()
+ * @used-by Df\Catalog\Test\product\attribute::df_product_att_val()
+ * @used-by Dfe\Sift\Payload\OQI::p()
  * @throws NSE
  */
 function df_product_att_val(P $p, string $c, string $d = ''):string {return df_att_val($p, df_product_att($c), $d);}
