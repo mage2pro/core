dd28cb9d485420f17dc316d8eddd30d42bcf71f1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:17:50 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/array/other/unset.php
+++ Core/lib/array/other/unset.php
@@ -10,10 +10,10 @@
  * 		$this->unsetData(['can_use_default_value', 'can_use_website_value', 'scope']);
  * 2022-11-26
  * We can not declare the argument $k as `string ...$a` because such a syntax rejects arrays: https://3v4l.org/jFdPm
- * @used-by \Df\Config\Backend::value()
- * @used-by \Df\Config\Backend\ArrayT::processI()
- * @used-by \Df\Framework\Request::clean()
- * @used-by \Dfe\Markdown\Observer\Catalog\ControllerAction::processPost()
+ * @used-by Df\Config\Backend::value()
+ * @used-by Df\Config\Backend\ArrayT::processI()
+ * @used-by Df\Framework\Request::clean()
+ * @used-by Dfe\Markdown\Observer\Catalog\ControllerAction::processPost()
  * @param array(string => mixed) $a
  * @param string|string[] ...$k
  * @return array(string => mixed)
