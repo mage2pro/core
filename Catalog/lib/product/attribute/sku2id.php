5696ce27dca1329e0557195175833d5546b28bb4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:11:25 +0100

    https://github.com/mage2pro/core/issues/440

--- Catalog/lib/product/attribute/sku2id.php
+++ Catalog/lib/product/attribute/sku2id.php
@@ -2,6 +2,6 @@
 /**
  * 2019-09-22
  * @see df_att_code2id()
- * @used-by \Dfe\Color\Observer\ProductImportBunchSaveAfter::execute()
+ * @used-by Dfe\Color\Observer\ProductImportBunchSaveAfter::execute()
  */
 function df_product_sku2id(string $sku):int {return (int)df_product_res()->getIdBySku($sku);}
