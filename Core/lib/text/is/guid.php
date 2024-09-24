9adf13e42a477d8b652adc40d6c3c584a0320f00
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:24:22 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/text/is/guid.php
+++ Core/lib/text/is/guid.php
@@ -2,7 +2,7 @@
 /**
  * 2019-01-11
  * 2019-11-15 https://stackoverflow.com/a/1253417
- * @used-by \Inkifi\Consolidation\Controller\Adminhtml\Index\Index::execute()
+ * @used-by Inkifi\Consolidation\Controller\Adminhtml\Index\Index::execute()
  */
 function df_is_guid(string $s):bool {return 36 === strlen($s) && preg_match(
 	'#^\{?[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}\}?$#', $s
