749e22ca11b1a822f44f270a3e94d052ae683e92
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:13:45 +0100

    https://github.com/mage2pro/core/issues/440

--- Catalog/lib/product/type.php
+++ Catalog/lib/product/type.php
@@ -7,7 +7,7 @@ use Magento\GroupedProduct\Model\Product\Type\Grouped as G;
 /**
  * 2021-04-24
  * @see df_configurable()
- * @used-by \MageSuper\Casat\Observer\ProductSaveBefore::execute() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/73)
+ * @used-by MageSuper\Casat\Observer\ProductSaveBefore::execute() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/73)
  */
 function df_product_is_bundle(P $p):bool {return B::TYPE_CODE === $p->getTypeId();}
 
@@ -15,6 +15,6 @@ function df_product_is_bundle(P $p):bool {return B::TYPE_CODE === $p->getTypeId(
  * 2017-04-20
  * @see df_configurable()
  * @see df_not_configurable()
- * @used-by \Dfe\Color\Observer\ProductSaveBefore::execute()
+ * @used-by Dfe\Color\Observer\ProductSaveBefore::execute()
  */
 function df_product_type_composite(string $t):bool {return in_array($t, [B::TYPE_CODE, C::TYPE_CODE, G::TYPE_CODE]);}
