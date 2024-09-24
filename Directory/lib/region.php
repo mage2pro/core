3081669c29a74c0daf68178dedd4624631924595
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:33:58 +0100

    https://github.com/mage2pro/core/issues/440

--- Directory/lib/region.php
+++ Directory/lib/region.php
@@ -2,6 +2,6 @@
 use Magento\Customer\Model\Address\AbstractAddress as A;
 /**
  * 2019-06-13
- * @used-by \KingPalm\B2B\Observer\RegisterSuccess::execute()
+ * @used-by KingPalm\B2B\Observer\RegisterSuccess::execute()
  */
 function df_region_name(A $a):string {return df_ets($a->getRegion() ?: $a->getRegionModel($a->getRegionId())->getName());}
