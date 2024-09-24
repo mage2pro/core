c78abce8c21c6ab18a310ce0e80a5703202ed1cc
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:46:45 +0100

    https://github.com/mage2pro/core/issues/440

--- Payment/lib/tm.php
+++ Payment/lib/tm.php
@@ -4,22 +4,22 @@ use Df\Payment\W\Event;
 /**
  * 2017-03-23
  * @used-by df_tmf()
- * @used-by \Df\Payment\Block\Info::tm()
- * @used-by \Df\Payment\Choice::tm()
- * @used-by \Df\Payment\Facade::tm()
- * @used-by \Df\StripeClone\Block\Info::prepare()
- * @used-by \Dfe\AlphaCommerceHub\API\Facade\BankCard::op()
- * @used-by \Dfe\AlphaCommerceHub\Method::_refund()
- * @used-by \Dfe\AlphaCommerceHub\W\Reader::reqFilter()
- * @used-by \Dfe\SecurePay\Refund::process()
- * @used-by \Dfe\SecurePay\Signer\Response::values()
+ * @used-by Df\Payment\Block\Info::tm()
+ * @used-by Df\Payment\Choice::tm()
+ * @used-by Df\Payment\Facade::tm()
+ * @used-by Df\StripeClone\Block\Info::prepare()
+ * @used-by Dfe\AlphaCommerceHub\API\Facade\BankCard::op()
+ * @used-by Dfe\AlphaCommerceHub\Method::_refund()
+ * @used-by Dfe\AlphaCommerceHub\W\Reader::reqFilter()
+ * @used-by Dfe\SecurePay\Refund::process()
+ * @used-by Dfe\SecurePay\Signer\Response::values()
  * @param string|object $m
  */
 function df_tm($m):TM {return TM::s($m);}
 
 /**
  * 2017-03-23
- * @used-by \Dfe\AllPay\Method::getInfoBlockType()
+ * @used-by Dfe\AllPay\Method::getInfoBlockType()
  * @param string|object $m
  * @return Event|string|null
  */
