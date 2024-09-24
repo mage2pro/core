33372dd0cf343f113764cbfb24eb5bd59bf5cd38
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:29:50 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/crypt.php
+++ Core/lib/crypt.php
@@ -3,7 +3,7 @@ use Magento\Framework\Encryption\Encryptor;
 use Magento\Framework\Encryption\EncryptorInterface as IEncryptor;
 
 /**
- * @used-by \Df\Config\Settings::p()
+ * @used-by Df\Config\Settings::p()
  * @return IEncryptor|Encryptor
  */
 function df_encryptor() {return df_o(IEncryptor::class);}
