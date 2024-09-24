65ca5e9e5c2594571f5e3198e3e27de7c0744a12
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:36:42 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/db/increment.php
+++ Framework/lib/db/increment.php
@@ -6,8 +6,8 @@ use Magento\Framework\DB\Select;
  * https://mage2.pro/t/518
  * https://github.com/magento/magento2/blob/d50ee54/app/code/Magento/ImportExport/Model/ResourceModel/Helper.php#L47-L62
  * @used-by df_sales_seq_next()
- * @used-by \Df\Payment\Source\Identification::get()
- * @used-by \Df\Sso\CustomerReturn::customerData()
+ * @used-by Df\Payment\Source\Identification::get()
+ * @used-by Df\Sso\CustomerReturn::customerData()
  * @uses \Magento\ImportExport\Model\ResourceModel\Helper::getNextAutoincrement()
  */
 function df_next_increment(string $t):int {return df_int(df_ie_helper()->getNextAutoincrement(df_table($t)));}
@@ -27,7 +27,7 @@ function df_next_increment_old(string $t):int {
  * 2016-01-27
  * «How to alter a database table»: https://mage2.pro/t/559
  * http://stackoverflow.com/a/970652
- * @used-by \Dfe\SalesSequence\Config\Next\Backend::updateNextNumber()
+ * @used-by Dfe\SalesSequence\Config\Next\Backend::updateNextNumber()
  */
 function df_next_increment_set(string $t, int $v):void {
 	df_conn()->query(sprintf('ALTER TABLE %s AUTO_INCREMENT = %d', df_db_quote(df_table($t)), $v));
