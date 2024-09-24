db59a13fabed79ec54b32b1e1ed38bf6dc427c35
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:55:00 +0100

    https://github.com/mage2pro/core/issues/440

--- Xml/lib/x.php
+++ Xml/lib/x.php
@@ -11,10 +11,10 @@ use Throwable as T;
  * @used-by df_xml2a()
  * @used-by df_xml_prettify()
  * @used-by df_xml_x()
- * @used-by \Df\Xml\A::p()
- * @used-by \Df\Xml\G::__construct()
- * @used-by \Dfe\Robokassa\Api\Options::p()
- * @used-by \Dfe\SecurePay\Refund::process()
+ * @used-by Df\Xml\A::p()
+ * @used-by Df\Xml\G::__construct()
+ * @used-by Dfe\Robokassa\Api\Options::p()
+ * @used-by Dfe\SecurePay\Refund::process()
  * @param X|string $x
  * @param F|T|bool|mixed $onE
  * @throws E
