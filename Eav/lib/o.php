ab434d6e9398c7925dbf3899b2415b7d2da8e9d7
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:35:01 +0100

    https://github.com/mage2pro/core/issues/440

--- Eav/lib/o.php
+++ Eav/lib/o.php
@@ -7,21 +7,21 @@ use Magento\Framework\EntityManager\MetadataPool;
  * 2015-10-12
  * @used-by df_customer_att()
  * @used-by df_eav_type()
- * @used-by \Df\Customer\AddAttribute\Address::p()
- * @used-by \Sharapov\Cabinetsbay\Setup\InstallData::install() (https://github.com/cabinetsbay/site/issues/98)
+ * @used-by Df\Customer\AddAttribute\Address::p()
+ * @used-by Sharapov\Cabinetsbay\Setup\InstallData::install() (https://github.com/cabinetsbay/site/issues/98)
  */
 function df_eav_config():Config {return df_o(Config::class);}
 
 /**
  * 2015-10-06
- * @used-by \Df\Customer\AddAttribute\Address::p()
- * @used-by \Df\Customer\AddAttribute\Customer::p()
- * @used-by \KingPalm\B2B\Setup\UpgradeSchema::_process()
+ * @used-by Df\Customer\AddAttribute\Address::p()
+ * @used-by Df\Customer\AddAttribute\Customer::p()
+ * @used-by KingPalm\B2B\Setup\UpgradeSchema::_process()
  */
 function df_eav_setup():EavSetup {return df_o(EavSetup::class);}
 
 /**
  * 2021-03-26
- * @used-by \CanadaSatellite\Theme\Plugin\Model\LinkManagement::aroundSaveChild(canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/44)
+ * @used-by CanadaSatellite\Theme\Plugin\Model\LinkManagement::aroundSaveChild(canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/44)
  */
 function df_metadata_pool():MetadataPool {return df_o(MetadataPool::class);}
