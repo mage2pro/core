022c2731139113ec029d1833e91e7386e1d3ade2
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:44:28 +0100

    https://github.com/mage2pro/core/issues/440

--- MediaStorage/lib/main.php
+++ MediaStorage/lib/main.php
@@ -2,6 +2,6 @@
 use Magento\MediaStorage\Model\File\Storage\Response as Resp;
 /**
  * 2020-12-13
- * @used-by \TFC\Core\Plugin\MediaStorage\App\Media::aroundLaunch()
+ * @used-by TFC\Core\Plugin\MediaStorage\App\Media::aroundLaunch()
  */
 function df_file_resp():Resp {return df_o(Resp::class);}
