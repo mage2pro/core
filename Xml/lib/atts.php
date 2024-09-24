c5318100b67b5f87ad0d75780ef25d59f2aa5f70
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:53:49 +0100

    https://github.com/mage2pro/core/issues/440

--- Xml/lib/atts.php
+++ Xml/lib/atts.php
@@ -3,7 +3,7 @@ use SimpleXMLElement as X;
 
 /**
  * 2024-09-23 https://www.php.net/manual/en/simplexmlelement.attributes.php
- * @used-by \Df\Xml\A::p()
+ * @used-by Df\Xml\A::p()
  * @return array(string => string)
  */
 function df_xml_atts(X $x):array {/** @var ?X  $aa */ return !($aa = $x->attributes()) ? [] : df_clean_r(
