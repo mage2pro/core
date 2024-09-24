0d45ac052eaffeda5c6a7a9ae479de0d1c0175a6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:47:16 +0100

    https://github.com/mage2pro/core/issues/440

--- Payment/lib/url-api.php
+++ Payment/lib/url-api.php
@@ -2,11 +2,11 @@
 use Df\Payment\Url;
 /**
  * 2017-03-23
- * @used-by \Df\Payment\Init\Action::action()
- * @used-by \Dfe\AllPay\Block\Info\BankCard::allpayAuthCode()
- * @used-by \Dfe\Moip\API\Client::urlBase()
- * @used-by \Dfe\SecurePay\Refund::process()
- * @used-by \Dfe\Vantiv\API\Client::urlBase()
+ * @used-by Df\Payment\Init\Action::action()
+ * @used-by Dfe\AllPay\Block\Info\BankCard::allpayAuthCode()
+ * @used-by Dfe\Moip\API\Client::urlBase()
+ * @used-by Dfe\SecurePay\Refund::process()
+ * @used-by Dfe\Vantiv\API\Client::urlBase()
  * @param string|object $m
  * @param string[] $stages
  * @param mixed ...$a [optional]
