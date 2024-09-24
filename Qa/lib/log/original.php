cc785db8ca329f82ae4dfa42b83921a3d11018fe
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:48:59 +0100

    https://github.com/mage2pro/core/issues/440

--- Qa/lib/log/original.php
+++ Qa/lib/log/original.php
@@ -3,7 +3,7 @@ use Monolog\Logger as L;
 use Psr\Log\LoggerInterface as IL;
 /**
  * 2024-09-02
- * @used-by \Df\Qa\Failure\Error::check()
+ * @used-by Df\Qa\Failure\Error::check()
  * @return IL|L
  */
 function df_logger():IL {return df_o(IL::class);}
