cffd0e68543dfe0f4ba575ceef52024de3f1ce85
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:40:32 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/url/params.php
+++ Framework/lib/url/params.php
@@ -12,7 +12,7 @@ function df_nosid():array {return ['_nosid' => true];}
 /**
  * 2020-01-19
  * @see \Magento\Store\App\Response\Redirect::_getUrl()
- * @used-by \Frugue\Store\Switcher::params()
+ * @used-by Frugue\Store\Switcher::params()
  * @return array(string => string)
  */
 function df_url_param_redirect(string $u = ''):array {return [IA::PARAM_NAME_URL_ENCODED => df_url_h()->getEncodedUrl($u)];}
