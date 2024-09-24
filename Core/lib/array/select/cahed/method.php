c8911988ae15c0b3cbb31754450bb45984b0ab25
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:18:06 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/array/select/cahed/method.php
+++ Core/lib/array/select/cahed/method.php
@@ -7,13 +7,13 @@ use Closure as F;
  * `object` as an argument type is not supported by PHP < 7.2: https://github.com/mage2pro/core/issues/174#user-content-object
  * 2024-06-03 We need to support PHP ≥ 7.1: https://github.com/mage2pro/core/issues/368
  * @see dfac()
- * @used-by \Df\Config\A::get()
- * @used-by \Df\Core\Visitor::r()
- * @used-by \Df\Framework\Form\Element\Fieldset::v()
- * @used-by \Df\Payment\TM::req()
- * @used-by \Df\Payment\TM::res0()
- * @used-by \Dfe\CheckoutCom\Response::a()
- * @used-by \Dfe\FacebookLogin\Customer::r()
+ * @used-by Df\Config\A::get()
+ * @used-by Df\Core\Visitor::r()
+ * @used-by Df\Framework\Form\Element\Fieldset::v()
+ * @used-by Df\Payment\TM::req()
+ * @used-by Df\Payment\TM::res0()
+ * @used-by Dfe\CheckoutCom\Response::a()
+ * @used-by Dfe\FacebookLogin\Customer::r()
  * @param object $o
  * @param string|string[] $k [optional]
  * @param mixed|callable|null $d [optional]
