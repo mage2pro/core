15f40d207bd667eb10a41589649828ed7dd13fd5
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:38:00 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/fs/path/ds/trim.php
+++ Framework/lib/fs/path/ds/trim.php
@@ -16,8 +16,8 @@ function df_trim_ds(...$a) {return df_call_a($a, function(string $p):string {ret
  * @used-by df_product_image_path2abs()
  * @used-by df_replace_store_code_in_url()
  * @used-by df_sys_path_abs()
- * @used-by \Dfe\Salesforce\Test\Basic::url()
- * @used-by \TFC\Core\Router::match() (tradefurniturecompany.co.uk, https://github.com/tradefurniturecompany/core/issues/40)
+ * @used-by Dfe\Salesforce\Test\Basic::url()
+ * @used-by TFC\Core\Router::match() (tradefurniturecompany.co.uk, https://github.com/tradefurniturecompany/core/issues/40)
  */
 function df_trim_ds_left(string $p):string {return df_trim_left($p, '/\\');}
 
@@ -25,8 +25,8 @@ function df_trim_ds_left(string $p):string {return df_trim_left($p, '/\\');}
  * 2016-10-14
  * @used-by df_add_ds_right()
  * @used-by df_magento_version_remote()
- * @used-by \Df\Payment\Method::canUseForCountryP()
- * @used-by \Dfe\BlackbaudNetCommunity\Url::build()
- * @used-by \Dfe\BlackbaudNetCommunity\Url::check()
+ * @used-by Df\Payment\Method::canUseForCountryP()
+ * @used-by Dfe\BlackbaudNetCommunity\Url::build()
+ * @used-by Dfe\BlackbaudNetCommunity\Url::check()
  */
 function df_trim_ds_right(string $p):string {return df_trim_right($p, '/\\');}
