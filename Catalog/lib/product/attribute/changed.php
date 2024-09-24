d79cb905c6cbc671acdb10172ef91e686ad05bba
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:11:03 +0100

    https://github.com/mage2pro/core/issues/440

--- Catalog/lib/product/attribute/changed.php
+++ Catalog/lib/product/attribute/changed.php
@@ -3,6 +3,6 @@ use Magento\Catalog\Model\Product as P;
 
 /**
  * 2021-04-24
- * @used-by \MageSuper\Casat\Observer\ProductSaveBefore::execute() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/73)
+ * @used-by MageSuper\Casat\Observer\ProductSaveBefore::execute() (canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/73)
  */
 function df_product_att_changed(P $p, string $k):bool {return $p->getStoreId() ? !is_null($p[$k]) : $p->dataHasChangedFor($k);}
