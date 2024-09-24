60b48d7cddb87800cf09ae22381aedc232be0b31
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 01:16:57 +0100

    https://github.com/mage2pro/core/issues/437

--- Qa/lib/validation/param.php
+++ Qa/lib/validation/param.php
@@ -14,7 +14,6 @@ function df_param_iso2(string $v, int $ord, int $sl = 0):string {return Q::asser
  * @used-by df_date_from_db()
  * @used-by df_eav_update()
  * @used-by df_fs_delete()
- * @used-by df_xml_children()
  * @used-by dfa_deep()
  * @used-by dfa_deep_unset()
  * @used-by \Df\Core\Html\Tag::openTagWithAttributesAsText()
--- Xml/lib/exists.php
+++
@@ -1,8 +0,0 @@
-<?php
-use SimpleXMLElement as X;
-
-/**
- * http://stackoverflow.com/questions/1560827#comment20135428_1562158
- * @used-by df_xml_children()
- */
-function df_xml_exists(X $x, string $child):bool {return isset($x->{$child});}
