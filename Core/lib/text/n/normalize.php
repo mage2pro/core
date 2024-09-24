6defd9002cf015f968f5e5e77d1a91af92d40146
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:24:50 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/text/n/normalize.php
+++ Core/lib/text/n/normalize.php
@@ -5,6 +5,6 @@
  * @used-by df_is_multiline()
  * @used-by df_single_line()
  * @used-by df_string()
- * @used-by \Df\Qa\Dumper::dumpS()
+ * @used-by Df\Qa\Dumper::dumpS()
  */
 function df_normalize(string $s):string {return strtr($s, ["\r\n" => "\n", "\r" => "\n"]);}
