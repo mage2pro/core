159e5a972f7ef3a2c801cceb65610ac6cd8f7f86
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:19:21 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/array/is.php
+++ Core/lib/array/is.php
@@ -15,8 +15,8 @@ if (!function_exists('array_is_list')) {
 	 * @used-by dfa_deep_slice()
 	 * @used-by dfa_insert()
 	 * @used-by dfak_transform()
-	 * @used-by \Df\Payment\ConfigProvider::configOptions()
-	 * @used-by \Df\Payment\Method::isAvailable()
+	 * @used-by Df\Payment\ConfigProvider::configOptions()
+	 * @used-by Df\Payment\Method::isAvailable()
 	 * @param array(int|string => mixed) $a
 	 */
 	function array_is_list(array $a):bool {
@@ -53,8 +53,8 @@ if (!function_exists('array_is_list')) {
  * @used-by df_error_create()
  * @used-by df_filter_f()
  * @used-by df_ksort()
- * @used-by \Df\Core\Exception::__construct()
- * @used-by \Df\Xml\G::importArray()
+ * @used-by Df\Core\Exception::__construct()
+ * @used-by Df\Xml\G::importArray()
  * @param array(int|string => mixed)|mixed $a
  */
 function df_is_assoc($a):bool {return is_array($a) && (!$a || !array_is_list($a));}
@@ -62,7 +62,7 @@ function df_is_assoc($a):bool {return is_array($a) && (!$a || !array_is_list($a)
 /**
  * 2023-07-25
  * @uses is_object()
- * @used-by \Df\Qa\Dumper::dumpArray()
+ * @used-by Df\Qa\Dumper::dumpArray()
  * @param iterable $a
  */
 function dfa_has_objects($a):bool {return !!df_find($a, 'is_object', [], [], 0, true);}
