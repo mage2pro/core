45c83ff1ddbcf2cbeb2d3b1d124761a1a7dd5315
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:28:09 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/text/parse.php
+++ Core/lib/text/parse.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 2018-09-27
- * @used-by \Dfe\TBCBank\API\Client::_construct()
+ * @used-by Dfe\TBCBank\API\Client::_construct()
  * @return array(string => string)
  */
 function df_parse_colon(string $s):array {return df_map_r(df_explode_n($s), function(string $s):array {return df_trim(
