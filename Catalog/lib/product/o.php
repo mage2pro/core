96de2e2fce5643711535c342f43734a7a55aae61
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:13:24 +0100

    https://github.com/mage2pro/core/issues/440

--- Catalog/lib/product/o.php
+++ Catalog/lib/product/o.php
@@ -7,13 +7,13 @@ use Magento\Catalog\Model\ResourceModel\Product\Action;
 
 /**
  * 2019-09-22 «Best way to update product's attribute value»: https://magento.stackexchange.com/a/157446
- * @used-by \Dfe\Color\Observer\ProductImportBunchSaveAfter::execute()
+ * @used-by Dfe\Color\Observer\ProductImportBunchSaveAfter::execute()
  */
 function df_product_action():Action {return df_o(Action::class);}
 
 /**
  * 2018-06-04
- * @used-by \Frugue\Configurable\Plugin\Swatches\Block\Product\Renderer\Configurable::aroundGetAllowProducts()
+ * @used-by Frugue\Configurable\Plugin\Swatches\Block\Product\Renderer\Configurable::aroundGetAllowProducts()
  */
 function df_product_h():ProductH {return df_o(ProductH::class);}
 
@@ -22,8 +22,8 @@ function df_product_h():ProductH {return df_o(ProductH::class);}
  * @see df_category_r()
  * @used-by df_product()
  * @used-by df_product_load()
- * @used-by \CanadaSatellite\Theme\Plugin\Model\LinkManagement::aroundSaveChild(canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/44)
- * @used-by \PPCs\Core\Plugin\Iksanika\Stockmanage\Controller\Adminhtml\Product\MassUpdateProducts::beforeExecute()
+ * @used-by CanadaSatellite\Theme\Plugin\Model\LinkManagement::aroundSaveChild(canadasatellite.ca, https://github.com/canadasatellite-ca/site/issues/44)
+ * @used-by PPCs\Core\Plugin\Iksanika\Stockmanage\Controller\Adminhtml\Product\MassUpdateProducts::beforeExecute()
  * @return IProductRepository|ProductRepository
  */
 function df_product_r() {return df_o(IProductRepository::class);}
