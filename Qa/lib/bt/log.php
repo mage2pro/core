73d13ed5fe20e9825dd5f7d8020ba53186163849
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:48:03 +0100

    https://github.com/mage2pro/core/issues/440

--- Qa/lib/bt/log.php
+++ Qa/lib/bt/log.php
@@ -6,8 +6,8 @@ use Throwable as Th; # 2023-08-03 "Treat `\Throwable` similar to `\Exception`": 
 /**
  * $p позволяет при записи стека вызовов пропустить несколько последних вызовов функций,
  * которые и так очевидны (например, вызов данной функции, вызов df_bt_s() и т.п.)
- * @used-by \Df\Core\Exception::__construct()
- * @used-by \Df\Store\Plugin\Model\App\Emulation::beforeStartEnvironmentEmulation()
+ * @used-by Df\Core\Exception::__construct()
+ * @used-by Df\Store\Plugin\Model\App\Emulation::beforeStartEnvironmentEmulation()
  * @param int|Th|array(array(string => string|int)) $p
  */
 function df_bt_log($p = 0):void {df_report('bt-{date}-{time}.log', df_bt_s(df_bt_inc($p)));}
@@ -18,7 +18,7 @@ function df_bt_log($p = 0):void {df_report('bt-{date}-{time}.log', df_bt_s(df_bt
  * которые и так очевидны (например, вызов данной функции, вызов df_bt_log() и т.п.)
  * @used-by df_bt_log()
  * @used-by df_log_l()
- * @used-by \Dfe\Sift\Test\CaseT\API\Account\Decisions::t01()
+ * @used-by Dfe\Sift\Test\CaseT\API\Account\Decisions::t01()
  * @param int|Th|array(array(string => string|int)) $p
  */
 function df_bt_s($p = 0):string {return Formatter::p(new Trace(df_bt(df_bt_inc($p))));}
