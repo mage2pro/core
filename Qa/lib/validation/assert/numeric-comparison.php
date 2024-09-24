0cbdab202c134d17cc8ee644b6cb3f1e99e5b5dd
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:49:47 +0100

    https://github.com/mage2pro/core/issues/440

--- Qa/lib/validation/assert/numeric-comparison.php
+++ Qa/lib/validation/assert/numeric-comparison.php
@@ -24,7 +24,7 @@ function df_assert_between($v, $min, $max, bool $inclusive = true) {
  * @used-by df_error_create()
  * @used-by df_module_name_by_path()
  * @used-by df_vector_sum()
- * @used-by \Df\Qa\Trace\Frame::url()
+ * @used-by Df\Qa\Trace\Frame::url()
  * @param string|int|float|bool $expected
  * @param string|int|float|bool $v
  * @param string|Th|null $m [optional]
@@ -62,8 +62,8 @@ function df_assert_gt($lowBound, $v, $m = null) {return $lowBound <= $v ? $v : d
 /**
  * @used-by df_float_positive()
  * @used-by df_nat()
- * @used-by \Df\Customer\Settings\BillingAddress::restore()
- * @used-by \Dfe\CurrencyFormat\FE::onFormInitialized()
+ * @used-by Df\Customer\Settings\BillingAddress::restore()
+ * @used-by Dfe\CurrencyFormat\FE::onFormInitialized()
  * @param int|float|string $v
  * @param string|Th|null $m [optional]
  * @return int|float
@@ -72,7 +72,7 @@ function df_assert_gt($lowBound, $v, $m = null) {return $lowBound <= $v ? $v : d
 function df_assert_gt0($v, $m = null) {return 0 <= $v ? $v : df_error($m ?: "A positive number is expected, but got $v.");}
 
 /**
- * @used-by \Mangoit\MediaclipHub\Model\Orders::byOId()
+ * @used-by Mangoit\MediaclipHub\Model\Orders::byOId()
  * @param int|float $highBound
  * @param int|float $v
  * @param string|Th|null $m [optional]
@@ -85,8 +85,8 @@ function df_assert_le($highBound, $v, $m = null) {return $highBound >= $v ? $v :
 
 /**
  * @used-by df_num_calendar_days_by_num_working_days()
- * @used-by \Df\Qa\Trace\Frame::methodParameter()
- * @used-by \RWCandy\Captcha\Assert::name()
+ * @used-by Df\Qa\Trace\Frame::methodParameter()
+ * @used-by RWCandy\Captcha\Assert::name()
  * @param int|float $highBound
  * @param int|float $v
  * @param string|Th|null $m [optional]
@@ -103,8 +103,8 @@ function df_assert_lt($highBound, $v, $m = null) {return $highBound >= $v ? $v :
  * @used-by df_file_name()
  * @used-by df_json_decode()
  * @used-by df_module_name_by_path()
- * @used-by \Df\Framework\Form\Element\ArrayT::onFormInitialized()
- * @used-by \Dfe\Stripe\Init\Action::redirectUrl()
+ * @used-by Df\Framework\Form\Element\ArrayT::onFormInitialized()
+ * @used-by Dfe\Stripe\Init\Action::redirectUrl()
  * @param string|int|float|bool $neResult
  * @param string|int|float|bool $v
  * @param string|Th|null $m [optional]
