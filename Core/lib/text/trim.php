bd468941c6774bd6b0328ce8bc764c6bcc4ce7fd
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:28:43 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/text/trim.php
+++ Core/lib/text/trim.php
@@ -7,13 +7,13 @@ use Laminas\Filter\StringTrim as Trim;
 /**
  * 2017-06-09
  * @used-by df_oqi_desc()
- * @used-by \Df\Payment\Charge::text()
- * @used-by \Df\Sentry\Serializer::chop()
- * @used-by \Dfe\IPay88\Charge::pCharge()
- * @used-by \Dfe\Qiwi\Charge::pBill()
- * @used-by \Dfe\TwoCheckout\LineItem::adjustText()
- * @used-by \Dfe\YandexKassa\Charge::pTaxLeaf()
- * @used-by \Dfe\YandexKassa\Result::attributes()
+ * @used-by Df\Payment\Charge::text()
+ * @used-by Df\Sentry\Serializer::chop()
+ * @used-by Dfe\IPay88\Charge::pCharge()
+ * @used-by Dfe\Qiwi\Charge::pBill()
+ * @used-by Dfe\TwoCheckout\LineItem::adjustText()
+ * @used-by Dfe\YandexKassa\Charge::pTaxLeaf()
+ * @used-by Dfe\YandexKassa\Result::attributes()
  */
 function df_chop(string $s, int $max = 0):string {return !$max || (mb_strlen($s = df_trim($s)) <= $max) ? $s :
 	df_trim_right(mb_substr($s, 0, $max - 1)) . '…'
@@ -39,18 +39,18 @@ function df_chop(string $s, int $max = 0):string {return !$max || (mb_strlen($s 
  * @used-by df_trim()
  * @used-by df_trim_ds()
  * @used-by df_xml_x()
- * @used-by \Df\Config\Source\LetterCase::apply()
- * @used-by \Df\Core\Html\Tag::content()
- * @used-by \Df\Framework\Form\Element::uidSt()
- * @used-by \Df\Qa\Dumper::dumpS()
- * @used-by \Df\Qa\Failure::sections()
- * @used-by \Dfe\Portal\Block\Content::getTemplate()
- * @used-by \Dfe\Portal\Controller\Index\Index::execute()
- * @used-by \Dfe\Portal\Router::match()
- * @used-by \Dfe\TwitterTimeline\Block::_toHtml()
- * @used-by \Dfe\TwoCheckout\Address::line()
- * @used-by \Dfe\TwoCheckout\Method::_refund()
- * @used-by \Inkifi\Core\Plugin\Catalog\Block\Product\View::afterSetLayout()
+ * @used-by Df\Config\Source\LetterCase::apply()
+ * @used-by Df\Core\Html\Tag::content()
+ * @used-by Df\Framework\Form\Element::uidSt()
+ * @used-by Df\Qa\Dumper::dumpS()
+ * @used-by Df\Qa\Failure::sections()
+ * @used-by Dfe\Portal\Block\Content::getTemplate()
+ * @used-by Dfe\Portal\Controller\Index\Index::execute()
+ * @used-by Dfe\Portal\Router::match()
+ * @used-by Dfe\TwitterTimeline\Block::_toHtml()
+ * @used-by Dfe\TwoCheckout\Address::line()
+ * @used-by Dfe\TwoCheckout\Method::_refund()
+ * @used-by Inkifi\Core\Plugin\Catalog\Block\Product\View::afterSetLayout()
  * @param string|string[] $s
  * @param string $charlist [optional]
  * @param bool|mixed|Closure $throw [optional]
@@ -92,8 +92,8 @@ function df_trim($s, $charlist = null, $throw = false) {return df_try(function()
  * Пусть пока будет так. Потом, если потребуется, добавлю дополнительную обработку спецсимволов Unicode.
  * 2017-08-18 Today I have noticed that $charlist = null does not work for @uses ltrim()
  * @used-by df_trim_ds_left()
- * @used-by \Df\Config\Settings::phpNameToKey()
- * @used-by \Dfe\PostFinance\W\Event::cardNumber()
+ * @used-by Df\Config\Settings::phpNameToKey()
+ * @used-by Dfe\PostFinance\W\Event::cardNumber()
  */
 function df_trim_left(string $s, string $charlist = ''):string {return ltrim($s, $charlist ?: " \t\n\r\0\x0B");}
 
@@ -152,14 +152,14 @@ function df_trim_text_a(string $s, array $trimA, callable $f):string {
  * @used-by df_trim_text_left_right()
  * @used-by dfpm_code_short()
  * @used-by dfsm_code_short()
- * @used-by \Df\Framework\Request::extra()
- * @used-by \Df\PaypalClone\Signer::_sign()
- * @used-by \Df\Qa\Trace\Frame::__toString()
- * @used-by \Dfe\Qiwi\W\Event::pid()
- * @used-by \Dfe\Stripe\Facade\Token::trimmed()
- * @used-by \Dfe\TwitterTimeline\Block::_toHtml()
- * @used-by \Dfe\Zoho\App::title()
- * @used-by \CabinetsBay\Catalog\B\Category::images() (https://github.com/cabinetsbay/catalog/issues/2)
+ * @used-by Df\Framework\Request::extra()
+ * @used-by Df\PaypalClone\Signer::_sign()
+ * @used-by Df\Qa\Trace\Frame::__toString()
+ * @used-by Dfe\Qiwi\W\Event::pid()
+ * @used-by Dfe\Stripe\Facade\Token::trimmed()
+ * @used-by Dfe\TwitterTimeline\Block::_toHtml()
+ * @used-by Dfe\Zoho\App::title()
+ * @used-by CabinetsBay\Catalog\B\Category::images() (https://github.com/cabinetsbay/catalog/issues/2)
  * @param string|string[] $s
  * @param string|string[] $trim
  * @return string|string[]
@@ -173,8 +173,8 @@ function df_trim_text_left($s, $trim) {return is_array($s) ? df_map(__FUNCTION__
 /**
  * 2021-12-12
  * @used-by df_trim_text()
- * @used-by \Df\Core\Text\Marker::unmark()
- * @used-by \Dfe\TwitterTimeline\Block::_toHtml()
+ * @used-by Df\Core\Text\Marker::unmark()
+ * @used-by Dfe\TwitterTimeline\Block::_toHtml()
  */
 function df_trim_text_left_right(string $s, string $left, string $right):string {return df_trim_text_right(
 	df_trim_text_left($s, $left), $right
@@ -190,9 +190,9 @@ function df_trim_text_left_right(string $s, string $left, string $right):string 
  * @used-by df_trim_text_left_right()
  * @used-by df_trim_text_right()
  * @used-by dfe_portal_stripe_customers()
- * @used-by \Df\Framework\Form\Element\Fieldset::nameFull()
- * @used-by \Dfe\Oro\Test\Basic::t02_orders_stripe()
- * @used-by \Dfe\TwitterTimeline\Block::_toHtml()
+ * @used-by Df\Framework\Form\Element\Fieldset::nameFull()
+ * @used-by Dfe\Oro\Test\Basic::t02_orders_stripe()
+ * @used-by Dfe\TwitterTimeline\Block::_toHtml()
  * @param string|string[] $s
  * @param string|string[] $trim
  * @return string|string[]
