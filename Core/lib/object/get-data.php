48414c662326a7de5ee636a47b4ffd7fa551d00a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:22:49 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/object/get-data.php
+++ Core/lib/object/get-data.php
@@ -20,8 +20,8 @@ function df_assert_gd($v) {return df_has_gd($v) ? $v : df_error(df_ucfirst(
  * 2020-02-04
  * @used-by dfad()
  * @used-by dfa_remove_objects()
- * @used-by \Df\Qa\Dumper::dumpObject()
- * @used-by \Df\Sentry\Extra::adjust()
+ * @used-by Df\Qa\Dumper::dumpObject()
+ * @used-by Df\Sentry\Extra::adjust()
  * @param mixed|_DO|AE|oAPI $v
  * @param F|bool|mixed $onE [optional]
  * @return array(string => mixed)
@@ -40,7 +40,7 @@ function df_gd($v, $onE = true):array {return df_try(function() use($v) {return
  * similar to `Magento\Framework\DataObject`": https://github.com/mage2pro/core/issues/290
  * @used-by df_assert_gd()
  * @used-by df_call()
- * @used-by \Df\Sentry\Extra::adjust()
+ * @used-by Df\Sentry\Extra::adjust()
  * @param mixed $v
  */
 function df_has_gd($v):bool {return $v instanceof _DO || $v instanceof AE || df_is_api_o($v);}
