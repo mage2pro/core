50cca36d20fd2220996ccc4ec4fc3d2b36448131
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:22:32 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/lang/other.php
+++ Core/lib/lang/other.php
@@ -3,11 +3,11 @@
  * 2017-02-07
  * @used-by df_lang_zh()
  * @used-by df_lang_ru()
- * @used-by \Df\Payment\Block\Info::extended()
- * @used-by \Df\Payment\Method::test()
- * @used-by \Df\Payment\Source\API\Key\Testable::test()
- * @used-by \Dfe\Moip\FE\Webhooks::onFormInitialized()
- * @used-by \Dfe\Paymill\Settings::test3DS()
+ * @used-by Df\Payment\Block\Info::extended()
+ * @used-by Df\Payment\Method::test()
+ * @used-by Df\Payment\Source\API\Key\Testable::test()
+ * @used-by Dfe\Moip\FE\Webhooks::onFormInitialized()
+ * @used-by Dfe\Paymill\Settings::test3DS()
  * $args — массив либо пустой, либо из 2 элементов с целочисленными индексами 0 и 1.
  * Если массив $args пуст, то функция возвращает $r.
  * Если массив $args непуст, то функция возвращает:
@@ -23,7 +23,7 @@ function df_b(array $args, bool $r) {return !$args ? $r : $args[intval(!$r)];}
  * @see dfc()
  * @see dfcf()
  * @used-by df_sprintf_strict()
- * @used-by \Df\Qa\Failure\Error::log()
+ * @used-by Df\Qa\Failure\Error::log()
  */
 function df_no_rec(Closure $f):void {
 	static $inProcess = []; /** @var bool[] $inProcess */
@@ -36,7 +36,7 @@ function df_no_rec(Closure $f):void {
 }
 
 /**
- * @used-by \Df\Core\Html\Tag::openTagWithAttributesAsText()
+ * @used-by Df\Core\Html\Tag::openTagWithAttributesAsText()
  * @param mixed $v
  * @return mixed
  */
