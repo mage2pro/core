f48bfa32c08d308fa983a500a0a85f95edf6853c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:33:52 +0100

    https://github.com/mage2pro/core/issues/440

--- Directory/lib/directory.php
+++ Directory/lib/directory.php
@@ -3,16 +3,16 @@ use Magento\Directory\Helper\Data as H;
 /**
  * 2016-04-05                               
  * @used-by df_is_postcode_required()
- * @used-by \Dfe\Customer\Plugin\Customer\Model\ResourceModel\AddressRepository::_validate()
- * @used-by \KingPalm\B2B\Block\RegionJS\Backend::_toHtml()
- * @used-by \KingPalm\B2B\Block\RegionJS\Frontend::_toHtml()
+ * @used-by Dfe\Customer\Plugin\Customer\Model\ResourceModel\AddressRepository::_validate()
+ * @used-by KingPalm\B2B\Block\RegionJS\Backend::_toHtml()
+ * @used-by KingPalm\B2B\Block\RegionJS\Frontend::_toHtml()
  * @used-by vendor/mage2pro/customer/view/frontend/templates/address/edit.phtml
  */
 function df_directory():H {return df_o(H::class);}
 
 /**
  * 2015-10-12
- * @used-by \Df\Sso\CustomerReturn::register()
- * @used-by \KingPalm\B2B\Setup\V140\MoveDataToAddress::p()
+ * @used-by Df\Sso\CustomerReturn::register()
+ * @used-by KingPalm\B2B\Setup\V140\MoveDataToAddress::p()
  */
 function df_is_postcode_required(string $iso2):bool {return !df_directory()->isZipCodeOptional($iso2);}
