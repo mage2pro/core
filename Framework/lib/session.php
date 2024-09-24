8cebc4be96f3b6f695f339fa173759c0de07f766
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:43:49 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/session.php
+++ Framework/lib/session.php
@@ -3,7 +3,7 @@ use Magento\Framework\Session\Config;
 use Magento\Framework\Session\Config\ConfigInterface as IConfig;
 /**
  * 2017-05-08
- * @used-by \Df\Framework\Plugin\Session\SessionManager::beforeStart()
+ * @used-by Df\Framework\Plugin\Session\SessionManager::beforeStart()
  * @return Config|IConfig
  */
 function df_session_config() {return df_o(IConfig::class);}
