3882b99f22b4f43194e9bac0c8e0967869d8d2d6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:24:40 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/text/n/explode.php
+++ Core/lib/text/n/explode.php
@@ -5,12 +5,12 @@
  * @used-by df_parse_colon()
  * @used-by df_tab()
  * @used-by df_zf_http_last_req()
- * @used-by \Df\Core\Text\Regex::getSubjectSplitted()
- * @used-by \Dfe\AllPay\Charge::descriptionOnKiosk()
- * @used-by \Dfe\Moip\P\Charge::pInstructionLines()
- * @used-by \Dfe\TBCBank\W\Reader::reqFilter()
- * @used-by \Doormall\Shipping\Partner\Entity::locations()
- * @used-by \Inkifi\Core\Plugin\Catalog\Block\Product\View::afterSetLayout()
+ * @used-by Df\Core\Text\Regex::getSubjectSplitted()
+ * @used-by Dfe\AllPay\Charge::descriptionOnKiosk()
+ * @used-by Dfe\Moip\P\Charge::pInstructionLines()
+ * @used-by Dfe\TBCBank\W\Reader::reqFilter()
+ * @used-by Doormall\Shipping\Partner\Entity::locations()
+ * @used-by Inkifi\Core\Plugin\Catalog\Block\Product\View::afterSetLayout()
  * @return string[]
  */
 function df_explode_n(string $s):array {return explode("\n", df_normalize(df_trim($s)));}
