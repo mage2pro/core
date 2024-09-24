3245345d96ad349fb40647a21ffb4c3cd87bb664
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:43:56 +0100

    https://github.com/mage2pro/core/issues/440

--- IE/lib/main.php
+++ IE/lib/main.php
@@ -2,6 +2,6 @@
 use Magento\CatalogImportExport\Model\Import\Product\StoreResolver;
 /**
  * 2019-09-20
- * @used-by \Dfe\Color\Observer\ProductImportBunchSaveAfter::execute()
+ * @used-by Dfe\Color\Observer\ProductImportBunchSaveAfter::execute()
  */
 function df_ie_store_r():StoreResolver {return df_o(StoreResolver::class);}
