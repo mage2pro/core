837495ce3d4793433589ff92985c8af1ace69906
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:53:08 +0100

    https://github.com/mage2pro/core/issues/440

--- UrlRewrite/lib/finder.php
+++ UrlRewrite/lib/finder.php
@@ -3,9 +3,9 @@ use Magento\UrlRewrite\Model\Storage\DbStorage;
 use Magento\UrlRewrite\Model\UrlFinderInterface as IFinder;
 /**
  * 2020-01-18 It can return also @see \Magento\UrlRewrite\Model\CompositeUrlFinder in Magento 2.3.3.
- * @used-by \Frugue\Store\Plugin\UrlRewrite\Model\StoreSwitcher\RewriteUrl::aroundSwitch()
- * @used-by \Frugue\Store\Plugin\UrlRewrite\Model\StoreSwitcher\RewriteUrl::findCurrentRewrite()
- * @used-by \TFC\Core\Router::match() (tradefurniturecompany.co.uk, https://github.com/tradefurniturecompany/core/issues/40)
+ * @used-by Frugue\Store\Plugin\UrlRewrite\Model\StoreSwitcher\RewriteUrl::aroundSwitch()
+ * @used-by Frugue\Store\Plugin\UrlRewrite\Model\StoreSwitcher\RewriteUrl::findCurrentRewrite()
+ * @used-by TFC\Core\Router::match() (tradefurniturecompany.co.uk, https://github.com/tradefurniturecompany/core/issues/40)
  * @return IFinder|DbStorage
  */
 function df_url_finder() {return df_o(IFinder::class);}
