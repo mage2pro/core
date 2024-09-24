884cf4249d7a85d0b7cc63c489e043b2b462c569
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:43:42 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/registry.php
+++ Framework/lib/registry.php
@@ -3,8 +3,8 @@ use Magento\Framework\Registry as R;
 
 /**
  * 2015-10-31
- * @used-by \MageKey\AdcPopup\Observer\QuoteAddAfter::execute()		tradefurniturecompany.co.uk
- * @used-by \Wolf\Filter\Observer\ControllerActionPredispatch::execute()
+ * @used-by MageKey\AdcPopup\Observer\QuoteAddAfter::execute()		tradefurniturecompany.co.uk
+ * @used-by Wolf\Filter\Observer\ControllerActionPredispatch::execute()
  * @param mixed $v
  */
 function df_register(string $k, $v):void {df_registry_o()->register($k, $v);}
@@ -12,9 +12,9 @@ function df_register(string $k, $v):void {df_registry_o()->register($k, $v);}
 /**
  * 2015-10-31
  * @used-by df_product_current()
- * @used-by \Dfe\Logo\Frontend::_toHtml()
- * @used-by \MageKey\AdcPopup\Observer\QuoteAddAfter::execute()		tradefurniturecompany.co.uk
- * @used-by \SayItWithAGift\Options\Frontend::_toHtml()
+ * @used-by Dfe\Logo\Frontend::_toHtml()
+ * @used-by MageKey\AdcPopup\Observer\QuoteAddAfter::execute()		tradefurniturecompany.co.uk
+ * @used-by SayItWithAGift\Options\Frontend::_toHtml()
  * @return mixed|null
  */
 function df_registry(string $k) {return df_registry_o()->registry($k);}
@@ -29,6 +29,6 @@ function df_registry_o():R {return df_o(R::class);}
 
 /**
  * 2020-03-01
- * @used-by \MageKey\AdcPopup\Observer\QuoteAddAfter::execute()		tradefurniturecompany.co.uk
+ * @used-by MageKey\AdcPopup\Observer\QuoteAddAfter::execute()		tradefurniturecompany.co.uk
  */
 function df_unregister(string $k):void {df_registry_o()->unregister($k);}
