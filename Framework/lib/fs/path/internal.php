ca34da8306bdc9be79b0e97b84d6a5790cf7c915
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:38:14 +0100

    https://github.com/mage2pro/core/issues/440

--- Framework/lib/fs/path/internal.php
+++ Framework/lib/fs/path/internal.php
@@ -4,7 +4,7 @@
  * 2024-06-09 It returns `true` if:
  * 		1) $p is empty
  * 		2) $p is absolute (not relative) and inside the Magento installation directory.
- * @used-by \Df\Framework\Plugin\Session\SessionManager::beforeStart()
- * @used-by \Df\Sentry\Trace::info()
+ * @used-by Df\Framework\Plugin\Session\SessionManager::beforeStart()
+ * @used-by Df\Sentry\Trace::info()
  */
 function df_path_is_internal(string $p):bool {return df_es($p) || df_starts_with(df_path_n($p), df_path_n(BP));}
