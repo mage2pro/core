ebb67b0324ef63d0d62b3e8b791af83524a4d1ab
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 10:24:04 +0100

    https://github.com/mage2pro/core/issues/440

--- Core/lib/reflection/text/part.php
+++ Core/lib/reflection/text/part.php
@@ -13,15 +13,15 @@ function df_class_f($c):string {return df_first(df_explode_class($c));}
  * 2016-10-20
  * Making $c optional leads to the error «get_class() called without object from outside a class»: https://3v4l.org/k6Hd5
  * @used-by df_class_llc()
- * @used-by \Df\API\Facade::path()
- * @used-by \Df\Payment\W\F::aspect()
- * @used-by \Df\Qa\Trace\Formatter::p()
- * @used-by \Dfe\AlphaCommerceHub\Test\CaseT\BankCard\CancelPayment::t01()
- * @used-by \Dfe\AlphaCommerceHub\Test\CaseT\BankCard\CapturePayment::t01()
- * @used-by \Dfe\AlphaCommerceHub\Test\CaseT\BankCard\RefundPayment::t01()
- * @used-by \Dfe\AlphaCommerceHub\Test\CaseT\PayPal\CapturePayment::t01()
- * @used-by \Dfe\AlphaCommerceHub\Test\CaseT\PayPal\PaymentStatus::t01()
- * @used-by \Dfe\AlphaCommerceHub\Test\CaseT\PayPal\RefundPayment::t01()
+ * @used-by Df\API\Facade::path()
+ * @used-by Df\Payment\W\F::aspect()
+ * @used-by Df\Qa\Trace\Formatter::p()
+ * @used-by Dfe\AlphaCommerceHub\Test\CaseT\BankCard\CancelPayment::t01()
+ * @used-by Dfe\AlphaCommerceHub\Test\CaseT\BankCard\CapturePayment::t01()
+ * @used-by Dfe\AlphaCommerceHub\Test\CaseT\BankCard\RefundPayment::t01()
+ * @used-by Dfe\AlphaCommerceHub\Test\CaseT\PayPal\CapturePayment::t01()
+ * @used-by Dfe\AlphaCommerceHub\Test\CaseT\PayPal\PaymentStatus::t01()
+ * @used-by Dfe\AlphaCommerceHub\Test\CaseT\PayPal\RefundPayment::t01()
  * @param string|object $c
  */
 function df_class_l($c):string {return df_last(df_explode_class($c));}
@@ -29,7 +29,7 @@ function df_class_l($c):string {return df_last(df_explode_class($c));}
 /**
  * 2018-01-30
  * 2021-10-27 @deprecared It is unused.
- * @used-by \CabinetsBay\Catalog\B\Featured::p() (https://github.com/cabinetsbay/catalog/issues/27)
+ * @used-by CabinetsBay\Catalog\B\Featured::p() (https://github.com/cabinetsbay/catalog/issues/27)
  * @param string|object $c
  */
 function df_class_llc($c):string {return strtolower(df_class_l($c));}
@@ -39,7 +39,7 @@ function df_class_llc($c):string {return strtolower(df_class_l($c));}
  * 2016-10-20
  * Making $c optional leads to the error «get_class() called without object from outside a class»: https://3v4l.org/k6Hd5
  * https://3v4l.org/k6Hd5
- * @used-by \Df\API\Settings::titleB()
+ * @used-by Df\API\Settings::titleB()
  * @param string|object $c
  */
 function df_class_second($c):string {return df_explode_class($c)[1];}
