de66c85c3c5e1e575c8ae64801ba8bc480095691
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-07 17:10:08 +0100

    https://github.com/mage2pro/core/issues/443

--- Framework/Log/Handler/ReCaptcha.php
+++ Framework/Log/Handler/ReCaptcha.php
@@ -5,6 +5,9 @@ namespace Df\Framework\Log\Handler;
 final class ReCaptcha extends \Df\Framework\Log\Handler {
 	/**
 	 * 2024-10-07
+	 * @see \Magento\ReCaptchaUi\Model\CaptchaResponseResolver::resolve():
+	 * 		throw new InputException(__('Can not resolve reCAPTCHA parameter.'));
+	 * https://github.com/magento/security-package/blob/1.1.6-p2/ReCaptchaUi/Model/CaptchaResponseResolver.php#L25
 	 * @used-by \Df\Framework\Log\Handler::p()
 	 */
 	#[\Override] /** @see \Df\Framework\Log\Handler::_p() */
