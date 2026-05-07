b1e636d8d88a2ee57d4013cec512d4904c4f7269
Dmitrii Fediuk <dfediuk@gmail.com>
2026-05-07 17:27:05 +0100

    refactoring

--- Framework/lib/view/page/type/.php
+++ Framework/lib/view/page/type/.php
@@ -1,21 +1,5 @@
 <?php
 /**
- * 2019-03-27
- * @used-by CabinetsBay\Catalog\Observer\LayoutLoadBefore::execute() (https://github.com/cabinetsbay/catalog/issues/3)
- * @used-by Frugue\Core\Plugin\Swatches\Helper\Media::afterGetImageConfig()
- */
-function df_is_catalog_product_list():bool {return df_handle('catalog_category_view');}
-
-/**
- * 2019-03-27
- * @used-by CabinetsBay\Catalog\Observer\LayoutLoadBefore::execute() (https://github.com/cabinetsbay/catalog/issues/47)
- * @used-by Dfe\Frontend\Block\ProductView\Css::_toHtml()
- * @used-by Frugue\Core\Plugin\Swatches\Helper\Media::afterGetImageConfig()
- * @used-by TFC\Core\Plugin\Theme\Block\Html\Breadcrumbs::aroundAddCrumb()
- */
-function df_is_catalog_product_view():bool {return df_handle('catalog_product_view');}
-
-/**
  * 2016-08-24
  * 2017-08-28 May be we should use @see df_action_is() here?
  * 2017-12-04
