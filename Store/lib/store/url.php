504f6a4914022940dab2155e1a2241dd147b6fd6
Dmitrii Fediuk <dfediuk@gmail.com>
2026-05-20 20:20:47 +0100

    refactoring

--- Store/lib/store/url.php
+++ Store/lib/store/url.php
@@ -1,15 +1,6 @@
 <?php
-use Df\Directory\Model\Country;
-use Magento\Framework\App\ScopeInterface as IScope;
-use Magento\Framework\Exception\NoSuchEntityException as NSE;
 use Magento\Framework\UrlInterface as U;
-use Magento\Sales\Model\Order as O;
 use Magento\Store\Api\Data\StoreInterface as IStore;
-use Magento\Store\Model\Information as Inf;
-use Magento\Store\Model\Store;
-use Magento\Store\Model\StoreManager;
-use Magento\Store\Model\StoreManagerInterface as IStoreManager;
-use Magento\Store\Model\StoreResolver;
 
 /**
  * 2017-03-15 Returns an empty string if the store's root URL is absent in the Magento database.
