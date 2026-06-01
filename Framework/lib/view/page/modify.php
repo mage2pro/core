5d6e25e21cb4dbf43dfe6a093e3c50921737551b
Dmitrii Fediuk <dfediuk@gmail.com>
2026-06-01 14:55:10 +0100

    refactoring

--- Framework/lib/view/page/modify.php
+++ Framework/lib/view/page/modify.php
@@ -43,6 +43,7 @@ function df_page_title(string $s):void {df_page_config()->getTitle()->set($s);}
  * 2026-06-01
  * 1) https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/meta/name/robots
  * 2) «Robots Exclusion Protocol»: https://datatracker.ietf.org/doc/html/rfc9309
+ * 3) https://developers.google.com/search/docs/crawling-indexing
  * @used-by CabinetsBay\Catalog\Observer\LayoutLoadBefore::execute() (https://github.com/cabinetsbay/site/issues/98)
  * @used-by NGP\Core\Observer\LayoutGenerateBlocksAfter::execute() (https://github.com/national-glass-partitions/core/issues/2)
  */
